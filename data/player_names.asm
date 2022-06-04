2e942a10c00d548d64c626c70a982aa6b37b345c
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-03 19:46:12 -0700

    Update player_names.asm

--- data/player_names.asm
+++ data/player_names.asm
@@ -10,10 +10,10 @@ ChrisNameMenuHeader:
 	db 5 ; items
 	db "NEW NAME@"
 MalePlayerNameArray:
-	db "CHRIS@"
-	db "MAT@"
-	db "ALLAN@"
-	db "JON@"
+	db "DASHIE@"
+	db "CHUGGA@"
+	db "HEGEL@"
+	db "ASH@"
 	db 2 ; title indent
 	db " NAME @" ; title
 
@@ -29,9 +29,9 @@ KrisNameMenuHeader:
 	db 5 ; items
 	db "NEW NAME@"
 FemalePlayerNameArray:
-	db "KRIS@"
-	db "AMANDA@"
-	db "JUANA@"
-	db "JODI@"
+	db "EGGIE@"
+	db "ILYANA@"
+	db "MASAE@"
+	db "MOIRA@"
 	db 2 ; title indent
 	db " NAME @" ; title
