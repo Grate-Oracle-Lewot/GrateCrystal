3987eb5428837f40f8680d6ce6ac18e0df8bcc8d
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-04 18:24:08 -0700

    Update unown.asm

--- data/pokemon/dex_entries/unown.asm
+++ data/pokemon/dex_entries/unown.asm
@@ -1,10 +1,10 @@
 	db "SYMBOL@" ; species name
 	dw 108, 110 ; height, weight
 
-	db   "Because different"
-	next "types of UNOWN"
-	next "exist, it is said"
+	db   "It defies known"
+	next "science of #MON"
+	next "typing. Its many"
 
-	page "that they must"
-	next "have a variety of"
-	next "abilities.@"
+	page "shapes are thought"
+	next "to hold some"
+	next "meaning.@"
