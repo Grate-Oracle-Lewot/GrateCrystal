40a2bb24730e2898931b24da1154628f8adffadb
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-04 18:14:55 -0700

    Update politoed.asm

--- data/pokemon/dex_entries/politoed.asm
+++ data/pokemon/dex_entries/politoed.asm
@@ -1,10 +1,10 @@
 	db "FROG@" ; species name
 	dw 307, 750 ; height, weight
 
-	db   "When it expands"
-	next "its throat to"
-	next "croak out a tune,"
+	db   "It took so long to"
+	next "become a frog that"
+	next "it became a ghost"
 
-	page "nearby POLIWAG and"
-	next "POLIWHIRL gather"
-	next "immediately.@"
+	page "in the process."
+	next "Now, it pines for"
+	next "its youth.@"
