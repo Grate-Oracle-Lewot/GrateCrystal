29b51dee6ad2110bb15184666230f47d90c91305
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-07 21:10:25 -0700

    Create whimsicott.asm

---
+++ data/pokemon/base_stats/whimsicott.asm
@@ -0,0 +1,21 @@
+	db WHIMSICOTT ; 253
+
+	db  60,  67,  85, 116,  77,  75
+	;   hp  atk  def  spd  sat  sdf
+
+	db GRASS, FAIRY ; type
+	db 75 ; catch rate
+	db 168 ; base exp
+	db SILVER_LEAF, GOLD_LEAF ; items
+	db GENDER_F50 ; gender ratio
+	db 100 ; unknown 1
+	db 10 ; step cycles to hatch
+	db 5 ; unknown 2
+	INCBIN "gfx/pokemon/whimsicott/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_FAST ; growth rate
+	dn EGG_PLANT, EGG_FAIRY ; egg groups
+
+	; tm/hm learnset
+	tmhm HEADBUTT, CURSE, TOXIC, HIDDEN_POWER, SUNNY_DAY, SWEET_SCENT, SNORE, HYPER_BEAM, PROTECT, GIGA_DRAIN, ENDURE, FRUSTRATION, SOLARBEAM, RETURN, PSYCHIC_M, SHADOW_BALL, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, DEFENSE_CURL, REST, ATTRACT, FAIRY_WIND, MOONBLAST, FLY, FLASH
+	; end
