41379ac366ec8557819d7bd7481e9ed1222d8665
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-07 19:31:22 -0700

    Create MewAntechamber.asm

---
+++ maps/MewAntechamber.asm
@@ -0,0 +1,42 @@
+	object_const_def
+	const MEWANTECHAMBER_BOULDER
+	const MEWANTECHAMBER_POKE_BALL1
+	const MEWANTECHAMBER_POKE_BALL2
+
+MewAntechamber_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+MewAntechamberBoulder:
+	jumpstd StrengthBoulderScript
+
+MewAntechamberVoltorbScript:
+	waitsfx
+	cry VOLTORB
+	loadvar VAR_BATTLETYPE, BATTLETYPE_TRAP
+	loadwildmon VOLTORB, 25
+	startbattle
+	disappear MEWANTECHAMBER_POKE_BALL1
+	reloadmapafterbattle
+	end
+
+MewAntechamberTMDarkPulse:
+	itemball TM_DARK_PULSE
+
+MewAntechamber_MapEvents:
+	db 0, 0 ; filler
+
+	def_warp_events
+	warp_event  5, 17, TOHJO_FALLS, 3
+	warp_event  6, 17, TOHJO_FALLS, 3
+	warp_event  4,  3, MEW_CHAMBER, 1
+
+	def_coord_events
+
+	def_bg_events
+
+	def_object_events
+	object_event  2,  4, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, MewAntechamberBoulder, -1
+	object_event  2,  6, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, MewAntechamberVoltorbScript, EVENT_MEW_ANTECHAMBER_VOLTORB
+	object_event  9,  2, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, MewAntechamberTMDarkPulse, EVENT_MEW_ANTECHAMBER_TM_DARK_PULSE
