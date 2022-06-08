99f2d1656e46296b4308345ca7f5468f3c99c25c
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-07 21:09:50 -0700

    Create yanmega.asm

---
+++ data/pokemon/base_stats/yanmega.asm
@@ -0,0 +1,21 @@
+	db YANMEGA ; 252
+
+	db  86,  76,  86,  95, 116,  56
+	;   hp  atk  def  spd  sat  sdf
+
+	db BUG, DRAGON ; type
+	db 30 ; catch rate
+	db 198 ; base exp
+	db SILVERPOWDER, BRIGHTPOWDER ; items
+	db GENDER_F50 ; gender ratio
+	db 100 ; unknown 1
+	db 21 ; step cycles to hatch
+	db 5 ; unknown 2
+	INCBIN "gfx/pokemon/yanmega/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_FAST ; growth rate
+	dn EGG_BUG, EGG_DRAGON ; egg groups
+
+	; tm/hm learnset
+	tmhm HEADBUTT, CURSE, TOXIC, HIDDEN_POWER, SUNNY_DAY, SNORE, PROTECT, GIGA_DRAIN, ENDURE, FRUSTRATION, SOLARBEAM, DRAGONBREATH, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DREAM_EATER, REST, ATTRACT, THIEF, STEEL_WING, FURY_CUTTER, FAIRY_WIND, CUT, FLY, STRENGTH, FLASH, TRI_ATTACK
+	; end
