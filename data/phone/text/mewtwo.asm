fead19b7db6c038fa6579fb45ad790b0d91271b3
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-07 19:20:47 -0700

    Create mewtwo.asm

---
+++ data/phone/text/mewtwo.asm
@@ -0,0 +1,16 @@
+MewtwoPhoneCallerText:
+	text "This is a message"
+	line "for the #MON"
+
+	para "trainer known as"
+	line "<PLAY_G>."
+
+	para "Your presence is"
+	line "requested at the"
+
+	para "RADIO TOWER in"
+	line "LAVENDER TOWN."
+
+	para "Come when you are"
+	line "ready."
+	done
