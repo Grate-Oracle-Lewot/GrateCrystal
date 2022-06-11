3963cf230a281caf85c6fd0e47905340184e3d08
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-10 19:54:33 -0700

    Update menu.asm

--- home/menu.asm
+++ home/menu.asm
@@ -287,27 +287,6 @@ MenuBoxCoord2Attr::
 	ld b, a
 	; fallthrough
 
-Coord2Attr:: ; unreferenced
-; Return the address of wAttrmap(c, b) in hl.
-	xor a
-	ld h, a
-	ld l, b
-	ld a, c
-	ld b, h
-	ld c, l
-	add hl, hl
-	add hl, hl
-	add hl, bc
-	add hl, hl
-	add hl, hl
-	ld c, a
-	xor a
-	ld b, a
-	add hl, bc
-	bccoord 0, 0, wAttrmap
-	add hl, bc
-	ret
-
 LoadMenuHeader::
 	call CopyMenuHeader
 	call PushWindow
@@ -330,9 +309,6 @@ MenuTextbox::
 	call LoadMenuTextbox
 	pop hl
 	jp PrintText
-
-Menu_DummyFunction:: ; unreferenced
-	ret
 
 LoadMenuTextbox::
 	ld hl, .MenuHeader
@@ -420,10 +396,6 @@ YesNoBox::
 
 PlaceYesNoBox::
 	jr _YesNoBox
-
-PlaceGenericTwoOptionBox:: ; unreferenced
-	call LoadMenuHeader
-	jr InterpretTwoOptionMenu
 
 _YesNoBox::
 ; Return nc (yes) or c (no).
@@ -723,15 +695,6 @@ PlaceNthMenuStrings::
 	call PlaceString
 	ret
 
-GetNthMenuStrings:: ; unreferenced
-	call GetMenuDataPointerTableEntry
-	inc hl
-	inc hl
-	ld a, [hli]
-	ld d, [hl]
-	ld e, a
-	ret
-
 MenuJumptable::
 	ld a, [wMenuSelection]
 	call GetMenuDataPointerTableEntry
@@ -839,12 +802,5 @@ InterpretBattleMenu::
 	ldh a, [hROMBank]
 	ld [wMenuData_2DMenuItemStringsBank], a
 	farcall _InterpretBattleMenu
-	ld a, [wMenuCursorPosition]
-	ret
-
-InterpretMobileMenu:: ; unreferenced
-	ldh a, [hROMBank]
-	ld [wMenuData_2DMenuItemStringsBank], a
-	farcall _InterpretMobileMenu
 	ld a, [wMenuCursorPosition]
 	ret
