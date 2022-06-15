49a549fd8230c720bfcaae7313c0e0b16559dfe1
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 11:43:45 -0700

    Update cloyster.asm

--- data/pokemon/base_stats/cloyster.asm
+++ data/pokemon/base_stats/cloyster.asm
@@ -3,7 +3,7 @@
 	db  50,  95, 180,  70,  85,  45
 	;   hp  atk  def  spd  sat  sdf
 
-	db WATER, ICE ; type
+	db WATER, STEEL ; type
 	db 60 ; catch rate
 	db 203 ; base exp
 	db PEARL, BIG_PEARL ; items
@@ -14,8 +14,8 @@
 	INCBIN "gfx/pokemon/cloyster/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_SLOW ; growth rate
-	dn EGG_WATER_3, EGG_WATER_3 ; egg groups
+	dn EGG_WATER_3, EGG_MINERAL ; egg groups
 
 	; tm/hm learnset
-	tmhm CURSE, TOXIC, HIDDEN_POWER, SNORE, BLIZZARD, HYPER_BEAM, ICY_WIND, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, REST, ATTRACT, SURF, WHIRLPOOL, ICE_BEAM
+	tmhm CURSE, ROLLOUT, TOXIC, ROCK_SMASH, HIDDEN_POWER, SNORE, BLIZZARD, HYPER_BEAM, ICY_WIND, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, RETURN, DIG, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SANDSTORM, SWIFT, REST, ATTRACT, HAIL, SURF, WHIRLPOOL, ICE_BEAM
 	; end
