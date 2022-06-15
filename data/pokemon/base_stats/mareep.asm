d698bb51413394e3c949ae129feef0f75c6ea8c4
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 13:11:43 -0700

    Update mareep.asm

--- data/pokemon/base_stats/mareep.asm
+++ data/pokemon/base_stats/mareep.asm
@@ -6,10 +6,10 @@
 	db ELECTRIC, ELECTRIC ; type
 	db 235 ; catch rate
 	db 59 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	db PRZCUREBERRY, PRZCUREBERRY ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
-	db 20 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/mareep/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
