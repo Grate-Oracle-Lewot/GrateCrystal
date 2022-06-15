d65b2d2ee77e45c8e9969ca09e6789ee46eda576
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 15:00:03 -0700

    Update unown.asm

--- data/pokemon/base_stats/unown.asm
+++ data/pokemon/base_stats/unown.asm
@@ -1,15 +1,15 @@
 	db UNOWN ; 201
 
-	db  48,  72,  48,  48,  72,  48
+	db  48,  72,  84,  84,  72,  96
 	;   hp  atk  def  spd  sat  sdf
 
-	db PSYCHIC_TYPE, PSYCHIC_TYPE ; type
-	db 225 ; catch rate
-	db 61 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	db CURSE_TYPE, CURSE_TYPE ; type
+	db 42 ; catch rate
+	db 84 ; base exp
+	db MYSTERYBERRY, TM_HIDDEN_POWER ; items
 	db GENDER_UNKNOWN ; gender ratio
 	db 100 ; unknown 1
-	db 40 ; step cycles to hatch
+	db 42 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/unown_a/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
@@ -17,5 +17,5 @@
 	dn EGG_NONE, EGG_NONE ; egg groups
 
 	; tm/hm learnset
-	tmhm
+	tmhm CURSE, TOXIC, PSYCH_UP, HIDDEN_POWER, SNORE, HYPER_BEAM, PROTECT, ENDURE, FRUSTRATION, RETURN, PSYCHIC_M, SHADOW_BALL, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DREAM_EATER, REST, THIEF, NIGHTMARE, FAIRY_WIND, MOONBLAST, DARK_PULSE, FLY, STRENGTH, FLASH, TRI_ATTACK
 	; end
