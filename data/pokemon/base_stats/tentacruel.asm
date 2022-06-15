8eddfddf52fb4249c2f1aedd2b916641413beec0
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 14:54:59 -0700

    Update tentacruel.asm

--- data/pokemon/base_stats/tentacruel.asm
+++ data/pokemon/base_stats/tentacruel.asm
@@ -6,7 +6,7 @@
 	db WATER, POISON ; type
 	db 60 ; catch rate
 	db 205 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	db ANTIVENOM, MAX_REPEL ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 20 ; step cycles to hatch
@@ -14,7 +14,7 @@
 	INCBIN "gfx/pokemon/tentacruel/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_SLOW ; growth rate
-	dn EGG_WATER_3, EGG_WATER_3 ; egg groups
+	dn EGG_WATER_3, EGG_INDETERMINATE ; egg groups
 
 	; tm/hm learnset
 	tmhm CURSE, TOXIC, HIDDEN_POWER, SNORE, BLIZZARD, HYPER_BEAM, ICY_WIND, PROTECT, RAIN_DANCE, GIGA_DRAIN, ENDURE, FRUSTRATION, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, REST, ATTRACT, CUT, SURF, WHIRLPOOL, ICE_BEAM
