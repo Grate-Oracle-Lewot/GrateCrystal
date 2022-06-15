e23ad44df4543e467578e198802b4c6b4ac6a906
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 14:39:46 -0700

    Update smoochum.asm

--- data/pokemon/base_stats/smoochum.asm
+++ data/pokemon/base_stats/smoochum.asm
@@ -3,13 +3,13 @@
 	db  45,  30,  15,  65,  85,  65
 	;   hp  atk  def  spd  sat  sdf
 
-	db ICE, PSYCHIC_TYPE ; type
+	db ICE, FAIRY ; type
 	db 45 ; catch rate
 	db 87 ; base exp
-	db ICE_BERRY, ICE_BERRY ; items
+	db ICE_BERRY, TM_ICE_PUNCH ; items
 	db GENDER_F100 ; gender ratio
 	db 100 ; unknown 1
-	db 25 ; step cycles to hatch
+	db 13 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/smoochum/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
@@ -17,5 +17,5 @@
 	dn EGG_NONE, EGG_NONE ; egg groups
 
 	; tm/hm learnset
-	tmhm DYNAMICPUNCH, CURSE, TOXIC, PSYCH_UP, HIDDEN_POWER, SWEET_SCENT, SNORE, BLIZZARD, ICY_WIND, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, RETURN, PSYCHIC_M, SHADOW_BALL, MUD_SLAP, DOUBLE_TEAM, ICE_PUNCH, SWAGGER, SLEEP_TALK, DREAM_EATER, REST, ATTRACT, THIEF, NIGHTMARE, ICE_BEAM
+	tmhm DYNAMICPUNCH, CURSE, TOXIC, PSYCH_UP, HIDDEN_POWER, SWEET_SCENT, SNORE, BLIZZARD, ICY_WIND, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, RETURN, PSYCHIC_M, SHADOW_BALL, MUD_SLAP, DOUBLE_TEAM, ICE_PUNCH, SWAGGER, SLEEP_TALK, DREAM_EATER, REST, ATTRACT, THIEF, NIGHTMARE, FAIRY_WIND, HAIL, NIGHT_TERROR, ICE_BEAM, TRI_ATTACK
 	; end
