c1ea1393096680feb0ef436625e2e13e40a885f1
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 13:48:16 -0700

    Update onix.asm

--- data/pokemon/base_stats/onix.asm
+++ data/pokemon/base_stats/onix.asm
@@ -6,16 +6,16 @@
 	db ROCK, GROUND ; type
 	db 45 ; catch rate
 	db 108 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	db HARD_STONE, METAL_COAT ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
-	db 25 ; step cycles to hatch
+	db 13 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/onix/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
-	dn EGG_MINERAL, EGG_MINERAL ; egg groups
+	dn EGG_MINERAL, EGG_DRAGON ; egg groups
 
 	; tm/hm learnset
-	tmhm HEADBUTT, CURSE, ROAR, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, PROTECT, ENDURE, FRUSTRATION, IRON_TAIL, EARTHQUAKE, RETURN, DIG, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SANDSTORM, REST, ATTRACT, STRENGTH
+	tmhm HEADBUTT, CURSE, ROAR, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, PROTECT, ENDURE, FRUSTRATION, IRON_TAIL, DRAGONBREATH, EARTHQUAKE, RETURN, DIG, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SANDSTORM, REST, ATTRACT, STRENGTH, FLAMETHROWER
 	; end
