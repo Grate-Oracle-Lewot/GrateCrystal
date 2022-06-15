dfb5e159fed61dd61d1ed314d2fd269b4bed677c
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 11:21:18 -0700

    Update articuno.asm

--- data/pokemon/base_stats/articuno.asm
+++ data/pokemon/base_stats/articuno.asm
@@ -4,18 +4,18 @@
 	;   hp  atk  def  spd  sat  sdf
 
 	db ICE, FLYING ; type
-	db 3 ; catch rate
+	db 10 ; catch rate
 	db 215 ; base exp
 	db NO_ITEM, NO_ITEM ; items
 	db GENDER_UNKNOWN ; gender ratio
 	db 100 ; unknown 1
-	db 80 ; step cycles to hatch
+	db 42 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/articuno/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_SLOW ; growth rate
-	dn EGG_NONE, EGG_NONE ; egg groups
+	dn EGG_FLYING, EGG_MONSTER ; egg groups
 
 	; tm/hm learnset
-	tmhm CURSE, ROAR, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, BLIZZARD, HYPER_BEAM, ICY_WIND, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, RETURN, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SANDSTORM, SWIFT, DETECT, REST, STEEL_WING, FLY, ICE_BEAM
+	tmhm CURSE, ROAR, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, BLIZZARD, HYPER_BEAM, ICY_WIND, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, RETURN, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SANDSTORM, SWIFT, REST, STEEL_WING, HAIL, FLY, ICE_BEAM, TRI_ATTACK
 	; end
