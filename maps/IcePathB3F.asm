6eb3f1097917ee99ea8d2f33e2f7e8c47d5ac93a
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 16:36:11 -0700

    Update IcePathB3F.asm

--- maps/IcePathB3F.asm
+++ maps/IcePathB3F.asm
@@ -1,6 +1,7 @@
 	object_const_def
 	const ICEPATHB3F_POKE_BALL
 	const ICEPATHB3F_ROCK
+	const ICEPATHB3F_BOULDER
 
 IcePathB3F_MapScripts:
 	def_scene_scripts
@@ -13,12 +14,16 @@ IcePathB3FNevermeltice:
 IcePathB3FRock:
 	jumpstd SmashRockScript
 
+IcePathB3FBoulder:
+	jumpstd StrengthBoulderScript
+
 IcePathB3F_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
 	warp_event  3,  5, ICE_PATH_B2F_MAHOGANY_SIDE, 2
 	warp_event 15,  5, ICE_PATH_B2F_BLACKTHORN_SIDE, 2
+	warp_event  3, 11, ICE_PATH_ARTICUNO_CHAMBER, 1
 
 	def_coord_events
 
@@ -27,3 +32,4 @@ IcePathB3F_MapEvents:
 	def_object_events
 	object_event  5,  7, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, IcePathB3FNevermeltice, EVENT_ICE_PATH_B3F_NEVERMELTICE
 	object_event  6,  6, SPRITE_ROCK, SPRITEMOVEDATA_SMASHABLE_ROCK, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, IcePathB3FRock, -1
+	object_event  8, 14, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, IcePathB3FBoulder, -1
