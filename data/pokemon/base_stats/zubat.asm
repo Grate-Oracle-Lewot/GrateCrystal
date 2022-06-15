9d3484f2e5d025116274111aca45ea3cc76e8290
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 15:14:42 -0700

    Update zubat.asm

--- data/pokemon/base_stats/zubat.asm
+++ data/pokemon/base_stats/zubat.asm
@@ -6,16 +6,16 @@
 	db POISON, FLYING ; type
 	db 255 ; catch rate
 	db 54 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	db BITTER_BERRY, DICTIONARY ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
-	db 15 ; step cycles to hatch
+	db 8 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/zubat/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
-	dn EGG_FLYING, EGG_FLYING ; egg groups
+	dn EGG_FLYING, EGG_GROUND ; egg groups
 
 	; tm/hm learnset
-	tmhm CURSE, TOXIC, HIDDEN_POWER, SUNNY_DAY, SNORE, PROTECT, GIGA_DRAIN, ENDURE, FRUSTRATION, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DETECT, REST, ATTRACT, THIEF, STEEL_WING
+	tmhm CURSE, TOXIC, HIDDEN_POWER, SUNNY_DAY, SNORE, PROTECT, GIGA_DRAIN, ENDURE, FRUSTRATION, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, SWIFT, REST, ATTRACT, THIEF, STEEL_WING, DARK_PULSE, NIGHT_TERROR
 	; end
