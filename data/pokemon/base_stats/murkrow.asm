70cb960eff27b66fa6c92694617a1553e42b1e99
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 13:26:27 -0700

    Update murkrow.asm

--- data/pokemon/base_stats/murkrow.asm
+++ data/pokemon/base_stats/murkrow.asm
@@ -1,21 +1,21 @@
 	db MURKROW ; 198
 
-	db  60,  85,  42,  91,  85,  42
+	db  80, 105,  42,  91,  95,  42
 	;   hp  atk  def  spd  sat  sdf
 
 	db DARK, FLYING ; type
-	db 30 ; catch rate
+	db 45 ; catch rate
 	db 107 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	db BLACKGLASSES, LIMBO_STICK ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
-	db 20 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/murkrow/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_SLOW ; growth rate
-	dn EGG_FLYING, EGG_FLYING ; egg groups
+	dn EGG_FLYING, EGG_GROUND ; egg groups
 
 	; tm/hm learnset
-	tmhm CURSE, TOXIC, PSYCH_UP, HIDDEN_POWER, SUNNY_DAY, SNORE, ICY_WIND, PROTECT, ENDURE, FRUSTRATION, RETURN, SHADOW_BALL, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DREAM_EATER, DETECT, REST, ATTRACT, THIEF, STEEL_WING, NIGHTMARE, FLY
+	tmhm CURSE, TOXIC, PSYCH_UP, HIDDEN_POWER, SUNNY_DAY, SNORE, ICY_WIND, PROTECT, ENDURE, FRUSTRATION, RETURN, SHADOW_BALL, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DREAM_EATER, REST, ATTRACT, THIEF, STEEL_WING, NIGHTMARE, DARK_PULSE, NIGHT_TERROR, FLY
 	; end
