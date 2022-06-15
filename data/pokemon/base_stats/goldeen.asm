bc4ca9774e7964d1af29cbc38dc07be6df8bf0cf
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 12:25:50 -0700

    Update goldeen.asm

--- data/pokemon/base_stats/goldeen.asm
+++ data/pokemon/base_stats/goldeen.asm
@@ -6,15 +6,15 @@
 	db WATER, WATER ; type
 	db 225 ; catch rate
 	db 111 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	db FRESH_WATER, MYSTIC_WATER ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
-	db 20 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/goldeen/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
-	dn EGG_WATER_2, EGG_WATER_2 ; egg groups
+	dn EGG_WATER_2, EGG_WATER_1 ; egg groups
 
 	; tm/hm learnset
 	tmhm CURSE, TOXIC, HIDDEN_POWER, SNORE, BLIZZARD, ICY_WIND, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, REST, ATTRACT, SURF, WATERFALL, ICE_BEAM
