a49aea77dd5c72aaaf0f08d553bfe06e826b93e7
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 14:43:09 -0700

    Update spinarak.asm

--- data/pokemon/base_stats/spinarak.asm
+++ data/pokemon/base_stats/spinarak.asm
@@ -1,21 +1,21 @@
 	db SPINARAK ; 167
 
-	db  40,  60,  40,  30,  40,  40
+	db  50,  60,  50,  37,  50,  50
 	;   hp  atk  def  spd  sat  sdf
 
 	db BUG, POISON ; type
 	db 255 ; catch rate
 	db 54 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	db ANTIDOTE, ANTIVENOM ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
-	db 15 ; step cycles to hatch
+	db 8 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/spinarak/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_FAST ; growth rate
-	dn EGG_BUG, EGG_BUG ; egg groups
+	dn EGG_BUG, EGG_WATER_3 ; egg groups
 
 	; tm/hm learnset
-	tmhm CURSE, TOXIC, HIDDEN_POWER, SUNNY_DAY, SNORE, PROTECT, GIGA_DRAIN, ENDURE, FRUSTRATION, SOLARBEAM, RETURN, DIG, PSYCHIC_M, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, REST, ATTRACT, THIEF, FLASH
+	tmhm CURSE, TOXIC, HIDDEN_POWER, SUNNY_DAY, SNORE, PROTECT, GIGA_DRAIN, ENDURE, FRUSTRATION, SOLARBEAM, RETURN, DIG, PSYCHIC_M, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, REST, ATTRACT, THIEF, DARK_PULSE, NIGHT_TERROR, FLASH
 	; end
