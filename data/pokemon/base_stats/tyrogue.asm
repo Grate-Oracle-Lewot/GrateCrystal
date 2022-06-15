a36c0f919274012415fcc48bbe4e4418e567929a
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 14:58:43 -0700

    Update tyrogue.asm

--- data/pokemon/base_stats/tyrogue.asm
+++ data/pokemon/base_stats/tyrogue.asm
@@ -6,10 +6,10 @@
 	db FIGHTING, FIGHTING ; type
 	db 75 ; catch rate
 	db 91 ; base exp
-	db NO_ITEM, NO_ITEM ; items
-	db GENDER_F0 ; gender ratio
+	db EVIOLITE, BERSERK_GENE ; items
+	db GENDER_F12_5 ; gender ratio
 	db 100 ; unknown 1
-	db 25 ; step cycles to hatch
+	db 13 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/tyrogue/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
@@ -17,5 +17,5 @@
 	dn EGG_NONE, EGG_NONE ; egg groups
 
 	; tm/hm learnset
-	tmhm HEADBUTT, CURSE, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, PROTECT, ENDURE, FRUSTRATION, RETURN, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DETECT, REST, ATTRACT, THIEF, STRENGTH
+	tmhm HEADBUTT, CURSE, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, PROTECT, ENDURE, FRUSTRATION, RETURN, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, REST, ATTRACT, THIEF, STRENGTH
 	; end
