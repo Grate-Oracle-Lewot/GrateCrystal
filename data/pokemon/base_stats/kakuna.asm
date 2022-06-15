52bb791901d09272400e3739530e0b6851cce916
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 12:50:09 -0700

    Update kakuna.asm

--- data/pokemon/base_stats/kakuna.asm
+++ data/pokemon/base_stats/kakuna.asm
@@ -6,7 +6,7 @@
 	db BUG, POISON ; type
 	db 120 ; catch rate
 	db 71 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	db X_DEFEND, IRON ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 15 ; step cycles to hatch
@@ -14,7 +14,7 @@
 	INCBIN "gfx/pokemon/kakuna/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
-	dn EGG_BUG, EGG_BUG ; egg groups
+	dn EGG_BUG, EGG_WATER_3 ; egg groups
 
 	; tm/hm learnset
 	tmhm
