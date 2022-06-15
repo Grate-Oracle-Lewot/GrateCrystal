dfcee58747a2fb5bd6c70a87953f7cbc7342d3b4
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 14:25:15 -0700

    Update rhyhorn.asm

--- data/pokemon/base_stats/rhyhorn.asm
+++ data/pokemon/base_stats/rhyhorn.asm
@@ -3,13 +3,13 @@
 	db  80,  85,  95,  25,  30,  30
 	;   hp  atk  def  spd  sat  sdf
 
-	db GROUND, ROCK ; type
+	db STEEL, ROCK ; type
 	db 120 ; catch rate
 	db 135 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	db IRON, METAL_COAT ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
-	db 20 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/rhyhorn/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
