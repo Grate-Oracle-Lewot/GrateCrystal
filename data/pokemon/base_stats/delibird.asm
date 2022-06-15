0af9f72621a070d49c36740e2a700b4b0a2e1559
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 11:55:18 -0700

    Update delibird.asm

--- data/pokemon/base_stats/delibird.asm
+++ data/pokemon/base_stats/delibird.asm
@@ -6,16 +6,16 @@
 	db ICE, FLYING ; type
 	db 45 ; catch rate
 	db 183 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	db RARE_CANDY, LUCKY_EGG ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
-	db 20 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/delibird/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_FAST ; growth rate
-	dn EGG_WATER_1, EGG_GROUND ; egg groups
+	dn EGG_FLYING, EGG_FAIRY ; egg groups
 
 	; tm/hm learnset
-	tmhm HEADBUTT, CURSE, TOXIC, HIDDEN_POWER, SNORE, BLIZZARD, ICY_WIND, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, RETURN, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DETECT, REST, ATTRACT, THIEF, FLY, ICE_BEAM
+	tmhm TOXIC, PSYCH_UP, HIDDEN_POWER, SUNNY_DAY, SWEET_SCENT, SNORE, BLIZZARD, ICY_WIND, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, SOLARBEAM, RETURN, SHADOW_BALL, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SANDSTORM, SWIFT, REST, ATTRACT, THIEF, FAIRY_WIND, HAIL, STRENGTH, FLASH, ICE_BEAM, TRI_ATTACK
 	; end
