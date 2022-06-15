3c558a333dfaa2e9bff234506238128f9645a9ef
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 11:34:52 -0700

    Update caterpie.asm

--- data/pokemon/base_stats/caterpie.asm
+++ data/pokemon/base_stats/caterpie.asm
@@ -6,15 +6,15 @@
 	db BUG, BUG ; type
 	db 255 ; catch rate
 	db 53 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	db BERRY, EVIOLITE ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
-	db 15 ; step cycles to hatch
+	db 8 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/caterpie/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
-	dn EGG_BUG, EGG_BUG ; egg groups
+	dn EGG_BUG, EGG_WATER_3 ; egg groups
 
 	; tm/hm learnset
 	tmhm
