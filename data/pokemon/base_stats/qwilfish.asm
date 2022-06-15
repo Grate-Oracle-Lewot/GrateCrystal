0382963b73b12957c5806e7b8049a30fd4a0b00f
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 14:18:57 -0700

    Update qwilfish.asm

--- data/pokemon/base_stats/qwilfish.asm
+++ data/pokemon/base_stats/qwilfish.asm
@@ -1,21 +1,21 @@
 	db QWILFISH ; 211
 
-	db  65,  95,  75,  85,  55,  55
+	db  75, 105,  85,  85,  60,  60
 	;   hp  atk  def  spd  sat  sdf
 
-	db WATER, POISON ; type
+	db WATER, FIRE ; type
 	db 45 ; catch rate
 	db 100 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	db SODA_POP, CHARCOAL ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
-	db 20 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/qwilfish/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
-	dn EGG_WATER_2, EGG_WATER_2 ; egg groups
+	dn EGG_WATER_2, EGG_INDETERMINATE ; egg groups
 
 	; tm/hm learnset
-	tmhm HEADBUTT, CURSE, ROLLOUT, TOXIC, HIDDEN_POWER, SNORE, BLIZZARD, ICY_WIND, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, SWIFT, DEFENSE_CURL, REST, ATTRACT, SURF, WHIRLPOOL, WATERFALL, ICE_BEAM
+	tmhm HEADBUTT, CURSE, ROLLOUT, TOXIC, HIDDEN_POWER, SUNNY_DAY, SNORE, BLIZZARD, ICY_WIND, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, FIRE_BLAST, SWIFT, DEFENSE_CURL, REST, ATTRACT, SURF, WHIRLPOOL, WATERFALL, FLAMETHROWER, ICE_BEAM, TRI_ATTACK
 	; end
