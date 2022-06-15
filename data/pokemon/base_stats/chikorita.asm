a4a3294d7f34077d513ac16e2f3305ef4e3acf99
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 11:40:43 -0700

    Update chikorita.asm

--- data/pokemon/base_stats/chikorita.asm
+++ data/pokemon/base_stats/chikorita.asm
@@ -6,10 +6,10 @@
 	db GRASS, GRASS ; type
 	db 45 ; catch rate
 	db 64 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	db GOLD_BERRY, MIRACLE_SEED ; items
 	db GENDER_F12_5 ; gender ratio
 	db 100 ; unknown 1
-	db 20 ; step cycles to hatch
+	db 11 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/chikorita/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
@@ -17,5 +17,5 @@
 	dn EGG_MONSTER, EGG_PLANT ; egg groups
 
 	; tm/hm learnset
-	tmhm HEADBUTT, CURSE, TOXIC, HIDDEN_POWER, SUNNY_DAY, SWEET_SCENT, SNORE, PROTECT, GIGA_DRAIN, ENDURE, FRUSTRATION, SOLARBEAM, IRON_TAIL, RETURN, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, DETECT, REST, ATTRACT, CUT, FLASH
+	tmhm HEADBUTT, CURSE, TOXIC, HIDDEN_POWER, SUNNY_DAY, SWEET_SCENT, SNORE, PROTECT, GIGA_DRAIN, ENDURE, FRUSTRATION, SOLARBEAM, IRON_TAIL, RETURN, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, REST, ATTRACT, CUT, FLASH, TRI_ATTACK
 	; end
