9ac9c76ee778f19103ed6b497c939f8069d054d9
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 12:18:27 -0700

    Update forretress.asm

--- data/pokemon/base_stats/forretress.asm
+++ data/pokemon/base_stats/forretress.asm
@@ -6,7 +6,7 @@
 	db BUG, STEEL ; type
 	db 75 ; catch rate
 	db 118 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	db IRON, BRICK_PIECE ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 20 ; step cycles to hatch
@@ -14,7 +14,7 @@
 	INCBIN "gfx/pokemon/forretress/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
-	dn EGG_BUG, EGG_BUG ; egg groups
+	dn EGG_BUG, EGG_MINERAL ; egg groups
 
 	; tm/hm learnset
 	tmhm HEADBUTT, CURSE, ROLLOUT, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SWEET_SCENT, SNORE, HYPER_BEAM, PROTECT, GIGA_DRAIN, ENDURE, FRUSTRATION, SOLARBEAM, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SANDSTORM, DEFENSE_CURL, REST, ATTRACT, STRENGTH
