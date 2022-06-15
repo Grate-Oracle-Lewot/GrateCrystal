e8a1bf0e6829ebfe49722be421d771c2c6689437
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 13:52:36 -0700

    Update pidgeot.asm

--- data/pokemon/base_stats/pidgeot.asm
+++ data/pokemon/base_stats/pidgeot.asm
@@ -6,7 +6,7 @@
 	db NORMAL, FLYING ; type
 	db 45 ; catch rate
 	db 172 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	db TM_RETURN, TM_SWIFT ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 15 ; step cycles to hatch
@@ -14,8 +14,8 @@
 	INCBIN "gfx/pokemon/pidgeot/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_SLOW ; growth rate
-	dn EGG_FLYING, EGG_FLYING ; egg groups
+	dn EGG_FLYING, EGG_GROUND ; egg groups
 
 	; tm/hm learnset
-	tmhm CURSE, TOXIC, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, ENDURE, FRUSTRATION, RETURN, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DETECT, REST, ATTRACT, THIEF, STEEL_WING, FLY
+	tmhm CURSE, TOXIC, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, ENDURE, FRUSTRATION, RETURN, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, REST, ATTRACT, THIEF, STEEL_WING, FAIRY_WIND, FLY
 	; end
