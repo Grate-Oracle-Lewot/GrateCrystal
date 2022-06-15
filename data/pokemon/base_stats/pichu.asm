7240c709daeb2037366ce6d76aa526111b51d825
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 13:51:54 -0700

    Update pichu.asm

--- data/pokemon/base_stats/pichu.asm
+++ data/pokemon/base_stats/pichu.asm
@@ -3,13 +3,13 @@
 	db  20,  40,  15,  60,  35,  35
 	;   hp  atk  def  spd  sat  sdf
 
-	db ELECTRIC, ELECTRIC ; type
+	db ELECTRIC, FAIRY ; type
 	db 190 ; catch rate
 	db 42 ; base exp
-	db NO_ITEM, BERRY ; items
+	db BERRY, LIGHT_BALL ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
-	db 10 ; step cycles to hatch
+	db 5 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/pichu/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
@@ -17,5 +17,5 @@
 	dn EGG_NONE, EGG_NONE ; egg groups
 
 	; tm/hm learnset
-	tmhm HEADBUTT, CURSE, ROLLOUT, TOXIC, ZAP_CANNON, HIDDEN_POWER, SNORE, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, IRON_TAIL, THUNDER, RETURN, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DEFENSE_CURL, DETECT, REST, ATTRACT, FLASH, THUNDERBOLT
+	tmhm HEADBUTT, CURSE, ROLLOUT, TOXIC, ZAP_CANNON, HIDDEN_POWER, SNORE, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, IRON_TAIL, THUNDER, RETURN, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DEFENSE_CURL, REST, ATTRACT, FAIRY_WIND, MOONBLAST, FLY, SURF, FLASH, THUNDERBOLT, TRI_ATTACK
 	; end
