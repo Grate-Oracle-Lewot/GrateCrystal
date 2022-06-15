1909aaaffea120d4508775da4c1cafe06fc3af04
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 12:37:22 -0700

    Update hoppip.asm

--- data/pokemon/base_stats/hoppip.asm
+++ data/pokemon/base_stats/hoppip.asm
@@ -6,10 +6,10 @@
 	db GRASS, FLYING ; type
 	db 255 ; catch rate
 	db 74 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	db MIRACLEBERRY, MIRACLE_SEED ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
-	db 20 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/hoppip/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
@@ -17,5 +17,5 @@
 	dn EGG_FAIRY, EGG_PLANT ; egg groups
 
 	; tm/hm learnset
-	tmhm HEADBUTT, CURSE, TOXIC, HIDDEN_POWER, SUNNY_DAY, SWEET_SCENT, SNORE, PROTECT, GIGA_DRAIN, ENDURE, FRUSTRATION, SOLARBEAM, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, DEFENSE_CURL, REST, ATTRACT, FLASH
+	tmhm HEADBUTT, CURSE, TOXIC, HIDDEN_POWER, SUNNY_DAY, SWEET_SCENT, SNORE, PROTECT, GIGA_DRAIN, ENDURE, FRUSTRATION, SOLARBEAM, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, DEFENSE_CURL, REST, ATTRACT, FAIRY_WIND, FLASH
 	; end
