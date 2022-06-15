070999aa604a88fe8ca402a2de5af6ec0a730d26
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 13:07:00 -0700

    Update magikarp.asm

--- data/pokemon/base_stats/magikarp.asm
+++ data/pokemon/base_stats/magikarp.asm
@@ -6,10 +6,10 @@
 	db WATER, WATER ; type
 	db 255 ; catch rate
 	db 20 ; base exp
-	db NO_ITEM, NO_ITEM ; items
+	db DRAGON_SCALE, TM_HYPER_BEAM ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
-	db 5 ; step cycles to hatch
+	db 3 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/magikarp/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
