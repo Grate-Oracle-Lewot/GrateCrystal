8c5f934010f787393b3b1f26676de341a700382b
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 18:07:07 -0700

    Update RockTunnelB1F.asm

--- maps/RockTunnelB1F.asm
+++ maps/RockTunnelB1F.asm
@@ -2,6 +2,7 @@
 	const ROCKTUNNELB1F_POKE_BALL1
 	const ROCKTUNNELB1F_POKE_BALL2
 	const ROCKTUNNELB1F_POKE_BALL3
+	const ROCKTUNNELB1F_ROCK
 
 RockTunnelB1F_MapScripts:
 	def_scene_scripts
@@ -17,6 +18,9 @@ RockTunnelB1FPPUp:
 RockTunnelB1FRevive:
 	itemball REVIVE
 
+RockTunnelB1FRock:
+	jumpstd SmashRockScript
+
 RockTunnelB1FHiddenMaxPotion:
 	hiddenitem MAX_POTION, EVENT_ROCK_TUNNEL_B1F_HIDDEN_MAX_POTION
 
@@ -28,6 +32,7 @@ RockTunnelB1F_MapEvents:
 	warp_event 17,  9, ROCK_TUNNEL_1F, 4
 	warp_event 23,  3, ROCK_TUNNEL_1F, 3
 	warp_event 25, 23, ROCK_TUNNEL_1F, 5
+	warp_event 11, 21, ROCK_TUNNEL_ZAPDOS_CHAMBER, 1
 
 	def_coord_events
 
@@ -38,3 +43,4 @@ RockTunnelB1F_MapEvents:
 	object_event  7, 25, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, RockTunnelB1FIron, EVENT_ROCK_TUNNEL_B1F_IRON
 	object_event  6, 17, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, RockTunnelB1FPPUp, EVENT_ROCK_TUNNEL_B1F_PP_UP
 	object_event 15,  2, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, RockTunnelB1FRevive, EVENT_ROCK_TUNNEL_B1F_REVIVE
+	object_event 11, 24, SPRITE_ROCK, SPRITEMOVEDATA_SMASHABLE_ROCK, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, RockTunnelB1FRock, -1
