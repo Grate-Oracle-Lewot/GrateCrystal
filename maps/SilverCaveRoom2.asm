c76ae01d7341f912e54419bc8b96ee55d1faae13
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 19:40:19 -0700

    Update SilverCaveRoom2.asm

--- maps/SilverCaveRoom2.asm
+++ maps/SilverCaveRoom2.asm
@@ -2,6 +2,8 @@
 	const SILVERCAVEROOM2_POKE_BALL1
 	const SILVERCAVEROOM2_POKE_BALL2
 	const SILVERCAVEROOM2_POKE_BALL3
+	const SILVERCAVEROOM2_CHRIS
+	const SILVERCAVEROOM2_SUPER_NERD
 
 SilverCaveRoom2_MapScripts:
 	def_scene_scripts
@@ -17,8 +19,64 @@ SilverCaveRoom2UltraBall:
 SilverCaveRoom2PPUp:
 	itemball PP_UP
 
+TrainerFred:
+	trainer CAL, FRED, EVENT_BEAT_TRAINER_FRED, TrainerFredSeenText, TrainerFredBeatenText, 0, .AfterScript
+
+.AfterScript:
+	endifjustbattled
+	opentext
+	writetext TrainerFredAfterBattleText
+	waitbutton
+	closetext
+	end
+
+TrainerMysticalmanRico:
+	trainer MYSTICALMAN, RICO, EVENT_BEAT_MYSTICALMAN_RICO, MysticalmanRicoSeenText, MysticalmanRicoBeatenText, 0, .AfterScript
+
+.AfterScript:
+	endifjustbattled
+	opentext
+	writetext MysticalmanRicoAfterBattleText
+	waitbutton
+	closetext
+	end
+
 SilverCaveRoom2HiddenMaxPotion:
 	hiddenitem MAX_POTION, EVENT_SILVER_CAVE_ROOM_2_HIDDEN_MAX_POTION
+
+TrainerFredSeenText:
+	text "Why does everyone"
+	line "say my #MON are"
+	cont "overpowered?"
+	done
+
+TrainerFredBeatenText:
+	text "See? They're not"
+	line "that strong!"
+	done
+
+TrainerFredAfterBattleText:
+	text "There's nothing"
+	line "wrong with using"
+	cont "legendaries."
+	done
+
+MysticalmanRicoSeenText:
+	text "Some #MON are"
+	line "just stronger than"
+	cont "others."
+
+	para "I'll show you!"
+	done
+
+MysticalmanRicoBeatenText:
+	text "Eh… whatever."
+	done
+
+MysticalmanRicoAfterBattleText:
+	text "I guess all #-"
+	line "MON can be strong."
+	done
 
 SilverCaveRoom2_MapEvents:
 	db 0, 0 ; filler
@@ -38,3 +96,5 @@ SilverCaveRoom2_MapEvents:
 	object_event 24, 10, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SilverCaveRoom2Calcium, EVENT_SILVER_CAVE_ROOM_2_CALCIUM
 	object_event 22, 24, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SilverCaveRoom2UltraBall, EVENT_SILVER_CAVE_ROOM_2_ULTRA_BALL
 	object_event  4, 20, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SilverCaveRoom2PPUp, EVENT_SILVER_CAVE_ROOM_2_PP_UP
+	object_event  9, 16, SPRITE_CHRIS, SPRITEMOVEDATA_SPINRANDOM_FAST, 0, 0, -1, -1, PAL_NPC_PINK, OBJECTTYPE_TRAINER, 2, TrainerFred, -1
+	object_event 24,  6, SPRITE_SUPER_NERD, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 3, TrainerMysticalmanRico, -1
