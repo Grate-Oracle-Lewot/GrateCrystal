12ae418df6fd0d9e59c40527977ec6fa202c6928
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-16 09:39:37 -0700

    Create whimsicott.asm

---
+++ data/pokemon/dex_entries/whimsicott.asm
@@ -0,0 +1,10 @@
+	db "WINDVEILED@" ; species name
+	dw 204, 146 ; height, weight
+
+	db   "It can slip"
+	next "through any gap,"
+	next "no matter how"
+
+	page "small. It leaves"
+	next "white fluff"
+	next "behind."
