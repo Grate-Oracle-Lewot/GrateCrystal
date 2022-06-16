ffc5526b0b00cd9a4dd9a7a61328b6037f2c0423
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 23:38:18 -0700

    Update probabilities.asm

--- data/wild/probabilities.asm
+++ data/wild/probabilities.asm
@@ -5,18 +5,20 @@ ENDM
 
 GrassMonProbTable:
 	table_width 2, GrassMonProbTable
-	mon_prob 30,  0 ; 30% chance
-	mon_prob 60,  1 ; 30% chance
-	mon_prob 80,  2 ; 20% chance
-	mon_prob 90,  3 ; 10% chance
-	mon_prob 95,  4 ;  5% chance
-	mon_prob 99,  5 ;  4% chance
-	mon_prob 100, 6 ;  1% chance
+	mon_prob 25,  0 ; 25% chance
+	mon_prob 50,  1 ; 25% chance
+	mon_prob 60,  2 ; 10% chance
+	mon_prob 70,  3 ; 10% chance
+	mon_prob 80,  4 ; 10% chance
+	mon_prob 90,  5 ; 10% chance
+	mon_prob 95,  6 ;  5% chance
+	mon_prob 100, 7 ;  5% chance
 	assert_table_length NUM_GRASSMON
 
 WaterMonProbTable:
 	table_width 2, WaterMonProbTable
-	mon_prob 60,  0 ; 60% chance
-	mon_prob 90,  1 ; 30% chance
-	mon_prob 100, 2 ; 10% chance
+	mon_prob 40,  0 ; 40% chance
+	mon_prob 80,  1 ; 40% chance
+	mon_prob 90,  2 ; 10% chance
+	mon_prob 100, 3 ; 10% chance
 	assert_table_length NUM_WATERMON
