8eb47240662737f01ff693ff607dbf6935dfd694
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-16 09:39:07 -0700

    Create yanmega.asm

---
+++ data/pokemon/dex_entries/yanmega.asm
@@ -0,0 +1,10 @@
+	db "OGREDARNER@" ; species name
+	dw 603, 1135 ; height, weight
+
+	db   "It bites off the"
+	next "heads of its prey"
+	next "in mid-flight."
+
+	page "Its wings can"
+	next "create shock waves"
+	next "that topple trees."
