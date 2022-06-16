d3dea75507d65a8904a246ab77bf156fde4d8909
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 19:39:15 -0700

    Update SilverCaveRoom1.asm

--- maps/SilverCaveRoom1.asm
+++ maps/SilverCaveRoom1.asm
@@ -3,6 +3,8 @@
 	const SILVERCAVEROOM1_POKE_BALL2
 	const SILVERCAVEROOM1_POKE_BALL3
 	const SILVERCAVEROOM1_POKE_BALL4
+	const SILVERCAVEROOM1_CHRIS
+	const SILVERCAVEROOM1_KIMONO_GIRL
 
 SilverCaveRoom1_MapScripts:
 	def_scene_scripts
@@ -21,11 +23,72 @@ SilverCaveRoom1EscapeRope:
 SilverCaveRoom1UltraBall:
 	itemball ULTRA_BALL
 
+TrainerIggy:
+	trainer CAL, IGGY, EVENT_BEAT_TRAINER_IGGY, TrainerIggySeenText, TrainerIggyBeatenText, 0, .AfterScript
+
+.AfterScript:
+	endifjustbattled
+	opentext
+	writetext TrainerIggyAfterBattleText
+	waitbutton
+	closetext
+	end
+
+TrainerKimonoGirlKyoshi:
+	trainer KIMONO_GIRL, KYOSHI, EVENT_BEAT_KIMONO_GIRL_KYOSHI, KimonoGirlKyoshiSeenText, KimonoGirlKyoshiBeatenText, 0, .AfterScript
+
+.AfterScript:
+	endifjustbattled
+	opentext
+	writetext KimonoGirlKyoshiAfterBattleText
+	waitbutton
+	closetext
+	end
+
 SilverCaveRoom1HiddenDireHit:
 	hiddenitem DIRE_HIT, EVENT_SILVER_CAVE_ROOM_1_HIDDEN_DIRE_HIT
 
 SilverCaveRoom1HiddenUltraBall:
 	hiddenitem ULTRA_BALL, EVENT_SILVER_CAVE_ROOM_1_HIDDEN_ULTRA_BALL
+
+TrainerIggySeenText:
+	text "I wanna be the"
+	line "very best…"
+
+	para "like no one ever"
+	line "was!"
+	done
+
+TrainerIggyBeatenText:
+	text "That wasn't my"
+	line "very best."
+	done
+
+TrainerIggyAfterBattleText:
+	text "You're pretty"
+	line "strong. I'll have"
+	cont "to train more."
+	done
+
+KimonoGirlKyoshiSeenText:
+	text "Interrupting my"
+	line "dance? How rude!"
+	done
+
+KimonoGirlKyoshiBeatenText:
+	text "Very well. I"
+	line "yield."
+	done
+
+KimonoGirlKyoshiAfterBattleText:
+	text "You must be the"
+	line "one who defeated"
+	cont "my five daughters."
+
+	para "Now I understand"
+	line "how. Your spirit"
+	cont "is true."
+	done
 
 SilverCaveRoom1_MapEvents:
 	db 0, 0 ; filler
@@ -45,3 +108,5 @@ SilverCaveRoom1_MapEvents:
 	object_event 15, 29, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SilverCaveRoom1Protein, EVENT_SILVER_CAVE_ROOM_1_PROTEIN
 	object_event  5, 30, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SilverCaveRoom1EscapeRope, EVENT_SILVER_CAVE_ROOM_1_ESCAPE_ROPE
 	object_event  7, 18, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SilverCaveRoom1UltraBall, EVENT_SILVER_CAVE_ROOM_1_ULTRA_BALL
+	object_event 11,  3, SPRITE_CHRIS, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_PINK, OBJECTTYPE_TRAINER, 3, TrainerIggy, -1
+	object_event  6, 21, SPRITE_KIMONO_GIRL, SPRITEMOVEDATA_SPINCOUNTERCLOCKWISE, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_TRAINER, 0, TrainerKimonoGirlKyoshi, -1
