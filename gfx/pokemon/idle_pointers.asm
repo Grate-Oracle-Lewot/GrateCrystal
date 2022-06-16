925d3fe0988e999ae2acd849f57cf5796bbdb231
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-16 00:59:27 -0700

    Update idle_pointers.asm

--- gfx/pokemon/idle_pointers.asm
+++ gfx/pokemon/idle_pointers.asm
@@ -251,4 +251,6 @@ AnimationIdlePointers:
 	dw LugiaAnimationIdle
 	dw HoOhAnimationIdle
 	dw CelebiAnimationIdle
+	dw YanmegaAnimationIdle
+	dw WhimsicottAnimationIdle
 	assert_table_length NUM_POKEMON
