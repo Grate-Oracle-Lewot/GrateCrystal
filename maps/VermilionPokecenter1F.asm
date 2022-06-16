34e40735868a2c3dd3c764327c2a6ade92d2358e
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 20:16:52 -0700

    Update VermilionPokecenter1F.asm

--- maps/VermilionPokecenter1F.asm
+++ maps/VermilionPokecenter1F.asm
@@ -1,5 +1,6 @@
 	object_const_def
 	const VERMILIONPOKECENTER1F_NURSE
+	const VERMILIONPOKECENTER1F_CHANSEY
 	const VERMILIONPOKECENTER1F_FISHING_GURU
 	const VERMILIONPOKECENTER1F_SAILOR
 	const VERMILIONPOKECENTER1F_BUG_CATCHER
@@ -11,6 +12,9 @@ VermilionPokecenter1F_MapScripts:
 
 VermilionPokecenter1FNurseScript:
 	jumpstd PokecenterNurseScript
+
+VermilionPokecenter1FChanseyScript:
+	jumpstd PokecenterChanseyScript
 
 VermilionPokecenter1FFishingGuruScript:
 	faceplayer
@@ -87,7 +91,8 @@ VermilionPokecenter1F_MapEvents:
 	def_bg_events
 
 	def_object_events
-	object_event  3,  1, SPRITE_NURSE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, VermilionPokecenter1FNurseScript, -1
+	object_event  3,  1, SPRITE_NURSE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, VermilionPokecenter1FNurseScript, -1
+	object_event  4,  1, SPRITE_CHANSEY, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, PAL_OW_PINK, OBJECTTYPE_SCRIPT, 0, VermilionPokecenter1FChanseyScript, -1
 	object_event  7,  2, SPRITE_FISHING_GURU, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, VermilionPokecenter1FFishingGuruScript, -1
 	object_event  6,  5, SPRITE_SAILOR, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 1, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, VermilionPokecenter1FSailorScript, -1
 	object_event  1,  5, SPRITE_BUG_CATCHER, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, VermilionPokecenter1FBugCatcherScript, -1
