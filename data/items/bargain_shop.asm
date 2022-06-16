a421fb68ff013248a0bff9e923f2c1bd083cedd5
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 20:47:23 -0700

    Update bargain_shop.asm

--- data/items/bargain_shop.asm
+++ data/items/bargain_shop.asm
@@ -1,8 +1,8 @@
 BargainShopData:
 	db 5
-	dbw NUGGET,     4500
-	dbw PEARL,       650
-	dbw BIG_PEARL,  3500
-	dbw STARDUST,    900
-	dbw STAR_PIECE, 4600
+	dbw ETHER,      1000
+	dbw MAX_ETHER,  1500
+	dbw ELIXER,     2500
+	dbw MAX_ELIXER, 3500
+	dbw RARE_CANDY, 4000
 	db -1
