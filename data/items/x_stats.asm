ba072030f06385fbfb604b603c9e0c832593c28d
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 20:50:03 -0700

    Update x_stats.asm

--- data/items/x_stats.asm
+++ data/items/x_stats.asm
@@ -1,6 +1,7 @@
 XItemStats:
-	;  item,      stat
-	db X_ATTACK,  ATTACK
-	db X_DEFEND,  DEFENSE
-	db X_SPEED,   SPEED
-	db X_SPECIAL, SP_ATTACK
+	;  item,        stat
+	db X_ATTACK,    ATTACK
+	db X_DEFEND,    DEFENSE
+	db X_SPEED,     SPEED
+	db X_SPECIAL,   SP_ATTACK
+	db X_SP_DEFEND, SP_DEFENSE
