1885570795c003b8e66fbfea82a90937c8b6e73f
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 21:08:23 -0700

    Update item_effects.asm

--- engine/items/item_effects.asm
+++ engine/items/item_effects.asm
@@ -511,6 +511,19 @@ PokeBallEffect:
 	ld hl, Text_GotchaMonWasCaught
 	call PrintText
 
+	ld a, [wTempSpecies]
+	ld l, a
+	ld a, [wCurPartyLevel]
+	ld h, a
+	push hl
+	farcall ApplyExperienceAfterEnemyCaught
+	pop hl
+	ld a, l
+	ld [wCurPartySpecies], a
+	ld [wTempSpecies], a
+	ld a, h
+	ld [wCurPartyLevel], a
+
 	call ClearSprites
 
 	ld a, [wTempSpecies]
@@ -2315,7 +2328,7 @@ ItemfinderEffect:
 	farcall ItemFinder
 	ret
 
-	PocketPCEffect:
+PocketPCEffect:
 	farcall PocketPCFunction
 	ret
 
