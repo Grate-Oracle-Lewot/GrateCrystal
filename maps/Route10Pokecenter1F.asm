543d39b241a66f4868a215b4846ef6a0481a0ce1
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 18:08:16 -0700

    Update Route10Pokecenter1F.asm

--- maps/Route10Pokecenter1F.asm
+++ maps/Route10Pokecenter1F.asm
@@ -1,5 +1,6 @@
 	object_const_def
 	const ROUTE10POKECENTER1F_NURSE
+	const ROUTE10POKECENTER1F_CHANSEY
 	const ROUTE10POKECENTER1F_GENTLEMAN
 	const ROUTE10POKECENTER1F_GYM_GUIDE
 	const ROUTE10POKECENTER1F_COOLTRAINER_F
@@ -11,6 +12,9 @@ Route10Pokecenter1F_MapScripts:
 
 Route10Pokecenter1FNurseScript:
 	jumpstd PokecenterNurseScript
+
+Route10Pokecenter1FChanseyScript:
+	jumpstd PokecenterChanseyScript
 
 Route10Pokecenter1FGentlemanScript:
 	jumptextfaceplayer Route10Pokecenter1FGentlemanText
@@ -91,7 +95,8 @@ Route10Pokecenter1F_MapEvents:
 	def_bg_events
 
 	def_object_events
-	object_event  3,  1, SPRITE_NURSE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Route10Pokecenter1FNurseScript, -1
+	object_event  3,  1, SPRITE_NURSE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_PINK, OBJECTTYPE_SCRIPT, 0, Route10Pokecenter1FNurseScript, -1
+	object_event  4,  1, SPRITE_CHANSEY, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, PAL_OW_PINK, OBJECTTYPE_SCRIPT, 0, Route10Pokecenter1FChanseyScript, -1
 	object_event  7,  6, SPRITE_GENTLEMAN, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, Route10Pokecenter1FGentlemanScript, -1
 	object_event  7,  2, SPRITE_GYM_GUIDE, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, Route10Pokecenter1FGymGuideScript, -1
 	object_event  1,  3, SPRITE_COOLTRAINER_F, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Route10Pokecenter1FCooltrainerFScript, -1
