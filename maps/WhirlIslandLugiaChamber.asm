45f61e25b56d5b5961f497093ef6d39515eabf3e
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-15 20:31:23 -0700

    Update WhirlIslandLugiaChamber.asm

--- maps/WhirlIslandLugiaChamber.asm
+++ maps/WhirlIslandLugiaChamber.asm
@@ -1,5 +1,6 @@
 	object_const_def
 	const WHIRLISLANDLUGIACHAMBER_LUGIA
+	const WHIRLISLANDLUGIACHAMBER_SLOWKING
 
 WhirlIslandLugiaChamber_MapScripts:
 	def_scene_scripts
@@ -30,15 +31,83 @@ Lugia:
 	pause 15
 	closetext
 	setevent EVENT_FOUGHT_LUGIA
-	loadvar VAR_BATTLETYPE, BATTLETYPE_FORCEITEM
+	loadvar VAR_BATTLETYPE, BATTLETYPE_SUICUNE
 	loadwildmon LUGIA, 60
 	startbattle
 	disappear WHIRLISLANDLUGIACHAMBER_LUGIA
 	reloadmapafterbattle
 	end
 
+WhirlIslandLugiaChamberSlowkingScript:
+	checkevent EVENT_FOUGHT_LUGIA
+	iftrue .AfterLugiaScript
+	checkitem SILVER_WING
+	iftrue .DuringLugiaScript
+	jumptext WhirlIslandLugiaChamberSlowkingBeforeText
+
+.DuringLugiaScript
+	jumptext WhirlIslandLugiaChamberSlowkingDuringText
+
+.AfterLugiaScript
+	jumptext WhirlIslandLugiaChamberSlowkingAfterText
+
 LugiaText:
 	text "Gyaaas!"
+	done
+
+WhirlIslandLugiaChamberSlowkingBeforeText:
+	text "SLOWKING: …"
+
+	para "The master of the"
+	line "legendary birds,"
+	cont "LUGIA…"
+
+	para "…"
+
+	para "It dwells here…"
+	line "But it will only"
+
+	para "appear before you"
+	line "if you have some-"
+	cont "thing with its"
+	cont "scent…"
+
+	para "…"
+
+	para "You may not find"
+	line "such a thing in"
+	cont "this region…"
+	done
+
+WhirlIslandLugiaChamberSlowkingDuringText:
+	text "SLOWKING: …"
+
+	para "The master of the"
+	line "legendary birds,"
+	cont "LUGIA…"
+
+	para "…"
+
+	para "It has appeared"
+	line "here for you…"
+
+	para "…"
+
+	para "Do as you will…"
+	done
+
+WhirlIslandLugiaChamberSlowkingAfterText:
+	text "SLOWKING: …"
+
+	para "I lost big at the"
+	line "GAME CORNER, so"
+
+	para "I'm hiding out"
+	line "here…"
+
+	para "…"
+
+	para "Please don't tell…"
 	done
 
 WhirlIslandLugiaChamber_MapEvents:
@@ -53,3 +122,4 @@ WhirlIslandLugiaChamber_MapEvents:
 
 	def_object_events
 	object_event  9,  5, SPRITE_LUGIA, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, Lugia, EVENT_WHIRL_ISLAND_LUGIA_CHAMBER_LUGIA
+	object_event 11, 12, SPRITE_SLOWPOKE, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, WhirlIslandLugiaChamberSlowkingScript, -1
