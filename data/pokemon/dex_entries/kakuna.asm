4cc77738838372fb56ad7fbc33de1036819eab81
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-17 15:35:17 -0700

    Update kakuna.asm

--- data/pokemon/dex_entries/kakuna.asm
+++ data/pokemon/dex_entries/kakuna.asm
@@ -1,10 +1,10 @@
 	db "COCOON@" ; species name
 	dw 200, 220 ; height, weight
 
-	db   "Nearly incapable"
-	next "of movement, it"
-	next "leans against"
+	db   "Though it can't"
+	next "move, its shape"
+	next "suggests claws,"
 
-	page "stout trees while"
-	next "waiting for its"
-	next "evolution.@"
+	page "and this often"
+	next "frightens away"
+	next "predators.@"
