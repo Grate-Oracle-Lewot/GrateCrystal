7574069fd9a2f94b8c0d17f2f15d7a4cc70e6b99
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-17 11:39:18 -0700

    Update aipom.asm

--- data/pokemon/dex_entries/aipom.asm
+++ data/pokemon/dex_entries/aipom.asm
@@ -1,10 +1,10 @@
 	db "PREHENSILE@" ; species name
 	dw 207, 250 ; height, weight
 
-	db   "It uses its tail"
-	next "to hang on to tree"
-	next "branches. It uses"
+	db   "Whether it is"
+	next "right- or left-"
+	next "handed determines"
 
-	page "its momentum to"
-	next "swing from one"
-	next "branch to another.@"
+	page "which side it"
+	next "holds its tail"
+	next "on.@"
