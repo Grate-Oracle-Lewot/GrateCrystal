cb064b2183eaad7daec5f16cc02fb9c160945014
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-17 12:57:38 -0700

    Update dodrio.asm

--- data/pokemon/dex_entries/dodrio.asm
+++ data/pokemon/dex_entries/dodrio.asm
@@ -1,10 +1,10 @@
 	db "TRIPLEBIRD@" ; species name
 	dw 511, 1880 ; height, weight
 
-	db   "An enemy that"
-	next "takes its eyes off"
-	next "any of the three"
+	db   "The third head"
+	next "that sprouted upon"
+	next "evolution is"
 
-	page "heads--even for a"
-	next "second--will get"
-	next "pecked severely.@"
+	page "treated as the"
+	next "baby of the bunch."
+	next "@"
