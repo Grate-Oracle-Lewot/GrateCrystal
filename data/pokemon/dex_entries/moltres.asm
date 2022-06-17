1dde0ddda3335f854d8bce87fb211d12a69ef5c9
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-17 11:46:11 -0700

    Update moltres.asm

--- data/pokemon/dex_entries/moltres.asm
+++ data/pokemon/dex_entries/moltres.asm
@@ -1,10 +1,10 @@
 	db "FLAME@" ; species name
 	dw 607, 1320 ; height, weight
 
-	db   "Legendary bird"
-	next "#MON. It is"
-	next "said to migrate"
+	db   "Its wings burn"
+	next "hotter than the"
+	next "hottest stars ever"
 
-	page "from the south"
-	next "along with the"
-	next "spring.@"
+	page "discovered. Such"
+	next "stars may be other"
+	next "MOLTRES.@"
