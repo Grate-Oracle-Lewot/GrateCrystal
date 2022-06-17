9f47b5796c1d3b6c6e20a6d0a4403d182a1c080f
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-17 16:03:05 -0700

    Update magnemite.asm

--- data/pokemon/dex_entries/magnemite.asm
+++ data/pokemon/dex_entries/magnemite.asm
@@ -1,10 +1,10 @@
 	db "MAGNET@" ; species name
 	dw 100, 130 ; height, weight
 
-	db   "The electricity"
-	next "emitted by the"
-	next "units on each side"
+	db   "It can be taught"
+	next "to control its"
+	next "magnetic charges"
 
-	page "of its body cause"
-	next "it to become a"
-	next "strong magnet.@"
+	page "to avoid disrupt-"
+	next "ing machinery and"
+	next "electronics.@"
