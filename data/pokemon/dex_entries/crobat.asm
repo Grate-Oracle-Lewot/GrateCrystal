17cc881ff6c0097dfd18ec63e076a3d01bc0e504
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-17 11:31:24 -0700

    Update crobat.asm

--- data/pokemon/dex_entries/crobat.asm
+++ data/pokemon/dex_entries/crobat.asm
@@ -1,10 +1,10 @@
 	db "BAT@" ; species name
 	dw 511, 1650 ; height, weight
 
-	db   "As a result of its"
-	next "pursuit of faster,"
-	next "yet more silent"
+	db   "This evolution was"
+	next "undiscovered for"
+	next "eons because no"
 
-	page "flight, a new set"
-	next "of wings grew on"
-	next "its hind legs.@"
+	page "one made a GOLBAT"
+	next "happy enough."
+	next "@"
