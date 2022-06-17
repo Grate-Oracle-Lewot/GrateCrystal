04c318b5b288ef9adb6b898a43d58bcc88fbce5b
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-17 13:09:16 -0700

    Update whimsicott.asm

--- data/pokemon/dex_entries/whimsicott.asm
+++ data/pokemon/dex_entries/whimsicott.asm
@@ -1,10 +1,10 @@
 	db "WINDVEILED@" ; species name
 	dw 204, 146 ; height, weight
 
-	db   "It can slip"
-	next "through any gap,"
-	next "no matter how"
+	db   "It can remove its"
+	next "cottony fluff like"
+	next "a jacket, but will"
 
-	page "small. It leaves"
-	next "white fluff"
-	next "behind."
+	page "only do so when it"
+	next "is sure it won't"
+	next "be stolen.@"
