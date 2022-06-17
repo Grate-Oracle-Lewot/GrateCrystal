ab3504b00b2a4f38a24a1f5c773289c9fe39add0
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-17 11:16:53 -0700

    Update abra.asm

--- data/pokemon/dex_entries/abra.asm
+++ data/pokemon/dex_entries/abra.asm
@@ -1,10 +1,10 @@
 	db "PSI@" ; species name
 	dw 211, 430 ; height, weight
 
-	db   "It hypnotizes"
-	next "itself so that it"
-	next "can teleport away"
+	db   "It is technically"
+	next "always asleep. Its"
+	next "long nap lets it"
 
-	page "when it senses"
-	next "danger, even"
-	next "if it is asleep.@"
+	page "build up psychic"
+	next "power for"
+	next "evolution.@"
