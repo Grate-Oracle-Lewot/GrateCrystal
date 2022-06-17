cbc05dc8efee905c32187229125df789348d6057
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-17 15:18:44 -0700

    Update pikachu.asm

--- data/pokemon/dex_entries/pikachu.asm
+++ data/pokemon/dex_entries/pikachu.asm
@@ -1,10 +1,10 @@
 	db "MOUSE@" ; species name
 	dw 104, 130 ; height, weight
 
-	db   "When it is anger-"
-	next "ed, it immediately"
-	next "discharges the"
+	db   "Though beloved"
+	next "worldwide, it is"
+	next "resistant to"
 
-	page "energy stored in"
-	next "the pouches in its"
-	next "cheeks.@"
+	page "domestication, and"
+	next "wild populations"
+	next "are rare.@"
