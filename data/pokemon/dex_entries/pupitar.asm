c2eb3d4ac8751f3c7012a5378f19608259f0fea0
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-17 15:09:14 -0700

    Update pupitar.asm

--- data/pokemon/dex_entries/pupitar.asm
+++ data/pokemon/dex_entries/pupitar.asm
@@ -1,10 +1,10 @@
 	db "HARD SHELL@" ; species name
 	dw 311, 3350 ; height, weight
 
-	db   "It will not stay"
-	next "still, even while"
-	next "it's a pupa. It"
+	db   "It's unknown what"
+	next "would emerge if a"
+	next "PUPITAR were"
 
-	page "already has arms"
-	next "and legs under its"
-	next "solid shell.@"
+	page "cracked open, as"
+	next "no one has managed"
+	next "to do it.@"
