8b0399beac5c1c3e68f8d6a14e9d8db75a00a8f7
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-17 22:55:22 -0700

    Update cries.asm

--- data/pokemon/cries.asm
+++ data/pokemon/cries.asm
@@ -257,8 +257,8 @@ PokemonCries::
 	mon_cry CRY_TYPHLOSION,    0,  256 ; LUGIA
 	mon_cry CRY_AIPOM,         0,  384 ; HO_OH
 	mon_cry CRY_ENTEI,       330,  273 ; CELEBI
-	mon_cry CRY_TOTODILE,   -470,  560 ; YANMEGA
-	mon_cry CRY_PSYDUCK,     420,  370 ; WHIMSICOTT
+	mon_cry CRY_TOTODILE,   -450,  575 ; YANMEGA
+	mon_cry CRY_TOTODILE,    100,  384 ; WHIMSICOTT
 	assert_table_length NUM_POKEMON
 	mon_cry CRY_NIDORAN_M,     0,    0 ; 254
 	mon_cry CRY_NIDORAN_M,     0,    0 ; 255
