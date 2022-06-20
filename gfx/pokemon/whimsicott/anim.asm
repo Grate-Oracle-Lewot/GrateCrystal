eaa4d989bea4845a34209b5c5df26fe8a5a5aa83
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-19 17:57:30 -0700

    Update anim.asm

--- gfx/pokemon/whimsicott/anim.asm
+++ gfx/pokemon/whimsicott/anim.asm
@@ -1,9 +1,10 @@
 	frame 0, 02
 	setrepeat 2
-	frame 1, 19
-	frame 0, 19
-	frame 2, 19
-	frame 0, 19
+	frame 1, 15
+	frame 0, 15
+	frame 2, 15
+	frame 0, 15
 	dorepeat 2
+	frame 4, 21
 	frame 0, 02
 	endanim
