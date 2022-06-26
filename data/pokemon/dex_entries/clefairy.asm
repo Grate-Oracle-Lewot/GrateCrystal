3201de43643bfeac106aee298f4fb73914faf769
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-26 13:35:34 -0700

    Update clefairy.asm

--- data/pokemon/dex_entries/clefairy.asm
+++ data/pokemon/dex_entries/clefairy.asm
@@ -1,10 +1,10 @@
 	db "FAIRY@" ; species name
 	dw 200, 170 ; height, weight
 
-	db   "Though rarely"
-	next "seen, it becomes"
-	next "easier to spot,"
+	db   "If one suddenly"
+	next "appears or vanish-"
+	next "es, a ring of"
 
-	page "for some reason,"
-	next "on the night of a "
-	next "full moon.@"
+	page "mushrooms can"
+	next "usually be found"
+	next "on the ground.@"
