8079660bf8eb1f1283ce387ea53518bf278b5556
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-28 18:13:34 -0700

    Create SafariZone.asm

---
+++ maps/SafariZone.asm
@@ -0,0 +1,23 @@
+	object_const_def
+
+MewChamber_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+SafariZoneTrashcan:
+	jumpstd TrashCanScript
+
+MewChamber_MapEvents:
+	db 0, 0 ; filler
+
+	def_warp_events
+	warp_event 30, 47, SAFARI_ZONE_GATE, 1
+	warp_event 30, 48, SAFARI_ZONE_GATE, 2
+
+	def_coord_events
+
+	def_bg_events
+	bg_event 32, 46, BGEVENT_READ, SafariZoneTrashcan
+
+	def_object_events
