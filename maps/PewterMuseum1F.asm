3a4b11dba9b4cc6ae781990c2d9b799186814e57
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-06-29 04:34:10 -0700

    Update PewterMuseum1F.asm

--- maps/PewterMuseum1F.asm
+++ maps/PewterMuseum1F.asm
@@ -9,6 +9,20 @@ PewterMuseum1F_MapScripts:
 PewterMuseum1FScientistScript:
 	jumptextfaceplayer PewterMuseum1FScientistText
 
+AerodactylFossilSignScript:
+	refreshscreen $0
+	trainerpic AEROBONES
+	waitbutton
+	closepokepic
+	jumptext AerodactylFossilSignText
+
+KabutopsFossilSignScript:
+	refreshscreen $0
+	trainerpic KABUBONES
+	waitbutton
+	closepokepic
+	jumptext KabutopsFossilSignText
+
 PewterMuseum1FScientistText:
 	text "Thanks to our"
 	line "successful fossil"
@@ -26,6 +40,57 @@ PewterMuseum1FScientistText:
 	line "exhibits, though…"
 	done
 
+AerodactylFossilSignText:
+	text "A fossil of a"
+	line "#MON that ruled"
+
+	para "the skies in anci-"
+	line "ent times, now"
+
+	para "identified as"
+	line "AERODACTYL."
+
+	para "Originally hypoth-"
+	line "esized to have"
+
+	para "gained the rock"
+	line "type through the"
+
+	para "fossilization pro-"
+	line "cess, research in-"
+	cont "to mega evolution"
+
+	para "has shown that it"
+	line "had the typing"
+
+	para "even in its pre-"
+	line "historic form."
+	done
+
+KabutopsFossilSignText:
+	text "A fossil of a"
+	line "#MON that ruled"
+
+	para "the seas in anci-"
+	line "ent times, now"
+
+	para "identified as"
+	line "KABUTOPS."
+
+	para "Despite its lanky"
+	line "appearance, it was"
+
+	para "a swift swimmer"
+	line "and could quickly"
+
+	para "attack prey with"
+	line "its scythe-like"
+
+	para "arms while being"
+	line "protected by its"
+	cont "rocky carapace."
+	done
+
 PewterMuseum1F_MapEvents:
 	db 0, 0 ; filler
 
@@ -39,6 +104,8 @@ PewterMuseum1F_MapEvents:
 	def_coord_events
 
 	def_bg_events
+	bg_event  2,  3, BGEVENT_READ, AerodactylFossilSignScript
+	bg_event  2,  6, BGEVENT_READ, KabutopsFossilSignScript
 
 	def_object_events
 	object_event 12,  3, SPRITE_SCIENTIST, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, PewterMuseum1FScientistScript, -1
