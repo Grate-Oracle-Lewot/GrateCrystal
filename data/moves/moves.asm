6896fc77a6f7a1244c6ad8f2bcedfa611b09e26f
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-07-03 14:32:12 -0700

    Update moves.asm

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -15,7 +15,7 @@ Moves:
 	table_width MOVE_LENGTH, Moves
 	move POUND,        EFFECT_NORMAL_HIT,         40, NORMAL,       PHYSICAL, 100, 35,   0
 	move KARATE_CHOP,  EFFECT_NORMAL_HIT,         50, FIGHTING,     PHYSICAL, 100, 25,   0
-	move DOUBLESLAP,   EFFECT_MULTI_HIT,          20, FAIRY,        PHYSICAL,  90, 10,   0
+	move DOUBLESLAP,   EFFECT_MULTI_HIT,          20, FAIRY,        PHYSICAL,  90, 20,   0
 	move COMET_PUNCH,  EFFECT_MULTI_HIT,          25, FIGHTING,     PHYSICAL,  90, 15,   0
 	move MEGA_PUNCH,   EFFECT_NORMAL_HIT,         90, NORMAL,       PHYSICAL, 100, 20,   0
 	move PAY_DAY,      EFFECT_PAY_DAY,            40, STEEL,        PHYSICAL, 100, 20,   0
