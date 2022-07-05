3bb3bc6cd433d50ed1e9e9c407f949b385f8bd64
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-07-04 22:20:34 -0700

    Update slugma.asm

--- data/pokemon/base_stats/slugma.asm
+++ data/pokemon/base_stats/slugma.asm
@@ -1,21 +1,21 @@
-	db SLOWPOKE ; 079
+	db SLUGMA ; 218
 
-	db  90,  65,  65,  15,  40,  40
+	db  40,  40,  40,  20,  70,  40
 	;   hp  atk  def  spd  sat  sdf
 
-	db WATER, PSYCHIC_TYPE ; type
+	db FIRE, FIRE ; type
 	db 190 ; catch rate
-	db 99 ; base exp
-	db SLOWPOKETAIL, KINGS_ROCK ; items
+	db 78 ; base exp
+	db X_SPEED, CARBOS ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
-	db 21 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	db 5 ; unknown 2
-	INCBIN "gfx/pokemon/slowpoke/front.dimensions"
+	INCBIN "gfx/pokemon/slugma/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
-	dn EGG_MONSTER, EGG_WATER_1 ; egg groups
+	dn EGG_INDETERMINATE, EGG_BUG ; egg groups
 
 	; tm/hm learnset
-	tmhm HEADBUTT, CURSE, TOXIC, ZAP_CANNON, PSYCH_UP, HIDDEN_POWER, SUNNY_DAY, SNORE, BLIZZARD, ICY_WIND, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, IRON_TAIL, EARTHQUAKE, RETURN, DIG, PSYCHIC_M, SHADOW_BALL, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, FIRE_BLAST, SWIFT, DREAM_EATER, REST, ATTRACT, NIGHTMARE, NIGHT_TERROR, SUBSTITUTE, SURF, STRENGTH, FLASH, FLAMETHROWER, ICE_BEAM
+	tmhm CURSE, ROLLOUT, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, PROTECT, ENDURE, FRUSTRATION, RETURN, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, FIRE_BLAST, DEFENSE_CURL, REST, ATTRACT, SUBSTITUTE, FLAMETHROWER
 	; end
