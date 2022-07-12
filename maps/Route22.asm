eb15bdf35d52e15c4a99cf6f18974a9186115c09
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-07-12 14:02:52 -0700

    Update Route22.asm

--- maps/Route22.asm
+++ maps/Route22.asm
@@ -6,6 +6,9 @@ Route22_MapScripts:
 VictoryRoadEntranceSign:
 	jumptext VictoryRoadEntranceSignText
 
+Route22HiddenFullRestore:
+	hiddenitem FULL_RESTORE, EVENT_ROUTE_22_HIDDEN_FULL_RESTORE
+
 VictoryRoadEntranceSignText:
 	text "#MON LEAGUE"
 
@@ -18,10 +21,12 @@ Route22_MapEvents:
 
 	def_warp_events
 	warp_event 13,  5, VICTORY_ROAD_GATE, 1
+	warp_event 28,  9, VICTORY_ROAD_GATE, 1
 
 	def_coord_events
 
 	def_bg_events
 	bg_event 15,  7, BGEVENT_READ, VictoryRoadEntranceSign
+	bg_event 30,  2, BGEVENT_ITEM, Route22HiddenFullRestore
 
 	def_object_events
