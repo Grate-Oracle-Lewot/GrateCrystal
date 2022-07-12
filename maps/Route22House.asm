3da613e6b59f37abb51b246299812722a7a3b7c0
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-07-12 14:59:00 -0700

    Create Route22House.asm

---
+++ maps/Route22House.asm
@@ -0,0 +1,19 @@
+	object_const_def
+
+Route22House_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+Route22House_MapEvents:
+	db 0, 0 ; filler
+
+	def_warp_events
+	warp_event  6,  7, ROUTE_22, 2
+	warp_event  7,  7, ROUTE_22, 2
+
+	def_coord_events
+
+	def_bg_events
+
+	def_object_events
