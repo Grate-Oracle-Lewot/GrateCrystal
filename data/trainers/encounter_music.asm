d02d8bec5730298875d2d125c41854f980cb3144
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-07-12 18:17:55 -0700

    Update encounter_music.asm

--- data/trainers/encounter_music.asm
+++ data/trainers/encounter_music.asm
@@ -71,6 +71,10 @@ TrainerEncounterMusic::
 	db MUSIC_HIKER_ENCOUNTER       ; officer
 	db MUSIC_ROCKET_ENCOUNTER      ; gruntf
 	db MUSIC_MYSTICALMAN_ENCOUNTER ; mysticalman
+	db MUSIC_INDIGO_PLATEAU        ; real karen
+	db MUSIC_INDIGO_PLATEAU        ; real koga
+	db MUSIC_ROCKET_ENCOUNTER      ; boss
+	db MUSIC_ROCKET_ENCOUNTER      ; jessie and james
 	db MUSIC_OFFICER_ENCOUNTER     ; aerobones
 	db MUSIC_OFFICER_ENCOUNTER     ; kabubones
 	assert_table_length NUM_TRAINER_CLASSES + 1
