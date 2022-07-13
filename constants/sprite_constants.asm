2730e471dadf1bc64ca23815dcaa23d9f20a2026
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-07-13 13:21:14 -0700

    Update sprite_constants.asm

--- constants/sprite_constants.asm
+++ constants/sprite_constants.asm
@@ -108,7 +108,6 @@
 	const SPRITE_JAMES ; 68
 	const SPRITE_GIOVANNI ; 69
 	const SPRITE_MR_FUJI ; 6a
-	const SPRITE_MISSINGNO ; 6b
 NUM_OVERWORLD_SPRITES EQU const_value - 1
 
 ; SpriteMons indexes (see data/sprites/sprite_mons.asm)
