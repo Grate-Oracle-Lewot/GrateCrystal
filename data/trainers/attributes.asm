e2e36158a2ad93ec827be85f1d5cfa366bb06dec
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-07-14 18:25:48 -0700

    Update attributes.asm

--- data/trainers/attributes.asm
+++ data/trainers/attributes.asm
@@ -15,7 +15,7 @@ TrainerClassAttributes:
 	dw CONTEXT_USE | SWITCH_SOMETIMES
 
 ; Bugsy
-	db SUPER_POTION, NO_ITEM ; items
+	db FULL_HEAL, NO_ITEM ; items
 	db 30 ; base reward
 	dw AI_BASIC | AI_SETUP | AI_SMART | AI_AGGRESSIVE | AI_CAUTIOUS | AI_STATUS | AI_RISKY
 	dw CONTEXT_USE | SWITCH_SOMETIMES
