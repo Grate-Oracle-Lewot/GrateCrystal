28fd94a8c6ec4665b1db516a9e0e989a78fd48ac
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-07-16 16:24:52 -0700

    Update mantine.asm

--- data/pokemon/dex_entries/mantine.asm
+++ data/pokemon/dex_entries/mantine.asm
@@ -1,10 +1,10 @@
 	db "KITE@" ; species name
 	dw 611, 4850 ; height, weight
 
-	db   "It swims along"
-	next "freely, eating"
-	next "things that swim"
+	db   "The REMORAID under"
+	next "its wings can be"
+	next "shot like missiles"
 
-	page "into its mouth."
-	next "Its whole body is"
-	next "very coarse.@"
+	page "with enough force"
+	next "to puncture a"
+	next "submarine.@"
