1b949ae2685804fc3198efd641345b20bd10419c
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-07-16 16:30:30 -0700

    Update hoothoot.asm

--- data/pokemon/dex_entries/hoothoot.asm
+++ data/pokemon/dex_entries/hoothoot.asm
@@ -1,10 +1,10 @@
 	db "OWL@" ; species name
 	dw 204, 470 ; height, weight
 
-	db   "It begins to hoot"
-	next "at the same time"
-	next "every day. Some"
+	db   "It's unconfirmed"
+	next "whether it only"
+	next "has one leg or if"
 
-	page "trainers use them"
-	next "in place of"
-	next "clocks.@"
+	page "it switches betw-"
+	next "een legs faster"
+	next "than can be seen.@"
