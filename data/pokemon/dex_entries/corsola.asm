cb43bd684ea1fabda5df43fc6240c8901830b215
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-07-16 13:42:01 -0700

    Update corsola.asm

--- data/pokemon/dex_entries/corsola.asm
+++ data/pokemon/dex_entries/corsola.asm
@@ -1,10 +1,10 @@
 	db "CORAL@" ; species name
 	dw 200, 110 ; height, weight
 
-	db   "The points on its"
-	next "head absorb"
-	next "nutrients from"
+	db   "What we think of"
+	next "as CORSOLA is act-"
+	next "ually a bunch of"
 
-	page "clean water. They"
-	next "cannot survive in"
-	next "polluted water.@"
+	page "tiny #MON form-"
+	next "ing a rocky mass."
+	next "@"
