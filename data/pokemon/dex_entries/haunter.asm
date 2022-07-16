af6e19ac022ff702fe541df87902dc6673d8bb77
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-07-16 15:05:23 -0700

    Update haunter.asm

--- data/pokemon/dex_entries/haunter.asm
+++ data/pokemon/dex_entries/haunter.asm
@@ -1,10 +1,10 @@
 	db "GAS@" ; species name
 	dw 503, 2 ; height, weight
 
-	db   "It hides in the"
-	next "dark, planning to"
-	next "take the life of"
+	db   "Scientists tried"
+	next "to see how far its"
+	next "hands could go and"
 
-	page "the next living"
-	next "thing that wanders"
-	next "close by.@"
+	page "still be moved by"
+	next "the body, and"
+	next "found no limit.@"
