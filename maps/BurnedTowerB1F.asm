9ac06c53469de2c7ef48ea1bba1748e214c3fe8f
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-07-20 15:26:03 -0700

    Update BurnedTowerB1F.asm

--- maps/BurnedTowerB1F.asm
+++ maps/BurnedTowerB1F.asm
@@ -237,7 +237,8 @@ BurnedTowerB1F_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event  7, 15, BURNED_TOWER_1F, 3
+	warp_event 10,  9, BURNED_TOWER_1F, 3
+	warp_event  7, 15, BURNED_TOWER_1F, 4
 
 	def_coord_events
 	coord_event 10,  6, SCENE_DEFAULT, ReleaseTheBeasts
