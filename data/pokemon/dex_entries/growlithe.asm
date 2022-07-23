bbe6d3a2b1d62a553621c7afe08fcff37387ae3c
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-07-23 13:43:14 -0700

    Update growlithe.asm

--- data/pokemon/dex_entries/growlithe.asm
+++ data/pokemon/dex_entries/growlithe.asm
@@ -1,10 +1,10 @@
 	db "PUPPY@" ; species name
 	dw 204, 420 ; height, weight
 
-	db   "It controls a big"
-	next "territory. If it"
-	next "detects an unknown"
+	db   "It's cute and"
+	next "lovable but also"
+	next "feisty and fiery."
 
-	page "smell, it roars"
-	next "loudly to force"
-	next "out the intruder.@"
+	page "It may bark all"
+	next "night to ward off"
+	next "intruders.@"
