4bc5667aa95a6a82a0da4f8cc083e7b119e3debf
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-07-23 14:32:03 -0700

    Update raticate.asm

--- data/pokemon/dex_entries/raticate.asm
+++ data/pokemon/dex_entries/raticate.asm
@@ -1,10 +1,10 @@
 	db "RAT@" ; species name
 	dw 204, 410 ; height, weight
 
-	db   "The webs on its"
-	next "hind legs enable"
-	next "it to cross"
+	db   "If it reaches a"
+	next "new landmass, it"
+	next "devastates the"
 
-	page "rivers. It search-"
-	next "es wide areas for"
-	next "food.@"
+	page "local ecosystem by"
+	next "eating rare #-"
+	next "MON EGGs.@"
