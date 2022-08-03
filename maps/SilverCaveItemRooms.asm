2db11c4eb302521c7e42191c9feebcd0e457921a
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-08-03 03:07:50 -0700

    Update SilverCaveItemRooms.asm

--- maps/SilverCaveItemRooms.asm
+++ maps/SilverCaveItemRooms.asm
@@ -1,4 +1,5 @@
 	object_const_def
+	const SILVERCAVEITEMROOMS_KRIS
 	const SILVERCAVEITEMROOMS_POKE_BALL1
 	const SILVERCAVEITEMROOMS_POKE_BALL2
 
@@ -7,11 +8,36 @@ SilverCaveItemRooms_MapScripts:
 
 	def_callbacks
 
+TrainerCalla:
+	trainer CAL_F, CALLA, EVENT_BEAT_TRAINER_CALLA, TrainerCallaSeenText, TrainerCallaBeatenText, 0, .AfterScript
+
+.AfterScript:
+	endifjustbattled
+	opentext
+	writetext TrainerCallaAfterBattleText
+	waitbutton
+	closetext
+	end
+
 SilverCaveItemRoomsMaxRevive:
 	itemball MAX_REVIVE
 
 SilverCaveItemRoomsFullRestore:
 	itemball FULL_RESTORE
+
+TrainerCallaSeenText:
+	text "I feel like I've"
+	line "seen you before…"
+	done
+
+TrainerCallaBeatenText:
+	text "That's that."
+	done
+
+TrainerCallaAfterBattleText:
+	text "It's not easy"
+	line "bein' green."
+	done
 
 SilverCaveItemRooms_MapEvents:
 	db 0, 0 ; filler
@@ -26,5 +52,6 @@ SilverCaveItemRooms_MapEvents:
 	def_bg_events
 
 	def_object_events
+	object_event 10, 10, SPRITE_KRIS, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_TRAINER, 1, TrainerCalla, -1
 	object_event  6,  3, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SilverCaveItemRoomsMaxRevive, EVENT_SILVER_CAVE_ITEM_ROOMS_MAX_REVIVE
 	object_event 15, 11, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SilverCaveItemRoomsFullRestore, EVENT_SILVER_CAVE_ITEM_ROOMS_FULL_RESTORE
