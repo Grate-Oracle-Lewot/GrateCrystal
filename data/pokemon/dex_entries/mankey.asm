b90d6d1c62df12880495faf1661e640b1048f6ef
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-08-05 14:27:01 -0700

    Update mankey.asm

--- data/pokemon/dex_entries/mankey.asm
+++ data/pokemon/dex_entries/mankey.asm
@@ -1,10 +1,10 @@
 	db "PIG MONKEY@" ; species name
 	dw 108, 620 ; height, weight
 
-	db   "It lives in groups"
-	next "in the treetops."
-	next "If it loses sight"
+	db   "Its pig nose is"
+	next "actually a whole"
+	next "SWINUB in its"
 
-	page "of its group, it"
-	next "becomes infuriated"
-	next "by its loneliness.@"
+	page "mouth that ate and"
+	next "replaced its"
+	next "tongue.@"
