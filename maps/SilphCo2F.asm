b4ad4bebedd19e1ac47a3fe2328dbdfe5a3e6112
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-08-08 13:44:42 -0700

    Update SilphCo2F.asm

--- maps/SilphCo2F.asm
+++ maps/SilphCo2F.asm
@@ -5,13 +5,23 @@ SilphCo2F_MapScripts:
 
 	def_callbacks
 
+SilphCo2FSign:
+	jumptext SilphCo2FSignText
+
+SilphCo2FSignText:
+	text "SILPH CO. 2F"
+	done
+
 SilphCo2F_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
+	warp_event 27,  2, SILPH_CO_1F, 3
+	warp_event 25,  2, SILPH_CO_3F, 1
 
 	def_coord_events
 
 	def_bg_events
+	bg_event 20,  1, BGEVENT_READ, SilphCo2FSign
 
 	def_object_events
