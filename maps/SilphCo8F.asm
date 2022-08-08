16d1907007ff3b90d7f25695523eff8409733f97
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-08-08 14:03:28 -0700

    Create SilphCo8F.asm

---
+++ maps/SilphCo8F.asm
@@ -0,0 +1,27 @@
+	object_const_def
+
+SilphCo8F_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+SilphCo8FSign:
+	jumptext SilphCo8FSignText
+
+SilphCo8FSignText:
+	text "SILPH CO. 8F"
+	done
+
+SilphCo8F_MapEvents:
+	db 0, 0 ; filler
+
+	def_warp_events
+	warp_event 15,  2, SILPH_CO_7F, 2
+	warp_event 17,  2, SILPH_CO_9F, 1
+
+	def_coord_events
+
+	def_bg_events
+	bg_event 20,  1, BGEVENT_READ, SilphCo8FSign
+
+	def_object_events
