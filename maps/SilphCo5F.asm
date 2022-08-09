cf25ff79223049b9b743c5f4d9c55a6ffef5c315
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-08-09 15:47:28 -0700

    Update SilphCo5F.asm

--- maps/SilphCo5F.asm
+++ maps/SilphCo5F.asm
@@ -1,12 +1,29 @@
 	object_const_def
+	const SILPHCO5F_CLERK
 
 SilphCo5F_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
 
+SilphCo5FClerkScript:
+	jumptextfaceplayer SilphCo5FClerkText
+
 SilphCo5FSign:
 	jumptext SilphCo5FSignText
+
+SilphCo5FClerkText:
+	text "Ghost #MON have"
+	line "stopped obscuring"
+
+	para "themselves ever"
+	line "since our SILPH"
+	cont "SCOPE came out…"
+
+	para "It's a real kick"
+	line "in the profit"
+	cont "margin."
+	done
 
 SilphCo5FSignText:
 	text "SILPH CO. 5F"
@@ -29,3 +46,4 @@ SilphCo5F_MapEvents:
 	bg_event 20,  1, BGEVENT_READ, SilphCo5FSign
 
 	def_object_events
+	object_event 12,  8, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, SilphCo5FClerkScript, -1
