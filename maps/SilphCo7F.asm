1206e62a172c217d4fd48a72df0a2b6dc84c290c
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-08-09 16:24:17 -0700

    Update SilphCo7F.asm

--- maps/SilphCo7F.asm
+++ maps/SilphCo7F.asm
@@ -1,12 +1,57 @@
 	object_const_def
+	const SILPHCO7F_CLERK1
+	const SILPHCO7F_CLERK2
+	const SILPHCO7F_CLERK3
+	const SILPHCO7F_RECEPTIONIST
 
 SilphCo7F_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
 
+SilphCo7FClerk1Script:
+	jumptextfaceplayer SilphCo7FClerk1Text
+
+SilphCo7FClerk2Script:
+	jumptextfaceplayer SilphCo7FClerk2Text
+
+SilphCo7FClerk3Script:
+	jumptextfaceplayer SilphCo7FClerk3Text
+
+SilphCo7FReceptionistScript:
+	jumptextfaceplayer SilphCo7FReceptionistText
+
 SilphCo7FSign:
 	jumptext SilphCo7FSignText
+
+SilphCo7FClerk1Text:
+	text "This way to the"
+	line "CHIEF's OFFICE."
+	done
+
+SilphCo7FClerk2Text:
+	text "I think we should"
+	line "get into the video"
+	cont "game business."
+
+	para "We need to compete"
+	line "with GAME FREAK in"
+	cont "CELADON!"
+	done
+
+SilphCo7FClerk3Text:
+	text "Must… have…"
+	line "coffee…"
+	done
+
+SilphCo7FReceptionistText:
+	text "These guys are all"
+	line "useless."
+
+	para "It's the women who"
+	line "hold these big co-"
+	cont "mpanies together."
+	done
 
 SilphCo7FSignText:
 	text "SILPH CO. 7F"
@@ -28,3 +73,7 @@ SilphCo7F_MapEvents:
 	bg_event 20,  1, BGEVENT_READ, SilphCo7FSign
 
 	def_object_events
+	object_event  1,  5, SPRITE_CLERK, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, SilphCo7FClerk1Script, -1
+	object_event 10, 12, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, SilphCo7FClerk2Script, -1
+	object_event 11, 11, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, SilphCo7FClerk3Script, -1
+	object_event  9,  9, SPRITE_RECEPTIONIST, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, SilphCo7FReceptionistScript, -1
