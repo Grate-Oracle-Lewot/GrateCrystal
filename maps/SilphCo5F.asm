b2864df92343342b4a6ce61ba33d975221d1e1a4
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-08-09 14:09:44 -0700

    Update SilphCo5F.asm

--- maps/SilphCo5F.asm
+++ maps/SilphCo5F.asm
@@ -20,6 +20,7 @@ SilphCo5F_MapEvents:
 	warp_event 23,  2, SILPH_CO_6F, 1
 	warp_event 11,  5, SILPH_CO_3F, 4
 	warp_event  3, 15, SILPH_CO_3F, 5
+	warp_event 27,  3, SILPH_CO_7F, 3
 
 	def_coord_events
 
