d334cbef61e9795283c1f67ae17b886014730166
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-08-09 15:17:01 -0700

    Update SilphCo2F.asm

--- maps/SilphCo2F.asm
+++ maps/SilphCo2F.asm
@@ -1,12 +1,21 @@
 	object_const_def
+	const SILPHCO2F_RECEPTIONIST
 
 SilphCo2F_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
 
+SilphCo2FReceptionistScript:
+	jumptextfaceplayer SilphCo2FReceptionistText
+
 SilphCo2FSign:
 	jumptext SilphCo2FSignText
+
+SilphCo2FReceptionistText:
+	text "Don't bug me, kid."
+	line "I'm on break."
+	done
 
 SilphCo2FSignText:
 	text "SILPH CO. 2F"
@@ -29,3 +38,4 @@ SilphCo2F_MapEvents:
 	bg_event 20,  1, BGEVENT_READ, SilphCo2FSign
 
 	def_object_events
+	object_event 10,  1, SPRITE_RECEPTIONIST, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, SilphCo2FReceptionistScript, -1
