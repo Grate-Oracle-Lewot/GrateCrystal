e2295960db017531491f19496a7262aec8ca0f2e
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-08-09 18:58:29 -0700

    Update SilphCo9F.asm

--- maps/SilphCo9F.asm
+++ maps/SilphCo9F.asm
@@ -1,4 +1,5 @@
 	object_const_def
+	const SILPHCO9F_SCIENTIST
 	const SILPHCO9F_RECEPTIONIST
 
 SilphCo9F_MapScripts:
@@ -6,11 +7,38 @@ SilphCo9F_MapScripts:
 
 	def_callbacks
 
+TrainerScientistHideki:
+	trainer SCIENTIST, HIDEKI, EVENT_BEAT_SCIENTIST_HIDEKI, ScientistHidekiSeenText, ScientistHidekiBeatenText, 0, .AfterScript
+
+.AfterScript:
+	endifjustbattled
+	opentext
+	writetext ScientistHidekiAfterBattleText
+	waitbutton
+	closetext
+	end
+
 SilphCo9FReceptionistScript:
 	jumptextfaceplayer SilphCo9FReceptionistText
 
 SilphCo9FSign:
 	jumptext SilphCo9FSignText
+
+ScientistHidekiSeenText:
+	text "I'm working on"
+	line "something very"
+	cont "important!"
+	done
+
+ScientistHidekiBeatenText:
+	text "Go away."
+	done
+
+ScientistHidekiAfterBattleText:
+	text "What am I working"
+	line "on? It's called a"
+	cont "power nap."
+	done
 
 SilphCo9FReceptionistText:
 	text "There must be a"
@@ -39,4 +67,5 @@ SilphCo9F_MapEvents:
 	bg_event 18,  1, BGEVENT_READ, SilphCo9FSign
 
 	def_object_events
+	object_event 19, 15, SPRITE_SCIENTIST, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 0, TrainerScientistHideki, -1
 	object_event  5,  7, SPRITE_OLD_LINK_RECEPTIONIST, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_PINK, OBJECTTYPE_SCRIPT, 0, SilphCo9FReceptionistScript, -1
