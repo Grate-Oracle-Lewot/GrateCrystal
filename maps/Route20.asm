90b180aeb144efbea2d81986ab93599f2f4210d4
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-08-10 16:10:31 -0700

    Update Route20.asm

--- maps/Route20.asm
+++ maps/Route20.asm
@@ -115,7 +115,7 @@ Route20_MapEvents:
 
 	def_warp_events
 	warp_event 38,  7, SEAFOAM_GYM, 1
-	warp_event 30,  5, SEAFOAM_CAVE_1F, 1
+	warp_event 30,  5, SEAFOAM_CAVE_PUZZLE_CHAMBER, 1
 
 	def_coord_events
 
