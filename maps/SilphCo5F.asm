680970e2bff929e8ec92eead91b784fd14727ceb
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-08-09 18:29:10 -0700

    Update SilphCo5F.asm

--- maps/SilphCo5F.asm
+++ maps/SilphCo5F.asm
@@ -1,4 +1,6 @@
 	object_const_def
+	const SILPHCO5F_SUPER_NERD
+	const SILPHCO5F_SCIENTIST
 	const SILPHCO5F_CLERK
 
 SilphCo5F_MapScripts:
@@ -6,11 +8,77 @@ SilphCo5F_MapScripts:
 
 	def_callbacks
 
+TrainerJugglerDalton:
+	trainer JUGGLER, DALTON, EVENT_BEAT_JUGGLER_DALTON, JugglerDaltonSeenText, JugglerDaltonBeatenText, 0, .AfterScript
+
+.AfterScript:
+	endifjustbattled
+	opentext
+	writetext JugglerDaltonAfterBattleText
+	waitbutton
+	closetext
+	end
+
+TrainerScientistMendel:
+	trainer SCIENTIST, MENDEL, EVENT_BEAT_SCIENTIST_MENDEL, ScientistMendelSeenText, ScientistMendelBeatenText, 0, .AfterScript
+
+.AfterScript:
+	endifjustbattled
+	opentext
+	writetext ScientistMendelAfterBattleText
+	waitbutton
+	closetext
+	end
+
 SilphCo5FClerkScript:
 	jumptextfaceplayer SilphCo5FClerkText
 
 SilphCo5FSign:
 	jumptext SilphCo5FSignText
+
+JugglerDaltonSeenText:
+	text "Hey-ho! SILPH CO.!"
+	line "Don'tcha know?"
+
+	para "It's time for my"
+	line "show, yo!"
+	done
+
+JugglerDaltonBeatenText:
+	text "Whoa! You stole"
+	line "the show!"
+	done
+
+JugglerDaltonAfterBattleText:
+	text "I'm the officially"
+	line "licensed company"
+	cont "entertainment!"
+
+	para "Here's my card!"
+
+	para "…Oops, I'm out."
+	done
+
+ScientistMendelSeenText:
+	text "I study the genes"
+	line "of grass #MON."
+	done
+
+ScientistMendelBeatenText:
+	text "Win or lose, it's"
+	line "useful data."
+	done
+
+ScientistMendelAfterBattleText:
+	text "We're close to"
+	line "growing a PARAS"
+
+	para "without mushrooms"
+	line "on its back."
+
+	para "It's going to be"
+	line "a pure bug type."
+	done
 
 SilphCo5FClerkText:
 	text "Ghost #MON have"
@@ -46,4 +114,6 @@ SilphCo5F_MapEvents:
 	bg_event 20,  1, BGEVENT_READ, SilphCo5FSign
 
 	def_object_events
+	object_event 18, 10, SPRITE_SUPER_NERD, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 3, TrainerJugglerDalton, -1
+	object_event  3,  2, SPRITE_SCIENTIST, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 0, TrainerScientistMendel, -1
 	object_event 12,  8, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, SilphCo5FClerkScript, -1
