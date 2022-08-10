5a480b74272ea7e318a1624e83d3d93fa1da8277
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-08-10 16:12:00 -0700

    Update SeafoamCave1F.asm

--- maps/SeafoamCave1F.asm
+++ maps/SeafoamCave1F.asm
@@ -9,7 +9,7 @@ SeafoamCave1F_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event  5,  5, ROUTE_20, 2
+	warp_event  5,  5, SEAFOAM_CAVE_PUZZLE_CHAMBER, 3
 	warp_event  3,  3, SEAFOAM_CAVE_B1F, 1
 
 	def_coord_events
