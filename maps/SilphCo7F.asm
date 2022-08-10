1a0581df7a6556f8662cfb7e41c075a2cbae289e
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-08-09 19:54:45 -0700

    Update SilphCo7F.asm

--- maps/SilphCo7F.asm
+++ maps/SilphCo7F.asm
@@ -4,6 +4,7 @@
 	const SILPHCO7F_CLERK2
 	const SILPHCO7F_CLERK3
 	const SILPHCO7F_RECEPTIONIST
+	const SILPHCO7F_POKE_BALL
 
 SilphCo7F_MapScripts:
 	def_scene_scripts
@@ -32,6 +33,9 @@ SilphCo7FClerk3Script:
 
 SilphCo7FReceptionistScript:
 	jumptextfaceplayer SilphCo7FReceptionistText
+
+SilphCo7FLightBall:
+	itemball LIGHT_BALL
 
 SilphCo7FSign:
 	jumptext SilphCo7FSignText
@@ -106,3 +110,4 @@ SilphCo7F_MapEvents:
 	object_event 10, 12, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, SilphCo7FClerk2Script, -1
 	object_event 11, 11, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, SilphCo7FClerk3Script, -1
 	object_event  9,  9, SPRITE_RECEPTIONIST, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, SilphCo7FReceptionistScript, -1
+	object_event 24, 11, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SilphCo7FLightBall, EVENT_SILPH_CO_7F_LIGHT_BALL
