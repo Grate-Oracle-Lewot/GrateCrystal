62c42d5518c0eaf1e1d0d63a752caadf8168f04b
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-08-11 12:27:19 -0700

    Update SafariZone.asm

--- maps/SafariZone.asm
+++ maps/SafariZone.asm
@@ -1,9 +1,17 @@
 	object_const_def
+	const SAFARIZONE_POKE_BALL1
+	const SAFARIZONE_POKE_BALL2
 
 SafariZone_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+
+SafariZoneThickClub:
+	itemball THICK_CLUB
+
+SafariZoneGoldTeeth:
+	itemball GOLD_TEETH
 
 SafariZoneTrashcan:
 	jumpstd TrashCanScript
@@ -21,3 +29,5 @@ SafariZone_MapEvents:
 	bg_event 32, 46, BGEVENT_READ, SafariZoneTrashcan
 
 	def_object_events
+	object_event  5, 22, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SafariZoneThickClub, EVENT_SAFARI_ZONE_THICK_CLUB
+	object_event 53, 12, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SafariZoneGoldTeeth, EVENT_SAFARI_ZONE_GOLD_TEETH
