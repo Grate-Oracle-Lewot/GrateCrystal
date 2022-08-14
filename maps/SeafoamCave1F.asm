fbc6bef962889928de3becfc8a5495d3bec9e9e5
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-08-14 16:23:18 -0700

    Update SeafoamCave1F.asm

--- maps/SeafoamCave1F.asm
+++ maps/SeafoamCave1F.asm
@@ -5,16 +5,21 @@ SeafoamCave1F_MapScripts:
 
 	def_callbacks
 
+SeafoamCave1FHiddenXSpDefend:
+	hiddenitem X_SP_DEFEND, EVENT_SEAFOAM_CAVE_1F_HIDDEN_X_SP_DEFEND
+
 SeafoamCave1F_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
 	warp_event  5,  5, SEAFOAM_CAVE_PUZZLE_CHAMBER, 3
 	warp_event  3,  3, SEAFOAM_CAVE_B1F, 1
+	warp_event 25,  5, SEAFOAM_GYM, 2
 
 	def_coord_events
 
 	def_bg_events
+	bg_event 26,  2, BGEVENT_ITEM, SeafoamCave1FHiddenXSpDefend
 
 	def_object_events
 
