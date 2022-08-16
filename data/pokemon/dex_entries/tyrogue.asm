683b3ec32dab71ad224bd3ea8ac197aefd57757a
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-08-15 19:20:02 -0700

    Update tyrogue.asm

--- data/pokemon/dex_entries/tyrogue.asm
+++ data/pokemon/dex_entries/tyrogue.asm
@@ -1,10 +1,10 @@
 	db "SCUFFLE@" ; species name
 	dw 204, 460 ; height, weight
 
-	db   "To brush up on its"
-	next "fighting skills,"
-	next "it will challenge"
+	db   "Despite being"
+	next "small and weak, it"
+	next "tries to challenge"
 
-	page "anyone. It has a"
-	next "very strong com-"
-	next "petitive spirit.@"
+	page "larger and more"
+	next "experienced foes"
+	next "to improve.@"
