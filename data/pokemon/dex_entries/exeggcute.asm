ecf201f8e825193383fbd505d7ea4d32705dd818
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-08-24 21:01:09 -0700

    Update exeggcute.asm

--- data/pokemon/dex_entries/exeggcute.asm
+++ data/pokemon/dex_entries/exeggcute.asm
@@ -1,10 +1,10 @@
 	db "EGG@" ; species name
 	dw 104, 60 ; height, weight
 
-	db   "If even one is"
-	next "separated from the"
-	next "group, the energy"
+	db   "It sometimes naps"
+	next "in supermarket egg"
+	next "cartons. Despite"
 
-	page "bond between the"
-	next "six will make them"
-	next "rejoin instantly.@"
+	page "this, it's unwise"
+	next "to try to have it"
+	next "for breakfast.@"
