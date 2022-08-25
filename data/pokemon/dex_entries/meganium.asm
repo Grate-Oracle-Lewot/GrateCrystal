d7a294c5626f134bba346c690e7350d544057979
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-08-24 20:37:45 -0700

    Update meganium.asm

--- data/pokemon/dex_entries/meganium.asm
+++ data/pokemon/dex_entries/meganium.asm
@@ -1,10 +1,10 @@
 	db "HERB@" ; species name
 	dw 511, 2220 ; height, weight
 
-	db   "Anyone who stands"
-	next "beside it becomes"
-	next "refreshed, just as"
+	db   "It dotes on its"
+	next "trainer like a"
+	next "grandparent."
 
-	page "if they were"
-	next "relaxing in a"
-	next "sunny forest.@"
+	page "Its petals have"
+	next "many medicinal"
+	next "properties.@"
