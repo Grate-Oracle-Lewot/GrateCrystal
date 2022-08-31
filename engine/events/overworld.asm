a83e7a4e6df66a588c413173c4a1a1f4614cfcac
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-08-31 14:43:25 -0700

    Update overworld.asm

--- engine/events/overworld.asm
+++ engine/events/overworld.asm
@@ -355,13 +355,13 @@ SurfFunction:
 	jr z, .alreadyfail
 	cp PLAYER_SURF_PIKA
 	jr z, .alreadyfail
+	call GetFacingObject
+	jr nc, .cannotsurf
 	call GetFacingTileCoord
 	call GetTileCollision
 	cp WATER_TILE
 	jr nz, .cannotsurf
 	call CheckDirection
-	jr c, .cannotsurf
-	farcall CheckFacingObject
 	jr c, .cannotsurf
 	ld a, $1
 	ret
@@ -1510,6 +1510,8 @@ FishFunction:
 	jr z, .fail
 	cp PLAYER_SURF_PIKA
 	jr z, .fail
+	call GetFacingObject
+	jr nc, .fail
 	call GetFacingTileCoord
 	call GetTileCollision
 	cp WATER_TILE
