167d91678188fa2381745bded5b8d82345ad177c
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-09-03 10:34:37 -0700

    Update wooper.asm

--- data/pokemon/dex_entries/wooper.asm
+++ data/pokemon/dex_entries/wooper.asm
@@ -1,10 +1,10 @@
-	db "WATER FISH@" ; species name
+	db "AXOLOTL@" ; species name
 	dw 104, 190 ; height, weight
 
-	db   "A mucous"
-	next "membrane covers"
-	next "its body. Touching"
+	db   "It lives in the"
+	next "mud of riverbeds"
+	next "or lakeshores."
 
-	page "it barehanded will"
-	next "cause a shooting"
-	next "pain.@"
+	page "If it doesn't"
+	next "evolve, it can"
+	next "live forever.@"
