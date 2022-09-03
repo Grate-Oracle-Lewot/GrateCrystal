02bea19e84c60f8c4f449081bd1d9b84b6fcb25a
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-09-03 10:15:19 -0700

    Update wartortle.asm

--- data/pokemon/dex_entries/wartortle.asm
+++ data/pokemon/dex_entries/wartortle.asm
@@ -1,10 +1,10 @@
 	db "TURTLE@" ; species name
 	dw 303, 500 ; height, weight
 
-	db   "Its long, furry"
-	next "tail is a symbol"
-	next "of longevity,"
+	db   "In olden times,"
+	next "its tail was some-"
+	next "times mistaken for"
 
-	page "making it quite"
-	next "popular among"
-	next "older people.@"
+	page "angelic wings,"
+	next "leading to rumors"
+	next "that it flies.@"
