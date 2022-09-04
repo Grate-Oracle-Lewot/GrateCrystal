0e36227d786123948cb54101ec6525502e8c5271
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-09-03 20:10:22 -0700

    Update move_constants.asm

--- constants/move_constants.asm
+++ constants/move_constants.asm
@@ -154,7 +154,7 @@
 	const DIZZY_PUNCH  ; 92
 	const SPORE        ; 93
 	const FLASH        ; 94
-	const PSYWAVE      ; 95
+	const AIR_SLASH    ; 95
 	const EARTH_POWER  ; 96
 	const ACID_ARMOR   ; 97
 	const CRABHAMMER   ; 98
