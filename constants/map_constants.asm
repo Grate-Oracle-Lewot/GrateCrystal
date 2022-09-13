982a0c62e4dae77bffa8ccbf64ed3e9cee77f091
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-09-13 16:11:38 -0700

    Update map_constants.asm

--- constants/map_constants.asm
+++ constants/map_constants.asm
@@ -489,7 +489,8 @@ ENDM
 	map_const DAY_OF_WEEK_SIBLINGS_HOUSE,                   4,  4 ; 11
 	map_const ROUTE_27_SANDSTORM_HOUSE,                     4,  4 ; 12
 	map_const ROUTE_29_ROUTE_46_GATE,                       5,  4 ; 13
-	map_const ROUTE_47,                                    50,  9 ; 14
+	map_const ROUTE_47,                                    30,  9 ; 14
+	map_const ROUTE_48,                                    20,  9 ; 15
 	endgroup
 
 	newgroup SAFFRON                                              ; 25
