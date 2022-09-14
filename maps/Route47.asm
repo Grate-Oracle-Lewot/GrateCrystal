371fd85780eb59f5a72da5ff186664b6c5cef966
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-09-13 17:02:44 -0700

    Create Route47.asm

---
+++ maps/Route47.asm
@@ -0,0 +1,18 @@
+	object_const_def
+
+Route47_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+Route47_MapEvents:
+	db 0, 0 ; filler
+
+	def_warp_events
+  warp_event 35, 11, ABANDONED_LAB_1F, 1
+
+	def_coord_events
+
+	def_bg_events
+
+	def_object_events
