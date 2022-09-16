312e2f04176e327671daf2da87da4be6a4db5b80
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-09-15 17:33:50 -0700

    Update Lounge2F.asm

--- maps/Lounge2F.asm
+++ maps/Lounge2F.asm
@@ -14,15 +14,278 @@ Lounge2F_MapScripts:
 
 	def_callbacks
 
+LoungeChuckScript:
+	faceplayer
+	opentext
+	writetext LoungeChuckTalkText
+	readvar VAR_WEEKDAY
+	ifequal FRIDAY, .OfferRematch
+	waitbutton
+	closetext
+	end
+
+.OfferRematch:
+	writetext LoungeChuckRematchText
+	yesorno
+	iftrue .DoRematch
+	writetext LoungeChuckRefuseText
+	waitbutton
+	closetext
+	end
+
+.DoRematch:
+	winlosstext LoungeChuckLossText, 0
+	loadtrainer CHUCK, CHUCK2
+	startbattle
+	reloadmapafterbattle
+	opentext
+	writetext LoungeChuckAfterText
+	waitbutton
+	closetext
+	end
+
+LoungeJanineScript:
+	faceplayer
+	opentext
+	writetext LoungeJanineTalkText
+	readvar VAR_WEEKDAY
+	ifequal FRIDAY, .OfferRematch
+	waitbutton
+	closetext
+	end
+
+.OfferRematch:
+	writetext LoungeJanineRematchText
+	yesorno
+	iftrue .DoRematch
+	writetext LoungeJanineRefuseText
+	waitbutton
+	closetext
+	end
+
+.DoRematch:
+	winlosstext LoungeJanineLossText, 0
+	loadtrainer JANINE, JANINE1
+	startbattle
+	reloadmapafterbattle
+	opentext
+	writetext LoungeJanineAfterText
+	waitbutton
+	closetext
+	end
+
+LoungeJasmineScript:
+	faceplayer
+	opentext
+	writetext LoungeJasmineTalkText
+	readvar VAR_WEEKDAY
+	ifequal SATURDAY, .OfferRematch
+	waitbutton
+	closetext
+	end
+
+.OfferRematch:
+	writetext LoungeJasmineRematchText
+	yesorno
+	iftrue .DoRematch
+	writetext LoungeJasmineRefuseText
+	waitbutton
+	closetext
+	end
+
+.DoRematch:
+	winlosstext LoungeJasmineLossText, 0
+	loadtrainer JASMINE, JASMINE2
+	startbattle
+	reloadmapafterbattle
+	opentext
+	writetext LoungeJasmineAfterText
+	waitbutton
+	closetext
+	end
+
+LoungeSabrinaScript:
+	faceplayer
+	opentext
+	writetext LoungeSabrinaTalkText
+	readvar VAR_WEEKDAY
+	ifequal SATURDAY, .OfferRematch
+	waitbutton
+	closetext
+	end
+
+.OfferRematch:
+	writetext LoungeSabrinaRematchText
+	yesorno
+	iftrue .DoRematch
+	writetext LoungeSabrinaRefuseText
+	waitbutton
+	closetext
+	end
+
+.DoRematch:
+	winlosstext LoungeSabrinaLossText, 0
+	loadtrainer SABRINA, SABRINA1
+	startbattle
+	reloadmapafterbattle
+	opentext
+	writetext LoungeSabrinaAfterText
+	waitbutton
+	closetext
+	end
+
+LoungePryceScript:
+	faceplayer
+	opentext
+	writetext LoungePryceTalkText
+	readvar VAR_WEEKDAY
+	ifequal SUNDAY, .OfferRematch
+	waitbutton
+	closetext
+	end
+
+.OfferRematch:
+	writetext LoungePryceRematchText
+	yesorno
+	iftrue .DoRematch
+	writetext LoungePryceRefuseText
+	waitbutton
+	closetext
+	end
+
+.DoRematch:
+	winlosstext LoungePryceLossText, 0
+	loadtrainer PRYCE, PRYCE2
+	startbattle
+	reloadmapafterbattle
+	opentext
+	writetext LoungePryceAfterText
+	waitbutton
+	closetext
+	end
+
+LoungeBlaineScript:
+	faceplayer
+	opentext
+	writetext LoungeBlaineTalkText
+	readvar VAR_WEEKDAY
+	ifequal SUNDAY, .OfferRematch
+	waitbutton
+	closetext
+	end
+
+.OfferRematch:
+	writetext LoungeBlaineRematchText
+	yesorno
+	iftrue .DoRematch
+	writetext LoungeBlaineRefuseText
+	waitbutton
+	closetext
+	end
+
+.DoRematch:
+	winlosstext LoungeBlaineLossText, 0
+	loadtrainer BLAINE, BLAINE1
+	startbattle
+	reloadmapafterbattle
+	opentext
+	writetext LoungeBlaineAfterText
+	waitbutton
+	closetext
+	end
+
+LoungeBlueScript:
 
 
 LoungeRedScript:
-	jumptextfaceplayer LoungeRedText
+	faceplayer
+	opentext
+	writetext LoungeRedText
+	waitbutton
+	closetext
+	checkevent EVENT_BEAT_RED
+	iffalse .EndConversation
+	winlosstext LoungeRedText, 0
+	loadtrainer RED, RED1
+	startbattle
+	reloadmapafterbattle
+.EndConversation:
+	end
 
 LoungeGymGuideScript:
 	jumptextfaceplayer LoungeGymGuideText
 
+LoungeChuckTalkText:
+	text "One! Two! Three!"
+	line "Four!"
 
+	para "Hey, <PLAY_G>!"
+	line "I'm working out!"
+	done
+
+LoungeChuckRematchText:
+	text "But I guess I"
+	line "should be working"
+
+	para "out my #MON!"
+	line "Let's battle!"
+	done
+
+LoungeChuckRefuseText:
+	text "You sure? I'll get"
+	line "a few more reps in"
+
+	para "while you think"
+	line "about it."
+	done
+
+LoungeChuckLossText:
+	text "Crushed again!"
+	done
+
+LoungeChuckAfterText:
+	text "You're seriously"
+	line "tough, kid!"
+	done
+
+LoungeJanineTalkText:
+	text "It's me, JANINE."
+	line "Or is it? Haha!"
+
+	para "My dad helped de-"
+	line "sign this lounge."
+
+	para "It's functional"
+	line "and ergonomic."
+	done
+
+LoungeJanineRematchText:
+	text "But you didn't"
+	line "come here to talk,"
+	cont "right?"
+
+	para "Shall we test our"
+	line "skills again?"
+	done
+
+LoungeJanineRefuseText:
+	text "Very well. But a"
+	line "true trainer never"
+
+	para "turns down a"
+	line "challenge."
+	done
+
+LoungeJanineLossText:
+	text "I just can't seem"
+	line "to win…"
+	done
+
+LoungeJanineAfterText:
+	text "As the sun rises"
+	line "in one region, it"
+	cont "sets in another…"
+	done
 
 LoungeRedText:
 	text "…"
