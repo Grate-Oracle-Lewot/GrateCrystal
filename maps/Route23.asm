eb2edd42386c47cbcbd6520f495a3526a0747df3
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-09-20 16:09:17 -0700

    Update Route23.asm

--- maps/Route23.asm
+++ maps/Route23.asm
@@ -1,4 +1,5 @@
 	object_const_def
+	const ROUTE23_OFFICER
 
 Route23_MapScripts:
 	def_scene_scripts
@@ -10,8 +11,30 @@ Route23_MapScripts:
 	setflag ENGINE_FLYPOINT_INDIGO_PLATEAU
 	endcallback
 
+Route23OfficerScript:
+	jumptextfaceplayer Route23OfficerText
+
 IndigoPlateauSign:
 	jumptext IndigoPlateauSignText
+
+LoungeSign:
+	jumptext LoungeSignText
+
+Route23OfficerText:
+	text "The #MON LEAGUE"
+	line "is further ahead."
+
+	para "Oh? You have all"
+	line "eight badges?"
+
+	para "That's cute."
+
+	para "You'll need more"
+	line "than that before"
+
+	para "I can let you in"
+	line "here."
+	done
 
 IndigoPlateauSignText:
 	text "INDIGO PLATEAU"
@@ -22,6 +45,10 @@ IndigoPlateauSignText:
 	para "#MON LEAGUE HQ"
 	done
 
+LoungeSignText:
+	text "PRIVATE LOUNGE"
+	done
+
 Route23_MapEvents:
 	db 0, 0 ; filler
 
@@ -30,10 +57,13 @@ Route23_MapEvents:
 	warp_event 10,  5, INDIGO_PLATEAU_POKECENTER_1F, 2
 	warp_event  9, 31, VICTORY_ROAD, 10
 	warp_event 10, 31, VICTORY_ROAD, 10
+	warp_event 15, 21, LOUNGE_1F, 1
 
 	def_coord_events
 
 	def_bg_events
 	bg_event 11,  7, BGEVENT_READ, IndigoPlateauSign
+	bg_event 13, 21, BGEVENT_READ, LoungeSign
 
 	def_object_events
+	object_event 15, 22, SPRITE_OFFICER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, Route23OfficerScript, EVENT_OPENED_MT_SILVER
