d8e5b0224ae3f736c1d7e6efeb1bd14054409263
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-09-20 22:57:55 -0700

    Update FuchsiaGym.asm

--- maps/FuchsiaGym.asm
+++ maps/FuchsiaGym.asm
@@ -122,7 +122,7 @@ PicnickerCindyScript:
 	iftrue .CindyUnmasked
 	applymovement FUCHSIAGYM_FUCHSIA_GYM_3, Movement_NinjaSpin
 	faceplayer
-	variablesprite SPRITE_FUCHSIA_GYM_3, SPRITE_COOLTRAINER_F
+	variablesprite SPRITE_FUCHSIA_GYM_3, SPRITE_LASS
 	special LoadUsedSpritesGFX
 .CindyUnmasked:
 	faceplayer
