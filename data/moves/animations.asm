9bc9addb75278c16ef6b5588a5cec45643d34fa8
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-09-26 20:51:53 -0700

    Update animations.asm

--- data/moves/animations.asm
+++ data/moves/animations.asm
@@ -2672,14 +2672,14 @@ BattleAnim_Scald:
 	anim_call BattleAnim_UserObj_2Row
 	anim_sound 16, 2, SFX_BUBBLEBEAM
 	anim_obj ANIM_OBJ_HYDRO_PUMP, 132, 72, $0
-	anim_bgeffect ANIM_BG_WATER, $1c, $0, $0
-	anim_wait 42
+	anim_wait 32
 	anim_sound 0, 1, SFX_EMBER
 	anim_obj ANIM_OBJ_EMBER, 132, 68, $30
+	anim_wait 16
+	anim_bgeffect ANIM_BG_WATER, $1c, $0, $0
 	anim_wait 24
 	anim_call BattleAnim_ShowMon_1
 	anim_bgeffect ANIM_BG_END_WATER, $0, $0, $0
-	anim_wait 8
 	anim_ret
 
 BattleAnim_Psybeam:
