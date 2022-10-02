e08f2dc79c104b2b41bb389da50b08e0d18516a3
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-10-02 16:01:02 -0700

    Update igglybuff.asm

--- data/pokemon/dex_entries/igglybuff.asm
+++ data/pokemon/dex_entries/igglybuff.asm
@@ -1,10 +1,10 @@
 	db "BALLOON@" ; species name
 	dw 100, 20 ; height, weight
 
-	db   "Instead of walking"
-	next "with its short"
-	next "legs, it moves"
+	db   "It's born with"
+	next "wondrous singing"
+	next "abilities, but"
 
-	page "around by bouncing"
-	next "on its soft,"
-	next "tender body.@"
+	page "hasn't yet learned"
+	next "when they should"
+	next "be used.@"
