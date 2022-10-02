ec17a34067d0363e4784cfb6760caf3bbf6358bf
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-10-02 16:24:55 -0700

    Update venomoth.asm

--- data/pokemon/dex_entries/venomoth.asm
+++ data/pokemon/dex_entries/venomoth.asm
@@ -1,10 +1,10 @@
-	db "POISONMOTH@" ; species name
+	db "MOTH@" ; species name
 	dw 411, 280 ; height, weight
 
-	db   "The scales it"
-	next "scatters will"
-	next "paralyze anyone"
+	db   "The toxic powder"
+	next "that falls from"
+	next "its wings has been"
 
-	page "who touches them,"
-	next "making that person"
-	next "unable to stand.@"
+	page "used by ninja for"
+	next "generations."
+	next "@"
