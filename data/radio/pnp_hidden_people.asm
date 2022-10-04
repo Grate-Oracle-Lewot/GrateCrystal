8d5fa9450863ae3f41c46d3517aed675712751da
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-10-04 16:38:15 -0700

    Update pnp_hidden_people.asm

--- data/radio/pnp_hidden_people.asm
+++ data/radio/pnp_hidden_people.asm
@@ -1,11 +1,17 @@
 ; Places and People will not describe these trainers.
 
 PnP_HiddenPeople:
+	db POKEMON_PROF
 	db WILL
 	db BRUNO
 	db KAREN
 	db KOGA
 	db CHAMPION
+	db BOSS
+	db JESSIE
+	db AEROBONES
+	db KABUBONES
+	db MISSINGNO
 	; fallthrough
 PnP_HiddenPeople_BeatE4:
 	db BROCK
@@ -16,11 +22,13 @@ PnP_HiddenPeople_BeatE4:
 	db SABRINA
 	db BLAINE
 	db BLUE
+	db REAL_KAREN
+	db REAL_KOGA
 	; fallthrough
 PnP_HiddenPeople_BeatKanto:
 	db RIVAL1
-	db POKEMON_PROF
 	db CAL
 	db RIVAL2
 	db RED
+	db CAL_F
 	db -1
