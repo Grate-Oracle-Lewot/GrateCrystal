c674a652f5f61b0f99779ee0798820775b45726f
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-10-04 18:06:42 -0700

    Update miltank.asm

--- data/pokemon/dex_entries/miltank.asm
+++ data/pokemon/dex_entries/miltank.asm
@@ -1,10 +1,10 @@
 	db "MILK COW@" ; species name
 	dw 311, 1660 ; height, weight
 
-	db   "In order to milk a"
-	next "MILTANK, one must"
-	next "have a knack for"
+	db   "Its milk is unique"
+	next "among #MON in"
+	next "that it can be"
 
-	page "rhythmically pull-"
-	next "ing up and down"
-	next "on its udders.@"
+	page "safely consumed by"
+	next "any species."
+	next "@"
