a75122060ddb95b624de7ac499255b6a61e0d61f
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-10-06 14:49:15 -0700

    Update stantler.asm

--- data/pokemon/dex_entries/stantler.asm
+++ data/pokemon/dex_entries/stantler.asm
@@ -1,10 +1,10 @@
 	db "BIG HORN@" ; species name
 	dw 407, 1570 ; height, weight
 
-	db   "The round balls"
-	next "found on the"
-	next "fallen antlers can"
+	db   "Eight immortal"
+	next "STANTLER pull"
+	next "SANTA's sleigh."
 
-	page "be ground into a"
-	next "powder that aids"
-	next "in sleeping.@"
+	page "Some whisper of a"
+	next "ninth with a glow-"
+	next "ing red nose.@"
