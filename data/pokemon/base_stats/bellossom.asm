b14ea54446645f89057dcb6ca26b99c112257316
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-10-05 18:24:06 -0700

    Update bellossom.asm

--- data/pokemon/base_stats/bellossom.asm
+++ data/pokemon/base_stats/bellossom.asm
@@ -13,7 +13,7 @@
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/bellossom/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
-	db GROWTH_MEDIUM_SLOW ; growth rate
+	db GROWTH_SLIGHTLY_SLOW ; growth rate
 	dn EGG_PLANT, EGG_FAIRY ; egg groups
 
 	; tm/hm learnset
