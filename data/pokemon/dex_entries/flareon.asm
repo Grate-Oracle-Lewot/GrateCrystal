7391e1aff03f7eeeb4bf20de9aedc529b3d307d5
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-10-08 11:07:54 -0700

    Update flareon.asm

--- data/pokemon/dex_entries/flareon.asm
+++ data/pokemon/dex_entries/flareon.asm
@@ -1,10 +1,10 @@
 	db "FLAME@" ; species name
 	dw 211, 550 ; height, weight
 
-	db   "Once it has stored"
-	next "up enough heat,"
-	next "this #MON's"
+	db   "Its fur is fire-"
+	next "proof, but becomes"
+	next "highly flammable"
 
-	page "body temperature"
-	next "can reach up to"
-	next "1700 degrees.@"
+	page "after being shed."
+	next "It's used to start"
+	next "campfires.@"
