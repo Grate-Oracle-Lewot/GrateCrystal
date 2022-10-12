a0ccd5a890d32c4fc2165825266b4046bb029038
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-10-12 10:43:55 -0700

    Update moves.asm

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -26,7 +26,7 @@ Moves:
 	move VICEGRIP,     EFFECT_NORMAL_HIT,         55, NORMAL,       PHYSICAL, 100, 30,   0
 	move GUILLOTINE,   EFFECT_OHKO,                0, STEEL,        PHYSICAL,  30,  5,   0
 	move SIGNAL_BEAM,  EFFECT_CONFUSE_HIT,        75, BUG,          SPECIAL,  100, 15,  10
-	move SWORDS_DANCE, EFFECT_ATTACK_UP_2,         0, FIGHTING,     STATUS,   100, 20,   0
+	move SWORDS_DANCE, EFFECT_ATTACK_UP_2,         0, FIGHTING,     STATUS,   100, 15,   0
 	move CUT,          EFFECT_NORMAL_HIT,         80, BUG,          PHYSICAL, 100, 15,   0
 	move GUST,         EFFECT_GUST,               40, FLYING,       SPECIAL,  100, 30,   0
 	move WING_ATTACK,  EFFECT_NORMAL_HIT,         60, FLYING,       PHYSICAL, 100, 25,   0
@@ -146,7 +146,7 @@ Moves:
 	move SPIKE_CANNON, EFFECT_MULTI_HIT,          20, STEEL,        PHYSICAL, 100, 15,   0
 	move HAIL,         EFFECT_HAIL,                0, ICE,          STATUS,   100,  5,   0
 	move AMNESIA,      EFFECT_SP_DEF_UP_2,         0, PSYCHIC_TYPE, STATUS,   100, 20,   0
-	move KINESIS,      EFFECT_SP_ATK_UP_2,         0, PSYCHIC_TYPE, STATUS,   100, 20,   0
+	move KINESIS,      EFFECT_SP_ATK_UP_2,         0, PSYCHIC_TYPE, STATUS,   100, 15,   0
 	move SOFTBOILED,   EFFECT_HEAL,                0, NORMAL,       STATUS,   100, 10,   0
 	move HI_JUMP_KICK, EFFECT_JUMP_KICK,          95, FIGHTING,     PHYSICAL,  90, 20,   0
 	move GLARE,        EFFECT_PARALYZE,            0, NORMAL,       STATUS,    84, 15,   0
