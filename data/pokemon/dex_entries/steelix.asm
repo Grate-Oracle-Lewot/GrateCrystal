7da80fe68817636de2888de7662be7d4cf97ee85
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-11-22 16:36:36 -0800

    Update steelix.asm

--- data/pokemon/dex_entries/steelix.asm
+++ data/pokemon/dex_entries/steelix.asm
@@ -1,10 +1,10 @@
 	db "IRON SNAKE@" ; species name
 	dw 3002, 8820 ; height, weight
 
-	db   "The many small"
-	next "metal particles"
-	next "that cover this"
+	db   "No manmade metal"
+	next "can compare to the"
+	next "durability of this"
 
-	page "#MON's body"
-	next "reflect bright"
-	next "light well.@"
+	page "#MON's metallic"
+	next "skin. It can swim"
+	next "through magma.@"
