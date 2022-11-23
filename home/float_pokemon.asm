ed95e07fbfd3b608a05348e73a64aacbff108209
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-11-22 20:33:12 -0800

    Update float_pokemon.asm

--- home/float_pokemon.asm
+++ home/float_pokemon.asm
@@ -3,15 +3,15 @@ FloatPokemon::
 	db VENOMOTH
 	db MAGNEMITE
 	db MAGNETON
-  db GASTLY
-  db HAUNTER
-  db KOFFING
-  db WEEZING
-  db MEW
-  db MISDREAVUS
-  db UNOWN
-  db LUGIA
-  db HO_OH
-  db CELEBI
-  db YANMEGA
+	db GASTLY
+	db HAUNTER
+	db KOFFING
+	db WEEZING
+	db MEW
+	db MISDREAVUS
+	db UNOWN
+	db LUGIA
+	db HO_OH
+	db CELEBI
+	db YANMEGA
 	db -1
