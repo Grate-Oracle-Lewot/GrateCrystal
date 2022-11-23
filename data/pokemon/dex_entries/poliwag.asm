0a1f972219db91c3bf8ccb38c9fc1044ca236767
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-11-22 17:38:29 -0800

    Update poliwag.asm

--- data/pokemon/dex_entries/poliwag.asm
+++ data/pokemon/dex_entries/poliwag.asm
@@ -1,10 +1,10 @@
 	db "TADPOLE@" ; species name
 	dw 200, 270 ; height, weight
 
-	db   "The swirl on its"
-	next "belly is its"
-	next "insides showing"
+	db   "It's born without"
+	next "legs and grows"
+	next "them over time."
 
-	page "through the skin."
-	next "It looks clearer"
-	next "after it eats.@"
+	page "However, it can't"
+	next "grow arms until it"
+	next "evolves.@"
