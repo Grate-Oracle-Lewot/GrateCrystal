856a010f28d53286bdc96155ab733b747309a66e
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-11-22 17:28:46 -0800

    Update pidgeotto.asm

--- data/pokemon/dex_entries/pidgeotto.asm
+++ data/pokemon/dex_entries/pidgeotto.asm
@@ -1,10 +1,10 @@
 	db "BIRD@" ; species name
 	dw 307, 660 ; height, weight
 
-	db   "It slowly flies in"
-	next "a circular pat-"
-	next "tern, all the"
+	db   "It navigates by"
+	next "sensing the world's"
+	next "magnetic field."
 
-	page "while keeping a"
-	next "sharp lookout for"
-	next "prey.@"
+	page "MAGNEMITE can"
+	next "interfere with its"
+	next "routing.@"
