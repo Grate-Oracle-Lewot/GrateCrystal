2f933bd5a0a8aa56893cfe7135859f24d5cfb87c
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-11-22 20:26:50 -0800

    Create float_pokemon.asm

---
+++ home/float_pokemon.asm
@@ -0,0 +1,17 @@
+FloatPokemon::
+	db BEEDRILL
+	db VENOMOTH
+	db MAGNEMITE
+	db MAGNETON
+  db GASTLY
+  db HAUNTER
+  db KOFFING
+  db WEEZING
+  db MEW
+  db MISDREAVUS
+  db UNOWN
+  db LUGIA
+  db HO_OH
+  db CELEBI
+  db YANMEGA
+	db -1
