cfe73c498fb1a6cbb85506359818a9a0f230e42a
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-11-22 18:33:10 -0800

    Update nidorina.asm

--- data/pokemon/dex_entries/nidorina.asm
+++ data/pokemon/dex_entries/nidorina.asm
@@ -1,10 +1,10 @@
 	db "POISON PIN@" ; species name
 	dw 207, 440 ; height, weight
 
-	db   "It has a docile"
-	next "nature. If it is"
-	next "threatened with"
+	db   "It's become inten-"
+	next "sely interested in"
+	next "attracting the"
 
-	page "attack, it raises"
-	next "the barbs that are"
-	next "all over its body.@"
+	page "attention of NIDO-"
+	next "RINO, but doesn't"
+	next "know how to.@"
