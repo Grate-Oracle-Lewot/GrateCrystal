00d5b503f4bd1aba5d0c616f47de64448565e538
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-16 08:44:03 -0800

    Create pikachu_bitmask_pointers.asm

---
+++ gfx/pokemon/pikachu_bitmask_pointers.asm
@@ -0,0 +1,7 @@
+PikachuBitmasksPointers:
+	table_width 2, PikachuBitmasksPointers
+	dw NormalPikachuBitmasks
+	dw FightingPikachuBitmasks
+	dw SurfingPikachuBitmasks
+	dw FlyingPikachuBitmasks
+	assert_table_length NUM_PIKACHU_FORMS
