23abbea447b93b898d1031181a74e001075d7a98
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-16 08:37:57 -0800

    Create pikachu_anim_pointers.asm

---
+++ gfx/pokemon/pikachu_anim_pointers.asm
@@ -0,0 +1,7 @@
+PikachuAnimationPointers:
+	table_width 2, PikachuAnimationPointers
+	dw NormalPikachuAnimation
+	dw FightingPikachuAnimation
+	dw SurfingPikachuCAnimation
+	dw FlyingPikachuAnimation
+	assert_table_length NUM_PIKACHU_FORMS
