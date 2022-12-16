d43e02f9276a9d92b77f401b45c8cd636fb1fd13
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-16 10:01:39 -0800

    Create pikachu_pic_pointers.asm

---
+++ data/pokemon/pikachu_pic_pointers.asm
@@ -0,0 +1,16 @@
+PikachuPicPointers::
+; entries correspond to Pikachu forms, two apiece
+	table_width 3 * 2, PikachuPicPointers
+	dba CrystalPikachuFrontpic
+	dba CrystalPikachuBackpic
+	dba GoldPikachuFrontpic
+	dba GoldPikachuBackpic
+	dba SilverPikachuFrontpic
+	dba SilverPikachuBackpic
+	dba FightingPikachuFrontpic
+	dba FightingPikachuBackpic
+	dba SurfingPikachuFrontpic
+	dba SurfingPikachuBackpic
+	dba FlyingPikachuFrontpic
+	dba FlyingPikachuBackpic
+	assert_table_length NUM_PIKACHU_FORMS
