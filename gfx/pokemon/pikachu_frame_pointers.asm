c581d1418de304239b5d787fa72e7711a386d781
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-16 08:46:25 -0800

    Create pikachu_frame_pointers.asm

---
+++ gfx/pokemon/pikachu_frame_pointers.asm
@@ -0,0 +1,7 @@
+PikachuFramesPointers:
+	table_width 2, PikachuFramesPointers
+	dw NormalPikachuFrames
+	dw FightingPikachuFrames
+	dw SurfingPikachuFrames
+	dw FlyingPikachuFrames
+	assert_table_length NUM_PIKACHU_FORMS
