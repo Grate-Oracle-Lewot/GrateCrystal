97371e89059c13cbc287100c0d3002e05f68c40f
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-17 12:09:53 -0800

    Update pikachu_pic_pointers.asm

--- data/pokemon/pikachu_pic_pointers.asm
+++ data/pokemon/pikachu_pic_pointers.asm
@@ -1,12 +1,18 @@
 PikachuPicPointers::
 ; entries correspond to Pikachu forms, two apiece
 	table_width 3 * 2, PikachuPicPointers
-	dba CrystalPikachuFrontpic
-	dba CrystalPikachuBackpic
+	dba RedPikachuFrontpic
+	dba RedPikachuBackpic
+	dba BluePikachuFrontpic
+	dba BluePikachuBackpic
+	dba YellowPikachuFrontpic
+	dba YellowPikachuBackpic
 	dba GoldPikachuFrontpic
 	dba GoldPikachuBackpic
 	dba SilverPikachuFrontpic
 	dba SilverPikachuBackpic
+	dba CrystalPikachuFrontpic
+	dba CrystalPikachuBackpic
 	dba FightingPikachuFrontpic
 	dba FightingPikachuBackpic
 	dba SurfingPikachuFrontpic
