a727c225e5aa171082987f007631d197a6d17ccc
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-18 22:35:39 -0800

    Update leaders.asm

--- data/trainers/leaders.asm
+++ data/trainers/leaders.asm
@@ -1,9 +1,6 @@
 ; These lists determine the battle music and victory music, and whether to
 ; award HAPPINESS_GYMBATTLE for winning.
 
-; Note: CHAMPION and RED are unused for battle music checks, since they are
-; accounted for prior to the list check.
-
 GymLeaders:
 	db FALKNER
 	db WHITNEY
@@ -17,8 +14,6 @@ GymLeaders:
 	db BRUNO
 	db KAREN
 	db KOGA
-	db CHAMPION
-	db RED
 ; fallthrough
 KantoGymLeaders:
 	db BROCK
