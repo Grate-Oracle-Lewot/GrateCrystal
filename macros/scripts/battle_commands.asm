c0d87787f204532c8092c8652b29faa9321f74e9
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-18 17:19:48 -0800

    Update battle_commands.asm

--- macros/scripts/battle_commands.asm
+++ macros/scripts/battle_commands.asm
@@ -94,13 +94,11 @@ ENDM
 	command endure                  ; 5a
 	command checkcurl               ; 5b
 	command rolloutpower            ; 5c
-	command effect0x5d              ; 5d
 	command furycutter              ; 5e
 	command attract                 ; 5f
 	command happinesspower          ; 60
 	command present                 ; 61
 	command damagecalc              ; 62
-	command frustrationpower        ; 63
 	command safeguard               ; 64
 	command checksafeguard          ; 65
 	command getmagnitude            ; 66
