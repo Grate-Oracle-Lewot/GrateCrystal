9338e3371c18a6f1a08dd873ed330f6a2c23682d
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-23 18:19:17 -0800

    Update specials.asm

--- engine/events/specials.asm
+++ engine/events/specials.asm
@@ -52,6 +52,13 @@ FindPartyMonAboveLevel:
 	jr z, FoundNone
 	jr FoundOne
 
+FindPartyMonAtLeastThatHappy:
+	ld a, [wScriptVar]
+	ld b, a
+	farcall _FindPartyMonAtLeastThatHappy
+	jr z, FoundNone
+	jr FoundOne
+
 FindPartyMonThatSpecies:
 	ld a, [wScriptVar]
 	ld b, a
