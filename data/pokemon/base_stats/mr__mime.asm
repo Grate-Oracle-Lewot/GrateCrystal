11fc818665d1a7814a97527d952529f4367d3c51
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-26 15:04:49 -0800

    Update mr__mime.asm

--- data/pokemon/base_stats/mr__mime.asm
+++ data/pokemon/base_stats/mr__mime.asm
@@ -6,7 +6,7 @@
 	db PSYCHIC_TYPE, FAIRY ; type
 	db 45 ; catch rate
 	db 159 ; base exp
-	db MYSTERYBERRY, BERSERK_GENE ; items
+	db CLEANSE_TAG, HANDWARMER ; items
 	db GENDER_F12_5 ; gender ratio
 	db 100 ; unknown 1
 	db 13 ; step cycles to hatch
