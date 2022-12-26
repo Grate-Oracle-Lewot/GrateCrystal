c6d57b89e95a833a001335f255c9caf617d0c1b3
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-26 15:02:44 -0800

    Update tyrogue.asm

--- data/pokemon/base_stats/tyrogue.asm
+++ data/pokemon/base_stats/tyrogue.asm
@@ -6,7 +6,7 @@
 	db FIGHTING, FIGHTING ; type
 	db 75 ; catch rate
 	db 91 ; base exp
-	db EVIOLITE, BERSERK_GENE ; items
+	db EVIOLITE, BLACKBELT_I ; items
 	db GENDER_F12_5 ; gender ratio
 	db 100 ; unknown 1
 	db 13 ; step cycles to hatch
