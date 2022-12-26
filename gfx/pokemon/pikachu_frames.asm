85a685047e8b8f5d23818414ab61e8946b8576b0
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-26 08:06:46 -0800

    Update pikachu_frames.asm

--- gfx/pokemon/pikachu_frames.asm
+++ gfx/pokemon/pikachu_frames.asm
@@ -1,3 +1,5 @@
+PikachusFrames: ; used only for BANK(PikachusFrames)
+
 RetroPikachuFrames: INCLUDE "gfx/pokemon/pikachu_retro/frames.asm"
 SilverPikachuFrames: INCLUDE "gfx/pokemon/pikachu_silver/frames.asm"
 CrystalPikachuFrames: INCLUDE "gfx/pokemon/pikachu_crystal/frames.asm"
