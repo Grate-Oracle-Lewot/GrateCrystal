86e48c2ccb25296e0774d68835af2b6e0a8c698a
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-29 04:08:43 -0800

    Update load_pics.asm

--- engine/gfx/load_pics.asm
+++ engine/gfx/load_pics.asm
@@ -100,6 +100,16 @@ GetPikachuForm:
 ; Increment to get 1-6
 	ldh a, [hQuotient + 3]
 	inc a
+	cp 0
+	jr z, .low_error
+	cp 7
+	jr z, .high_error
+.low_error
+	ld a, 1
+	jr .final_answer
+.high_error
+	ld a, 6
+.final_answer
 	ld [wPikachuForm], a
 	ret
 
