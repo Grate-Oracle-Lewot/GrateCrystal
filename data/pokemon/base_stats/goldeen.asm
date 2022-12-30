22a2d7a22acf423fbb27243f6770ead952b5432c
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-30 03:30:35 -0800

    Update goldeen.asm

--- data/pokemon/base_stats/goldeen.asm
+++ data/pokemon/base_stats/goldeen.asm
@@ -8,11 +8,8 @@
 	db 111 ; base exp
 	db FRESH_WATER, MYSTIC_WATER ; items
 	db GENDER_F50 ; gender ratio
-	db 100 ; unknown 1
 	db 10 ; step cycles to hatch
-	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/goldeen/front.dimensions"
-	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_WATER_2, EGG_WATER_1 ; egg groups
 
