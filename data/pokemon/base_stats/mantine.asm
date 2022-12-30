95070cdf58097ec5ec69c4135d1aab65b59b0ff8
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-30 03:59:27 -0800

    Update mantine.asm

--- data/pokemon/base_stats/mantine.asm
+++ data/pokemon/base_stats/mantine.asm
@@ -8,11 +8,8 @@
 	db 168 ; base exp
 	db FRESH_WATER, TM_STEEL_WING ; items
 	db GENDER_F50 ; gender ratio
-	db 100 ; unknown 1
 	db 13 ; step cycles to hatch
-	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/mantine/front.dimensions"
-	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_SLOW ; growth rate
 	dn EGG_WATER_1, EGG_WATER_2 ; egg groups
 
