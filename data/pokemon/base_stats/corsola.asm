14fd496f1ff88e2d30fbbb60053febf3db81ab51
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-30 03:14:10 -0800

    Update corsola.asm

--- data/pokemon/base_stats/corsola.asm
+++ data/pokemon/base_stats/corsola.asm
@@ -8,11 +8,8 @@
 	db 113 ; base exp
 	db BRICK_PIECE, CALCIUM ; items
 	db GENDER_F75 ; gender ratio
-	db 100 ; unknown 1
 	db 10 ; step cycles to hatch
-	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/corsola/front.dimensions"
-	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_FAST ; growth rate
 	dn EGG_MINERAL, EGG_WATER_3 ; egg groups
 
