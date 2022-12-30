f799423a127828b36c75637b734261e2987f8ef1
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-30 04:41:21 -0800

    Update spinarak.asm

--- data/pokemon/base_stats/spinarak.asm
+++ data/pokemon/base_stats/spinarak.asm
@@ -8,11 +8,8 @@
 	db 54 ; base exp
 	db ANTIDOTE, ANTIVENOM ; items
 	db GENDER_F50 ; gender ratio
-	db 100 ; unknown 1
 	db 8 ; step cycles to hatch
-	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/spinarak/front.dimensions"
-	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_FAST ; growth rate
 	dn EGG_BUG, EGG_WATER_3 ; egg groups
 
