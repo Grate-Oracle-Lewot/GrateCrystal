949f74405f8da3e2fa6fdfdef54e907ab0ecf7c2
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-30 03:18:42 -0800

    Update donphan.asm

--- data/pokemon/base_stats/donphan.asm
+++ data/pokemon/base_stats/donphan.asm
@@ -8,11 +8,8 @@
 	db 189 ; base exp
 	db TM_ROLLOUT, LEFTOVERS ; items
 	db GENDER_F50 ; gender ratio
-	db 100 ; unknown 1
 	db 20 ; step cycles to hatch
-	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/donphan/front.dimensions"
-	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_GROUND, EGG_MONSTER ; egg groups
 
