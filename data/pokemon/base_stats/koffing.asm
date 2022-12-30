6a1242a108198bbdcf5da06da4533cd533e7ce01
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-30 03:50:30 -0800

    Update koffing.asm

--- data/pokemon/base_stats/koffing.asm
+++ data/pokemon/base_stats/koffing.asm
@@ -8,11 +8,8 @@
 	db 114 ; base exp
 	db SMOKE_BALL, ANTIVENOM ; items
 	db GENDER_F50 ; gender ratio
-	db 100 ; unknown 1
 	db 10 ; step cycles to hatch
-	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/koffing/front.dimensions"
-	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_INDETERMINATE, EGG_FAIRY ; egg groups
 
