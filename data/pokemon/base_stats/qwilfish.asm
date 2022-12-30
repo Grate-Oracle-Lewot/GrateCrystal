8f83cdc10d1f52085b09dede1c89be0da8841b75
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-30 04:24:30 -0800

    Update qwilfish.asm

--- data/pokemon/base_stats/qwilfish.asm
+++ data/pokemon/base_stats/qwilfish.asm
@@ -8,11 +8,8 @@
 	db 121 ; base exp
 	db TM_SCALD, SODA_POP ; items
 	db GENDER_F50 ; gender ratio
-	db 100 ; unknown 1
 	db 10 ; step cycles to hatch
-	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/qwilfish/front.dimensions"
-	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_WATER_2, EGG_INDETERMINATE ; egg groups
 
