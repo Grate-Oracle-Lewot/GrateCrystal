1aefb651da372e0f05ffa9b4bd6dfdce5424913d
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-30 04:02:31 -0800

    Update mew.asm

--- data/pokemon/base_stats/mew.asm
+++ data/pokemon/base_stats/mew.asm
@@ -8,11 +8,8 @@
 	db 64 ; base exp
 	db BERRY, MASTER_BALL ; items
 	db GENDER_UNKNOWN ; gender ratio
-	db 100 ; unknown 1
 	db 64 ; step cycles to hatch
-	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/mew/front.dimensions"
-	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_SLOW ; growth rate
 	dn EGG_FAIRY, EGG_GROUND ; egg groups
 
