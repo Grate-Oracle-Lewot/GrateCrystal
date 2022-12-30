f610ea6f2400dc5dff31738e981d96360fe693ca
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-30 05:01:22 -0800

    Update whimsicott.asm

--- data/pokemon/base_stats/whimsicott.asm
+++ data/pokemon/base_stats/whimsicott.asm
@@ -8,11 +8,8 @@
 	db 168 ; base exp
 	db SILVER_LEAF, GOLD_LEAF ; items
 	db GENDER_F50 ; gender ratio
-	db 100 ; unknown 1
 	db 10 ; step cycles to hatch
-	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/whimsicott/front.dimensions"
-	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_PLANT, EGG_FAIRY ; egg groups
 
