6d83d26bc43c8e99b37b632b7ce7cdb106c57466
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-30 03:41:32 -0800

    Update hitmontop.asm

--- data/pokemon/base_stats/hitmontop.asm
+++ data/pokemon/base_stats/hitmontop.asm
@@ -8,11 +8,8 @@
 	db 138 ; base exp
 	db FOCUS_BAND, BLACKBELT_I ; items
 	db GENDER_F12_5 ; gender ratio
-	db 100 ; unknown 1
 	db 25 ; step cycles to hatch
-	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/hitmontop/front.dimensions"
-	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_HUMANSHAPE, EGG_FAIRY ; egg groups
 
