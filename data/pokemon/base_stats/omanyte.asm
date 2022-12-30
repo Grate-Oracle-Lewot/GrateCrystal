e07b66e8903c0954ebc4cd51b12862779e3338e7
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-30 04:11:21 -0800

    Update omanyte.asm

--- data/pokemon/base_stats/omanyte.asm
+++ data/pokemon/base_stats/omanyte.asm
@@ -8,11 +8,8 @@
 	db 120 ; base exp
 	db HARD_STONE, MYSTIC_WATER ; items
 	db GENDER_F12_5 ; gender ratio
-	db 100 ; unknown 1
 	db 15 ; step cycles to hatch
-	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/omanyte/front.dimensions"
-	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_WATER_1, EGG_WATER_3 ; egg groups
 
