3826ea472881ff9036118aad7800a0a214da9885
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-30 05:02:54 -0800

    Update wooper.asm

--- data/pokemon/base_stats/wooper.asm
+++ data/pokemon/base_stats/wooper.asm
@@ -8,11 +8,8 @@
 	db 52 ; base exp
 	db FRESH_WATER, LIMBO_STICK ; items
 	db GENDER_F50 ; gender ratio
-	db 100 ; unknown 1
 	db 10 ; step cycles to hatch
-	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/wooper/front.dimensions"
-	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_WATER_1, EGG_GROUND ; egg groups
 
