6000134e4fcf87295192d707c18c21bff1a60c20
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-30 05:04:19 -0800

    Update yanmega.asm

--- data/pokemon/base_stats/yanmega.asm
+++ data/pokemon/base_stats/yanmega.asm
@@ -8,11 +8,8 @@
 	db 198 ; base exp
 	db SILVERPOWDER, BRIGHTPOWDER ; items
 	db GENDER_F50 ; gender ratio
-	db 100 ; unknown 1
 	db 21 ; step cycles to hatch
-	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/yanmega/front.dimensions"
-	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_BUG, EGG_DRAGON ; egg groups
 
