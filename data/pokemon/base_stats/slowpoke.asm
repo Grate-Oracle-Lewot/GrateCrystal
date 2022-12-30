49af84c7c3db91964368bf3da0478bd9e708b20e
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-30 04:36:09 -0800

    Update slowpoke.asm

--- data/pokemon/base_stats/slowpoke.asm
+++ data/pokemon/base_stats/slowpoke.asm
@@ -1,6 +1,6 @@
 	db SLOWPOKE ; 079
 
-	db  90,  65,  65,  15,  40,  40
+	db  90,  65,  65,  10,  40,  40
 	;   hp  atk  def  spd  sat  sdf
 
 	db PSYCHIC_TYPE, PSYCHIC_TYPE ; type
@@ -8,11 +8,8 @@
 	db 99 ; base exp
 	db SLOWPOKETAIL, KINGS_ROCK ; items
 	db GENDER_F50 ; gender ratio
-	db 100 ; unknown 1
 	db 21 ; step cycles to hatch
-	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/slowpoke/front.dimensions"
-	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_SLIGHTLY_FAST ; growth rate
 	dn EGG_MONSTER, EGG_WATER_1 ; egg groups
 
