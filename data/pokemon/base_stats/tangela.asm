9b632ecf748fcd6f29acb026f1f6e829c984a7c7
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-30 04:49:19 -0800

    Update tangela.asm

--- data/pokemon/base_stats/tangela.asm
+++ data/pokemon/base_stats/tangela.asm
@@ -8,11 +8,8 @@
 	db 188 ; base exp
 	db LEMONADE, ALOE_VERA ; items
 	db GENDER_F50 ; gender ratio
-	db 100 ; unknown 1
 	db 10 ; step cycles to hatch
-	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/tangela/front.dimensions"
-	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_PLANT, EGG_WATER_1 ; egg groups
 
