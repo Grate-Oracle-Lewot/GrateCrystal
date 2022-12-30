7b4fa6658f1816418d52589122ba6a376b5e1cd1
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-30 04:21:51 -0800

    Update porygon2.asm

--- data/pokemon/base_stats/porygon2.asm
+++ data/pokemon/base_stats/porygon2.asm
@@ -8,11 +8,8 @@
 	db 180 ; base exp
 	db UP_GRADE, UP_GRADE ; items
 	db GENDER_UNKNOWN ; gender ratio
-	db 100 ; unknown 1
 	db 20 ; step cycles to hatch
-	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/porygon2/front.dimensions"
-	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_MINERAL, EGG_INDETERMINATE ; egg groups
 
