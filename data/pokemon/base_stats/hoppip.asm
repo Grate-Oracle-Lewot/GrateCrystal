e6b4f29d80d24badf829c808d6a5601fc5904c36
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-30 03:42:41 -0800

    Update hoppip.asm

--- data/pokemon/base_stats/hoppip.asm
+++ data/pokemon/base_stats/hoppip.asm
@@ -8,11 +8,8 @@
 	db 74 ; base exp
 	db MIRACLEBERRY, MIRACLE_SEED ; items
 	db GENDER_F50 ; gender ratio
-	db 100 ; unknown 1
 	db 10 ; step cycles to hatch
-	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/hoppip/front.dimensions"
-	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_SLIGHTLY_SLOW ; growth rate
 	dn EGG_FAIRY, EGG_PLANT ; egg groups
 
