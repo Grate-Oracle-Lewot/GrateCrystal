1c00d71d37b2a39f7bce366bcb1c58d4d57312bb
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-30 03:40:45 -0800

    Update hitmonchan.asm

--- data/pokemon/base_stats/hitmonchan.asm
+++ data/pokemon/base_stats/hitmonchan.asm
@@ -8,11 +8,8 @@
 	db 140 ; base exp
 	db TM_DYNAMICPUNCH, BLACKBELT_I ; items
 	db GENDER_F12_5 ; gender ratio
-	db 100 ; unknown 1
 	db 25 ; step cycles to hatch
-	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/hitmonchan/front.dimensions"
-	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_HUMANSHAPE, EGG_FAIRY ; egg groups
 
