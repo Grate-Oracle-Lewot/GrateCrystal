f94fd0a3bda120342ff0816cd9ca69effaf810ed
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2022-12-31 16:51:17 -0800

    Create second_pikachu_types.asm

---
+++ home/second_pikachu_types.asm
@@ -0,0 +1,10 @@
+SecondPikachuTypes:
+; entries correspond to PIKACHU_* constants
+  table_width 1, SecondPikachuTypes
+  db ELECTRIC ; PIKACHU_RETRO
+  db NORMAL   ; PIKACHU_SILVER
+  db FAIRY    ; PIKACHU_CRYSTAL
+  db FIGHTING ; PIKACHU_FIGHTING
+  db WATER    ; PIKACHU_SURFING
+  db FLYING   ; PIKACHU_FLYING
+  assert_table_length NUM_PIKACHU_FORMS + 1
