0cdc04c83fa85dba4c519288538b01ac6d3f20e5
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-01-02 03:54:18 -0800

    Update anim.asm

--- gfx/pokemon/pikachu_fighting/anim.asm
+++ gfx/pokemon/pikachu_fighting/anim.asm
@@ -1,10 +1,14 @@
 	frame 0, 05
 	frame 4, 10
 	frame 0, 05
-	setrepeat 2
 	frame 1, 04
 	frame 2, 04
 	frame 3, 04
-	dorepeat 2
+	frame 1, 04
+	frame 2, 04
+	frame 3, 04
+	frame 1, 04
+	frame 2, 04
+	frame 3, 04
 	frame 0, 02
 	endanim
