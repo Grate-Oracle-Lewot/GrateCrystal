7421c4e65fcdd655bbd6e9c56c70d8929a2b11d5
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-01-05 16:30:27 -0800

    Update BlackthornMart.asm

--- maps/BlackthornMart.asm
+++ maps/BlackthornMart.asm
@@ -1,5 +1,6 @@
 	object_const_def
 	const BLACKTHORNMART_CLERK
+	const BLACKTHORNMART_BIRD
 	const BLACKTHORNMART_COOLTRAINER_M
 	const BLACKTHORNMART_BLACK_BELT
 
@@ -13,6 +14,9 @@ BlackthornMartClerkScript:
 	pokemart MARTTYPE_STANDARD, MART_BLACKTHORN
 	closetext
 	end
+
+BlackthornMartDelibirdScript:
+	jumpstd MartDelibirdScript
 
 BlackthornMartCooltrainerMScript:
 	jumptextfaceplayer BlackthornMartCooltrainerMText
@@ -56,5 +60,6 @@ BlackthornMart_MapEvents:
 
 	def_object_events
 	object_event  1,  3, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_SILVER, OBJECTTYPE_SCRIPT, 0, BlackthornMartClerkScript, -1
+	object_event  1,  2, SPRITE_BIRD, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, BlackthornMartDelibirdScript, -1
 	object_event  7,  6, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 2, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, BlackthornMartCooltrainerMScript, -1
 	object_event  5,  2, SPRITE_BLACK_BELT, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, BlackthornMartBlackBeltScript, -1
