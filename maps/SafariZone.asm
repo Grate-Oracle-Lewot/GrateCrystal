1f7bcd5dcef5bf0921e5e2f44ba060e3281eb49b
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-01-05 20:23:13 -0800

    Update SafariZone.asm

--- maps/SafariZone.asm
+++ maps/SafariZone.asm
@@ -3,6 +3,8 @@
 	const SAFARIZONE_BIRD2
 	const SAFARIZONE_LAPRAS
 	const SAFARIZONE_CHANSEY
+	const SAFARIZONE_FLOWER
+	const SAFARIZONE_FISH
 	const SAFARIZONE_POKE_BALL1
 	const SAFARIZONE_POKE_BALL2
 
@@ -47,6 +49,24 @@ SafariZoneBlisseyScript:
 	reloadmapafterbattle
 	end
 
+SafariZoneSunfloraScript:
+	waitsfx
+	cry SUNFLORA
+	loadwildmon SUNFLORA, 54
+	startbattle
+	disappear SAFARIZONE_FLOWER
+	reloadmapafterbattle
+	end
+
+SafariZoneMagikarpScript:
+	waitsfx
+	cry MAGIKARP
+	loadwildmon MAGIKARP, 53
+	startbattle
+	disappear SAFARIZONE_FISH
+	reloadmapafterbattle
+	end
+
 SafariZoneThickClub:
 	itemball THICK_CLUB
 
@@ -73,5 +93,7 @@ SafariZone_MapEvents:
 	object_event 32,  6, SPRITE_BIRD, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, SafariZoneXatuScript, EVENT_SAFARI_ZONE_XATU
 	object_event 19,  8, SPRITE_SURF, SPRITEMOVEDATA_SWIM_WANDER, 1, 1, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, SafariZoneLaprasScript, EVENT_SAFARI_ZONE_LAPRAS
 	object_event 52, 12, SPRITE_CHANSEY, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_PINK, OBJECTTYPE_SCRIPT, 0, SafariZoneBlisseyScript, EVENT_SAFARI_ZONE_BLISSEY
+	object_event 11, 31, SPRITE_FLOWER, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, SafariZoneSunfloraScript, EVENT_SAFARI_ZONE_SUNFLORA
+	object_event 29, 29, SPRITE_FISH, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, SafariZoneMagikarpScript, EVENT_SAFARI_ZONE_MAGIKARP
 	object_event  0, 12, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SafariZoneThickClub, EVENT_SAFARI_ZONE_THICK_CLUB
 	object_event 51, 33, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SafariZoneGoldTeeth, EVENT_SAFARI_ZONE_GOLD_TEETH
