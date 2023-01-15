638746bac941ebe8a8c2ea9d3cc44244fe1dd42c
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-01-15 15:49:51 -0800

    Update anim.asm

--- gfx/pokemon/castaweigh/anim.asm
+++ gfx/pokemon/castaweigh/anim.asm
@@ -1,1 +1,16 @@
+	frame 0, 02
+	frame 2, 08
+	frame 0, 10
+	frame 2, 12
+	frame 0, 08
+	frame 2, 42
+	frame 0, 05
+	frame 1, 05
+	frame 0, 04
+	frame 1, 05
+	frame 0, 04
+	frame 1, 05
+	frame 0, 04
+	frame 1, 05
+	frame 0, 02
 	endanim
