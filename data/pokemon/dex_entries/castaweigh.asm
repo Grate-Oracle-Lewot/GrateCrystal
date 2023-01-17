5ab47af6f51ef4e1b33069f6fb9cf3245b98f55d
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-01-17 02:47:47 -0800

    Create castaweigh.asm

---
+++ data/pokemon/dex_entries/castaweigh.asm
@@ -0,0 +1,10 @@
+	db "@" ; species name
+	dw 421, 8421 ; height, weight
+
+	db   "They're thought to"
+	next "be vengeful spir-"
+	next "its of those lost"
+
+	page "at sea. They will"
+	next "eat anything."
+	next "@"
