97225781f83ff70b6035b96b1605ed7c2b35aef2
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-01-17 03:13:09 -0800

    Create cottonee.asm

---
+++ data/pokemon/dex_entries/cottonee.asm
@@ -0,0 +1,10 @@
+	db "COTTONPUFF@" ; species name
+	dw 100, 013 ; height, weight
+
+	db   "A kind COTTONEE"
+	next "may offer itself"
+	next "as a pillow to a"
+
+	page "weary traveler."
+	next "Bold travelers"
+	next "force the issue.@"
