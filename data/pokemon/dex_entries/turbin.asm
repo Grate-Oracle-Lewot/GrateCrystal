6c9cd99f72fb2fb2e72ebf332f66dd2111076e60
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-01-17 02:33:18 -0800

    Create turbin.asm

---
+++ data/pokemon/dex_entries/turbin.asm
@@ -0,0 +1,10 @@
+	db "COIL SHELL@" ; species name
+	dw 411, 2920 ; height, weight
+
+	db   "SHELLDER only evo-"
+	next "lves into TURBIN"
+	next "when it bites a"
+
+	page "SLOWPOKE, so it's"
+	next "never seen on its"
+	next "own in the wild.@"
