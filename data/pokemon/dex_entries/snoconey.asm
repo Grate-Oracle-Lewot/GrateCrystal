4537406824b5279abf98e6a6513ded7722171dd2
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-01-17 02:43:41 -0800

    Create snoconey.asm

---
+++ data/pokemon/dex_entries/snoconey.asm
@@ -0,0 +1,10 @@
+	db "YUKI USAGI@" ; species name
+	dw 108, 210 ; height, weight
+
+	db   "It's known to build"
+	next "sculptures of it-"
+	next "self out of snow"
+
+	page "to confound preda-"
+	next "tors. It's fast on"
+	next "two stubby feet.@"
