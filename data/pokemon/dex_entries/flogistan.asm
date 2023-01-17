c11521e14549f7f5d2c8a2a9602a198a6b02fc7c
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-01-17 03:03:24 -0800

    Create flogistan.asm

---
+++ data/pokemon/dex_entries/flogistan.asm
@@ -0,0 +1,10 @@
+	db "CRACKLING@" ; species name
+	dw 211, 384 ; height, weight
+
+	db   "It continually"
+	next "gathers sticks to"
+	next "keep the fire in"
+
+	page "its backpack burn-"
+	next "ing. With this it"
+	next "cooks people.@"
