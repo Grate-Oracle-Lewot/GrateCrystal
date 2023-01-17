66cb8257822832bc1f05c0d9a05c9c34c2bee7ae
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-01-16 22:43:05 -0800

    Update anim.asm

--- gfx/pokemon/snoconey/anim.asm
+++ gfx/pokemon/snoconey/anim.asm
@@ -2,10 +2,11 @@
 	frame 1, 21
 	frame 0, 08
 	frame 2, 08
+	frame 0, 07
+	frame 2, 07
 	frame 0, 08
 	frame 2, 08
-	frame 0, 08
-	frame 2, 08
-	frame 0, 08
-	frame 2, 08
+	frame 0, 07
+	frame 2, 07
+	frame 0, 05
 	endanim
