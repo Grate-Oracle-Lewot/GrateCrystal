7bccb3675c3e7502b7306ae3ff20e74ca58fabb9
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-01-17 02:51:02 -0800

    Create finull.asm

---
+++ data/pokemon/dex_entries/finull.asm
@@ -0,0 +1,10 @@
+	db "NIHILISM@" ; species name
+	dw 200, 110 ; height, weight
+
+	db   "CELEBI lived past"
+	next "the end of time"
+	next "and returned as"
+
+	page "FINULL. It's lost"
+	next "all motivation and"
+	next "hardly moves.@"
