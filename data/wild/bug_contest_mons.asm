d50b4e33121fc97070aaf09e966304781cab2875
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-01-18 22:48:42 -0800

    Update bug_contest_mons.asm

--- data/wild/bug_contest_mons.asm
+++ data/wild/bug_contest_mons.asm
@@ -2,16 +2,17 @@ ContestMons:
 	;   %, species,   min, max
 	db 10, CATERPIE,    1, 20
 	db 10, WEEDLE,      1, 20
-	db  9, LEDYBA,      5, 20
-	db  9, SPINARAK,    5, 20
+	db  5, METAPOD,     5, 20
+	db  5, KAKUNA,      5, 20
 	db  9, VENONAT,     5, 20
 	db  9, PARAS,       5, 20
-	db  6, METAPOD,     5, 20
-	db  6, KAKUNA,      5, 20
+	db  8, LEDYBA,      5, 20
+	db  8, SPINARAK,    5, 20
 	db  6, YANMA,       5, 20
-	db  6, HERACROSS,   5, 20
+	db  5, HERACROSS,   5, 20
+	db  5, JIRK,        1, 20
 	db  5, BUTTERFREE, 10, 20
 	db  5, BEEDRILL,   10, 20
 	db  5, SCYTHER,    10, 20
 	db  5, PINSIR,     10, 20
-	db -1, PINECO,     10, 10
+	db -1, PINECO,     10, 10 ; end
