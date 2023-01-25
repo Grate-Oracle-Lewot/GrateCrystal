5a651a261b3b5a34adbd123a618682aed60cff70
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-01-25 00:38:47 -0800

    Update move_constants.asm

--- constants/move_constants.asm
+++ constants/move_constants.asm
@@ -144,7 +144,7 @@
 	const HI_JUMP_KICK ; 88
 	const GLARE        ; 89
 	const DREAM_EATER  ; 8a
-	const POISON_GAS   ; 8b
+	const DRAGON_PULSE ; 8b
 	const DAZZLY_GLEAM ; 8c
 	const LEECH_LIFE   ; 8d
 	const LOVELY_KISS  ; 8e
