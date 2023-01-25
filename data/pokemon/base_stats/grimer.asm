be0fb95d4aea670afacd2ce564838ed53fe492e4
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-01-25 00:04:35 -0800

    Update grimer.asm

--- data/pokemon/base_stats/grimer.asm
+++ data/pokemon/base_stats/grimer.asm
@@ -3,7 +3,7 @@
 	db  80,  80,  50,  25,  40,  50
 	;   hp  atk  def  spd  sat  sdf
 
-	db POISON, DARK ; type
+	db POISON, POISON ; type
 	db 190 ; catch rate
 	db 90 ; base exp
 	db TINYMUSHROOM, BIG_MUSHROOM ; items
