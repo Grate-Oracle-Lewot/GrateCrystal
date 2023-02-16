a0e8ef05c4ff72172bd770ac853395c67af4c80d
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-02-15 19:02:05 -0800

    Update animations.asm

--- data/moves/animations.asm
+++ data/moves/animations.asm
@@ -2421,13 +2421,17 @@ BattleAnim_IronHead:
 
 BattleAnim_LovelyKiss:
 	anim_3gfx ANIM_GFX_OBJECTS, ANIM_GFX_ANGELS, ANIM_GFX_BUBBLE
+	anim_bgeffect ANIM_BG_CYCLE_MID_OBPALS_GRAY_AND_YELLOW, $0, $2, $0
 	anim_obj ANIM_OBJ_LOVELY_KISS, 152, 40, $0
 	anim_wait 32
-	anim_sound 0, 0, SFX_SWEET_SCENT
+	anim_sound 0, 1, SFX_LICK
 	anim_obj ANIM_OBJ_HEART, 128, 40, $0
+	anim_wait 42
+	anim_clearobjs
+	anim_sound 0, 0, SFX_SWEET_SCENT
 	anim_call BattleAnimSub_Drain
 	anim_wait 128
-	anim_wait 42
+	anim_wait 48
 	anim_ret
 
 BattleAnim_Bonemerang:
