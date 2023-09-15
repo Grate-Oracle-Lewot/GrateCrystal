be703d5b1f9632b8a4f816c45d4e9858e09a9bd1
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-09-15 15:08:13 -0700

    Update focus_energy.asm

--- engine/battle/move_effects/focus_energy.asm
+++ engine/battle/move_effects/focus_energy.asm
@@ -11,41 +11,5 @@ BattleCommand_FocusEnergy:
 	jp StdBattleTextbox
 
 .already_pumped
-; new to Grate Crystal: act like Bulk Up (adapted from Curse)
-	ld bc, wPlayerStatLevels
-	ldh a, [hBattleTurn]
-	and a
-	jr z, .go
-	ld bc, wEnemyStatLevels
-
-.go
-; If no stats can be increased, don't.
-
-; Attack
-	ld a, [bc]
-	cp MAX_STAT_LEVEL
-	jr c, .raise
-
-; Defense
-	inc bc
-	ld a, [bc]
-	cp MAX_STAT_LEVEL
-	jr nc, .cantraise
-
-.raise
-; Raise Attack and Defense.
-	call AnimateCurrentMove
-	call ResetMiss
-	call BattleCommand_AttackUp
-	call BattleCommand_StatUpMessage
-	call ResetMiss
-	call BattleCommand_DefenseUp
-	jp BattleCommand_StatUpMessage
-
-.cantraise
-; Can't raise either stat.
-	ld b, ABILITY + 1
-	call GetStatName
 	call AnimateFailedMove
-	ld hl, WontRiseAnymoreText
-	jp StdBattleTextbox
+	jp PrintButItFailed
