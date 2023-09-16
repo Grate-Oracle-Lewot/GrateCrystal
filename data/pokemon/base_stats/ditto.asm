04fc60388fe9cc6c29578d00135f42ec4a6b7651
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-09-15 18:45:14 -0700

    Update ditto.asm

--- data/pokemon/base_stats/ditto.asm
+++ data/pokemon/base_stats/ditto.asm
@@ -6,7 +6,7 @@
 	db NORMAL, NORMAL ; type
 	db 35 ; catch rate
 	db 84 ; base exp
-	db PINK_BOW, EVERSTONE ; items
+	db EVERSTONE, TRIDENT ; items
 	db GENDER_UNKNOWN ; gender ratio
 	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/ditto/front.dimensions"
