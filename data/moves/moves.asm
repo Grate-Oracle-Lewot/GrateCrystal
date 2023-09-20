ad440ac421ffc063949780dde442754c35631663
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-09-19 17:22:05 -0700

    Update moves.asm

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -184,7 +184,7 @@ Moves:
 	move SPIDER_WEB,   EFFECT_MEAN_LOOK,           0, BUG,          STATUS,   100, 10,   0
 	move MIND_READER,  EFFECT_LOCK_ON,             0, PSYCHIC_TYPE, STATUS,   100,  5,   0
 	move NIGHTMARE,    EFFECT_NIGHTMARE,           0, GHOST,        STATUS,   100, 15,   0
-	move FLAME_WHEEL,  EFFECT_BURN_HIT,           60, FIRE,         PHYSICAL, 100, 25,  10
+	move FLAME_WHEEL,  EFFECT_FLAME_WHEEL,        60, FIRE,         PHYSICAL, 100, 20,   0
 	move SNORE,        EFFECT_SNORE,              40, NORMAL,       SPECIAL,  100, 20,  30
 	move CURSE,        EFFECT_CURSE,               0, GHOST,        STATUS,   100, 10,   0
 	move FLAIL,        EFFECT_REVERSAL,            1, NORMAL,       PHYSICAL, 100, 15,   0
