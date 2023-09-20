e917ce3a35bd7b3a687e67af81c8cd3bea965419
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-09-19 19:10:44 -0700

    Update Route30BerryHouse.asm

--- maps/Route30BerryHouse.asm
+++ maps/Route30BerryHouse.asm
@@ -13,7 +13,7 @@ Route30BerryHousePokefanMScript:
 	iftrue .GotBerry
 	writetext Route30BerrySpeechHouseMonEatBerriesText
 	promptbutton
-	verbosegiveitem X_EVADE
+	verbosegiveitem GOLD_BERRY
 	iffalse .NoRoom
 	setevent EVENT_GOT_BERRY_FROM_ROUTE_30_HOUSE
 .GotBerry:
