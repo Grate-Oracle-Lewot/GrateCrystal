84d18fb630ca959862b3173929d3678817acbe0c
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-09-22 18:25:13 -0700

    Update CeladonDeptStore4F.asm

--- maps/CeladonDeptStore4F.asm
+++ maps/CeladonDeptStore4F.asm
@@ -1,5 +1,7 @@
 	object_const_def
-	const CELADONDEPTSTORE4F_CLERK
+	const CELADONDEPTSTORE4F_CLERK1
+	const CELADONDEPTSTORE4F_CLERK2
+	const CELADONDEPTSTORE4F_CLERK3
 	const CELADONDEPTSTORE4F_SUPER_NERD
 	const CELADONDEPTSTORE4F_YOUNGSTER
 
@@ -8,10 +10,24 @@ CeladonDeptStore4F_MapScripts:
 
 	def_callbacks
 
-CeladonDeptStore4FClerkScript:
+CeladonDeptStore4FClerk1Script:
 	faceplayer
 	opentext
-	pokemart MARTTYPE_STANDARD, MART_CELADON_4F
+	pokemart MARTTYPE_STANDARD, MART_CELADON_4F_1
+	closetext
+	end
+
+CeladonDeptStore4FClerk2Script:
+	faceplayer
+	opentext
+	pokemart MARTTYPE_STANDARD, MART_CELADON_4F_2
+	closetext
+	end
+
+CeladonDeptStore4FClerk3Script:
+	faceplayer
+	opentext
+	pokemart MARTTYPE_STANDARD, MART_CELADON_4F_3
 	closetext
 	end
 
@@ -65,6 +81,8 @@ CeladonDeptStore4F_MapEvents:
 	bg_event  3,  0, BGEVENT_READ, CeladonDeptStore4FElevatorButton
 
 	def_object_events
-	object_event 13,  5, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, CeladonDeptStore4FClerkScript, -1
+	object_event 13,  5, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, CeladonDeptStore4FClerk1Script, -1
+	object_event 14,  5, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, CeladonDeptStore4FClerk2Script, -1
+	object_event 15,  5, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, CeladonDeptStore4FClerk3Script, -1
 	object_event  7,  6, SPRITE_SUPER_NERD, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, CeladonDeptStore4FSuperNerdScript, -1
 	object_event  8,  2, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CeladonDeptStore4FYoungsterScript, -1
