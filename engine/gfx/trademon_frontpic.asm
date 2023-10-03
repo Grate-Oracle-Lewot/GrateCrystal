af965699101b2c14e78a4abff09493c368c9594f
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-10-03 12:56:31 -0700

    Update trademon_frontpic.asm

--- engine/gfx/trademon_frontpic.asm
+++ engine/gfx/trademon_frontpic.asm
@@ -6,6 +6,8 @@ GetTrademonFrontpic:
 	push af
 	predef GetPikachuForm
 	ld hl, wOTTrademonDVs
+	predef GetPinsirGender
+	ld hl, wOTTrademonDVs
 	predef GetUnownLetter
 	pop af
 	ld [wCurPartySpecies], a
