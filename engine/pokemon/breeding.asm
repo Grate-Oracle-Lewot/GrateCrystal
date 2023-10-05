306a658b55eff2d1d9eb58112ea82ce77412cd63
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-10-04 19:41:21 -0700

    Update breeding.asm

--- engine/pokemon/breeding.asm
+++ engine/pokemon/breeding.asm
@@ -629,9 +629,6 @@ GetEggFrontpic:
 	predef GetPikachuForm
 	ld a, MON_DVS
 	call GetPartyParamLocation
-	predef GetPinsirGender
-	ld a, MON_DVS
-	call GetPartyParamLocation
 	predef GetUnownLetter
 	pop de
 	predef_jump GetMonFrontpic
@@ -644,9 +641,6 @@ GetHatchlingFrontpic:
 	ld a, MON_DVS
 	call GetPartyParamLocation
 	predef GetPikachuForm
-	ld a, MON_DVS
-	call GetPartyParamLocation
-	predef GetPinsirGender
 	ld a, MON_DVS
 	call GetPartyParamLocation
 	predef GetUnownLetter
