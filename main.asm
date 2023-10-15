2ac96a23bc070896e899e3e539e8de1d836b3cdc
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-10-15 13:53:40 -0700

    Update main.asm

--- main.asm
+++ main.asm
@@ -293,7 +293,6 @@ INCLUDE "engine/events/buena.asm"
 INCLUDE "engine/events/dratini.asm"
 INCLUDE "engine/events/battle_tower/rules.asm"
 INCLUDE "mobile/mobile_22_2.asm"
-INCLUDE "engine/events/pikachu.asm"
 
 
 SECTION "bank23", ROMX
