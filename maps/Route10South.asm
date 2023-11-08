dbed691c4267d6043ef5a52c1b2e4a946d50b9e4
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-11-07 17:49:12 -0800

    Update Route10South.asm

--- maps/Route10South.asm
+++ maps/Route10South.asm
@@ -69,8 +69,8 @@ PokefanmRobertAfterBattleText:
 Route10SignText:
 	text "ROUTE 10"
 
-	para "CERULEAN CITY -"
-	line "LAVENDER TOWN"
+	para "LAVENDER TOWN -"
+	line "CERULEAN CITY"
 	done
 
 Route10South_MapEvents:
