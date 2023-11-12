c1212ce307f3bd739185ebcefe8ba629b3de0858
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-11-11 17:54:12 -0800

    Update animations.asm

--- data/moves/animations.asm
+++ data/moves/animations.asm
@@ -2363,8 +2363,9 @@ BattleAnim_PetalDance:
 	anim_wait 11
 	anim_loop 8, .loop
 	anim_sound 0, 0, SFX_SKETCH
-	anim_wait 128
-	anim_wait 64
+	anim_wait 84
+	anim_sound 0, 0, SFX_SKETCH
+	anim_wait 108
 	anim_sound 0, 1, SFX_COMET_PUNCH
 	anim_obj ANIM_OBJ_HIT_BIG_YFIX, 136, 56, $0
 	anim_wait 16
