9187c91710b441a0f2d105dfec335696d1d15ecc
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-11-12 12:59:14 -0800

    Update effect_command_pointers.asm

--- data/battle/effect_command_pointers.asm
+++ data/battle/effect_command_pointers.asm
@@ -172,5 +172,4 @@ BattleCommandPointers:
 	dw BattleCommand_SuperEffectiveLoopText
 	dw BattleCommand_StartLoop
 	dw BattleCommand_Curl
-	dw BattleCommand_Dig
 	assert_table_length NUM_EFFECT_COMMANDS
