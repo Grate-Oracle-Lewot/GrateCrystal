cace71357c880530bbb8dbb8c79546e794996144
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-11-24 12:14:41 -0800

    Update pokepic.asm

--- engine/events/pokepic.asm
+++ engine/events/pokepic.asm
@@ -46,3 +46,30 @@ PokepicMenuHeader:
 	menu_coords 6, 4, 14, 13
 	dw NULL
 	db 1 ; default option
+
+Trainerpic::
+	ld hl, PokepicMenuHeader
+	call CopyMenuHeader
+	call MenuBox
+	call UpdateSprites
+	call ApplyTilemap
+	ld b, SCGB_POKEPIC
+	call GetSGBLayout
+	xor a
+	ldh [hBGMapMode], a
+	ld a, [wTrainerClass]
+	ld de, vTiles3 tile $80
+	farcall GetTrainerPic
+	ld a, [wMenuBorderTopCoord]
+	inc a
+	ld b, a
+	ld a, [wMenuBorderLeftCoord]
+	inc a
+	ld c, a
+	call Coord2Tile
+	ld a, $80
+	ldh [hGraphicStartTile], a
+	lb bc, 7, 7
+	predef PlaceGraphic
+	call WaitBGMap
+	ret
