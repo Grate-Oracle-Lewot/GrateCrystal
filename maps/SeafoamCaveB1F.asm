78fcace47a262465f87f17f2a05030f38224f445
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2023-11-27 17:22:44 -0800

    Create SeafoamCaveB1F.asm

---
+++ maps/SeafoamCaveB1F.asm
@@ -0,0 +1,19 @@
+	object_const_def
+
+SeafoamCaveB1F_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+SeafoamCaveB1F_MapEvents:
+	db 0, 0 ; filler
+
+	def_warp_events
+	warp_event 11, 16, SEAFOAM_CAVE_1F, 2
+	warp_event 24, 27, SEAFOAM_CAVE_B1F, 1
+
+	def_coord_events
+
+	def_bg_events
+
+	def_object_events
