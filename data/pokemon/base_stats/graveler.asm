42c77666d979a464a9367a50add3fce88752c547
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 14:59:43 -0800

    Update graveler.asm

--- data/pokemon/base_stats/graveler.asm
+++ data/pokemon/base_stats/graveler.asm
@@ -8,7 +8,7 @@
 	db 134 ; base exp
 	db EVERSTONE, KINGS_ROCK ; items
 	db GENDER_F50 ; gender ratio
-	db 15 ; step cycles to hatch
+	db 8 ; step cycles to hatch
 	INCBIN "gfx/pokemon/graveler/front.dimensions"
 	db GROWTH_SLIGHTLY_SLOW ; growth rate
 	dn EGG_MINERAL, EGG_HUMANSHAPE ; egg groups
