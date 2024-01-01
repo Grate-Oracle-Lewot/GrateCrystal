cc2d842caf1c4aaf0c5910811ccfee7856f0448f
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 15:28:31 -0800

    Update kingdra.asm

--- data/pokemon/base_stats/kingdra.asm
+++ data/pokemon/base_stats/kingdra.asm
@@ -8,7 +8,7 @@
 	db 207 ; base exp
 	db SMOKE_BALL, DRAGON_SCALE ; items
 	db GENDER_F50 ; gender ratio
-	db 20 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/kingdra/front.dimensions"
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_WATER_1, EGG_DRAGON ; egg groups
