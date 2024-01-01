9e6c88110da3a3476da40d5a03c1cc9e3ec9cbb1
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 15:54:32 -0800

    Update quagsire.asm

--- data/pokemon/base_stats/quagsire.asm
+++ data/pokemon/base_stats/quagsire.asm
@@ -8,7 +8,7 @@
 	db 137 ; base exp
 	db LIMBO_STICK, SOFT_SAND ; items
 	db GENDER_F50 ; gender ratio
-	db 20 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/quagsire/front.dimensions"
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_WATER_1, EGG_GROUND ; egg groups
