b0eb5dd6a4511ca7c415548a879e6200401a5bac
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 14:47:10 -0800

    Update bulbasaur.asm

--- data/pokemon/base_stats/bulbasaur.asm
+++ data/pokemon/base_stats/bulbasaur.asm
@@ -5,7 +5,7 @@
 
 	db GRASS, POISON ; type
 	db 45 ; catch rate
-	db 64 ; base exp
+	db 65 ; base exp
 	db GOLD_BERRY, MIRACLE_SEED ; items
 	db GENDER_F12_5 ; gender ratio
 	db 11 ; step cycles to hatch
