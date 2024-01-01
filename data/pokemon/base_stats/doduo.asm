b5007f2b494d43966b0ec36c645c0863ac39e9e7
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 15:07:57 -0800

    Update doduo.asm

--- data/pokemon/base_stats/doduo.asm
+++ data/pokemon/base_stats/doduo.asm
@@ -8,7 +8,7 @@
 	db 96 ; base exp
 	db SHARP_BEAK, EXP_SHARE ; items
 	db GENDER_F50 ; gender ratio
-	db 10 ; step cycles to hatch
+	db 11 ; step cycles to hatch
 	INCBIN "gfx/pokemon/doduo/front.dimensions"
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_FLYING, EGG_GROUND ; egg groups
