e35dd5629e36c37dcca1b0c3ed947c5ab320713c
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 14:56:14 -0800

    Update poliwrath.asm

--- data/pokemon/base_stats/poliwrath.asm
+++ data/pokemon/base_stats/poliwrath.asm
@@ -8,7 +8,7 @@
 	db 185 ; base exp
 	db WATER_STONE, KINGS_ROCK ; items
 	db GENDER_F50 ; gender ratio
-	db 20 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/poliwrath/front.dimensions"
 	db GROWTH_MEDIUM_SLOW ; growth rate
 	dn EGG_WATER_1, EGG_INDETERMINATE ; egg groups
