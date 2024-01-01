0db353a63294ead87f038415ded4a9e5a6921efa
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 14:38:53 -0800

    Update crobat.asm

--- data/pokemon/base_stats/crobat.asm
+++ data/pokemon/base_stats/crobat.asm
@@ -8,7 +8,7 @@
 	db 204 ; base exp
 	db BITTER_BERRY, DICTIONARY ; items
 	db GENDER_F50 ; gender ratio
-	db 15 ; step cycles to hatch
+	db 8 ; step cycles to hatch
 	INCBIN "gfx/pokemon/crobat/front.dimensions"
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_FLYING, EGG_GROUND ; egg groups
