6af56483d68df97856d18ce142382b46d676a2ff
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 15:19:56 -0800

    Update marowak.asm

--- data/pokemon/base_stats/marowak.asm
+++ data/pokemon/base_stats/marowak.asm
@@ -8,7 +8,7 @@
 	db 124 ; base exp
 	db THICK_CLUB, THICK_CLUB ; items
 	db GENDER_F50 ; gender ratio
-	db 20 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/marowak/front.dimensions"
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_MONSTER, EGG_GROUND ; egg groups
