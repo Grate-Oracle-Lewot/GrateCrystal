d2dcb107778ee67a8f69712be8ee57e64bc60d6c
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 15:31:33 -0800

    Update gyarados.asm

--- data/pokemon/base_stats/gyarados.asm
+++ data/pokemon/base_stats/gyarados.asm
@@ -8,7 +8,7 @@
 	db 214 ; base exp
 	db NO_ITEM, NO_ITEM ; items
 	db GENDER_F50 ; gender ratio
-	db 5 ; step cycles to hatch
+	db 3 ; step cycles to hatch
 	INCBIN "gfx/pokemon/gyarados/front.dimensions"
 	db GROWTH_SLOW ; growth rate
 	dn EGG_WATER_2, EGG_DRAGON ; egg groups
