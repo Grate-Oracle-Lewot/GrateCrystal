5c8565bd625ab0fedacb73933d2ca3dd56f4e419
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 15:52:10 -0800

    Update skiploom.asm

--- data/pokemon/base_stats/skiploom.asm
+++ data/pokemon/base_stats/skiploom.asm
@@ -8,7 +8,7 @@
 	db 136 ; base exp
 	db MIRACLEBERRY, MIRACLE_SEED ; items
 	db GENDER_F50 ; gender ratio
-	db 20 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/skiploom/front.dimensions"
 	db GROWTH_SLIGHTLY_SLOW ; growth rate
 	dn EGG_FAIRY, EGG_PLANT ; egg groups
