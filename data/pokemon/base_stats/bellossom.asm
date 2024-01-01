0212da34a4b3edfc981f6f5f5636c3d5ff699d39
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 14:40:31 -0800

    Update bellossom.asm

--- data/pokemon/base_stats/bellossom.asm
+++ data/pokemon/base_stats/bellossom.asm
@@ -8,7 +8,7 @@
 	db 184 ; base exp
 	db SUN_STONE, LEAF_STONE ; items
 	db GENDER_F50 ; gender ratio
-	db 20 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/bellossom/front.dimensions"
 	db GROWTH_SLIGHTLY_SLOW ; growth rate
 	dn EGG_PLANT, EGG_FAIRY ; egg groups
