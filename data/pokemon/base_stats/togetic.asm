7e27c15a813d59440b52c706650583f4e210fda7
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 15:48:27 -0800

    Update togetic.asm

--- data/pokemon/base_stats/togetic.asm
+++ data/pokemon/base_stats/togetic.asm
@@ -8,7 +8,7 @@
 	db 167 ; base exp
 	db MAX_REVIVE, PP_UP ; items
 	db GENDER_F12_5 ; gender ratio
-	db 10 ; step cycles to hatch
+	db 5 ; step cycles to hatch
 	INCBIN "gfx/pokemon/togetic/front.dimensions"
 	db GROWTH_FAST ; growth rate
 	dn EGG_FLYING, EGG_FAIRY ; egg groups
