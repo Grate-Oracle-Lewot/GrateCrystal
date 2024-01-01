4a01102b790993b92731e44d1b893b8068202c20
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 15:17:59 -0800

    Update kingler.asm

--- data/pokemon/base_stats/kingler.asm
+++ data/pokemon/base_stats/kingler.asm
@@ -8,7 +8,7 @@
 	db 206 ; base exp
 	db PROTEIN, PEARL ; items
 	db GENDER_F50 ; gender ratio
-	db 20 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/kingler/front.dimensions"
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_WATER_3, EGG_BUG ; egg groups
