b4daf8ddc0004e94b51761fbc0548111d519ce8e
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 15:12:18 -0800

    Update cloyster.asm

--- data/pokemon/base_stats/cloyster.asm
+++ data/pokemon/base_stats/cloyster.asm
@@ -8,7 +8,7 @@
 	db 203 ; base exp
 	db PEARL, BIG_PEARL ; items
 	db GENDER_F50 ; gender ratio
-	db 20 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/cloyster/front.dimensions"
 	db GROWTH_MEDIUM_SLOW ; growth rate
 	dn EGG_WATER_3, EGG_MINERAL ; egg groups
