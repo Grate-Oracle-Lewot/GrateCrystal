132a7e4feb815982fccc1b6250e3ae74ddd64e5a
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 14:33:08 -0800

    Update nidorina.asm

--- data/pokemon/base_stats/nidorina.asm
+++ data/pokemon/base_stats/nidorina.asm
@@ -8,7 +8,7 @@
 	db 117 ; base exp
 	db POISON_BARB, MOON_STONE ; items
 	db GENDER_F100 ; gender ratio
-	db 20 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/nidorina/front.dimensions"
 	db GROWTH_MEDIUM_SLOW ; growth rate
 	dn EGG_MONSTER, EGG_GROUND ; egg groups
