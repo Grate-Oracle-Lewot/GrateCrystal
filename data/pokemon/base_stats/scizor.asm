3e9e03ae64a38dd4c8572331d744545d9317d167
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 15:58:40 -0800

    Update scizor.asm

--- data/pokemon/base_stats/scizor.asm
+++ data/pokemon/base_stats/scizor.asm
@@ -8,7 +8,7 @@
 	db 200 ; base exp
 	db QUICK_CLAW, METAL_COAT ; items
 	db GENDER_F50 ; gender ratio
-	db 25 ; step cycles to hatch
+	db 13 ; step cycles to hatch
 	INCBIN "gfx/pokemon/scizor/front.dimensions"
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_BUG, EGG_DRAGON ; egg groups
