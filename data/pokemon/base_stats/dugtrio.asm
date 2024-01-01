a3a15a82ec0043709b87bae5a6f08fec7c5e0bc7
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 14:43:33 -0800

    Update dugtrio.asm

--- data/pokemon/base_stats/dugtrio.asm
+++ data/pokemon/base_stats/dugtrio.asm
@@ -8,7 +8,7 @@
 	db 153 ; base exp
 	db TM_DIG, TRIDENT ; items
 	db GENDER_F50 ; gender ratio
-	db 20 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/dugtrio/front.dimensions"
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_GROUND, EGG_INDETERMINATE ; egg groups
