5308769ef76bae654aeb6abb9c4674d889c40953
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 15:11:19 -0800

    Update muk.asm

--- data/pokemon/base_stats/muk.asm
+++ data/pokemon/base_stats/muk.asm
@@ -8,7 +8,7 @@
 	db 157 ; base exp
 	db BIG_MUSHROOM, NUGGET ; items
 	db GENDER_F50 ; gender ratio
-	db 20 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/muk/front.dimensions"
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_INDETERMINATE, EGG_MINERAL ; egg groups
