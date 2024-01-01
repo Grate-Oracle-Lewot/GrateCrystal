0d898c1a25a12c9fde140c8c8097248b340fd518
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 15:13:48 -0800

    Update gengar.asm

--- data/pokemon/base_stats/gengar.asm
+++ data/pokemon/base_stats/gengar.asm
@@ -8,7 +8,7 @@
 	db 190 ; base exp
 	db MAX_REVIVE, MOON_STONE ; items
 	db GENDER_F50 ; gender ratio
-	db 20 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/gengar/front.dimensions"
 	db GROWTH_MEDIUM_SLOW ; growth rate
 	dn EGG_INDETERMINATE, EGG_HUMANSHAPE ; egg groups
