8ed663d0e7f91c62d3cf746454de381e997d3859
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 14:37:21 -0800

    Update wigglytuff.asm

--- data/pokemon/base_stats/wigglytuff.asm
+++ data/pokemon/base_stats/wigglytuff.asm
@@ -8,7 +8,7 @@
 	db 109 ; base exp
 	db PINK_BOW, MOON_STONE ; items
 	db GENDER_F75 ; gender ratio
-	db 10 ; step cycles to hatch
+	db 5 ; step cycles to hatch
 	INCBIN "gfx/pokemon/wigglytuff/front.dimensions"
 	db GROWTH_FAST ; growth rate
 	dn EGG_FAIRY, EGG_INDETERMINATE ; egg groups
