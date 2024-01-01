d7de3fc2d4792913dc5bffa826ece855d8cc09d6
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 15:59:15 -0800

    Update ursaring.asm

--- data/pokemon/base_stats/ursaring.asm
+++ data/pokemon/base_stats/ursaring.asm
@@ -8,7 +8,7 @@
 	db 200 ; base exp
 	db BERRY, LEFTOVERS ; items
 	db GENDER_F50 ; gender ratio
-	db 20 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/ursaring/front.dimensions"
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_GROUND, EGG_HUMANSHAPE ; egg groups
