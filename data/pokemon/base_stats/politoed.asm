dfc404d0df660825c057cc06157b91fa83113c56
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 14:54:59 -0800

    Update politoed.asm

--- data/pokemon/base_stats/politoed.asm
+++ data/pokemon/base_stats/politoed.asm
@@ -8,7 +8,7 @@
 	db 185 ; base exp
 	db SPELL_TAG, KINGS_ROCK ; items
 	db GENDER_F50 ; gender ratio
-	db 20 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/politoed/front.dimensions"
 	db GROWTH_MEDIUM_SLOW ; growth rate
 	dn EGG_WATER_1, EGG_INDETERMINATE ; egg groups
