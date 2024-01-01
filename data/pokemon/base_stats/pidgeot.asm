fa25151bc85d23376ea72bb4e2ef1a82ea211623
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 14:19:36 -0800

    Update pidgeot.asm

--- data/pokemon/base_stats/pidgeot.asm
+++ data/pokemon/base_stats/pidgeot.asm
@@ -8,7 +8,7 @@
 	db 172 ; base exp
 	db TM_RETURN, TM_SWIFT ; items
 	db GENDER_F50 ; gender ratio
-	db 15 ; step cycles to hatch
+	db 8 ; step cycles to hatch
 	INCBIN "gfx/pokemon/pidgeot/front.dimensions"
 	db GROWTH_SLIGHTLY_SLOW ; growth rate
 	dn EGG_FLYING, EGG_GROUND ; egg groups
