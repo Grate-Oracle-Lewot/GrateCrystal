c91646736b10b3107733b49b4ef8e2ec08a10f28
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 14:22:23 -0800

    Update raichu.asm

--- data/pokemon/base_stats/raichu.asm
+++ data/pokemon/base_stats/raichu.asm
@@ -8,7 +8,7 @@
 	db 122 ; base exp
 	db BERRY, LIGHT_BALL ; items
 	db GENDER_F50 ; gender ratio
-	db 10 ; step cycles to hatch
+	db 5 ; step cycles to hatch
 	INCBIN "gfx/pokemon/raichu/front.dimensions"
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_GROUND, EGG_FAIRY ; egg groups
