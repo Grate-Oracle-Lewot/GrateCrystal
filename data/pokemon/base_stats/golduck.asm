694fa35d6ea5665354aae2f2617c54c05d088305
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 14:52:09 -0800

    Update golduck.asm

--- data/pokemon/base_stats/golduck.asm
+++ data/pokemon/base_stats/golduck.asm
@@ -8,7 +8,7 @@
 	db 174 ; base exp
 	db TM_PSYCHIC_M, FRESH_WATER ; items
 	db GENDER_F50 ; gender ratio
-	db 20 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/golduck/front.dimensions"
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_WATER_1, EGG_FLYING ; egg groups
