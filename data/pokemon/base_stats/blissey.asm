0ac2c76969f17ea0a1e894a2187cf19a04ba3ef0
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 16:03:19 -0800

    Update blissey.asm

--- data/pokemon/base_stats/blissey.asm
+++ data/pokemon/base_stats/blissey.asm
@@ -8,7 +8,7 @@
 	db 255 ; base exp
 	db LUCKY_EGG, LUCKY_PUNCH ; items
 	db GENDER_F100 ; gender ratio
-	db 40 ; step cycles to hatch
+	db 20 ; step cycles to hatch
 	INCBIN "gfx/pokemon/blissey/front.dimensions"
 	db GROWTH_FAST ; growth rate
 	dn EGG_FAIRY, EGG_HUMANSHAPE ; egg groups
