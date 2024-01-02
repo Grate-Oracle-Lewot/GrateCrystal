dd1f64355bd16ca17a1beb7dec2ac8e0e740ea6d
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 19:23:52 -0800

    Update scripts.asm

--- data/maps/scripts.asm
+++ data/maps/scripts.asm
@@ -290,11 +290,7 @@ INCLUDE "maps/Route5CleanseTagHouse.asm"
 SECTION "Map Scripts 16", ROMX
 
 INCLUDE "maps/PewterCity.asm"
-INCLUDE "maps/WhirlIslandNW.asm"
-INCLUDE "maps/WhirlIslandNE.asm"
-INCLUDE "maps/WhirlIslandSW.asm"
-INCLUDE "maps/WhirlIslandCave.asm"
-INCLUDE "maps/WhirlIslandSE.asm"
+INCLUDE "maps/WhirlIsland1F.asm"
 INCLUDE "maps/WhirlIslandB1F.asm"
 INCLUDE "maps/WhirlIslandB2F.asm"
 INCLUDE "maps/WhirlIslandLugiaChamber.asm"
