75061ed3d4847531786d5d19ccb9e2d0351674c8
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 18:37:46 -0800

    Create WhirlIsland1F.asm

---
+++ maps/WhirlIsland1F.asm
@@ -0,0 +1,19 @@
+WhirlIsland1F_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+WhirlIsland1F_MapEvents:
+	db 0, 0 ; filler
+
+	def_warp_events
+	warp_event  5,  7, ROUTE_41, 1
+	warp_event  5,  3, WHIRL_ISLAND_B1F, 1
+	warp_event  3, 15, WHIRL_ISLAND_SW, 4
+	warp_event  7, 15, WHIRL_ISLAND_CAVE, 2
+
+	def_coord_events
+
+	def_bg_events
+
+	def_object_events
