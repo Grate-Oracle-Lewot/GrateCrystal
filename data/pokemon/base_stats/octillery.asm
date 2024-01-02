b250b3ab0b4899b4c6dcfb415a4610f99535fc7d
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 16:01:33 -0800

    Update octillery.asm

--- data/pokemon/base_stats/octillery.asm
+++ data/pokemon/base_stats/octillery.asm
@@ -8,7 +8,7 @@
 	db 164 ; base exp
 	db HP_UP, X_ACCURACY ; items
 	db GENDER_F50 ; gender ratio
-	db 20 ; step cycles to hatch
+	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/octillery/front.dimensions"
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_WATER_1, EGG_WATER_2 ; egg groups
