c39cd152783db1eb7b81886f01ab31e9e9325040
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-01 16:10:58 -0800

    Update espeon.asm

--- data/pokemon/base_stats/espeon.asm
+++ data/pokemon/base_stats/espeon.asm
@@ -8,7 +8,7 @@
 	db 197 ; base exp
 	db SUN_STONE, SUN_STONE ; items
 	db GENDER_F12_5 ; gender ratio
-	db 18 ; step cycles to hatch
+	db 19 ; step cycles to hatch
 	INCBIN "gfx/pokemon/espeon/front.dimensions"
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_GROUND, EGG_FAIRY ; egg groups
