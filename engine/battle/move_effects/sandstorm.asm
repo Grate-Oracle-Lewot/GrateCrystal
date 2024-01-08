b9e2fad02d3c963a89601e1ac4c62390596b8e86
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-07 20:40:54 -0800

    Update sandstorm.asm

--- engine/battle/move_effects/sandstorm.asm
+++ engine/battle/move_effects/sandstorm.asm
@@ -1,10 +1,4 @@
 BattleCommand_StartSandstorm:
-; startsandstorm
-
-	ld a, [wBattleWeather]
-	cp WEATHER_SANDSTORM
-	jr z, .failed
-
 	ld a, WEATHER_SANDSTORM
 	ld [wBattleWeather], a
 	ld a, 5
@@ -12,7 +6,3 @@ BattleCommand_StartSandstorm:
 	call AnimateCurrentMove
 	ld hl, SandstormBrewedText
 	jp StdBattleTextbox
-
-.failed
-	call AnimateFailedMove
-	jp PrintButItFailed
