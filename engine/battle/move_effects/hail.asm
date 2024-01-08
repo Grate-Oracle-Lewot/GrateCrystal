370cdce452b05eb58d3364cf17f96ffbf0bc02cd
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-07 20:41:21 -0800

    Update hail.asm

--- engine/battle/move_effects/hail.asm
+++ engine/battle/move_effects/hail.asm
@@ -1,10 +1,4 @@
 BattleCommand_StartHail:
-; starthail
-
-	ld a, [wBattleWeather]
-	cp WEATHER_HAIL
-	jr z, .failed
-
 	ld a, WEATHER_HAIL
 	ld [wBattleWeather], a
 	ld a, 5
@@ -12,7 +6,3 @@ BattleCommand_StartHail:
 	call AnimateCurrentMove
 	ld hl, ItStartedToHailText
 	jp StdBattleTextbox
-
-.failed
-	call AnimateFailedMove
-	jp PrintButItFailed
