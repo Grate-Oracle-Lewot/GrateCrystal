9afbdc2c69033a20a6c46511d7ade05bc76e5451
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-01-09 13:38:27 -0800

    Update item_effects.asm

--- engine/items/item_effects.asm
+++ engine/items/item_effects.asm
@@ -535,6 +535,11 @@ PokeBallEffect:
 	call CheckReceivedDex
 	jr z, .skip_pokedex
 
+; skip new dex entry in bug contest, because Nayru's dex screws up the caught mon comparison display
+	ld a, [wBattleType]
+	cp BATTLETYPE_CONTEST
+	jp z, .catch_bug_contest_mon
+
 	ld hl, NewDexDataText
 	call PrintText
 
@@ -546,8 +551,6 @@ PokeBallEffect:
 
 .skip_pokedex
 	ld a, [wBattleType]
-	cp BATTLETYPE_CONTEST
-	jp z, .catch_bug_contest_mon
 	cp BATTLETYPE_CELEBI
 	jr nz, .not_celebi
 	ld hl, wBattleResult
