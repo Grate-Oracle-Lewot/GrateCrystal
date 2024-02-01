4822f6d513d0cc258f22e4feb67fe5a8709eea34
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-02-01 13:35:34 -0800

    Update tilemap.asm

--- home/tilemap.asm
+++ home/tilemap.asm
@@ -58,12 +58,6 @@ CGBOnly_CopyTilemapAtOnce::
 	jr z, WaitBGMap
 
 CopyTilemapAtOnce::
-	jr _CopyTilemapAtOnce
-
-CopyAttrmapAndTilemapToWRAMBank3: ; unreferenced
-	farcall HDMATransferAttrmapAndTilemapToWRAMBank3
-	ret
-
 _CopyTilemapAtOnce:
 	ldh a, [hBGMapMode]
 	push af
