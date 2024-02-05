66c8e81c74afff0a768e8ec90dca0d8566b81841
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-02-04 16:17:42 -0800

    Update magneton.asm

--- data/pokemon/dex_entries/magneton.asm
+++ data/pokemon/dex_entries/magneton.asm
@@ -1,8 +1,8 @@
 	db "MAGNET@" ; species name
 	dw 303, 1320 ; height, weight
 
-	db   "When a MAGNEMITE"
-	next "is ready to evolve"
+	db   "When a MAGNEMITE's"
+	next "ready to evolve,"
 	next "its magnetic field"
 
 	page "pulls in two other"
