d651496a6fb8b40c3ae50793a8518131b4b558d5
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-02-05 13:21:35 -0800

    Create egg.asm

---
+++ data/pokemon/base_stats/egg.asm
@@ -0,0 +1,18 @@
+	db EGG ; 254
+
+	db  10,  10,  10,  10,  10,  10
+	;   hp  atk  def  spd  sat  sdf
+
+	db CURSE_TYPE, CURSE_TYPE ; type
+	db 255 ; catch rate
+	db 10 ; base exp
+	db NO_ITEM, NO_ITEM ; items
+	db GENDER_F50 ; gender ratio
+	db 10 ; step cycles to hatch
+	INCBIN "gfx/pokemon/egg/front.dimensions"
+	db GROWTH_SLOW ; growth rate
+	dn EGG_NONE, EGG_NONE ; egg groups
+
+	; tm/hm learnset
+	tmhm
+	; end
