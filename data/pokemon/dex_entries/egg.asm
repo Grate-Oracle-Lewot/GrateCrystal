d948c5a3e718d0cd9eee1c95007a37cae62cbca5
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-02-05 13:24:10 -0800

    Create egg.asm

---
+++ data/pokemon/dex_entries/egg.asm
@@ -0,0 +1,10 @@
+	db "EGG@" ; species name
+	dw 0, 0 ; height, weight
+
+	db   "A #MON EGG"
+	next "that has yet to"
+	next "hatch."
+
+	page "No one knows what"
+	next "will emerge until"
+	next "it does.@"
