f98a631b2d3f30248de471f1194f75a8fbe54191
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-02-12 13:42:15 -0800

    Update Pokecenter2F.asm

--- maps/Pokecenter2F.asm
+++ maps/Pokecenter2F.asm
@@ -323,14 +323,12 @@ LinkReceptionistScript_GenderSwap:
 	checkflag ENGINE_PLAYER_IS_FEMALE
 	iftrue .FTMRight
 	setflag ENGINE_PLAYER_IS_FEMALE
-	setflag PLAYERGENDER_FEMALE_F
 	clearflag ENGINE_KRIS_IN_CABLE_CLUB
 	callasm .SetWRAMFemale
 	setval (PAL_NPC_BLUE << 4)
 	sjump .DoneRight
 .FTMRight:
 	clearflag ENGINE_PLAYER_IS_FEMALE
-	clearflag PLAYERGENDER_FEMALE_F
 	setflag ENGINE_KRIS_IN_CABLE_CLUB
 	callasm .SetWRAMMale
 	setval (PAL_NPC_RED << 4)
@@ -353,14 +351,12 @@ LinkReceptionistScript_GenderSwap:
 	checkflag ENGINE_PLAYER_IS_FEMALE
 	iftrue .FTMLeft
 	setflag ENGINE_PLAYER_IS_FEMALE
-	setflag PLAYERGENDER_FEMALE_F
 	clearflag ENGINE_KRIS_IN_CABLE_CLUB
 	callasm .SetWRAMFemale
 	setval (PAL_NPC_BLUE << 4)
 	sjump .DoneLeft
 .FTMLeft:
 	clearflag ENGINE_PLAYER_IS_FEMALE
-	clearflag PLAYERGENDER_FEMALE_F
 	setflag ENGINE_KRIS_IN_CABLE_CLUB
 	callasm .SetWRAMMale
 	setval (PAL_NPC_RED << 4)
