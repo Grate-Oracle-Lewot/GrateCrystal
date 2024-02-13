a7ac8d1970c5e5c59b8e118d50645968bac7a67d
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-02-12 23:25:49 -0800

    Update Pokecenter2F.asm

--- maps/Pokecenter2F.asm
+++ maps/Pokecenter2F.asm
@@ -299,7 +299,6 @@ LinkReceptionistScript_Battle:
 	end
 
 LinkReceptionistScript_GenderSwap:
-	sjump Script_TimeCapsuleClosed ; dummied out for now
 	faceplayer
 	opentext
 	writetext Text_AskSwapGender
