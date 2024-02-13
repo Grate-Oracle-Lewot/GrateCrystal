520a42a22141c5dfc78fdec319eabaf2ec6017cc
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-02-12 16:55:16 -0800

    Update ElmsLab.asm

--- maps/ElmsLab.asm
+++ maps/ElmsLab.asm
@@ -234,6 +234,7 @@ ChikoritaPokeBallScript:
 	playsound SFX_CAUGHT_MON
 	waitsfx
 	promptbutton
+	setfalg ENGINE_PLAINBADGE
 	givepoke MEGANIUM, 100, GOLD_BERRY
 	closetext
 	applymovement PLAYER, AfterChikoritaMovement
