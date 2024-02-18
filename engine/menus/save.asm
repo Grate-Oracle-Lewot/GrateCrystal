ed69e6f2b0765e03054fbdaab85b8e68a96639c7
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-02-18 13:25:22 -0800

    Update save.asm

--- engine/menus/save.asm
+++ engine/menus/save.asm
@@ -816,19 +816,55 @@ _SaveData:
 	call CopyBytes
 	call CloseSRAM
 
-	farcall StagePartyDataForMysteryGift
 	ld a, BANK(sMysteryGiftData)
 	call OpenSRAM
 	ld a, TRUE
 	ld [sMysteryGiftTrainerHouseFlag], a
-	ld hl, wMysteryGiftStaging
-	ld de, sMysteryGiftTrainer
-	ld bc, wMysteryGiftTrainerEnd - wMysteryGiftTrainer
-	call CopyBytes
 	ld hl, wPlayerName
 	ld de, sMysteryGiftPartnerName
 	ld bc, NAME_LENGTH
 	call CopyBytes
+	ld hl, wPartyMons
+	ld de, sMysteryGiftTrainer
+	ld bc, PARTY_LENGTH
+.loop
+	ld a, [hli]
+	cp -1
+	jr z, .party_end
+	cp EGG
+	jr z, .next
+	push hl
+	; copy level
+	ld hl, MON_LEVEL
+	add hl, bc
+	ld a, [hl]
+	ld [de], a
+	inc de
+	; copy species
+	ld hl, MON_SPECIES
+	add hl, bc
+	ld a, [hl]
+	ld [de], a
+	inc de
+	; copy moves
+	ld hl, MON_MOVES
+	add hl, bc
+	push bc
+	ld bc, NUM_MOVES
+	call CopyBytes
+	pop bc
+	pop hl
+.next
+	push hl
+	ld hl, PARTYMON_STRUCT_LENGTH
+	add hl, bc
+	ld b, h
+	ld c, l
+	pop hl
+	jr .loop
+.party_end
+	ld a, -1
+	ld [de], a
 	jp CloseSRAM
 
 _SaveBackupData:
