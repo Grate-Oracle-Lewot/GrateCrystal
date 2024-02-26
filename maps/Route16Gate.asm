f937154382ee13deb06cc9a4b89863a4c1883d5a
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-02-26 14:32:29 -0800

    Update Route16Gate.asm

--- maps/Route16Gate.asm
+++ maps/Route16Gate.asm
@@ -3,8 +3,12 @@
 
 Route16Gate_MapScripts:
 	def_scene_scripts
+	scene_script .DummyScene ; SCENE_DEFAULT
 
 	def_callbacks
+
+.DummyScene:
+	end
 
 Route16GateOfficerScript:
 	jumptextfaceplayer Route16GateOfficerText
