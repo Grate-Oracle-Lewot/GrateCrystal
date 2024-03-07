170b3b63cd17dd7e32c6206e6e80e135cd587779
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-03-06 19:08:50 -0800

    Update CianwoodPhotoStudio.asm

--- maps/CianwoodPhotoStudio.asm
+++ maps/CianwoodPhotoStudio.asm
@@ -25,6 +25,9 @@ CianwoodPhotoStudioFishingGuruScript:
 	closetext
 	end
 
+CianwoodPhotoStudioLandscapeShot:
+	jumptext CianwoodPhotoStudioLandscapeShotText
+
 CianwoodPhotoStudioFishingGuruText_Question:
 	text "You have magnifi-"
 	line "cent #MON with"
@@ -46,6 +49,11 @@ CianwoodPhotoStudioFishingGuruText_No:
 	line "memento…"
 	done
 
+CianwoodPhotoStudioLandscapeShotText:
+	text "It's a beautiful"
+	line "landscape shot."
+	done
+
 CianwoodPhotoStudio_MapEvents:
 	db 0, 0 ; filler
 
@@ -57,6 +65,8 @@ CianwoodPhotoStudio_MapEvents:
 	def_coord_events
 
 	def_bg_events
+	bg_event  3,  0, BGEVENT_READ, CianwoodPhotoStudioLandscapeShot
+	bg_event  5,  0, BGEVENT_READ, CianwoodPhotoStudioLandscapeShot
 
 	def_object_events
 	object_event  3,  5, SPRITE_FISHING_GURU, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, CianwoodPhotoStudioFishingGuruScript, -1
