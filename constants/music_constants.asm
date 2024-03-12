b88cf0fda5773dff2561cb47a5e3cb671c6aa9a5
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-03-12 13:45:47 -0700

    Update music_constants.asm

--- constants/music_constants.asm
+++ constants/music_constants.asm
@@ -116,6 +116,7 @@
 	const MUSIC_MEWTWO_BATTLE                ; 6e
 	const MUSIC_MASKED_RIDER                 ; 6f
 	const MUSIC_POINT_BREAK                  ; 70
+	const MUSIC_JESSIE_JAMES_ENCOUNTER       ; 71
 NUM_MUSIC_SONGS EQU const_value
 
 ; GetMapMusic picks music for these values (see home/map.asm)
