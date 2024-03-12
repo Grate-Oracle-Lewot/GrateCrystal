28c4b8e430f2464d36bee6062d0378d4cf2a4081
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-03-12 14:05:27 -0700

    Update pnp_hidden_people.asm

--- data/radio/pnp_hidden_people.asm
+++ data/radio/pnp_hidden_people.asm
@@ -2,18 +2,16 @@
 
 PnP_HiddenPeople:
 	db POKEMON_PROF
-	db WILL
-	db BRUNO
-	db KAREN
-	db KOGA
-	db CHAMPION
-	db BOSS
+	db TWINS
 	db JESSIE
 	db AEROBONES
 	db KABUBONES
 	db MISSINGNO
+	db CAL
+	db CAL_F
 	; fallthrough
 PnP_HiddenPeople_BeatE4:
+	db CHAMPION
 	db BROCK
 	db MISTY
 	db LT_SURGE
@@ -22,13 +20,19 @@ PnP_HiddenPeople_BeatE4:
 	db SABRINA
 	db BLAINE
 	db BLUE
+	db WILL
+	db BRUNO
+	db KOGA
+	db BOSS
 	db REAL_KAREN
 	db REAL_KOGA
+	db RIVAL1
 	; fallthrough
 PnP_HiddenPeople_BeatKanto:
-	db RIVAL1
-	db CAL
 	db RIVAL2
+	db LORELEI
+	db AGATHA
+	db REAL_BRUNO
+	db REAL_WILL
 	db RED
-	db CAL_F
 	db -1
