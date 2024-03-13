2f3795ccddab973c75ed771f6c25c2e3a71ee88d
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-03-12 20:13:59 -0700

    Update FuchsiaGym.asm

--- maps/FuchsiaGym.asm
+++ maps/FuchsiaGym.asm
@@ -24,6 +24,7 @@ FuchsiaGymJanineScript:
 	loadtrainer JANINE, JANINE1
 	startbattle
 	reloadmapafterbattle
+	clearevent EVENT_DARKROOM_GAUNTLET
 	setmapscene CIANWOOD_DARKROOM, SCENE_CIANWOODDARKROOM_RESET_GAUNTLET
 	setevent EVENT_BEAT_JANINE
 	special LoadUsedSpritesGFX
