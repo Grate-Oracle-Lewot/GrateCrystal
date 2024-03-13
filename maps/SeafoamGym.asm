d71ef8153bb606bd06e9a49c0a13af3bc8963cf5
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-03-12 19:25:14 -0700

    Update SeafoamGym.asm

--- maps/SeafoamGym.asm
+++ maps/SeafoamGym.asm
@@ -25,6 +25,7 @@ SeafoamGymBlaineScript:
 	appear SEAFOAMGYM_GYM_GUIDE
 .ReturnAfterBattle:
 	reloadmapafterbattle
+	setmapscene CIANWOOD_DARKROOM, SCENE_CIANWOODDARKROOM_RESET_GAUNTLET
 	setevent EVENT_BEAT_BLAINE
 	opentext
 	writetext ReceivedVolcanoBadgeText
