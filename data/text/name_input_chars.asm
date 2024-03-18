1743f8b170f8385a972000c5153d5197d9ad518b
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-03-18 12:36:30 -0700

    Update name_input_chars.asm

--- data/text/name_input_chars.asm
+++ data/text/name_input_chars.asm
@@ -4,7 +4,7 @@ NameInputLower:
 	db "a b c d e f g h i"
 	db "j k l m n o p q r"
 	db "s t u v w x y z  "
-	db "× ( ) : ; [ ] <PK> <MN>"
+	db "é ( ) : ; ♂ ♀ <PK> <MN>"
 	db "UPPER  DEL   END "
 
 BoxNameInputLower:
@@ -19,13 +19,13 @@ NameInputUpper:
 	db "A B C D E F G H I"
 	db "J K L M N O P Q R"
 	db "S T U V W X Y Z  "
-	db "- ? ! / . ,      "
+	db "- <INV_QU> ? <INV_EX> ! / . , &"
 	db "lower  DEL   END "
 
 BoxNameInputUpper:
 	db "A B C D E F G H I"
 	db "J K L M N O P Q R"
 	db "S T U V W X Y Z  "
-	db "× ( ) : ; [ ] <PK> <MN>"
-	db "- ? ! ♂ ♀ / . , &"
+	db "× ( ) : ; ♂ ♀ <PK> <MN>"
+	db "- <INV_QU> ? <INV_EX> ! / . , &"
 	db "lower  DEL   END "
