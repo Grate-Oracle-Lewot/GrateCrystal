bdb4de9520602954cd92b34acd48bc77e75493c4
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-03-18 18:30:16 -0700

    Update PlayersHouse1F.asm

--- maps/PlayersHouse1F.asm
+++ maps/PlayersHouse1F.asm
@@ -46,7 +46,6 @@ MeetMomScript:
 	setflag ENGINE_POKEGEAR
 	setflag ENGINE_PHONE_CARD
 	addcellnum PHONE_MOM
-	giveitem FLOWER_MAIL
 	setscene SCENE_FINISHED
 	setevent EVENT_PLAYERS_HOUSE_MOM_1
 	clearevent EVENT_PLAYERS_HOUSE_MOM_2
