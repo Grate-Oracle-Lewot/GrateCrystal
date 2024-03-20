fb8abd36bba471b49eae36953f75cdacbf8aa6e9
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-03-19 19:27:57 -0700

    Update NewBarkTown.asm

--- maps/NewBarkTown.asm
+++ maps/NewBarkTown.asm
@@ -126,10 +126,6 @@ NewBarkTownSilverScript:
 	end
 
 NewBarkTownSign:
-	loadvar VAR_BATTLETYPE, BATTLETYPE_SUICUNE
-	loadwildmon MEWTWO, 75
-	startbattle
-	reloadmapafterbattle
 	jumptext NewBarkTownSignText
 
 NewBarkTownPlayersHouseSign:
