cc6122d0aec2a3347730d32d065bfe5eccfaac3c
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-03-23 19:39:44 -0700

    Update icon_constants.asm

--- constants/icon_constants.asm
+++ constants/icon_constants.asm
@@ -1,55 +1,57 @@
 ; IconPointers indexes (see data/icon_pointers.asm)
 	const_def
-	const ICON_NULL
-	const ICON_POLIWAG
-	const ICON_JIGGLYPUFF
-	const ICON_DIGLETT
-	const ICON_PIKACHU
-	const ICON_STARYU
-	const ICON_FISH
-	const ICON_BIRD
-	const ICON_MONSTER
-	const ICON_CLEFAIRY
-	const ICON_ODDISH
-	const ICON_BUG
-	const ICON_GHOST
-	const ICON_LAPRAS
-	const ICON_HUMANSHAPE
-	const ICON_FOX
-	const ICON_EQUINE
-	const ICON_SHELL
-	const ICON_BLOB
-	const ICON_SERPENT
-	const ICON_VOLTORB
-	const ICON_SQUIRTLE
-	const ICON_BULBASAUR
-	const ICON_CHARMANDER
-	const ICON_CATERPILLAR
-	const ICON_UNOWN
-	const ICON_GEODUDE
-	const ICON_FIGHTER
-	const ICON_EGG
-	const ICON_JELLYFISH
-	const ICON_MOTH
-	const ICON_BAT
-	const ICON_SNORLAX
-	const ICON_HO_OH
-	const ICON_LUGIA
-	const ICON_GYARADOS
-	const ICON_SLOWPOKE
-	const ICON_SUDOWOODO
-	const ICON_BIGMON
-	const ICON_CHANSEY
-	const ICON_FLOWER
-	const ICON_FLEA
-	const ICON_SPIRAL
-	const ICON_SEAL
-	const ICON_PINECO
-	const ICON_CELEBI
-	const ICON_RAIKOU
-	const ICON_ENTEI
-	const ICON_SUICUNE
-	const ICON_MANTINE
+	const ICON_NULL        ;  0
+	const ICON_POLIWAG     ;  1
+	const ICON_JIGGLYPUFF  ;  2
+	const ICON_DIGLETT     ;  3
+	const ICON_PIKACHU     ;  4
+	const ICON_STARYU      ;  5
+	const ICON_FISH        ;  6
+	const ICON_BIRD        ;  7
+	const ICON_MONSTER     ;  8
+	const ICON_CLEFAIRY    ;  9
+	const ICON_ODDISH      ; 10
+	const ICON_BUG         ; 11
+	const ICON_GHOST       ; 12
+	const ICON_LAPRAS      ; 13
+	const ICON_HUMANSHAPE  ; 14
+	const ICON_FOX         ; 15
+	const ICON_EQUINE      ; 16
+	const ICON_SHELL       ; 17
+	const ICON_BLOB        ; 18
+	const ICON_SERPENT     ; 19
+	const ICON_VOLTORB     ; 20
+	const ICON_SQUIRTLE    ; 21
+	const ICON_BULBASAUR   ; 22
+	const ICON_CHARMANDER  ; 23
+	const ICON_CATERPILLAR ; 24
+	const ICON_UNOWN       ; 25
+	const ICON_GEODUDE     ; 26
+	const ICON_FIGHTER     ; 27
+	const ICON_EGG         ; 28
+	const ICON_JELLYFISH   ; 29
+	const ICON_MOTH        ; 30
+	const ICON_BAT         ; 31
+	const ICON_SNORLAX     ; 32
+	const ICON_HO_OH       ; 33
+	const ICON_LUGIA       ; 34
+	const ICON_GYARADOS    ; 35
+	const ICON_SLOWPOKE    ; 36
+	const ICON_SUDOWOODO   ; 37
+	const ICON_BIGMON      ; 38
+	const ICON_CHANSEY     ; 39
+	const ICON_FLOWER      ; 40
+	const ICON_FLEA        ; 41
+	const ICON_SPIRAL      ; 42
+	const ICON_SEAL        ; 43
+	const ICON_PINECO      ; 44
+	const ICON_CELEBI      ; 45
+	const ICON_RAIKOU      ; 46
+	const ICON_ENTEI       ; 47
+	const ICON_SUICUNE     ; 48
+	const ICON_MANTINE     ; 49
+	const ICON_ARBOK       ; 50
+	const ICON_VILEPLUME   ; 51
 NUM_ICONS EQU const_value - 1
 
 ; LoadMenuMonIcon.Jumptable indexes (see engine/gfx/mon_icons.asm)
@@ -60,7 +62,7 @@ NUM_ICONS EQU const_value - 1
 	const MONICON_TRADE
 	const MONICON_MOBILE1
 	const MONICON_MOBILE2
-	const MONICON_UNUSED
+	const MONICON_UNUSED ; Nayru's dex
 
 ; party menu icon palettes
 	const_def
