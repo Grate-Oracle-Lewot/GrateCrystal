4b169899ef4df2abcefeb4769ec305ba89b7c721
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-03-30 12:47:29 -0700

    Update halloffame.asm

--- engine/events/halloffame.asm
+++ engine/events/halloffame.asm
@@ -77,8 +77,7 @@ HallOfFame_PlayMusicDE:
 	call PlayMusic
 	call DelayFrame
 	pop de
-	call PlayMusic
-	ret
+	jp PlayMusic
 
 AnimateHallOfFame:
 	xor a
@@ -114,8 +113,7 @@ AnimateHallOfFame:
 	ld [wMusicFade], a
 	call RotateThreePalettesRight
 	ld c, 8
-	call DelayFrames
-	ret
+	jp DelayFrames
 
 .DisplayNewHallOfFamer:
 	call DisplayHOFMon
@@ -272,8 +270,7 @@ AnimateHOFMonEntrance:
 	xor a
 	ldh [hBGMapMode], a
 	ldh [hSCY], a
-	call HOF_SlideFrontpic
-	ret
+	jr HOF_SlideFrontpic
 
 HOF_SlideBackpic:
 .backpicloop
@@ -517,8 +514,7 @@ DisplayHOFMon:
 	hlcoord 10, 16
 	ld de, wTempMonID
 	lb bc, PRINTNUM_LEADINGZEROS | 2, 5
-	call PrintNum
-	ret
+	jp PrintNum
 
 HOF_AnimatePlayerPic:
 	call ClearBGPalettes
