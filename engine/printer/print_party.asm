7616cd2c3e5aa2f76cc4b8105b232d04ac869daf
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-03-31 23:49:57 -0700

    Update print_party.asm

--- engine/printer/print_party.asm
+++ engine/printer/print_party.asm
@@ -229,8 +229,7 @@ PrintPartyMonPage1:
 	call WaitBGMap
 	ld b, SCGB_STATS_SCREEN_HP_PALS
 	call GetSGBLayout
-	call SetPalettes
-	ret
+	jp SetPalettes
 
 PrintPartyMonPage2:
 	call ClearBGPalettes
@@ -280,13 +279,11 @@ PrintPartyMonPage2:
 	call WaitBGMap
 	ld b, SCGB_STATS_SCREEN_HP_PALS
 	call GetSGBLayout
-	call SetPalettes
-	ret
+	jp SetPalettes
 
 .PrintTempMonStats:
 	lb bc, 2, 3
-	call PrintNum
-	ret
+	jp PrintNum
 
 GetCurPartyMonName:
 	ld bc, NAME_LENGTH
@@ -308,8 +305,7 @@ PlaceMoveNameString:
 	ld de, PrintParty_NoMoveString
 
 .got_string
-	call PlaceString
-	ret
+	jp PlaceString
 
 PlaceGenderAndShininess:
 	farcall GetGender
