bfd0452780399578ae3ca8bac03adba56658c472
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-03-31 23:04:38 -0700

    Update caught_data.asm

--- engine/pokemon/caught_data.asm
+++ engine/pokemon/caught_data.asm
@@ -207,8 +207,7 @@ SetBoxMonCaughtData:
 	call OpenSRAM
 	ld hl, sBoxMon1CaughtLevel
 	call SetBoxmonOrEggmonCaughtData
-	call CloseSRAM
-	ret
+	jp CloseSRAM
 
 SetGiftBoxMonCaughtData:
 	push bc
@@ -217,8 +216,7 @@ SetGiftBoxMonCaughtData:
 	ld hl, sBoxMon1CaughtLevel
 	pop bc
 	call SetGiftMonCaughtData
-	call CloseSRAM
-	ret
+	jp CloseSRAM
 
 SetGiftPartyMonCaughtData:
 	ld a, [wPartyCount]
