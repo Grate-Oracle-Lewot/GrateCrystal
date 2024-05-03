9d80bf69593f9b8d7521f80554bbc73f94a58b64
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-05-02 21:00:50 -0700

    Update PowerPlant.asm

--- maps/PowerPlant.asm
+++ maps/PowerPlant.asm
@@ -4,7 +4,6 @@
 	const POWERPLANT_GYM_GUIDE2
 	const POWERPLANT_OFFICER2
 	const POWERPLANT_GYM_GUIDE3
-	const POWERPLANT_MANAGER
 	const POWERPLANT_FOREST
 
 PowerPlant_MapScripts:
@@ -131,61 +130,6 @@ PowerPlantGymGuide4Script:
 	closetext
 	end
 
-PowerPlantManager:
-	faceplayer
-	opentext
-	checkevent EVENT_RETURNED_MACHINE_PART
-	iftrue .ReturnedMachinePart
-	checkitem MACHINE_PART
-	iftrue .FoundMachinePart
-	checkevent EVENT_MET_MANAGER_AT_POWER_PLANT
-	iftrue .MetManager
-	writetext PowerPlantManagerWhoWouldRuinMyGeneratorText
-	waitbutton
-	closetext
-	setevent EVENT_MET_MANAGER_AT_POWER_PLANT
-	clearevent EVENT_CERULEAN_GYM_ROCKET
-	clearevent EVENT_FOUND_MACHINE_PART_IN_CERULEAN_GYM
-	setmapscene CERULEAN_GYM, SCENE_CERULEANGYM_GRUNT_RUNS_OUT
-	setscene SCENE_POWERPLANT_GUARD_GETS_PHONE_CALL
-	end
-
-.MetManager:
-	writetext PowerPlantManagerIWontForgiveCulpritText
-	waitbutton
-	closetext
-	end
-
-.FoundMachinePart:
-	writetext PowerPlantManagerThatsThePartText
-	promptbutton
-	takeitem MACHINE_PART
-	setevent EVENT_RETURNED_MACHINE_PART
-	clearevent EVENT_SAFFRON_TRAIN_STATION_POPULATION
-	setevent EVENT_ROUTE_5_6_POKEFAN_M_BLOCKS_UNDERGROUND_PATH
-	setevent EVENT_ROUTE_24_ROCKET
-	setevent EVENT_RESTORED_POWER_TO_KANTO
-	clearevent EVENT_GOLDENROD_TRAIN_STATION_GENTLEMAN
-.ReturnedMachinePart:
-	checkevent EVENT_GOT_TM07_ZAP_CANNON
-	iftrue .GotZapCannon
-	writetext PowerPlantManagerTakeThisTMText
-	promptbutton
-	verbosegiveitem TM_ZAP_CANNON
-	iffalse .NoRoom
-	setevent EVENT_GOT_TM07_ZAP_CANNON
-	writetext PowerPlantManagerTM07IsZapCannonText
-	waitbutton
-.NoRoom:
-	closetext
-	end
-
-.GotZapCannon:
-	writetext PowerPlantManagerMyBelovedGeneratorText
-	waitbutton
-	closetext
-	end
-
 Forest:
 	faceplayer
 	opentext
@@ -283,15 +227,18 @@ PowerPlantGymGuide2GeneratorIsRunningAgainText:
 	done
 
 PowerPlantOfficer2ManagerHasBeenSadAndFuriousText:
-	text "The POWER PLANT's"
-	line "MANAGER is up"
-	cont "ahead."
+	text "Huh? You want to"
+	line "see the MANAGER?"
 
-	para "But since someone"
-	line "wrecked the gener-"
-	cont "ator, he's been"
-	cont "both sad and"
-	cont "furious…"
+	para "His office is at"
+	line "the back of the"
+	cont "second floor."
+
+	para "Be careful not to"
+	line "get lost in there."
+
+	para "We don't need a"
+	line "second crisis!"
 	done
 
 PowerPlantOfficer2ManagerHasBeenCheerfulText:
@@ -320,81 +267,13 @@ PowerPlantGymGuide4WeCanGetMagnetTrainRunningText:
 	line "running again."
 	done
 
-PowerPlantManagerWhoWouldRuinMyGeneratorText:
-	text "MANAGER: I, I, I'm"
-	line "ready to blast"
-	cont "someone!"
-
-	para "Who would dare"
-	line "ruin my generator?"
-
-	para "I spent so much"
-	line "time on it!"
-
-	para "If I catch him,"
-	line "he's going to get"
-
-	para "a taste of my ZAP"
-	line "CANNON!"
-	done
-
-PowerPlantManagerIWontForgiveCulpritText:
-	text "MANAGER: I won't"
-	line "forgive him!"
-
-	para "The culprit can"
-	line "cry and apologize,"
-
-	para "but I'll still"
-	line "hammer him!"
-
-	para "Gahahahah!"
-	done
-
-PowerPlantManagerThatsThePartText:
-	text "MANAGER: Ah! Yeah!"
-
-	para "That's the missing"
-	line "PART from my be-"
-	cont "loved generator!"
-	cont "You found it?"
-	done
-
-PowerPlantManagerTakeThisTMText:
-	text "Wahah! Thanks!"
-
-	para "Here! Take this TM"
-	line "as a reward!"
-	done
-
-PowerPlantManagerTM07IsZapCannonText:
-	text "MANAGER: TM07 is"
-	line "my ZAP CANNON."
-
-	para "It's a powerful"
-	line "technique!"
-
-	para "It's not what any-"
-	line "one would consider"
-
-	para "accurate, but it"
-	line "packs a wallop!"
-	done
-
-PowerPlantManagerMyBelovedGeneratorText:
-	text "MANAGER: My be-"
-	line "loved generator!"
-
-	para "Keep pumping the"
-	line "electricity out!"
-	done
-
 PowerPlant_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
 	warp_event  2, 17, ROUTE_10_NORTH, 2
 	warp_event  3, 17, ROUTE_10_NORTH, 2
+	warp_event  7,  0, POWER_PLANT_2F, 1
 
 	def_coord_events
 	coord_event  5, 12, SCENE_POWERPLANT_GUARD_GETS_PHONE_CALL, PowerPlantGuardPhoneScript
@@ -408,6 +287,5 @@ PowerPlant_MapEvents:
 	object_event  2,  9, SPRITE_GYM_GUIDE, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, PowerPlantGymGuide1Script, -1
 	object_event  6, 11, SPRITE_GYM_GUIDE, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, PowerPlantGymGuide2Script, -1
 	object_event  9,  3, SPRITE_OFFICER, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, PowerPlantOfficer2Script, -1
-	object_event  7,  2, SPRITE_GYM_GUIDE, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, PowerPlantGymGuide4Script, -1
-	object_event 14, 10, SPRITE_FISHER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, PowerPlantManager, -1
+	object_event 14, 10, SPRITE_GYM_GUIDE, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, PowerPlantGymGuide4Script, -1
 	object_event  5,  5, SPRITE_GYM_GUIDE, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, Forest, -1
