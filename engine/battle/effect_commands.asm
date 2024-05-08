e0b7458eacab1fb115fdb6c41c399d1852d36d02
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-05-07 17:05:39 -0700

    Update effect_commands.asm

--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -2615,6 +2615,8 @@ PlayerAttackDamage:
 	ld b, a
 	ld c, [hl]
 
+	call HailDefenseBoost
+
 	ld a, [wEnemyScreens]
 	bit SCREENS_REFLECT, a
 	jr z, .physicalcrit
@@ -2868,6 +2870,8 @@ EnemyAttackDamage:
 	ld a, [hli]
 	ld b, a
 	ld c, [hl]
+
+	call HailDefenseBoost
 
 	ld a, [wPlayerScreens]
 	bit SCREENS_REFLECT, a
@@ -6797,6 +6801,36 @@ SandstormSpDefBoost:
 	jr z, .start_boost
 	ld a, [hl]
 	cp ROCK
+	ret nz
+
+.start_boost
+	ld h, b
+	ld l, c
+	srl b
+	rr c
+	add hl, bc
+	ld b, h
+	ld c, l
+	ret
+
+HailDefenseBoost: 
+; First, check if Hail is active.
+	ld a, [wBattleWeather]
+	cp WEATHER_HAIL
+	ret nz
+
+; Then, check the opponent's types.
+	ld hl, wEnemyMonType1
+	ldh a, [hBattleTurn]
+	and a
+	jr z, .ok
+	ld hl, wBattleMonType1
+.ok
+	ld a, [hli]
+	cp ICE
+	jr z, .start_boost
+	ld a, [hl]
+	cp ICE
 	ret nz
 
 .start_boost
