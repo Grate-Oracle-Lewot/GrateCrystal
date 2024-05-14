4a19445e6e55c45aab22c4b76df2994b70c1769e
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-05-13 22:05:01 -0700

    Update core.asm

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -9065,25 +9065,11 @@ BattleStartMessage:
 
 	ld hl, WantToBattlePluralText
 	call IsPluralTrainer
-	jp z, .PlaceBattleStartText
+	jr z, .PlaceBattleStartText
 	ld hl, WantsToBattleText
 	jr .PlaceBattleStartText
 
 .wild
-	ld a, [wTempWildMonSpecies]
-	cp UNOWN
-	jr nz, .no_fade_in
-
-	xor a
-	ld [wNumHits], a
-	ld a, 1
-	ldh [hBattleTurn], a
-	ld a, 2 ; fade anim
-	ld [wBattleAnimParam], a
-	ld de, ANIM_SEND_OUT_MON
-	call Call_PlayBattleAnim
-
-.no_fade_in
 	call BattleCheckEnemyShininess
 	jr nc, .not_shiny
 
