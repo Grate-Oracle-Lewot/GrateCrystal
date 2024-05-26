716158850f9ce27474d238bbbc2dc995adb145ed
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-05-25 21:50:25 -0700

    Update nightmare.asm

--- engine/battle/move_effects/nightmare.asm
+++ engine/battle/move_effects/nightmare.asm
@@ -6,13 +6,9 @@ BattleCommand_Nightmare:
 	call CheckSubstituteOpp
 	jr nz, .failed
 
-; If target's held item blocks Sleep, move on to trying to inflict Nightmare, in case target used Rest or napped due to disobedience.
-	call GetOpponentItem
-	ld a, b
-	cp HELD_PREVENT_SLEEP
-	jr z, .done_sleep
-
 ; If target is already Sleeping, move on to inflicting Nightmare.
+; This includes targets who are Safeguarded or holding a Noisemaker but who have used Rest or napped due to disobedience.
+; Safeguard does not protect against the Nightmare status, which is consistent with vanilla.
 	ld a, BATTLE_VARS_STATUS_OPP
 	call GetBattleVarAddr
 	ld d, h
@@ -26,12 +22,21 @@ BattleCommand_Nightmare:
 	and a
 	jr nz, .failed
 
-; If Nightmare's Sleep-infliction missed, move on to trying to inflict Nightmare, in case target was already Sleeping.
-; Safeguard works by forcing a miss, which matters in case Safeguarded target used Rest or napped due to disobedience.
-; (Safeguard does not protect against the Nightmare status, which is consistent with vanilla.)
+; If target's held item blocks Sleep, fail completely with special text.
+	call GetOpponentItem
+	ld a, b
+	cp HELD_PREVENT_SLEEP
+	jr z, .item_fail
+
+; If target has Safeguard up, fail completely with special text.
+	call SafeCheckSafeguard
+	jr nz, .safeguard_fail
+
+; If Nightmare's Sleep-infliction missed, move on to trying to inflict Nightmare.
+; I think this is redundant (that is, it could just jump to .failed instead), but I'm keeping it just to be safe.
 	ld a, [wAttackMissed]
 	and a
-	jp nz, .done_sleep
+	jr nz, .done_sleep
 
 ; Animate Nightmare for Sleep-infliction. Animation will play again later for Nightmare-infliction.
 	call AnimateCurrentMove
@@ -54,6 +59,7 @@ BattleCommand_Nightmare:
 
 	farcall UseHeldStatusHealingItem
 ; If Nightmare-induced Sleep is cured by a held item, don't bother trying to inflict Nightmare due to grammatical awkwardness of "but it failed" in that case.
+; This is effectively similar to special-text fails for Noisemaker and Safeguard, just cutting off after the "recovered with item" text.
 	ret nz
 	call OpponentCantMove
 
@@ -85,3 +91,22 @@ BattleCommand_Nightmare:
 .failed
 	call AnimateFailedMove
 	jp PrintButItFailed
+
+; Fail Sleep and Nightmare with "protected by item" text.
+; In the event that an item holder used Rest or napped due to disobedience, we never make it here because Sleep was checked for before the item.
+.item_fail
+	ld a, [hl]
+	ld [wNamedObjectIndex], a
+	call GetItemName
+	ld hl, ProtectedByText
+	jr .special_fail
+
+; Fail Sleep and Nightmare with "protected by Safeguard" text.
+; In the event that a Safeguarded target used Rest or napped due to disobedience, we never make it here because Sleep was checked for before Safeguard.
+.safeguard_fail
+	ld hl, SafeguardProtectText
+.special_fail
+	push hl
+	call AnimateFailedMove
+	pop hl
+	jp StdBattleTextbox
