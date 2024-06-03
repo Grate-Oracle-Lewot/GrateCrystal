c83b3cbc5ebf7b1b679af5c0796081b66706fe05
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-06-02 21:16:06 -0700

    Update core.asm

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -6751,6 +6751,15 @@ ApplyPrzEffectOnSpeed:
 	ld a, [wBattleMonStatus]
 	and 1 << PAR
 	ret z
+
+	ld hl, wBattleMonType1
+	ld a, [hli]
+	cp FLYING
+	ret z
+	ld a, [hl]
+	cp FLYING
+	ret z
+
 	ld hl, wBattleMonSpeed + 1
 	ld a, [hld]
 	ld b, a
@@ -6772,6 +6781,15 @@ ApplyPrzEffectOnSpeed:
 	ld a, [wEnemyMonStatus]
 	and 1 << PAR
 	ret z
+
+	ld hl, wEnemyMonType1
+	ld a, [hli]
+	cp FLYING
+	ret z
+	ld a, [hl]
+	cp FLYING
+	ret z
+
 	ld hl, wEnemyMonSpeed + 1
 	ld a, [hld]
 	ld b, a
