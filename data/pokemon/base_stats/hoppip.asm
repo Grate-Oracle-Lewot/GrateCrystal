55f37cfcdb8498665e023682cf175348fe6d81cf
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-06-09 21:21:39 -0700

    Update hoppip.asm

--- data/pokemon/base_stats/hoppip.asm
+++ data/pokemon/base_stats/hoppip.asm
@@ -6,7 +6,7 @@
 	db GRASS, FLYING ; type
 	db 255 ; catch rate
 	db 74 ; base exp
-	db MIRACLEBERRY, MIRACLE_SEED ; items
+	db NO_ITEM, MIRACLEBERRY ; items
 	db GENDER_F50 ; gender ratio
 	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/hoppip/front.dimensions"
