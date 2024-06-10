23dde5fd782de1f3f3c769944d4949515899f3ce
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-06-09 21:21:08 -0700

    Update hoothoot.asm

--- data/pokemon/base_stats/hoothoot.asm
+++ data/pokemon/base_stats/hoothoot.asm
@@ -6,7 +6,7 @@
 	db NORMAL, FLYING ; type
 	db 255 ; catch rate
 	db 58 ; base exp
-	db MINT_BERRY, NOISEMAKER ; items
+	db NO_ITEM, MINT_BERRY ; items
 	db GENDER_F50 ; gender ratio
 	db 8 ; step cycles to hatch
 	INCBIN "gfx/pokemon/hoothoot/front.dimensions"
