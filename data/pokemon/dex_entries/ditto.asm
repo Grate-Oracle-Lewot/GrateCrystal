978669b2ffc891217fa5e805e8c0f0de7bcd1ba7
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-06-22 11:52:34 -0700

    Update ditto.asm

--- data/pokemon/dex_entries/ditto.asm
+++ data/pokemon/dex_entries/ditto.asm
@@ -1,10 +1,10 @@
-	db "TRANSFORM@" ; species name
+	db "FORMLESS@" ; species name
 	dw 100, 90 ; height, weight
 
-	db   "This #MON is a"
-	next "result of attempts"
-	next "to clone MEW."
+	db   "An old philosophy"
+	next "posits that all"
+	next "#MON are really"
 
-	page "However, it can"
-	next "only use TRANS-"
-	next "FORM.@"
+	page "disguised DITTO,"
+	next "and perhaps humans"
+	next "too.@"
