9531ea2d2dbde5449e1805a17bb8e2c1d2b160b3
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-06-23 13:11:37 -0700

    Update marowak.asm

--- data/pokemon/dex_entries/marowak.asm
+++ data/pokemon/dex_entries/marowak.asm
@@ -1,10 +1,10 @@
 	db "BONEKEEPER@" ; species name
 	dw 303, 990 ; height, weight
 
-	db   "Upon CUBONE's"
-	next "death, the skull"
-	next "fuses to its"
+	db   "Its spirit is"
+	next "cursed to bear the"
+	next "symbol of its"
 
-	page "spirit's head. It"
-	next "wanders graveyards"
-	next "aimlessly.@"
+	page "former lies, yet"
+	next "it feels no"
+	next "remorse.@"
