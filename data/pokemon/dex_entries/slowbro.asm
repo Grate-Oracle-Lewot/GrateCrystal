f11dbd955b7874945ce7f36eca99dd3c4c99bd5e
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-07-11 19:43:35 -0700

    Update slowbro.asm

--- data/pokemon/dex_entries/slowbro.asm
+++ data/pokemon/dex_entries/slowbro.asm
@@ -1,10 +1,10 @@
 	db "HERMITCRAB@" ; species name
 	dw 503, 1730 ; height, weight
 
-	db   "When a SLOWPOKE"
-	next "has its tail bit"
-	next "by a TURBIN, it"
+	db   "SHELLDER evolved"
+	next "into TURBIN after"
+	next "biting SLOWPOKE's"
 
-	page "becomes a SLOWBRO,"
-	next "gaining strength"
-	next "if not speed.@"
+	page "tail. SLOWBRO is"
+	next "saddled with it"
+	next "for life.@"
