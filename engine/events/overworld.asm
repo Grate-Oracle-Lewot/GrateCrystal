8c64d9a7895ea1a9858ccbb1a95918b8c09941e8
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-07-20 21:20:44 -0700

    Update overworld.asm

--- engine/events/overworld.asm
+++ engine/events/overworld.asm
@@ -275,7 +275,7 @@ FlashFunction:
 .CheckUseFlash:
 ; Flash
 	ld de, ENGINE_ZEPHYRBADGE
-	farcall CheckBadge
+	call CheckBadge
 	jr c, .nozephyrbadge
 	push hl
 	farcall SpecialAerodactylChamber
@@ -649,7 +649,7 @@ WaterfallFunction:
 .TryWaterfall:
 ; Waterfall
 	ld de, ENGINE_RISINGBADGE
-	farcall CheckBadge
+	call CheckBadge
 	ld a, $80
 	ret c
 	call CheckMapCanWaterfall
