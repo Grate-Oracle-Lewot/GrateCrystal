ecfd92e2b8228583902cb721dfe82caf85bba909
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-07-20 20:16:30 -0700

    Update pokemon_data_constants.asm

--- constants/pokemon_data_constants.asm
+++ constants/pokemon_data_constants.asm
@@ -193,7 +193,8 @@ NUM_TREEMON_SETS EQU const_value
 ; rock smash groups, for Nayru's pokedex
 ; const TREEMON_SET_ROCK is originally within NUM_TREEMON_SETS
 	const_def
-	const TREEMON_SET_ROCK ; 0
+	const TREEMON_SET_ROCK       ; 0
+	const TREEMON_SET_ROCK_KANTO ; 1
 NUM_ROCKSMASH_SETS EQU const_value
 
 ; ChangeHappiness arguments (see data/events/happiness_changes.asm)
