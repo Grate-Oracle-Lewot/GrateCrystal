cc7009b1a4e5a83f00a66b31915d88a12f8b660c
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-07-24 18:46:05 -0700

    Update MountMoonB2F.asm

--- maps/MountMoonB2F.asm
+++ maps/MountMoonB2F.asm
@@ -1,4 +1,5 @@
 	object_const_def
+	const MOUNTMOONB2F_SUPER_NERD
 	const MOUNTMOONB2F_ROCK1
 	const MOUNTMOONB2F_ROCK2
 	const MOUNTMOONB2F_POKE_BALL
@@ -8,11 +9,39 @@ MountMoonB2F_MapScripts:
 
 	def_callbacks
 
+TrainerSuperNerdNeil:
+	trainer SUPER_NERD, NEIL, EVENT_BEAT_SUPER_NERD_NEIL, SuperNerdNeilSeenText, SuperNerdNeilBeatenText, 0, .Script
+
+.Script:
+	endifjustbattled
+	opentext
+	writetext SuperNerdNeilAfterBattleText
+	waitbutton
+	closetext
+	end
+
 MountMoonB2FStarPiece:
 	itemball STAR_PIECE
 
 MountMoonB2FRock:
 	jumpstd SmashRockScript
+
+SuperNerdNeilSeenText:
+	text "You've set off a"
+	line "time bomb!"
+	done
+
+SuperNerdNeilBeatenText:
+	text "Kablooie!"
+	done
+
+SuperNerdNeilAfterBattleText:
+	text "Sometimes people"
+	line "find fossils here."
+
+	para "Try smashing"
+	line "rocks!"
+	done
 
 MountMoonB2F_MapEvents:
 	db 0, 0 ; filler
@@ -30,6 +59,7 @@ MountMoonB2F_MapEvents:
 	def_bg_events
 
 	def_object_events
+	object_event 11, 18, SPRITE_SUPER_NERD, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_TRAINER, 4, TrainerSuperNerdNeil, -1
 	object_event 13,  5, SPRITE_ROCK, SPRITEMOVEDATA_SMASHABLE_ROCK, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, MountMoonB2FRock, -1
 	object_event 14,  5, SPRITE_ROCK, SPRITEMOVEDATA_SMASHABLE_ROCK, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, MountMoonB2FRock, -1
 	object_event 20,  6, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_ITEMBALL, 0, MountMoonB2FStarPiece, EVENT_MOUNT_MOON_B2F_STAR_PIECE
