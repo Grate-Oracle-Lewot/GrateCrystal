3a324497137f66dbe1f67656327782b09e5006a1
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-07-25 18:42:37 -0700

    Update MountMoonB2F.asm

--- maps/MountMoonB2F.asm
+++ maps/MountMoonB2F.asm
@@ -47,19 +47,19 @@ MountMoonB2F_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event 15, 25, MOUNT_MOON_B1F, 4
-	warp_event 25,  9, MOUNT_MOON_B1F, 5
-	warp_event 21, 17, MOUNT_MOON_B1F, 6
-	warp_event 29,  3, MOUNT_MOON_B1F, 7
-	warp_event 25, 23, MOUNT_MOON_B1F, 8
-	warp_event  5,  7, MOUNT_MOON_B1F, 9
+	warp_event 21, 29, MOUNT_MOON_B1F, 4
+	warp_event 31, 13, MOUNT_MOON_B1F, 5
+	warp_event 27, 21, MOUNT_MOON_B1F, 6
+	warp_event 35,  7, MOUNT_MOON_B1F, 7
+	warp_event 31, 27, MOUNT_MOON_B1F, 8
+	warp_event 11, 11, MOUNT_MOON_B1F, 9
 
 	def_coord_events
 
 	def_bg_events
 
 	def_object_events
-	object_event 11, 18, SPRITE_SUPER_NERD, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_TRAINER, 4, TrainerSuperNerdNeil, -1
-	object_event 13,  5, SPRITE_ROCK, SPRITEMOVEDATA_SMASHABLE_ROCK, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, MountMoonB2FRock, -1
-	object_event 14,  5, SPRITE_ROCK, SPRITEMOVEDATA_SMASHABLE_ROCK, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, MountMoonB2FRock, -1
-	object_event 20,  6, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_ITEMBALL, 0, MountMoonB2FStarPiece, EVENT_MOUNT_MOON_B2F_STAR_PIECE
+	object_event 17, 22, SPRITE_SUPER_NERD, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_TRAINER, 4, TrainerSuperNerdNeil, -1
+	object_event 19,  9, SPRITE_ROCK, SPRITEMOVEDATA_SMASHABLE_ROCK, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, MountMoonB2FRock, -1
+	object_event 20,  9, SPRITE_ROCK, SPRITEMOVEDATA_SMASHABLE_ROCK, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, MountMoonB2FRock, -1
+	object_event 26, 11, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_ITEMBALL, 0, MountMoonB2FStarPiece, EVENT_MOUNT_MOON_B2F_STAR_PIECE
