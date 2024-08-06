9cfee4c53928e51845f0ae52659fa58d5e1c761d
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-08-06 16:29:10 -0700

    Update pokedex_moves_page.asm

--- engine/pokedex/pokedex_moves_page.asm
+++ engine/pokedex/pokedex_moves_page.asm
@@ -464,7 +464,7 @@ Pokedex_Calc_EggMovesPtr:
 	ld [wPokedexEntryType], a
 	call Pokedex_PrintPageNum ; page num is also returned in a
 	ld a, [wPokedexEntryPageNum]
-	ld c, 5 ; we can print 5 Egg moves per page
+	ld c, MAX_NUM_MOVES ; we can print MAX_NUM_MOVES Egg moves per page
 	call SimpleMultiply ; double this num and add to first byte after Evo's 0
 	ld b, 0
 	ld c, a
@@ -480,7 +480,6 @@ Pokedex_Calc_EggMovesPtr:
 	ld hl, EggMovePointers
 	add hl, bc ; trying to add the species number in only 'a' will overflow a
 	add hl, bc ; add twice to double the index, words/PTRs are TWO bytes ea
-	; pop af ; preserve current species or else move sets will be messed up for stage2+ mons
 	ld a, [wCurSpecies]
 	ld [wCurPartySpecies], a
 	ld [wTempSpecies], a
@@ -490,16 +489,9 @@ Pokedex_Calc_EggMovesPtr:
 	call GetFarWord
 .check_if_any
 	ld a, BANK("Egg Moves")
-	call GetFarByte
+	call GetFarByte ; a will be -1 if no egg moves
 	pop bc
 	add hl, bc
-	push af ; -1 if no egg moves
-	push hl
-	hlcoord 2, 9
-	ld de, .EggMoves_text
-	call PlaceString
-	pop hl
-	pop af
 	cp -1
 	ret nz
 	; if we reach here, the mon has no egg moves at all
@@ -508,17 +500,15 @@ Pokedex_Calc_EggMovesPtr:
 	ld a, DEXENTRY_LVLUP
 	call DexEntry_NextCategory
 	; print NONE
-	hlcoord 3, 11
+	hlcoord 3, 9
 	ld de, DexEntry_NONE_text
+	call PlaceString
 	jp PlaceString
 
-.EggMoves_text:
-	db "EGG MOVES@"
-
 Pokedex_Print_Egg_moves:
-; Print No more than 5 moves
+; Print No more than MAX_NUM_MOVES moves
 	ld b, 0
-	ld c, 0 ; our move counter, max of 4 for 5 moves
+	ld c, 0 ; our move counter, max of MAX_NUM_MOVES - 1 for MAX_NUM_MOVES moves
 	; our adjusted pointer based on page num is in hl
 .Egg_loop
 	ld a, BANK("Egg Moves")
@@ -529,19 +519,20 @@ Pokedex_Print_Egg_moves:
 	push hl
 	ld [wNamedObjectIndex], a ; all the "Name" Funcs use this 
 	call GetMoveName ; returns the string pointer in de
-	hlcoord 3, 11
+	hlcoord 3, 9
 	call DexEntry_adjusthlcoord
 	push bc
 	call PlaceString ; places Move Name
 	pop bc
 	pop hl
-	ld a, $4 ; means we just printed 5th move
+	ld a, MAX_NUM_MOVES - 1 ; means we just printed last move on page
 	cp c
 	jr z, .MaxedPage
 	inc c
 	jr .Egg_loop
-.MaxedPage ; Printed 5 moves. Moves are still left. Inc the Page counter
-; CheckNextByte, we don't want blank screen if we just printed last move in slot 5
+
+.MaxedPage ; Printed MAX_NUM_MOVES moves. Moves are still left. Inc the Page counter
+; CheckNextByte, we don't want blank screen if we just printed last move
 	ld a, BANK("Egg Moves")
 	call GetFarByte ; Move # returned in "a"
 	cp -1
@@ -552,6 +543,7 @@ Pokedex_Print_Egg_moves:
 	ld [wTempSpecies], a
 	ld [wTempMonSpecies], a
 	ret
+
 .FoundEnd
 	ld a, DEXENTRY_LVLUP
 	call DexEntry_NextCategory
