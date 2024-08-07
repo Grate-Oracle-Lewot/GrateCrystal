7314f1e12c58d5497f5fba55bc9a242914bba908
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-08-06 17:47:37 -0700

    Update pokedex_area_page.asm

--- engine/pokedex/pokedex_area_page.asm
+++ engine/pokedex/pokedex_area_page.asm
@@ -900,9 +900,7 @@ Dex_Check_Surf:
 	and a
 	jr z, .found
 	pop hl ; points to map group/num
-	ld a, [wPokedexStatus]
-	inc a
-	ld [wPokedexStatus], a
+	call DexArea_IncWildMonIndex
 	ld b, 0
 	ld c, WATER_WILDDATA_LENGTH
 	add hl, bc
