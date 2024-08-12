1a9ff6bacb7027e13f2a7c7b0b8d527ca1c928e3
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-08-12 15:29:08 -0700

    Update cgb_layouts.asm

--- engine/gfx/cgb_layouts.asm
+++ engine/gfx/cgb_layouts.asm
@@ -491,11 +491,6 @@ _CGB_Pokedex_PicsPage:
 	ld bc, 2
 	ld a, 0 | Y_FLIP | VRAM_BANK_1
 	call ByteFill
-; > CRY, set VRAM	
-	hlcoord 14, 17, wAttrmap
-	lb bc, 1, 2
-	ld a, 0 | VRAM_BANK_1 ; VRAM 1
-	call FillBoxCGB
 
 	call InitPartyMenuOBPals
 	call ApplyAttrmap
