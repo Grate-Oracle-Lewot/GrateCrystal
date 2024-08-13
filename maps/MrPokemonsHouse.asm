00e91318ac5cf1a3cbdba41f75395ba923cd264a
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-08-13 14:31:24 -0700

    Update MrPokemonsHouse.asm

--- maps/MrPokemonsHouse.asm
+++ maps/MrPokemonsHouse.asm
@@ -175,7 +175,7 @@ MrPokemonsHouse_OakScript:
 	playsound SFX_ITEM
 	waitsfx
 	setflag ENGINE_POKEDEX
-	special FillPokedex
+;	special FillPokedex
 	writetext MrPokemonsHouse_OakText2
 	waitbutton
 	closetext
@@ -566,7 +566,7 @@ MrPokemonsHouse_MapEvents:
 
 	def_warp_events
 	warp_event  2,  7, ROUTE_30, 2
-	warp_event  3,  7, ROUTE_30, 2
+	warp_event  3,  7, ECRUTEAK_CITY, 2
 
 	def_coord_events
 
