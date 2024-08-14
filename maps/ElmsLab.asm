da97636b73de9144a090b844f27d8db0d97f4751
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-08-13 17:39:32 -0700

    Update ElmsLab.asm

--- maps/ElmsLab.asm
+++ maps/ElmsLab.asm
@@ -206,7 +206,7 @@ TotodilePokeBallScript:
 	playsound SFX_CAUGHT_MON
 	waitsfx
 	promptbutton
-	givepoke TOTODILE, 100, GOLD_BERRY
+	givepoke TOTODILE, 39, GOLD_BERRY
 	closetext
 	applymovement PLAYER, AfterTotodileMovement
 	sjump ElmDirectionsScript
