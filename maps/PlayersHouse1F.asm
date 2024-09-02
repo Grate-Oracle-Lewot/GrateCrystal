147065b29897a2c29bf960a343a12b6c252e49d9
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-09-02 11:18:02 -0700

    Update PlayersHouse1F.asm

--- maps/PlayersHouse1F.asm
+++ maps/PlayersHouse1F.asm
@@ -391,7 +391,7 @@ PlayersHouse1F_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event  6,  7, SAFFRON_CITY, 2
+	warp_event  6,  7, GOLDENROD_CITY, 2
 	warp_event  7,  7, NEW_BARK_TOWN, 2
 	warp_event  9,  0, PLAYERS_HOUSE_2F, 1
 
