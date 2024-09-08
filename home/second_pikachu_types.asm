bd5fe62aee584a35e114f4e240b4b3be1d053558
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-09-08 16:20:28 -0700

    Update second_pikachu_types.asm

--- home/second_pikachu_types.asm
+++ home/second_pikachu_types.asm
@@ -8,3 +8,21 @@ SecondPikachuTypes::
   db WATER    ; PIKACHU_SURFING
   db FLYING   ; PIKACHU_FLYING
   assert_table_length NUM_PIKACHU_FORMS
+
+GetSecondPikachuType::
+; must be called when [wCurPartySpecies] = PIKACHU
+	; we want to get the [wPikachuForm]th entry from the SecondPikachuTypes table
+	ld a, [wPikachuForm]
+	dec a ; is evidently necessary
+	ld hl, SecondPikachuTypes
+
+	; add a to hl, efficiently: https://github.com/pret/pokecrystal/wiki/Optimizing-assembly-code#add-a-to-a-16-bit-register
+	add l
+	ld l, a
+	adc h
+	sub l
+	ld h, a
+
+	; get the form and return it in a
+	ld a, [hl]
+	ret
