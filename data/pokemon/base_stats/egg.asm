e7ff82885fe85bc85b576102e206fd877476445e
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-09-10 12:36:51 -0700

    Update egg.asm

--- data/pokemon/base_stats/egg.asm
+++ data/pokemon/base_stats/egg.asm
@@ -7,7 +7,7 @@
 	db 255 ; catch rate
 	db 10 ; base exp
 	db NO_ITEM, NO_ITEM ; items
-	db GENDER_F50 ; gender ratio
+	db GENDER_UNKNOWN ; gender ratio
 	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/egg/front.dimensions"
 	db GROWTH_SLOW ; growth rate
