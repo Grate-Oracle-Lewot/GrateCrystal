5a42a97c61009e88227668749245b72b39e658fd
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-09-15 18:58:55 -0700

    Update restart_clock.asm

--- engine/rtc/restart_clock.asm
+++ engine/rtc/restart_clock.asm
@@ -45,6 +45,8 @@ RestartClock:
 	set NO_TEXT_SCROLL, [hl]
 	call LoadStandardMenuHeader
 	call ClearTilemap
+	ld hl, .ClockSetWithControlPadText
+	call PrintText
 	call .SetClock
 	call ExitMenu
 	pop bc
@@ -55,6 +57,10 @@ RestartClock:
 
 .ClockTimeMayBeWrongText:
 	text_far _ClockTimeMayBeWrongText
+	text_end
+
+.ClockSetWithControlPadText:
+	text_far _ClockSetWithControlPadText
 	text_end
 
 .SetClock:
