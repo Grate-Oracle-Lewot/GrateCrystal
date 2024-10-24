8aad698d6b64ae6f63bb99555e69c8d800bba4c6
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-10-24 13:21:02 -0700

    Update link.asm

--- engine/link/link.asm
+++ engine/link/link.asm
@@ -197,7 +197,7 @@ endc
 	cp USING_INTERNAL_CLOCK
 	ld c, 66
 	call z, DelayFrames
-	ld de, MUSIC_ROUTE_30
+	ld de, MUSIC_BUG_CATCHING_CONTEST
 	call PlayMusic
 	jp InitTradeMenuDisplay
 
@@ -524,7 +524,7 @@ endc
 	jp ExitLinkCommunications
 
 .ready_to_trade
-	ld de, MUSIC_ROUTE_30
+	ld de, MUSIC_BUG_CATCHING_CONTEST
 	call PlayMusic
 	jp InitTradeMenuDisplay
 
