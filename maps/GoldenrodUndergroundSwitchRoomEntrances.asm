62d592ad61a06e2e42b779d11f9dc859407f82f8
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-10-25 18:24:23 -0700

    Update GoldenrodUndergroundSwitchRoomEntrances.asm

--- maps/GoldenrodUndergroundSwitchRoomEntrances.asm
+++ maps/GoldenrodUndergroundSwitchRoomEntrances.asm
@@ -47,16 +47,13 @@ ENDM
 
 GoldenrodUndergroundSwitchRoomEntrances_MapScripts:
 	def_scene_scripts
-	scene_script .DummyScene0 ; SCENE_DEFAULT
-	scene_script .DummyScene1 ; SCENE_FINISHED
+	scene_script .DummyScene ; SCENE_DEFAULT
+	scene_script .DummyScene ; SCENE_FINISHED
 
 	def_callbacks
 	callback MAPCALLBACK_TILES, .UpdateDoorPositions
 
-.DummyScene0:
-	end
-
-.DummyScene1:
+.DummyScene:
 	end
 
 .UpdateDoorPositions:
