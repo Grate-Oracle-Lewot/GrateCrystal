7b6a7d66c3ab9f4722e663ba9dff9c5b630b144e
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-10-25 18:18:56 -0700

    Update TeamRocketBaseB2F.asm

--- maps/TeamRocketBaseB2F.asm
+++ maps/TeamRocketBaseB2F.asm
@@ -16,24 +16,15 @@
 
 TeamRocketBaseB2F_MapScripts:
 	def_scene_scripts
-	scene_script .DummyScene0 ; SCENE_DEFAULT
-	scene_script .DummyScene1 ; SCENE_TEAMROCKETBASEB2F_ROCKET_BOSS
-	scene_script .DummyScene2 ; SCENE_TEAMROCKETBASEB2F_ELECTRODES
-	scene_script .DummyScene3 ; SCENE_TEAMROCKETBASEB2F_NOTHING
+	scene_script .DummyScene ; SCENE_DEFAULT
+	scene_script .DummyScene ; SCENE_TEAMROCKETBASEB2F_ROCKET_BOSS
+	scene_script .DummyScene ; SCENE_TEAMROCKETBASEB2F_ELECTRODES
+	scene_script .DummyScene ; SCENE_TEAMROCKETBASEB2F_NOTHING
 
 	def_callbacks
 	callback MAPCALLBACK_TILES, .TransmitterDoorCallback
 
-.DummyScene0:
-	end
-
-.DummyScene1:
-	end
-
-.DummyScene2:
-	end
-
-.DummyScene3:
+.DummyScene:
 	end
 
 .TransmitterDoorCallback:
