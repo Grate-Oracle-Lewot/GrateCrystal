fe74b5cb3bf0eff064cac27ddf94013296d1c985
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-10-25 19:13:55 -0700

    Update magnitude_power.asm

--- data/moves/magnitude_power.asm
+++ data/moves/magnitude_power.asm
@@ -1,9 +1,9 @@
 MagnitudePower:
 	; chance, power, magnitude #
-	db  10 percent + 1,   5,  4 ; 10%
+	db  10 percent,       5,  4 ; 10%
 	db  20 percent,      25,  5 ; 10%
 	db  40 percent,      50,  6 ; 20%
-	db  60 percent + 1,  75,  7 ; 20%
-	db  80 percent + 1, 100,  8 ; 20%
+	db  60 percent,      75,  7 ; 20%
+	db  80 percent,     100,  8 ; 20%
 	db  90 percent,     125,  9 ; 10%
 	db 100 percent,     150, 10 ; 10%
