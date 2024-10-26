dbac5c4db10192a7d266bde7b0118c6d7950f7ed
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-10-25 18:20:21 -0700

    Update Route27.asm

--- maps/Route27.asm
+++ maps/Route27.asm
@@ -11,15 +11,12 @@
 
 Route27_MapScripts:
 	def_scene_scripts
-	scene_script .DummyScene0 ; SCENE_DEFAULT
-	scene_script .DummyScene1 ; SCENE_FINISHED
+	scene_script .DummyScene ; SCENE_DEFAULT
+	scene_script .DummyScene ; SCENE_FINISHED
 
 	def_callbacks
 
-.DummyScene0:
-	end
-
-.DummyScene1:
+.DummyScene:
 	end
 
 FirstStepIntoKantoLeftScene:
