32833309ad7a7bc6ced0e546b67e7d2208d60308
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-10-27 23:13:20 -0700

    Update thief.asm

--- engine/battle/move_effects/thief.asm
+++ engine/battle/move_effects/thief.asm
@@ -1,26 +1,21 @@
 BattleCommand_Thief:
-; thief
-
 	ldh a, [hBattleTurn]
 	and a
 	jr nz, .enemy
 
 ; The player needs to be able to steal an item.
-
 	call .playeritem
 	ld a, [hl]
 	and a
 	ret nz
 
 ; The enemy needs to have an item to steal.
-
 	call .enemyitem
 	ld a, [hl]
 	and a
 	ret z
 
 ; Can't steal mail.
-
 	ld [wNamedObjectIndex], a
 	ld d, a
 	farcall ItemIsMail
@@ -53,21 +48,18 @@ BattleCommand_Thief:
 .enemy
 
 ; The enemy can't already have an item.
-
 	call .enemyitem
 	ld a, [hl]
 	and a
 	ret nz
 
 ; The player must have an item to steal.
-
 	call .playeritem
 	ld a, [hl]
 	and a
 	ret z
 
 ; Can't steal mail!
-
 	ld [wNamedObjectIndex], a
 	ld d, a
 	farcall ItemIsMail
@@ -78,7 +70,6 @@ BattleCommand_Thief:
 	ret nz
 
 ; If the enemy steals your item, it's gone for good if you don't get it back.
-
 	call .playeritem
 	xor a
 	ld [hl], a
