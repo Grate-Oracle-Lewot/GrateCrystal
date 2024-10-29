f7278d3bdf9235fbb0cd9de954777837dcff70f7
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-10-28 23:27:45 -0700

    Update rain_dance_moves.asm

--- data/battle/ai/rain_dance_moves.asm
+++ data/battle/ai/rain_dance_moves.asm
@@ -1,4 +1,8 @@
-; AI_SMART prefers these moves during rain.
+; AI_SMART encourages Rain Dance if the user also knows any of these moves.
+; These moves aren't encouraged any more than usual during rain,
+; but AI_AGGRESSIVE will pick them if they do the most damage.
+; Thunder has its own AI that does encourage it in the rain.
+; Scald is absent from this list due to rain preventing burns.
 
 RainDanceMoves:
 	db WATER_GUN
@@ -6,7 +10,6 @@ RainDanceMoves:
 	db SURF
 	db BUBBLEBEAM
 	db THUNDER
-	db SCALD
 	db WATERFALL
 	db CLAMP
 	db BUBBLE
