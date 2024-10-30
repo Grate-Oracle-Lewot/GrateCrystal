c3ef39304a09861ee35fb491897f84f94d3fbb3c
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-10-29 21:12:02 -0700

    Update catch_rate_items.asm

--- data/items/catch_rate_items.asm
+++ data/items/catch_rate_items.asm
@@ -1,6 +1,5 @@
-; Pokémon traded from RBY do not have held items, so GSC usually interprets the
-; catch rate as an item. However, if the catch rate appears in this table, the
-; item associated with the table entry is used instead.
+; Pokémon traded from RBY do not have held items, so GSC usually interprets the catch rate as an item.
+; However, if the catch rate appears in this table, the item associated with the table entry is used instead.
 
 TimeCapsule_CatchRateItems:
 	db BICYCLE,       GOLD_BERRY
