f2426d81859d1c5aba9c3c9b7fbb90995ef85a22
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-11-12 12:20:51 -0800

    Update core.asm

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -6805,7 +6805,66 @@ ApplyStatusEffectOnEnemyStats:
 ApplyStatusEffectOnStats:
 	ldh [hBattleTurn], a
 	call ApplyPrzEffectOnSpeed
-	jp ApplyBrnEffectOnAttack
+	; fallthrough
+
+ApplyBrnEffectOnAttack:
+	ldh a, [hBattleTurn]
+	and a
+	jr z, .enemy
+	ld a, [wBattleMonStatus]
+	and 1 << BRN
+	ret z
+
+	ld hl, wBattleMonType1
+	ld a, [hli]
+	cp FIGHTING
+	ret z
+	ld a, [hl]
+	cp FIGHTING
+	ret z
+
+	ld hl, wBattleMonAttack + 1
+	ld a, [hld]
+	ld b, a
+	ld a, [hl]
+	srl a
+	rr b
+	ld [hli], a
+	or b
+	jr nz, .player_ok
+	ld b, $1 ; min attack
+
+.player_ok
+	ld [hl], b
+	ret
+
+.enemy
+	ld a, [wEnemyMonStatus]
+	and 1 << BRN
+	ret z
+
+	ld hl, wEnemyMonType1
+	ld a, [hli]
+	cp FIGHTING
+	ret z
+	ld a, [hl]
+	cp FIGHTING
+	ret z
+
+	ld hl, wEnemyMonAttack + 1
+	ld a, [hld]
+	ld b, a
+	ld a, [hl]
+	srl a
+	rr b
+	ld [hli], a
+	or b
+	jr nz, .enemy_ok
+	ld b, $1 ; min attack
+
+.enemy_ok
+	ld [hl], b
+	ret
 
 ApplyPrzEffectOnSpeed:
 	ldh a, [hBattleTurn]
@@ -6865,65 +6924,6 @@ ApplyPrzEffectOnSpeed:
 	or b
 	jr nz, .enemy_ok
 	ld b, $1 ; min speed
-
-.enemy_ok
-	ld [hl], b
-	ret
-
-ApplyBrnEffectOnAttack:
-	ldh a, [hBattleTurn]
-	and a
-	jr z, .enemy
-	ld a, [wBattleMonStatus]
-	and 1 << BRN
-	ret z
-
-	ld hl, wBattleMonType1
-	ld a, [hli]
-	cp FIGHTING
-	ret z
-	ld a, [hl]
-	cp FIGHTING
-	ret z
-
-	ld hl, wBattleMonAttack + 1
-	ld a, [hld]
-	ld b, a
-	ld a, [hl]
-	srl a
-	rr b
-	ld [hli], a
-	or b
-	jr nz, .player_ok
-	ld b, $1 ; min attack
-
-.player_ok
-	ld [hl], b
-	ret
-
-.enemy
-	ld a, [wEnemyMonStatus]
-	and 1 << BRN
-	ret z
-
-	ld hl, wEnemyMonType1
-	ld a, [hli]
-	cp FIGHTING
-	ret z
-	ld a, [hl]
-	cp FIGHTING
-	ret z
-
-	ld hl, wEnemyMonAttack + 1
-	ld a, [hld]
-	ld b, a
-	ld a, [hl]
-	srl a
-	rr b
-	ld [hli], a
-	or b
-	jr nz, .enemy_ok
-	ld b, $1 ; min attack
 
 .enemy_ok
 	ld [hl], b
