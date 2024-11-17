ffbae676cc4ebae433912e08f0125c7ed6e58197
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-11-16 19:16:36 -0800

    Update dittos.asm

--- data/battle_tower/dittos.asm
+++ data/battle_tower/dittos.asm
@@ -7,6 +7,240 @@ BattleTowerDittos:
 	dw 0 ; OT ID
 	dt 0 ; Exp
 	; Stat exp
+	bigdw 20535
+	bigdw 20535
+	bigdw 20535
+	bigdw 20535
+	bigdw 20535
+	dn 15, 15, 15, 15 ; DVs
+	db 8, 8, 0, 0 ; PP
+	db 255 ; Happiness
+	db 0, 0, 0 ; Pokerus, Caught data
+	db 10 ; Level
+	db 0, 0 ; Status
+	bigdw 303 ; HP
+	bigdw 303 ; Max HP
+	bigdw 198 ; Atk
+	bigdw 198 ; Def
+	bigdw 598 ; Spd
+	bigdw 198 ; SAtk
+	bigdw 198 ; SDef
+	db "@@@@@@@@@@@"
+
+	db DITTO
+	db X_SPEED
+	db TRANSFORM, SELFDESTRUCT, NO_MOVE, NO_MOVE
+	dw 0 ; OT ID
+	dt 0 ; Exp
+	; Stat exp
+	bigdw 25535
+	bigdw 25535
+	bigdw 25535
+	bigdw 25535
+	bigdw 25535
+	dn 15, 15, 15, 15 ; DVs
+	db 8, 8, 0, 0 ; PP
+	db 255 ; Happiness
+	db 0, 0, 0 ; Pokerus, Caught data
+	db 20 ; Level
+	db 0, 0 ; Status
+	bigdw 303 ; HP
+	bigdw 303 ; Max HP
+	bigdw 198 ; Atk
+	bigdw 198 ; Def
+	bigdw 598 ; Spd
+	bigdw 198 ; SAtk
+	bigdw 198 ; SDef
+	db "@@@@@@@@@@@"
+
+	db DITTO
+	db X_SPEED
+	db TRANSFORM, SELFDESTRUCT, NO_MOVE, NO_MOVE
+	dw 0 ; OT ID
+	dt 0 ; Exp
+	; Stat exp
+	bigdw 30535
+	bigdw 30535
+	bigdw 30535
+	bigdw 30535
+	bigdw 30535
+	dn 15, 15, 15, 15 ; DVs
+	db 8, 8, 0, 0 ; PP
+	db 255 ; Happiness
+	db 0, 0, 0 ; Pokerus, Caught data
+	db 30 ; Level
+	db 0, 0 ; Status
+	bigdw 303 ; HP
+	bigdw 303 ; Max HP
+	bigdw 198 ; Atk
+	bigdw 198 ; Def
+	bigdw 598 ; Spd
+	bigdw 198 ; SAtk
+	bigdw 198 ; SDef
+	db "@@@@@@@@@@@"
+
+	db DITTO
+	db X_SPEED
+	db TRANSFORM, SELFDESTRUCT, NO_MOVE, NO_MOVE
+	dw 0 ; OT ID
+	dt 0 ; Exp
+	; Stat exp
+	bigdw 35535
+	bigdw 35535
+	bigdw 35535
+	bigdw 35535
+	bigdw 35535
+	dn 15, 15, 15, 15 ; DVs
+	db 8, 8, 0, 0 ; PP
+	db 255 ; Happiness
+	db 0, 0, 0 ; Pokerus, Caught data
+	db 40 ; Level
+	db 0, 0 ; Status
+	bigdw 303 ; HP
+	bigdw 303 ; Max HP
+	bigdw 198 ; Atk
+	bigdw 198 ; Def
+	bigdw 598 ; Spd
+	bigdw 198 ; SAtk
+	bigdw 198 ; SDef
+	db "@@@@@@@@@@@"
+
+	db DITTO
+	db X_SPEED
+	db TRANSFORM, SELFDESTRUCT, NO_MOVE, NO_MOVE
+	dw 0 ; OT ID
+	dt 0 ; Exp
+	; Stat exp
+	bigdw 40535
+	bigdw 40535
+	bigdw 40535
+	bigdw 40535
+	bigdw 40535
+	dn 15, 15, 15, 15 ; DVs
+	db 8, 8, 0, 0 ; PP
+	db 255 ; Happiness
+	db 0, 0, 0 ; Pokerus, Caught data
+	db 50 ; Level
+	db 0, 0 ; Status
+	bigdw 303 ; HP
+	bigdw 303 ; Max HP
+	bigdw 198 ; Atk
+	bigdw 198 ; Def
+	bigdw 598 ; Spd
+	bigdw 198 ; SAtk
+	bigdw 198 ; SDef
+	db "@@@@@@@@@@@"
+
+	db DITTO
+	db X_SPEED
+	db TRANSFORM, SELFDESTRUCT, NO_MOVE, NO_MOVE
+	dw 0 ; OT ID
+	dt 0 ; Exp
+	; Stat exp
+	bigdw 45535
+	bigdw 45535
+	bigdw 45535
+	bigdw 45535
+	bigdw 45535
+	dn 15, 15, 15, 15 ; DVs
+	db 8, 8, 0, 0 ; PP
+	db 255 ; Happiness
+	db 0, 0, 0 ; Pokerus, Caught data
+	db 60 ; Level
+	db 0, 0 ; Status
+	bigdw 303 ; HP
+	bigdw 303 ; Max HP
+	bigdw 198 ; Atk
+	bigdw 198 ; Def
+	bigdw 598 ; Spd
+	bigdw 198 ; SAtk
+	bigdw 198 ; SDef
+	db "@@@@@@@@@@@"
+
+	db DITTO
+	db X_SPEED
+	db TRANSFORM, SELFDESTRUCT, NO_MOVE, NO_MOVE
+	dw 0 ; OT ID
+	dt 0 ; Exp
+	; Stat exp
+	bigdw 50535
+	bigdw 50535
+	bigdw 50535
+	bigdw 50535
+	bigdw 50535
+	dn 15, 15, 15, 15 ; DVs
+	db 8, 8, 0, 0 ; PP
+	db 255 ; Happiness
+	db 0, 0, 0 ; Pokerus, Caught data
+	db 70 ; Level
+	db 0, 0 ; Status
+	bigdw 303 ; HP
+	bigdw 303 ; Max HP
+	bigdw 198 ; Atk
+	bigdw 198 ; Def
+	bigdw 598 ; Spd
+	bigdw 198 ; SAtk
+	bigdw 198 ; SDef
+	db "@@@@@@@@@@@"
+
+	db DITTO
+	db X_SPEED
+	db TRANSFORM, SELFDESTRUCT, NO_MOVE, NO_MOVE
+	dw 0 ; OT ID
+	dt 0 ; Exp
+	; Stat exp
+	bigdw 55535
+	bigdw 55535
+	bigdw 55535
+	bigdw 55535
+	bigdw 55535
+	dn 15, 15, 15, 15 ; DVs
+	db 8, 8, 0, 0 ; PP
+	db 255 ; Happiness
+	db 0, 0, 0 ; Pokerus, Caught data
+	db 80 ; Level
+	db 0, 0 ; Status
+	bigdw 303 ; HP
+	bigdw 303 ; Max HP
+	bigdw 198 ; Atk
+	bigdw 198 ; Def
+	bigdw 598 ; Spd
+	bigdw 198 ; SAtk
+	bigdw 198 ; SDef
+	db "@@@@@@@@@@@"
+
+	db DITTO
+	db X_SPEED
+	db TRANSFORM, SELFDESTRUCT, NO_MOVE, NO_MOVE
+	dw 0 ; OT ID
+	dt 0 ; Exp
+	; Stat exp
+	bigdw 60535
+	bigdw 60535
+	bigdw 60535
+	bigdw 60535
+	bigdw 60535
+	dn 15, 15, 15, 15 ; DVs
+	db 8, 8, 0, 0 ; PP
+	db 255 ; Happiness
+	db 0, 0, 0 ; Pokerus, Caught data
+	db 90 ; Level
+	db 0, 0 ; Status
+	bigdw 303 ; HP
+	bigdw 303 ; Max HP
+	bigdw 198 ; Atk
+	bigdw 198 ; Def
+	bigdw 598 ; Spd
+	bigdw 198 ; SAtk
+	bigdw 198 ; SDef
+	db "@@@@@@@@@@@"
+
+	db DITTO
+	db X_SPEED
+	db TRANSFORM, SELFDESTRUCT, NO_MOVE, NO_MOVE
+	dw 0 ; OT ID
+	dt 0 ; Exp
+	; Stat exp
 	bigdw 65535
 	bigdw 65535
 	bigdw 65535
