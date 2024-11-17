32efaf89551dced564eb546e8d58cfd653ce6af4
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-11-16 21:16:48 -0800

    Update jirks.asm

--- data/battle_tower/jirks.asm
+++ data/battle_tower/jirks.asm
@@ -1,264 +1,264 @@
-BattleTowerDittos:
-	table_width NICKNAMED_MON_STRUCT_LENGTH, BattleTowerDittos
+BattleTowerJirks:
+	table_width NICKNAMED_MON_STRUCT_LENGTH, BattleTowerJirks
 
-	db DITTO
-	db X_SPEED
-	db TRANSFORM, SELFDESTRUCT, NO_MOVE, NO_MOVE
+	db JIRK
+	db X_EVADE
+	db CONFUSE_RAY, SPORE, MIND_READER, HORN_DRILL
 	dw 0 ; OT ID
 	dt 0 ; Exp
 	; Stat exp
-	bigdw 20535
-	bigdw 20535
-	bigdw 20535
-	bigdw 20535
-	bigdw 20535
-	dn 15, 15, 15, 15 ; DVs
-	db 8, 8, 0, 0 ; PP
-	db 255 ; Happiness
+	bigdw 50000
+	bigdw 0
+	bigdw 50000
+	bigdw 50000
+	bigdw 50000
+	dn 1, 15, 15, 1 ; DVs
+	db 16, 16, 8, 8 ; PP
+	db 0 ; Happiness
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 10 ; Level
 	db 0, 0 ; Status
-	bigdw 36 ; HP
-	bigdw 36 ; Max HP
-	bigdw 21 ; Atk
-	bigdw 21 ; Def
-	bigdw 61 ; Spd
-	bigdw 21 ; SAtk
-	bigdw 21 ; SDef
+	bigdw 323 ; HP
+	bigdw 323 ; Max HP
+	bigdw 155 ; Atk
+	bigdw 218 ; Def
+	bigdw 290 ; Spd
+	bigdw 230 ; SAtk
+	bigdw 190 ; SDef
 	db "@@@@@@@@@@@"
 
-	db DITTO
-	db X_SPEED
-	db TRANSFORM, SELFDESTRUCT, NO_MOVE, NO_MOVE
+	db JIRK
+	db X_EVADE
+	db CONFUSE_RAY, SPORE, MIND_READER, HORN_DRILL
 	dw 0 ; OT ID
 	dt 0 ; Exp
 	; Stat exp
-	bigdw 25535
-	bigdw 25535
-	bigdw 25535
-	bigdw 25535
-	bigdw 25535
-	dn 15, 15, 15, 15 ; DVs
-	db 8, 8, 0, 0 ; PP
-	db 255 ; Happiness
+	bigdw 50000
+	bigdw 0
+	bigdw 50000
+	bigdw 50000
+	bigdw 50000
+	dn 1, 15, 15, 1 ; DVs
+	db 16, 16, 8, 8 ; PP
+	db 0 ; Happiness
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 20 ; Level
 	db 0, 0 ; Status
-	bigdw 63 ; HP
-	bigdw 63 ; Max HP
-	bigdw 38 ; Atk
-	bigdw 38 ; Def
-	bigdw 118 ; Spd
-	bigdw 38 ; SAtk
-	bigdw 38 ; SDef
+	bigdw 323 ; HP
+	bigdw 323 ; Max HP
+	bigdw 155 ; Atk
+	bigdw 218 ; Def
+	bigdw 290 ; Spd
+	bigdw 230 ; SAtk
+	bigdw 190 ; SDef
 	db "@@@@@@@@@@@"
 
-	db DITTO
-	db X_SPEED
-	db TRANSFORM, SELFDESTRUCT, NO_MOVE, NO_MOVE
+	db JIRK
+	db X_EVADE
+	db CONFUSE_RAY, SPORE, MIND_READER, HORN_DRILL
 	dw 0 ; OT ID
 	dt 0 ; Exp
 	; Stat exp
-	bigdw 30535
-	bigdw 30535
-	bigdw 30535
-	bigdw 30535
-	bigdw 30535
-	dn 15, 15, 15, 15 ; DVs
-	db 8, 8, 0, 0 ; PP
-	db 255 ; Happiness
+	bigdw 50000
+	bigdw 0
+	bigdw 50000
+	bigdw 50000
+	bigdw 50000
+	dn 1, 15, 15, 1 ; DVs
+	db 16, 16, 8, 8 ; PP
+	db 0 ; Happiness
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 30 ; Level
 	db 0, 0 ; Status
-	bigdw 91 ; HP
-	bigdw 91 ; Max HP
-	bigdw 56 ; Atk
-	bigdw 56 ; Def
-	bigdw 176 ; Spd
-	bigdw 56 ; SAtk
-	bigdw 56 ; SDef
+	bigdw 323 ; HP
+	bigdw 323 ; Max HP
+	bigdw 155 ; Atk
+	bigdw 218 ; Def
+	bigdw 290 ; Spd
+	bigdw 230 ; SAtk
+	bigdw 190 ; SDef
 	db "@@@@@@@@@@@"
 
-	db DITTO
-	db X_SPEED
-	db TRANSFORM, SELFDESTRUCT, NO_MOVE, NO_MOVE
+	db JIRK
+	db X_EVADE
+	db CONFUSE_RAY, SPORE, MIND_READER, HORN_DRILL
 	dw 0 ; OT ID
 	dt 0 ; Exp
 	; Stat exp
-	bigdw 35535
-	bigdw 35535
-	bigdw 35535
-	bigdw 35535
-	bigdw 35535
-	dn 15, 15, 15, 15 ; DVs
-	db 8, 8, 0, 0 ; PP
-	db 255 ; Happiness
+	bigdw 50000
+	bigdw 0
+	bigdw 50000
+	bigdw 50000
+	bigdw 50000
+	dn 1, 15, 15, 1 ; DVs
+	db 16, 16, 8, 8 ; PP
+	db 0 ; Happiness
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 40 ; Level
 	db 0, 0 ; Status
-	bigdw 120 ; HP
-	bigdw 120 ; Max HP
-	bigdw 75 ; Atk
-	bigdw 75 ; Def
-	bigdw 235 ; Spd
-	bigdw 75 ; SAtk
-	bigdw 75 ; SDef
+	bigdw 323 ; HP
+	bigdw 323 ; Max HP
+	bigdw 155 ; Atk
+	bigdw 218 ; Def
+	bigdw 290 ; Spd
+	bigdw 230 ; SAtk
+	bigdw 190 ; SDef
 	db "@@@@@@@@@@@"
 
-	db DITTO
-	db X_SPEED
-	db TRANSFORM, SELFDESTRUCT, NO_MOVE, NO_MOVE
+	db JIRK
+	db X_EVADE
+	db CONFUSE_RAY, SPORE, MIND_READER, HORN_DRILL
 	dw 0 ; OT ID
 	dt 0 ; Exp
 	; Stat exp
-	bigdw 40535
-	bigdw 40535
-	bigdw 40535
-	bigdw 40535
-	bigdw 40535
-	dn 15, 15, 15, 15 ; DVs
-	db 8, 8, 0, 0 ; PP
-	db 255 ; Happiness
+	bigdw 50000
+	bigdw 0
+	bigdw 50000
+	bigdw 50000
+	bigdw 50000
+	dn 1, 15, 15, 1 ; DVs
+	db 16, 16, 8, 8 ; PP
+	db 0 ; Happiness
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 50 ; Level
 	db 0, 0 ; Status
-	bigdw 150 ; HP
-	bigdw 150 ; Max HP
-	bigdw 95 ; Atk
-	bigdw 95 ; Def
-	bigdw 295 ; Spd
-	bigdw 95 ; SAtk
-	bigdw 95 ; SDef
+	bigdw 323 ; HP
+	bigdw 323 ; Max HP
+	bigdw 155 ; Atk
+	bigdw 218 ; Def
+	bigdw 290 ; Spd
+	bigdw 230 ; SAtk
+	bigdw 190 ; SDef
 	db "@@@@@@@@@@@"
 
-	db DITTO
-	db X_SPEED
-	db TRANSFORM, SELFDESTRUCT, NO_MOVE, NO_MOVE
+	db JIRK
+	db X_EVADE
+	db CONFUSE_RAY, SPORE, MIND_READER, HORN_DRILL
 	dw 0 ; OT ID
 	dt 0 ; Exp
 	; Stat exp
-	bigdw 45535
-	bigdw 45535
-	bigdw 45535
-	bigdw 45535
-	bigdw 45535
-	dn 15, 15, 15, 15 ; DVs
-	db 8, 8, 0, 0 ; PP
-	db 255 ; Happiness
+	bigdw 50000
+	bigdw 0
+	bigdw 50000
+	bigdw 50000
+	bigdw 50000
+	dn 1, 15, 15, 1 ; DVs
+	db 16, 16, 8, 8 ; PP
+	db 0 ; Happiness
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 60 ; Level
 	db 0, 0 ; Status
-	bigdw 179 ; HP
-	bigdw 179 ; Max HP
-	bigdw 114 ; Atk
-	bigdw 114 ; Def
-	bigdw 354 ; Spd
-	bigdw 114 ; SAtk
-	bigdw 114 ; SDef
+	bigdw 323 ; HP
+	bigdw 323 ; Max HP
+	bigdw 155 ; Atk
+	bigdw 218 ; Def
+	bigdw 290 ; Spd
+	bigdw 230 ; SAtk
+	bigdw 190 ; SDef
 	db "@@@@@@@@@@@"
 
-	db DITTO
-	db X_SPEED
-	db TRANSFORM, SELFDESTRUCT, NO_MOVE, NO_MOVE
+	db JIRK
+	db X_EVADE
+	db CONFUSE_RAY, SPORE, MIND_READER, HORN_DRILL
 	dw 0 ; OT ID
 	dt 0 ; Exp
 	; Stat exp
-	bigdw 50535
-	bigdw 50535
-	bigdw 50535
-	bigdw 50535
-	bigdw 50535
-	dn 15, 15, 15, 15 ; DVs
-	db 8, 8, 0, 0 ; PP
-	db 255 ; Happiness
+	bigdw 50000
+	bigdw 0
+	bigdw 50000
+	bigdw 50000
+	bigdw 50000
+	dn 1, 15, 15, 1 ; DVs
+	db 16, 16, 8, 8 ; PP
+	db 0 ; Happiness
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 70 ; Level
 	db 0, 0 ; Status
-	bigdw 210 ; HP
-	bigdw 210 ; Max HP
-	bigdw 135 ; Atk
-	bigdw 135 ; Def
-	bigdw 415 ; Spd
-	bigdw 135 ; SAtk
-	bigdw 135 ; SDef
+	bigdw 323 ; HP
+	bigdw 323 ; Max HP
+	bigdw 155 ; Atk
+	bigdw 218 ; Def
+	bigdw 290 ; Spd
+	bigdw 230 ; SAtk
+	bigdw 190 ; SDef
 	db "@@@@@@@@@@@"
 
-	db DITTO
-	db X_SPEED
-	db TRANSFORM, SELFDESTRUCT, NO_MOVE, NO_MOVE
+	db JIRK
+	db X_EVADE
+	db CONFUSE_RAY, SPORE, MIND_READER, HORN_DRILL
 	dw 0 ; OT ID
 	dt 0 ; Exp
 	; Stat exp
-	bigdw 55535
-	bigdw 55535
-	bigdw 55535
-	bigdw 55535
-	bigdw 55535
-	dn 15, 15, 15, 15 ; DVs
-	db 8, 8, 0, 0 ; PP
-	db 255 ; Happiness
+	bigdw 50000
+	bigdw 0
+	bigdw 50000
+	bigdw 50000
+	bigdw 50000
+	dn 1, 15, 15, 1 ; DVs
+	db 16, 16, 8, 8 ; PP
+	db 0 ; Happiness
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 80 ; Level
 	db 0, 0 ; Status
-	bigdw 240 ; HP
-	bigdw 240 ; Max HP
+	bigdw 323 ; HP
+	bigdw 323 ; Max HP
 	bigdw 155 ; Atk
-	bigdw 155 ; Def
-	bigdw 475 ; Spd
-	bigdw 155 ; SAtk
-	bigdw 155 ; SDef
+	bigdw 218 ; Def
+	bigdw 290 ; Spd
+	bigdw 230 ; SAtk
+	bigdw 190 ; SDef
 	db "@@@@@@@@@@@"
 
-	db DITTO
-	db X_SPEED
-	db TRANSFORM, SELFDESTRUCT, NO_MOVE, NO_MOVE
+	db JIRK
+	db X_EVADE
+	db CONFUSE_RAY, SPORE, MIND_READER, HORN_DRILL
 	dw 0 ; OT ID
 	dt 0 ; Exp
 	; Stat exp
-	bigdw 60535
-	bigdw 60535
-	bigdw 60535
-	bigdw 60535
-	bigdw 60535
-	dn 15, 15, 15, 15 ; DVs
-	db 8, 8, 0, 0 ; PP
-	db 255 ; Happiness
+	bigdw 50000
+	bigdw 0
+	bigdw 50000
+	bigdw 50000
+	bigdw 50000
+	dn 1, 15, 15, 1 ; DVs
+	db 16, 16, 8, 8 ; PP
+	db 0 ; Happiness
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 90 ; Level
 	db 0, 0 ; Status
-	bigdw 271 ; HP
-	bigdw 271 ; Max HP
-	bigdw 176 ; Atk
-	bigdw 176 ; Def
-	bigdw 536 ; Spd
-	bigdw 176 ; SAtk
-	bigdw 176 ; SDef
+	bigdw 323 ; HP
+	bigdw 323 ; Max HP
+	bigdw 155 ; Atk
+	bigdw 218 ; Def
+	bigdw 290 ; Spd
+	bigdw 230 ; SAtk
+	bigdw 190 ; SDef
 	db "@@@@@@@@@@@"
 
-	db DITTO
-	db X_SPEED
-	db TRANSFORM, SELFDESTRUCT, NO_MOVE, NO_MOVE
+	db JIRK
+	db X_EVADE
+	db CONFUSE_RAY, SPORE, MIND_READER, HORN_DRILL
 	dw 0 ; OT ID
 	dt 0 ; Exp
 	; Stat exp
-	bigdw 65535
-	bigdw 65535
-	bigdw 65535
-	bigdw 65535
-	bigdw 65535
-	dn 15, 15, 15, 15 ; DVs
-	db 8, 8, 0, 0 ; PP
-	db 255 ; Happiness
+	bigdw 50000
+	bigdw 0
+	bigdw 50000
+	bigdw 50000
+	bigdw 50000
+	dn 1, 15, 15, 1 ; DVs
+	db 16, 16, 8, 8 ; PP
+	db 0 ; Happiness
 	db 0, 0, 0 ; Pokerus, Caught data
 	db 100 ; Level
 	db 0, 0 ; Status
-	bigdw 303 ; HP
-	bigdw 303 ; Max HP
-	bigdw 198 ; Atk
-	bigdw 198 ; Def
-	bigdw 598 ; Spd
-	bigdw 198 ; SAtk
-	bigdw 198 ; SDef
+	bigdw 323 ; HP
+	bigdw 323 ; Max HP
+	bigdw 155 ; Atk
+	bigdw 218 ; Def
+	bigdw 290 ; Spd
+	bigdw 230 ; SAtk
+	bigdw 190 ; SDef
 	db "@@@@@@@@@@@"
 
 	assert_table_length 10
