a1716e86fcceac0f433e2c149105b146cee4c6b8
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-11-17 13:15:22 -0800

    Update mod_party.asm

--- engine/events/battle_tower/mod_party.asm
+++ engine/events/battle_tower/mod_party.asm
@@ -4,14 +4,14 @@ ModifyBattleTowerParty:
 
 	ld a, [sBTMonPrevTrainer1]
 	cp DITTO
-	jr z, .end
+	jr z, .jirk
 	ld a, [sBTMonPrevPrevTrainer1]
 	cp DITTO
-	jr z, .end
+	jr z, .jirk
 
 	call Random
 	cp 10
-	jr nc, .end
+	jr nc, .jirk
 
 	ld a, [wBTChoiceOfLvlGroup]
 	dec a
@@ -49,7 +49,57 @@ ModifyBattleTowerParty:
 	ld a, [wBT_OTMon1]
 	ld [sBTMonPrevTrainer1], a
 
+.jirk
+	ld a, [sBTMonPrevTrainer3]
+	cp JIRK
+	jr z, .end
+	ld a, [sBTMonPrevPrevTrainer3]
+	cp JIRK
+	jr z, .end
+
+	call Random
+	cp 10
+	jr nc, .end
+
+	ld a, [wBTChoiceOfLvlGroup]
+	dec a
+	ld hl, BattleTowerJirks
+	ld bc, NICKNAMED_MON_STRUCT_LENGTH
+	call AddNTimes
+
+	; overwrite the third mon in the current team
+	ld de, wBT_OTMon3
+	ld bc, NICKNAMED_MON_STRUCT_LENGTH
+	call CopyBytes
+
+	ld a, [wNamedObjectIndex]
+	push af
+	push de
+	ld hl, -NICKNAMED_MON_STRUCT_LENGTH
+	add hl, de
+	ld a, [hl]
+	ld [wNamedObjectIndex], a
+	ld bc, PARTYMON_STRUCT_LENGTH
+	add hl, bc
+	push hl
+	call GetPokemonName
+	ld h, d
+	ld l, e
+	pop de
+	ld bc, MON_NAME_LENGTH
+	call CopyBytes
+	pop de
+	pop af
+
+	ld [wNamedObjectIndex], a
+	ld a, [sBTMonPrevTrainer3]
+	ld [sBTMonPrevPrevTrainer3], a
+	ld a, [wBT_OTMon3]
+	ld [sBTMonPrevTrainer3], a
+
 .end
 	jp CloseSRAM
 
 INCLUDE "data/battle_tower/dittos.asm"
+
+INCLUDE "data/battle_tower/jirks.asm"
