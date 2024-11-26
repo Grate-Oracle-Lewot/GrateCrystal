d280e2d13dd71d813cb0a00856f726930f1b28be
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-11-26 15:15:22 -0800

    Create special_classes.asm

---
+++ data/battle_tower/special_classes.asm
@@ -0,0 +1,33 @@
+BattleTowerSpecialTrainers:
+	table_width (NAME_LENGTH - 1) + 1, BattleTowerSpecialTrainers
+	; name, class
+	db "BROCK@@@@@", BROCK
+	db "MISTY@@@@@", MISTY
+	db "LT.SURGE@@", LT_SURGE
+	db "ERIKA@@@@@", ERIKA
+	db "JANINE@@@@", JANINE
+	db "SABRINA@@@", SABRINA
+	db "BLAINE@@@@", BLAINE
+	db "BLUE@@@@@@", BLUE
+	db "FALKNER@@@", FALKNER
+	db "BUGSY@@@@@", BUGSY
+	db "WHITNEY@@@", WHITNEY
+	db "MORTY@@@@@", MORTY
+	db "CHUCK@@@@@", CHUCK
+	db "JASMINE@@@", JASMINE
+	db "PRYCE@@@@@", PRYCE
+	db "CLAIR@@@@@", CLAIR
+	db "KOGA@@@@@@", REAL_KOGA
+	db "KAREN@@@@@", REAL_KAREN
+	assert_table_length BATTLETOWER_NUM_SPECIAL_TRAINERS
+
+BattleTowerBossTrainers:
+	table_width (NAME_LENGTH - 1) + 1, BattleTowerBossTrainers
+	; name, class
+	db "A.D.@@@@@@", WILL
+	db "LUCAS@@@@@", KOGA
+	db "PERCY@@@@@", BRUNO
+	db "LEWOT@@@@@", KAREN
+	db "LANCE@@@@@", CHAMPION
+	db "RED@@@@@@@", RED
+	assert_table_length BATTLETOWER_NUM_BOSS_TRAINERS
