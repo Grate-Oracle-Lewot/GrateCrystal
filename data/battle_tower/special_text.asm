929bcc6b12313775e95170a80b0be9604a93f73d
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-11-26 20:56:48 -0800

    Create special_text.asm

---
+++ data/battle_tower/special_text.asm
@@ -0,0 +1,374 @@
+BTBrockGreetingText:
+	text ""
+	line ""
+	done
+
+BTBrockLossText:
+	text ""
+	line ""
+	done
+
+BTBrockWinText:
+	text ""
+	line ""
+	done
+
+BTMistyGreetingText:
+	text ""
+	line ""
+	done
+
+BTMistyLossText:
+	text ""
+	line ""
+	done
+
+BTMistyWinText:
+	text ""
+	line ""
+	done
+
+BTLtSurgeGreetingText:
+	text ""
+	line ""
+	done
+
+BTLtSurgeLossText:
+	text ""
+	line ""
+	done
+
+BTLtSurgeWinText:
+	text ""
+	line ""
+	done
+
+BTErikaGreetingText:
+	text ""
+	line ""
+	done
+
+BTErikaLossText:
+	text ""
+	line ""
+	done
+
+BTErikaWinText:
+	text ""
+	line ""
+	done
+
+BTJanineGreetingText:
+	text ""
+	line ""
+	done
+
+BTJanineLossText:
+	text ""
+	line ""
+	done
+
+BTJanineWinText:
+	text ""
+	line ""
+	done
+
+BTSabrinaGreetingText:
+	text ""
+	line ""
+	done
+
+BTSabrinaLossText:
+	text ""
+	line ""
+	done
+
+BTSabrinaWinText:
+	text ""
+	line ""
+	done
+
+BTBlaineGreetingText:
+	text ""
+	line ""
+	done
+
+BTBlaineLossText:
+	text ""
+	line ""
+	done
+
+BTBlaineWinText:
+	text ""
+	line ""
+	done
+
+BTBlueGreetingText:
+	text ""
+	line ""
+	done
+
+BTBlueLossText:
+	text ""
+	line ""
+	done
+
+BTBlueWinText:
+	text ""
+	line ""
+	done
+
+BTFalknerGreetingText:
+	text ""
+	line ""
+	done
+
+BTFalknerLossText:
+	text ""
+	line ""
+	done
+
+BTFalknerWinText:
+	text ""
+	line ""
+	done
+
+BTBugsyGreetingText:
+	text ""
+	line ""
+	done
+
+BTBugsyLossText:
+	text ""
+	line ""
+	done
+
+BTBugsyWinText:
+	text ""
+	line ""
+	done
+
+BTWhitneyGreetingText:
+	text ""
+	line ""
+	done
+
+BTWhitneyLossText:
+	text ""
+	line ""
+	done
+
+BTWhitneyWinText:
+	text ""
+	line ""
+	done
+
+BTMortyGreetingText:
+	text ""
+	line ""
+	done
+
+BTMortyLossText:
+	text ""
+	line ""
+	done
+
+BTMortyWinText:
+	text ""
+	line ""
+	done
+
+BTChuckGreetingText:
+	text ""
+	line ""
+	done
+
+BTChuckLossText:
+	text ""
+	line ""
+	done
+
+BTChuckWinText:
+	text ""
+	line ""
+	done
+
+BTJasmineGreetingText:
+	text ""
+	line ""
+	done
+
+BTJasmineLossText:
+	text ""
+	line ""
+	done
+
+BTJasmineWinText:
+	text ""
+	line ""
+	done
+
+BTPryceGreetingText:
+	text ""
+	line ""
+	done
+
+BTPryceLossText:
+	text ""
+	line ""
+	done
+
+BTPryceWinText:
+	text ""
+	line ""
+	done
+
+BTClairGreetingText:
+	text ""
+	line ""
+	done
+
+BTClairLossText:
+	text ""
+	line ""
+	done
+
+BTClairWinText:
+	text ""
+	line ""
+	done
+
+BTKogaGreetingText:
+	text ""
+	line ""
+	done
+
+BTKogaLossText:
+	text ""
+	line ""
+	done
+
+BTKogaWinText:
+	text ""
+	line ""
+	done
+
+BTKarenGreetingText:
+	text ""
+	line ""
+	done
+
+BTKarenLossText:
+	text ""
+	line ""
+	done
+
+BTKarenWinText:
+	text ""
+	line ""
+	done
+
+BTADGreetingText:
+	text ""
+	line ""
+	done
+
+BTADLossText:
+	text ""
+	line ""
+	done
+
+BTADWinText:
+	text ""
+	line ""
+	done
+
+BTLucasGreetingText:
+	text ""
+	line ""
+	done
+
+BTLucasLossText:
+	text ""
+	line ""
+	done
+
+BTLucasWinText:
+	text ""
+	line ""
+	done
+
+BTPercyGreetingText:
+	text ""
+	line ""
+	done
+
+BTPercyLossText:
+	text ""
+	line ""
+	done
+
+BTPercyWinText:
+	text ""
+	line ""
+	done
+
+BTLewotGreetingText:
+	text ""
+	line ""
+	done
+
+BTLewotLossText:
+	text ""
+	line ""
+	done
+
+BTLewotWinText:
+	text ""
+	line ""
+	done
+
+BTLanceGreetingText:
+	text ""
+	line ""
+	done
+
+BTLanceLossText:
+	text ""
+	line ""
+	done
+
+BTLanceWinText:
+	text ""
+	line ""
+	done
+
+BTRedGreetingText:
+	text ""
+	line ""
+	done
+
+BTRedLossText:
+	text ""
+	line ""
+	done
+
+BTRedWinText:
+	text ""
+	line ""
+	done
+
+BTDefaultGreetingText:
+	text ""
+	line ""
+	done
+
+BTDefaultLossText:
+	text ""
+	line ""
+	done
+
+BTDefaultWinText:
+	text ""
+	line ""
+	done
