44a16102b1c8c75352c6815c9730e5ec163c1aab
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-11-29 16:33:04 -0800

    Update BattleTower1F.asm

--- maps/BattleTower1F.asm
+++ maps/BattleTower1F.asm
@@ -85,34 +85,18 @@ Script_ChooseChallenge:
 	setscene SCENE_FINISHED
 	setval BATTLETOWERACTION_SET_EXPLANATION_READ ; set 1, [sBattleTowerSaveFileFlags]
 	special BattleTowerAction
-	clearevent EVENT_BATTLE_TOWER_TOP_FLOOR
 	checkevent EVENT_BEAT_RED
-	iffalse .SkipTopFloor
-	writetext Text_ChallengeTheTopFloor
-	yesorno
-	iftrue Script_ChallengeTopFloor
-.SkipTopFloor:
 	special BattleTowerRoomMenu
 	ifequal $a, Script_Menu_ChallengeExplanationCancel
 	ifnotequal $0, Script_MobileError
 	setval BATTLETOWERACTION_11
 	special BattleTowerAction
-Script_RightThisWay:
 	writetext Text_RightThisWayToYourBattleRoom
 	waitbutton
 	closetext
 	setval BATTLETOWERACTION_CHOOSEREWARD
 	special BattleTowerAction
 	sjump Script_WalkToBattleTowerElevator
-
-Script_ChallengeTopFloor:
-	setevent EVENT_BATTLE_TOWER_TOP_FLOOR
-	callasm .CallAsm
-	sjump Script_RightThisWay
-
-.CallAsm:
-	farcall BattleTowerTopFloorLoadSixSpecialAndOneBossTrainer
-	ret
 
 Script_ResumeBattleTowerChallenge:
 	closetext
@@ -348,16 +332,6 @@ Text_BattleTowerIntroduction_2:
 
 	para "resume your ROOM"
 	line "challenge."
-
-	para "On the top floor,"
-	line "you can battle GYM"
-	cont "LEADERS, ELITE"
-	cont "FOUR members, and"
-	cont "CHAMPIONS."
-
-	para "Only accomplished"
-	line "trainers may take"
-	cont "on the top floor."
 
 	para ""
 	done
