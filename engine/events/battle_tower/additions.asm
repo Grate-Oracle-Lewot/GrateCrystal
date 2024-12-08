af77f036d178b53709fc7947d72fa8cf1ce0f77e
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-12-08 15:01:29 -0800

    Update additions.asm

--- engine/events/battle_tower/additions.asm
+++ engine/events/battle_tower/additions.asm
@@ -192,6 +192,23 @@ BattleTowerLoadRed:
 BattleTowerLoadSpecialParty:
 	ld bc, NICKNAMED_MON_STRUCT_LENGTH * 3
 	call CopyBytes
+
+	ld a, BANK(sBTMonPrevTrainer1)
+	call OpenSRAM
+	ld a, [sBTMonPrevTrainer1]
+	ld [sBTMonPrevPrevTrainer1], a
+	ld a, [sBTMonPrevTrainer2]
+	ld [sBTMonPrevPrevTrainer2], a
+	ld a, [sBTMonPrevTrainer3]
+	ld [sBTMonPrevPrevTrainer3], a
+	ld a, [wBT_OTMon1]
+	ld [sBTMonPrevTrainer1], a
+	ld a, [wBT_OTMon2]
+	ld [sBTMonPrevTrainer2], a
+	ld a, [wBT_OTMon3]
+	ld [sBTMonPrevTrainer3], a
+	call CloseSRAM
+
 	pop af
 	ldh [rSVBK], a
 	ret
