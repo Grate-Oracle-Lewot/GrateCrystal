28d3f4d4d4453f15914098ef5d9977b2549324ac
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-12-21 12:19:53 -0800

    Update CeruleanCaveB1F.asm

--- maps/CeruleanCaveB1F.asm
+++ maps/CeruleanCaveB1F.asm
@@ -1,5 +1,6 @@
 	object_const_def
-	const CERULEANCAVEB1F_ROCK1
+	const CERULEANCAVEB1F_ROCK
+	const CERULEANCAVEB1F_POKE_BALL
 
 CeruleanCaveB1F_MapScripts:
 	def_scene_scripts
@@ -8,6 +9,9 @@ CeruleanCaveB1F_MapScripts:
 
 CeruleanCaveB1FRock:
 	jumpstd SmashRockScript
+
+CeruleanCaveB1FFearowbot:
+	itemball FEAROWBOT
 
 CeruleanCaveB1F_MapEvents:
 	db 0, 0 ; filler
@@ -22,3 +26,4 @@ CeruleanCaveB1F_MapEvents:
 
 	def_object_events
 	object_event 19,  6, SPRITE_ROCK, SPRITEMOVEDATA_SMASHABLE_ROCK, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CeruleanCaveB1FRock, -1
+	object_event 20,  3, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, PAL_NPC_SILVER, OBJECTTYPE_ITEMBALL, 0, CeruleanCaveB1FFearowbot, EVENT_CERULEAN_CAVE_B1F_FEAROWBOT
