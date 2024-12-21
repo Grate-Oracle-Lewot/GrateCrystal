997627dfaf5341fb8cc34082f62e9da1ba5cd027
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-12-21 15:06:22 -0800

    Update Route28SteelWingHouse.asm

--- maps/Route28SteelWingHouse.asm
+++ maps/Route28SteelWingHouse.asm
@@ -14,7 +14,7 @@ Celebrity:
 	iftrue .AlreadyGotItem
 	writetext CelebrityText1
 	promptbutton
-	verbosegiveitem FEAROWBOT
+	verbosegiveitem TM_STEEL_WING
 	iffalse .Done
 	setevent EVENT_GOT_TM47_STEEL_WING
 .Done:
