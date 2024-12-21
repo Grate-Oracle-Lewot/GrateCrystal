fdcc348a4d6559552cc9760fa2d0e7d35eb23dd9
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-12-21 11:49:08 -0800

    Create CeruleanCave2F.asm

---
+++ maps/CeruleanCave2F.asm
@@ -0,0 +1,24 @@
+	object_const_def
+
+CeruleanCave2F_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+CeruleanCave2F_MapEvents:
+	db 0, 0 ; filler
+
+	def_warp_events
+	warp_event 29, 11, CERULEAN_CAVE_1F, 2
+	warp_event 35,  5, CERULEAN_CAVE_1F, 3
+	warp_event 13,  5, CERULEAN_CAVE_1F, 4
+	warp_event 25, 13, CERULEAN_CAVE_1F, 5
+	warp_event  7, 17, CERULEAN_CAVE_1F, 6
+	warp_event  5,  7, CERULEAN_CAVE_1F, 7
+	warp_event 30, 19, CERULEAN_CAVE_1F, 1
+
+	def_coord_events
+
+	def_bg_events
+
+	def_object_events
