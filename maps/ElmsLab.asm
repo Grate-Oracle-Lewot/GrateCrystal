3e0879d4723ab212eb735d1ad725db7939917d73
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2024-12-21 21:17:58 -0800

    Update ElmsLab.asm

--- maps/ElmsLab.asm
+++ maps/ElmsLab.asm
@@ -196,7 +196,6 @@ TotodilePokeBallScript:
 	waitsfx
 	promptbutton
 	givepoke TOTODILE, 5, GOLD_BERRY
-	giveitem PICKAXE
 	closetext
 	applymovement PLAYER, AfterTotodileMovement
 	sjump ElmDirectionsScript
@@ -1291,7 +1290,7 @@ ElmsLab_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event  4, 11, OLIVINE_CITY, 1
+	warp_event  4, 11, NEW_BARK_TOWN, 1
 	warp_event  5, 11, NEW_BARK_TOWN, 1
 
 	def_coord_events
