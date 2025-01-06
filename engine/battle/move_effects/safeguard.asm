3f7f57f75264305cae9b672b426117436d5e78d8
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-01-05 18:11:14 -0800

    Update safeguard.asm

--- engine/battle/move_effects/safeguard.asm
+++ engine/battle/move_effects/safeguard.asm
@@ -7,15 +7,9 @@ BattleCommand_Safeguard:
 	ld hl, wEnemyScreens
 	ld de, wEnemySafeguardCount
 .ok
-	bit SCREENS_SAFEGUARD, [hl]
-	jr nz, .failed
 	set SCREENS_SAFEGUARD, [hl]
 	ld a, 5
 	ld [de], a
 	call AnimateCurrentMove
 	ld hl, CoveredByVeilText
 	jp StdBattleTextbox
-
-.failed
-	call AnimateFailedMove
-	jp PrintButItFailed
