2d1af7a4f37773964b1ee607b2f76648164e2157
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-01-14 16:53:52 -0800

    Update bulbasaur.asm

--- data/pokemon/dex_entries/bulbasaur.asm
+++ data/pokemon/dex_entries/bulbasaur.asm
@@ -1,10 +1,10 @@
 	db "BULB@" ; species name
 	dw 204, 150 ; height, weight
 
-	db   "It can't survive"
-	next "without the bulb"
-	next "on its back, and"
+	db   "If removed, the"
+	next "bulb on its back"
+	next "will light up when"
 
-	page "the bulb can't"
-	next "survive without"
-	next "it.@"
+	page "plugged into an"
+	next "electrical socket."
+	next "@"
