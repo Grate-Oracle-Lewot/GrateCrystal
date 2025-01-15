a64f5828f94d50f816d8f9ddf81b60aa3fdafbdb
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-01-15 06:57:15 -0800

    Update smeargle.asm

--- data/pokemon/dex_entries/smeargle.asm
+++ data/pokemon/dex_entries/smeargle.asm
@@ -1,10 +1,10 @@
 	db "PAINTER@" ; species name
 	dw 311, 1280 ; height, weight
 
-	db   "If it paints a"
-	next "picture of a"
-	next "#MON using a"
+	db   "No matter how"
+	next "renowned its art"
+	next "becomes, it's"
 
-	page "move, it learns to"
-	next "perform that move"
-	next "itself.@"
+	page "always its own"
+	next "harshest critic."
+	next "@"
