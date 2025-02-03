2bf957103b49476398ed6dc1271d06a4e95a31f4
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-02-03 09:29:16 -0800

    Update residual_moves.asm

--- data/battle/ai/residual_moves.asm
+++ data/battle/ai/residual_moves.asm
@@ -2,15 +2,12 @@
 
 ResidualMoves:
 	db MIST
-	db LEECH_SEED
 	db POISONPOWDER
 	db STUN_SPORE
 	db THUNDER_WAVE
-	db TOXIC
 	db GLARE
 	db TRANSFORM
 	db SPIDER_WEB
 	db BELLY_DRUM
 	db MEAN_LOOK
-	db ATTRACT
 	db -1 ; end
