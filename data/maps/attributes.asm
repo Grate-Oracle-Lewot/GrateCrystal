146ce60d46f9ca44e0045a97788b617a88a93986
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-02-03 22:25:08 -0800

    Update attributes.asm

--- data/maps/attributes.asm
+++ data/maps/attributes.asm
@@ -106,14 +106,14 @@ ENDM
 	map_attributes ViridianCity, VIRIDIAN_CITY, $0f, NORTH | SOUTH | WEST
 	connection north, Route2, ROUTE_2, 5
 	connection south, Route1, ROUTE_1, 10
-	connection west, Route22, ROUTE_22, 4
+	connection west, Route22, ROUTE_22, -35
 
 	map_attributes Route2, ROUTE_2, $0f, NORTH | SOUTH
 	connection north, PewterCity, PEWTER_CITY, -5
 	connection south, ViridianCity, VIRIDIAN_CITY, -5
 
 	map_attributes Route22, ROUTE_22, $2c, EAST
-	connection east, ViridianCity, VIRIDIAN_CITY, -4
+	connection east, ViridianCity, VIRIDIAN_CITY, 35
 
 	map_attributes PewterCity, PEWTER_CITY, $0f, SOUTH | EAST
 	connection south, Route2, ROUTE_2, 5
