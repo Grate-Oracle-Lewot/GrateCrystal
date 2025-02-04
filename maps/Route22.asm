a73b17c90983f5b788c3dad025deaf36b325c2dd
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-02-03 21:49:26 -0800

    Update Route22.asm

--- maps/Route22.asm
+++ maps/Route22.asm
@@ -3,30 +3,41 @@ Route22_MapScripts:
 
 	def_callbacks
 
-VictoryRoadEntranceSign:
-	jumptext VictoryRoadEntranceSignText
+VictoryRoadEntranceSign1:
+	jumptext VictoryRoadEntranceSign1Text
+
+VictoryRoadEntranceSign2:
+	jumptext VictoryRoadEntranceSign2Text
 
 Route22HiddenTimerBall:
 	hiddenitem TIMER_BALL, EVENT_ROUTE_22_HIDDEN_TIMER_BALL
 
-VictoryRoadEntranceSignText:
+VictoryRoadEntranceSign1Text:
 	text "ROUTE 22"
 
 	para "VIRIDIAN CITY -"
 	line "VICTORY ROAD"
 	done
 
+VictoryRoadEntranceSign2Text:
+	text "ROUTE 22"
+
+	para "VICTORY ROAD -"
+	line "VIRIDIAN CITY"
+	done
+
 Route22_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event 13,  5, VICTORY_ROAD_GATE, 1
-	warp_event 28, 75, ROUTE_22_HOUSE, 1
+	warp_event  9,  5, VICTORY_ROAD_GATE, 1
+	warp_event 20, 67, ROUTE_22_HOUSE, 1
 
 	def_coord_events
 
 	def_bg_events
-	bg_event 15, 73, BGEVENT_READ, VictoryRoadEntranceSign
-	bg_event 30, 68, BGEVENT_ITEM, Route22HiddenTimerBall
+	bg_event 11, 73, BGEVENT_READ, VictoryRoadEntranceSign1
+	bg_event  9,  7, BGEVENT_READ, VictoryRoadEntranceSign2
+	bg_event 26, 68, BGEVENT_ITEM, Route22HiddenTimerBall
 
 	def_object_events
