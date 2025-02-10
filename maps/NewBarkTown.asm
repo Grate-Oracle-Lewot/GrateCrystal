affa6db163d3960a65b7646fbeb0c026344290c6
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-02-10 11:23:26 -0800

    Update NewBarkTown.asm

--- maps/NewBarkTown.asm
+++ maps/NewBarkTown.asm
@@ -126,42 +126,9 @@ NewBarkTownSilverScript:
 
 NewBarkTownSign:
 	winlosstext NewBarkTownSignText, 0
-	loadtrainer SUPER_NERD, TERU
-	startbattle
-	reloadmapafterbattle
-	special HealParty
-	loadtrainer SCIENTIST, JED
-	startbattle
-	reloadmapafterbattle
-	special HealParty
-	loadtrainer COOLTRAINERM, TYLER
-	startbattle
-	reloadmapafterbattle
-	special HealParty
-	loadtrainer COOLTRAINERF, KELLY
-	startbattle
-	reloadmapafterbattle
-	special HealParty
-	loadtrainer GENTLEMAN, GREGORY
-	startbattle
-	reloadmapafterbattle
-	special HealParty
-	loadtrainer PICNICKER, BROOKE
-	startbattle
-	reloadmapafterbattle
-	special HealParty
-	loadtrainer POKEFANF, RUTH
-	startbattle
-	reloadmapafterbattle
-	special HealParty
-	loadtrainer ENGINEER, BERNIE
-	startbattle
-	reloadmapafterbattle
-	special HealParty
 	loadtrainer POKEFANM, JOSHUA
 	startbattle
 	reloadmapafterbattle
-	special HealParty
 	jumptext NewBarkTownSignText
 
 NewBarkTownPlayersHouseSign:
