9fa1f89915abb516d166d8d0591a6181eb43ed12
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-02-09 21:30:22 -0800

    Update CherrygroveCity.asm

--- maps/CherrygroveCity.asm
+++ maps/CherrygroveCity.asm
@@ -162,7 +162,6 @@ CherrygroveSilverSceneNorth:
 
 .AfterYourDefeat:
 	setevent EVENT_LOST_FIRST_RIVAL_BATTLE
-	loadwildmon RATTATA, 1 ; hacky patch
 	playmusic MUSIC_RIVAL_AFTER
 	opentext
 	writetext CherrygroveRivalText_YouLost
