6f2afe15a4cabe43b665483a350d14a69f6f3b19
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-02-10 18:30:00 -0800

    Create pikachu_party.asm

---
+++ data/trainers/pikachu_party.asm
@@ -0,0 +1,160 @@
+SixPikachuParty:
+	table_width NICKNAMED_MON_STRUCT_LENGTH, SixPikachuParty
+
+	db PIKACHU
+	db GOLD_BERRY
+	db THUNDERBOLT, THUNDERPUNCH, LIGHT_SCREEN, TRI_ATTACK
+	dw 0 ; OT ID
+	dt 125000 ; Exp
+	; Stat exp
+	bigdw 42000
+	bigdw 42000
+	bigdw 42000
+	bigdw 42000
+	bigdw 42000
+	dn 9, 9, 15, 14 ; DVs
+	db 10, 15, 10, 10 ; PP
+	db 255 ; Happiness
+	db 0, 0, 0 ; Pokerus, Caught data
+	db 42 ; Level
+	db 0, 0 ; Status
+	bigdw  ; HP
+	bigdw  ; Max HP
+	bigdw  ; Atk
+	bigdw  ; Def
+	bigdw  ; Spd
+	bigdw  ; SAtk
+	bigdw  ; SDef
+	db "PIKACHU@@@@"
+
+	db PIKACHU
+	db GOLD_BERRY
+	db THUNDERBOLT, QUICK_ATTACK, HYPER_FANG, SWIFT
+	dw 0 ; OT ID
+	dt 125000 ; Exp
+	; Stat exp
+	bigdw 42000
+	bigdw 42000
+	bigdw 42000
+	bigdw 42000
+	bigdw 42000
+	dn 9, 15, 15, 14 ; DVs
+	db 10, 30, 10, 25 ; PP
+	db 255 ; Happiness
+	db 0, 0, 0 ; Pokerus, Caught data
+	db 42 ; Level
+	db 0, 0 ; Status
+	bigdw  ; HP
+	bigdw  ; Max HP
+	bigdw  ; Atk
+	bigdw  ; Def
+	bigdw  ; Spd
+	bigdw  ; SAtk
+	bigdw  ; SDef
+	db "PIKACHU@@@@"
+
+	db PIKACHU
+	db GOLD_BERRY
+	db THUNDERBOLT, DOUBLESLAP, ATTRACT, DAZZLY_GLEAM
+	dw 0 ; OT ID
+	dt 125000 ; Exp
+	; Stat exp
+	bigdw 42000
+	bigdw 42000
+	bigdw 42000
+	bigdw 42000
+	bigdw 42000
+	dn 11, 11, 15, 14 ; DVs
+	db 10, 20, 10, 15 ; PP
+	db 255 ; Happiness
+	db 0, 0, 0 ; Pokerus, Caught data
+	db 42 ; Level
+	db 0, 0 ; Status
+	bigdw  ; HP
+	bigdw  ; Max HP
+	bigdw  ; Atk
+	bigdw  ; Def
+	bigdw  ; Spd
+	bigdw  ; SAtk
+	bigdw  ; SDef
+	db "PIKACHU@@@@"
+
+ 	db PIKACHU
+	db GOLD_BERRY
+	db THUNDERBOLT, THUNDERPUNCH, HI_JUMP_KICK, AURA_SPHERE
+	dw 0 ; OT ID
+	dt 125000 ; Exp
+	; Stat exp
+	bigdw 42000
+	bigdw 42000
+	bigdw 42000
+	bigdw 42000
+	bigdw 42000
+	dn 13, 11, 15, 14 ; DVs
+	db 10, 15, 20, 10 ; PP
+	db 255 ; Happiness
+	db 0, 0, 0 ; Pokerus, Caught data
+	db 42 ; Level
+	db 0, 0 ; Status
+	bigdw  ; HP
+	bigdw  ; Max HP
+	bigdw  ; Atk
+	bigdw  ; Def
+	bigdw  ; Spd
+	bigdw  ; SAtk
+	bigdw  ; SDef
+	db "PIKACHU@@@@"
+
+ 	db PIKACHU
+	db GOLD_BERRY
+	db THUNDERBOLT, THUNDER, RAIN_DANCE, SURF
+	dw 0 ; OT ID
+	dt 125000 ; Exp
+	; Stat exp
+	bigdw 42000
+	bigdw 42000
+	bigdw 42000
+	bigdw 42000
+	bigdw 42000
+	dn 13, 15, 15, 14 ; DVs
+	db 10, 5, 5, 10 ; PP
+	db 255 ; Happiness
+	db 0, 0, 0 ; Pokerus, Caught data
+	db 42 ; Level
+	db 0, 0 ; Status
+	bigdw  ; HP
+	bigdw  ; Max HP
+	bigdw  ; Atk
+	bigdw  ; Def
+	bigdw  ; Spd
+	bigdw  ; SAtk
+	bigdw  ; SDef
+	db "PIKACHU@@@@"
+
+ 	db PIKACHU
+	db GOLD_BERRY
+	db THUNDERBOLT, SIGNAL_BEAM, DOUBLE_TEAM, FLY
+	dw 0 ; OT ID
+	dt 125000 ; Exp
+	; Stat exp
+	bigdw 42000
+	bigdw 42000
+	bigdw 42000
+	bigdw 42000
+	bigdw 42000
+	dn 15, 14, 15, 14 ; DVs
+	db 10, 15, 15, 15 ; PP
+	db 255 ; Happiness
+	db 0, 0, 0 ; Pokerus, Caught data
+	db 42 ; Level
+	db 0, 0 ; Status
+	bigdw  ; HP
+	bigdw  ; Max HP
+	bigdw  ; Atk
+	bigdw  ; Def
+	bigdw  ; Spd
+	bigdw  ; SAtk
+	bigdw  ; SDef
+	db "PIKACHU@@@@"
+
+	assert_table_length 6
