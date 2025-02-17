a8a6f9ba9f83b2c41c82cf4af9087187256293e5
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-02-17 11:23:17 -0800

    Update buena_prizes.asm

--- data/items/buena_prizes.asm
+++ data/items/buena_prizes.asm
@@ -1,6 +1,5 @@
 BuenaPrizeItems:
 	table_width 2, BuenaPrizeItems
-	db ULTRA_BALL,   1
 	db BERRY_JUICE,  2
 	db CATCH_CHARM,  4
 	db HARDHAT,      4
@@ -9,4 +8,5 @@ BuenaPrizeItems:
 	db QUICK_CLAW,   6
 	db SCOPE_LENS,   6
 	db LEFTOVERS,    9
+	db BIG_PEARL,    9
 	assert_table_length NUM_BUENA_PRIZES
