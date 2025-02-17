cd3594be88639ac50e74dcf1899a8f55757dd388
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-02-17 14:44:25 -0800

    Update sunny_day.asm

--- engine/battle/move_effects/sunny_day.asm
+++ engine/battle/move_effects/sunny_day.asm
@@ -2,6 +2,17 @@ BattleCommand_StartSun:
 	ld a, WEATHER_SUN
 	ld [wBattleWeather], a
 	call GetWeatherTurns
+	cp 6
+	jr nc, .done
+	call GetUserItem
+	ld a, [hl]
+	cp SUN_STONE
+	jr nz, .five
+	ld a, 8
+	jr .done
+.five
+	ld a, 5
+.done
 	ld [wWeatherCount], a
 	call AnimateCurrentMove
 	ld hl, SunGotBrightText
