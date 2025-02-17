8feecabeecd0ab7ebb07b5a73c1a3c762d781702
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-02-16 17:32:47 -0800

    Update trainer_data_constants.asm

--- constants/trainer_data_constants.asm
+++ constants/trainer_data_constants.asm
@@ -24,29 +24,29 @@ NO_AI EQU 0
 
 ; TRNATTR_AI_ITEM_SWITCH bit flags
 	const_def
-	const SWITCH_OFTEN_F     ; 0
-	const SWITCH_RARELY_F    ; 1
-	const SWITCH_SOMETIMES_F ; 2
+	const SWITCH_SOMETIMES_F ; 0
+	const SWITCH_OFTEN_F     ; 1
+	const SWITCH_RARELY_F    ; 2
 	const_skip               ; 3
-	const ALWAYS_USE_F       ; 4
-	const UNKNOWN_USE_F      ; 5
-	const CONTEXT_USE_F      ; 6
+	const CONTEXT_USE_F      ; 4
+	const ALWAYS_USE_F       ; 5
 
+SWITCH_SOMETIMES   EQU 1 << SWITCH_SOMETIMES_F
 SWITCH_OFTEN       EQU 1 << SWITCH_OFTEN_F
 SWITCH_RARELY      EQU 1 << SWITCH_RARELY_F
-SWITCH_SOMETIMES   EQU 1 << SWITCH_SOMETIMES_F
-ALWAYS_USE         EQU 1 << ALWAYS_USE_F
-UNKNOWN_USE        EQU 1 << UNKNOWN_USE_F
 CONTEXT_USE        EQU 1 << CONTEXT_USE_F
+ALWAYS_USE         EQU 1 << ALWAYS_USE_F
 
 ; TrainerTypes bits (see engine/battle/read_trainer_party.asm)
 	const_def
 	const TRAINERTYPE_MOVES_F    ; 0
 	const TRAINERTYPE_ITEM_F     ; 1
 	const TRAINERTYPE_NICKNAME_F ; 2
+	const TRAINERTYPE_DVS_F      ; 3
 
 ; Trainer party types (see data/trainers/parties.asm)
 TRAINERTYPE_NORMAL     EQU 0
 TRAINERTYPE_MOVES      EQU 1 << TRAINERTYPE_MOVES_F
 TRAINERTYPE_ITEM       EQU 1 << TRAINERTYPE_ITEM_F
 TRAINERTYPE_NICKNAME   EQU 1 << TRAINERTYPE_NICKNAME_F
+TRAINERTYPE_DVS        EQU 1 << TRAINERTYPE_DVS_F
