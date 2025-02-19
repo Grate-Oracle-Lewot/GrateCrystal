be24f9c76c01b7ff5102c8e11b134ebe7eb00bd1
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-02-19 14:35:27 -0800

    Update map_constants.asm

--- constants/map_constants.asm
+++ constants/map_constants.asm
@@ -515,9 +515,9 @@ ENDM
 	map_const TEAM_ROCKET_BASE_B1F,                        15,  9 ;  92
 	map_const TEAM_ROCKET_BASE_B2F,                        15,  9 ;  93
 	map_const TEAM_ROCKET_BASE_B3F,                        15,  9 ;  94
-	map_const ABANDONED_LAB_1F,                            15, 14 ;  95
-	map_const ABANDONED_LAB_2F,                            15, 14 ;  96
-	map_const ABANDONED_LAB_B1F,                           15, 14 ;  97
+	map_const ABANDONED_LAB_B1F,                           15, 14 ;  95
+	map_const ABANDONED_LAB_1F,                            15, 14 ;  96
+	map_const ABANDONED_LAB_2F,                            15, 14 ;  97
 	map_const ABANDONED_LAB_ROOF,                          20, 18 ;  98
 	map_const ICE_PATH_1F,                                 20, 18 ;  99
 	map_const ICE_PATH_B1F,                                10, 18 ; 100
