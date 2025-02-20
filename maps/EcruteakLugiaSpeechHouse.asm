07a6b978f63b279a3cd695d9eb371c3a3748bdac
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-02-19 18:07:38 -0800

    Update EcruteakLugiaSpeechHouse.asm

--- maps/EcruteakLugiaSpeechHouse.asm
+++ maps/EcruteakLugiaSpeechHouse.asm
@@ -1,6 +1,7 @@
 	object_const_def
 	const ECRUTEAKLUGIASPEECHHOUSE_GRAMPS
 	const ECRUTEAKLUGIASPEECHHOUSE_YOUNGSTER
+	const ECRUTEAKITEMFINDERHOUSE_POKEDEX
 
 EcruteakLugiaSpeechHouse_MapScripts:
 	def_scene_scripts
@@ -12,6 +13,9 @@ EcruteakLugiaSpeechHouseGrampsScript:
 
 EcruteakLugiaSpeechHouseYoungsterScript:
 	jumptextfaceplayer EcruteakLugiaSpeechHouseYoungsterText
+
+EcruteakHistoryBook:
+	jumptext EcruteakHistoryBookText
 
 LugiaSpeechHouseRadio:
 	jumpstd Radio2Script
@@ -35,11 +39,64 @@ EcruteakLugiaSpeechHouseGrampsText:
 	done
 
 EcruteakLugiaSpeechHouseYoungsterText:
-	text "Is there really a"
-	line "#MON that big?"
+	text "You know those"
+	line "SWAG BEACONS that"
+	cont "find hidden items?"
 
-	para "If it exists, it"
-	line "must be powerful."
+	para "There used to be"
+	line "something called"
+
+	para "an ITEMFINDER that"
+	line "did that…"
+
+	para "But then they rea-"
+	line "lized they could"
+
+	para "make more money by"
+	line "selling disposable"
+	cont "versions."
+
+	para "Gotta love"
+	line "capitalism."
+	done
+
+EcruteakHistoryBookText:
+	text "In ECRUTEAK, there"
+	line "were two towers."
+
+	para "Each tower was the"
+	line "roost of powerful"
+	cont "dragon #MON."
+
+	text "ECRUTEAK was also"
+	line "home to three"
+
+	para "#MON that raced"
+	line "around the town."
+
+	para "One night, the"
+	line "three #MON"
+
+	para "stopped to rest in"
+	line "the BRASS TOWER."
+
+	para "But the tower was"
+	line "stricken by light-"
+	cont "ning and burned to"
+	cont "the ground."
+
+	para "The fire was only"
+	line "put out by a rain-"
+	cont "storm."
+
+	para "It's said that the"
+	line "tower's dragon re-"
+	cont "vived the fallen"
+	para "#MON."
+
+	para "But the two drag-"
+	line "ons were never"
+	cont "seen again…"
 	done
 
 EcruteakLugiaSpeechHouse_MapEvents:
@@ -57,3 +114,4 @@ EcruteakLugiaSpeechHouse_MapEvents:
 	def_object_events
 	object_event  2,  3, SPRITE_GRAMPS, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EcruteakLugiaSpeechHouseGrampsScript, -1
 	object_event  5,  4, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EcruteakLugiaSpeechHouseYoungsterScript, -1
+	object_event  3,  3, SPRITE_POKEDEX, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EcruteakHistoryBook, -1
