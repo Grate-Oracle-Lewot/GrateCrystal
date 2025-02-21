61a2eb72c95457311ad648e7c10c0e5a5b4e4a67
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-02-20 18:45:30 -0800

    Create OaksLab2F.asm

---
+++ maps/OaksLab2F.asm
@@ -0,0 +1,18 @@
+	object_const_def
+
+OaksLab2F_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+OaksLab2F_MapEvents:
+	db 0, 0 ; filler
+
+	def_warp_events
+  warp_event 27, 24, OAKS_LAB, 3
+
+	def_coord_events
+
+	def_bg_events
+
+	def_object_events
