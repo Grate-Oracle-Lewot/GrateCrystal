4890af871b00ebeef2c968fe82954e3e668d39ed
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-02-28 18:48:59 -0800

    Update wildmons.asm

--- engine/overworld/wildmons.asm
+++ engine/overworld/wildmons.asm
@@ -193,6 +193,7 @@ TryWildEncounter::
 
 .EncounterRate:
 	call GetMapEncounterRate
+	call ApplyRunningEffectOnEncounterRate
 	call ApplyMusicEffectOnEncounterRate
 	call ApplyCleanseTagEffectOnEncounterRate
 	call Random
@@ -210,6 +211,14 @@ GetMapEncounterRate:
 	ld b, 0
 	add hl, bc
 	ld b, [hl]
+	ret
+
+ApplyRunningEffectOnEncounterRate::
+; Running doubles encounter rate.
+	ld a, [wPlayerState]
+	cp PLAYER_RUN
+	ret nz
+	sla b
 	ret
 
 ApplyMusicEffectOnEncounterRate::
