37f1979c68c5b8e1f4d450762dd16f4f66b91836
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-03-02 20:16:59 -0800

    Update reckless_moves.asm

--- data/battle/ai/reckless_moves.asm
+++ data/battle/ai/reckless_moves.asm
@@ -1,8 +1,7 @@
 ; AI_AGGRESSIVE wants to discourage all moves but the one that does the most damage.
 ; It can't properly account for multi-hit moves, so their effects are checked for in this list,
 ; and given a 50% chance to not be discouraged when they otherwise would be.
-; The computer selecting one of these simulates a player gambling on a high number of hits.
-; That being said, only EFFECT_MULTI_HIT has a variable number of hits.
+; AI_SMART encourages these moves if they're super effective and discourages them if they're not very effective.
 
 RecklessMoves:
 	db EFFECT_MULTI_HIT
