270169338511f12974647ee721ee3a3bd20609a2
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-03-03 15:45:50 -0800

    Update pokedex.asm

--- engine/pokedex/pokedex.asm
+++ engine/pokedex/pokedex.asm
@@ -3377,6 +3377,12 @@ _NewPokedexEntry:
 	call EnableLCD
 	call WaitBGMap
 	call GetBaseData
+
+	ld a, [wBackupUnownLetter]
+	ld [wUnownLetter], a
+	ld a, [wBackupPikachuForm]
+	ld [wPikachuForm], a
+
 	ld de, vTiles2
 	predef GetMonFrontpic
 	ld a, SCGB_POKEDEX
