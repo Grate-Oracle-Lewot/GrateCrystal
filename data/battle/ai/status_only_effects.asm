9fab84c0ab0c03224c066bcb3af1c2c05c69b47e
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-03-02 19:32:09 -0800

    Update status_only_effects.asm

--- data/battle/ai/status_only_effects.asm
+++ data/battle/ai/status_only_effects.asm
@@ -1,11 +1,14 @@
-; AI_BASIC discourages StatusOnlyEffects if the player already has a status condition.
+; AI_BASIC dismisses StatusOnlyEffects if the player already has a status condition.
 ; AI_STATUS encourages StatusBideEffects if the player is storing energy with Bide.
-; StatusBideEffects are supposed to include StatusOnlyEffects.
+; AI_TROLL always encourages StatusBideEffects.
 
 StatusBideEffects:
 	db EFFECT_CONFUSE
 	db EFFECT_LEECH_SEED
+	db EFFECT_SPITE
 	db EFFECT_NIGHTMARE
+	db EFFECT_SWAGGER
+	db EFFECT_ATTRACT
 StatusOnlyEffects:
 	db EFFECT_SLEEP
 	db EFFECT_TOXIC
