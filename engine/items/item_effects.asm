db26a1e65ce922175ccca70eebd0bf4f9b9eb4f6
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-03-03 15:40:31 -0800

    Update item_effects.asm

--- engine/items/item_effects.asm
+++ engine/items/item_effects.asm
@@ -437,6 +437,11 @@ PokeBallEffect:
 	ld a, [wEnemyMonDVs + 1]
 	ld [hl], a
 
+	ld a, [wUnownLetter]
+	ld [wBackupUnownLetter], a
+	ld a, [wPikachuForm]
+	ld [wBackupPikachuForm], a
+
 	ld a, [wTempEnemyMonSpecies]
 	ld [wCurPartySpecies], a
 	ld a, [wEnemyMonLevel]
