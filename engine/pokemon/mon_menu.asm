c05a9cdc1b2cc451d0eb0944bfb1093c38f60441
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-03-06 19:23:35 -0800

    Update mon_menu.asm

--- engine/pokemon/mon_menu.asm
+++ engine/pokemon/mon_menu.asm
@@ -459,7 +459,7 @@ CancelPokemonAction:
 
 GiveTakeItemMenuData:
 	db MENU_SPRITE_ANIMS | MENU_BACKUP_TILES ; flags
-	menu_coords 13, 11, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1
+	menu_coords 13, 10, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 2
 	dw .Items
 	db 1 ; default option
 
