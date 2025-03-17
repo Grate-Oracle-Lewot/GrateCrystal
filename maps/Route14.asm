be0b50ce3810e372d740bd79aeb254ddff3604b5
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-03-17 12:34:42 -0700

    Update Route14.asm

--- maps/Route14.asm
+++ maps/Route14.asm
@@ -3,6 +3,7 @@
 	const ROUTE14_YOUNGSTER
 	const ROUTE14_POKEFAN_M2
 	const ROUTE14_KIM
+	const ROUTE14_BUGSY
 
 Route14_MapScripts:
 	def_scene_scripts
@@ -49,6 +50,34 @@ TrainerPokefanmTrevor:
 	waitbutton
 	closetext
 	end
+
+Route14BugsyScript:
+	faceplayer
+	opentext
+	writetext Route14BugsyText
+	waitbutton
+	closetext
+	readvar VAR_FACING
+	ifequal RIGHT, .Around
+	applymovement ROUTE14_BUGSY, Route14BugsyDirectMovement
+	sjump .End
+
+.Around:
+	applymovement ROUTE14_BUGSY, Route14BugsyAroundMovement
+.End:
+	disappear ROUTE14_BUGSY
+	end
+
+Route14BugsyAroundMovement:
+	step UP
+Route14BugsyDirectMovement:
+	step LEFT
+	step LEFT
+	step LEFT
+	step LEFT
+	step LEFT
+	step LEFT
+	step_end
 
 PokefanmCarterSeenText:
 	text "Let me tell you,"
@@ -113,6 +142,48 @@ PokefanmTrevorAfterBattleText:
 	line "got my #MON…"
 	done
 
+Route14BugsyText:
+	text "BUGSY: <PLAYER>?"
+
+	para "Fancy running into"
+	line "you all the way"
+	cont "out here!"
+
+	para "Me? I came to"
+	line "KANTO to see if"
+
+	para "there was anything"
+	line "left of the lab on"
+
+	para "CINNABAR ISLAND,"
+	line "but…"
+
+	para "There reeeeaaally"
+	line "wasn't."
+
+	para "Then I wandered"
+	line "over this way"
+
+	para "looking for bugs,"
+	line "you know how it"
+	cont "is."
+
+	para "But! I just got"
+	line "word from PROF."
+	cont "OAK's lab!"
+
+	para "With resources"
+	line "like that…"
+
+	para "Well, it's a long"
+	line "story."
+
+	para "Meet me there if"
+	line "you're interested!"
+
+	para "Toodeloo!"
+	done
+
 Route14_MapEvents:
 	db 0, 0 ; filler
 
@@ -127,3 +198,4 @@ Route14_MapEvents:
 	object_event 11, 27, SPRITE_YOUNGSTER, SPRITEMOVEDATA_SPINRANDOM_FAST, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_TRAINER, 3, TrainerBirdKeeperRoy, -1
 	object_event  6, 11, SPRITE_POKEFAN_M, SPRITEMOVEDATA_SPINRANDOM_FAST, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_TRAINER, 3, TrainerPokefanmTrevor, -1
 	object_event  7,  5, SPRITE_TEACHER, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 4, Kim, -1
+	object_event 15, 30, SPRITE_BUGSY, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, Route14BugsyScript, EVENT_BUGSY_ON_ROUTE_14
