cc071937a310a0751725bc1d4809a6c5109736c1
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-03-17 16:05:08 -0700

    Update core.asm

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -7154,11 +7154,6 @@ Call_PlayBattleAnim_OnlyIfVisible:
 	; fallthrough
 
 Call_PlayBattleAnim:
-	ld a, BATTLE_VARS_SUBSTATUS4
-	call GetBattleVar
-	bit SUBSTATUS_SUBSTITUTE, a
-	ret nz
-
 	ld a, e
 	ld [wFXAnimID], a
 	ld a, d
