1fca7e491abe8e55350d3d6bb36708952b603d71
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-03-18 15:57:40 -0700

    Update OaksLab2F.asm

--- maps/OaksLab2F.asm
+++ maps/OaksLab2F.asm
@@ -105,51 +105,147 @@ OaksLab2FBugsyScript:
 	end
 
 OaksLab2FBugCatcherRobScript:
+	loadvar VAR_BATTLETYPE, BATTLETYPE_INVERSE
+	trainer BUG_CATCHER, ROB, EVENT_INVERSE_BEAT_BUG_CATCHER_ROB, InverseBugCatcherRobSeenText, InverseBugCatcherRobBeatenText, 0, .Script
 
+.Script:
+	endifjustbattled
+	opentext
+	writetext InverseBugCatcherRobAfterBattleText
+	waitbutton
+	closetext
 	end
 
 OaksLab2FScientistTaylorScript:
+	loadvar VAR_BATTLETYPE, BATTLETYPE_INVERSE
+	trainer SCIENTIST, TAYLOR, EVENT_INVERSE_BEAT_SCIENTIST_TAYLOR, InverseScientistTaylorSeenText, InverseScientistTaylorBeatenText, 0, .Script
 
+.Script:
+	endifjustbattled
+	opentext
+	writetext InverseScientistTaylorAfterBattleText
+	waitbutton
+	closetext
 	end
 
 OaksLab2FScientistElvinScript:
+	loadvar VAR_BATTLETYPE, BATTLETYPE_INVERSE
+	trainer SCIENTIST, ELVIN, EVENT_INVERSE_BEAT_SCIENTIST_ELVIN, InverseScientistElvinSeenText, InverseScientistElvinBeatenText, 0, .Script
 
+.Script:
+	endifjustbattled
+	opentext
+	writetext InverseScientistElvinAfterBattleText
+	waitbutton
+	closetext
 	end
 
 OaksLab2FFisherJonahScript:
+	loadvar VAR_BATTLETYPE, BATTLETYPE_INVERSE
+	trainer FISHER, JONAH, EVENT_INVERSE_BEAT_FISHER_JONAH, InverseFisherJonahSeenText, InverseFisherJonahBeatenText, 0, .Script
 
+.Script:
+	endifjustbattled
+	opentext
+	writetext InverseFisherJonahAfterBattleText
+	waitbutton
+	closetext
 	end
 
 OaksLab2FCooltrainMBlakeScript:
+	loadvar VAR_BATTLETYPE, BATTLETYPE_INVERSE
+	trainer COOLTRAINERM, BLAKE, EVENT_INVERSE_BEAT_COOLTRAINERM_BLAKE, InverseCooltrainerMBlakeSeenText, InverseCoolertrainerMBlakeBeatenText, 0, .Script
 
+.Script:
+	endifjustbattled
+	opentext
+	writetext InverseCooltrainerMBlakeAfterBattleText
+	waitbutton
+	closetext
 	end
 
 OaksLab2FCooltrainerFStellaScript:
+	loadvar VAR_BATTLETYPE, BATTLETYPE_INVERSE
+	trainer COOLTRAINERF, STELLA, EVENT_INVERSE_BEAT_COOLTRAINERF_STELLA, InverseCooltrainerFStellaSeenText, InverseCooltrainerFStellaBeatenText, 0, .Script
 
+.Script:
+	endifjustbattled
+	opentext
+	writetext InverseCooltrainerFStellaAfterBattleText
+	waitbutton
+	closetext
 	end
 
 OaksLab2FSuperNerdShawnScript:
+	loadvar VAR_BATTLETYPE, BATTLETYPE_INVERSE
+	trainer SUPER_NERD, SHAWN, EVENT_INVERSE_BEAT_SUPER_NERD_SHAWN, InverseSuperNerdShawnSeenText, InverseSuperNerdShawnBeatenText, 0, .Script
 
+.Script:
+	endifjustbattled
+	opentext
+	writetext InverseSuperNerdShawnAfterBattleText
+	waitbutton
+	closetext
 	end
 
 OaksLab2FPokemaniacTannerScript:
+	loadvar VAR_BATTLETYPE, BATTLETYPE_INVERSE
+	trainer POKEMANIAC, TANNER, EVENT_INVERSE_BEAT_POKEMANIAC_TANNER, InversePokemaniacTannerSeenText, InversePokemaniacTannerBeatenText, 0, .Script
 
+.Script:
+	endifjustbattled
+	opentext
+	writetext InversePokemaniacTannerAfterBattleText
+	waitbutton
+	closetext
 	end
 
 OaksLab2FPokefanMJoshuaScript:
+	loadvar VAR_BATTLETYPE, BATTLETYPE_INVERSE
+	trainer POKEFANM, JOSHUA, EVENT_INVERSE_BEAT_POKEFANM_JOSHUA, InversePokefanMJoshuaSeenText, InversePokefanMJoshuaBeatenText, 0, .Script
 
+.Script:
+	endifjustbattled
+	opentext
+	writetext InversePokefanMJoshuaAfterBattleText
+	waitbutton
+	closetext
 	end
 
 OaksLab2FHikerKennyScript:
+	loadvar VAR_BATTLETYPE, BATTLETYPE_INVERSE
+	trainer HIKER, KENNY, EVENT_INVERSE_BEAT_HIKER_KENNY, InverseHikerKennySeenText, InverseHikerKennyBeatenText, 0, .Script
 
+.Script:
+	endifjustbattled
+	opentext
+	writetext InverseHikerKennyAfterBattleText
+	waitbutton
+	closetext
 	end
 
 OaksLab2FPokefanFGeorgiaScript:
+	loadvar VAR_BATTLETYPE, BATTLETYPE_INVERSE
+	trainer POKEFANF, GEORGIA, EVENT_INVERSE_BEAT_POKEFANF_GEORGIA, InversePokefanFGeorgiaSeenText, InversePokefanFGeorgiaBeatenText, 0, .Script
 
+.Script:
+	endifjustbattled
+	opentext
+	writetext InversePokefanFGeorgiaAfterBattleText
+	waitbutton
+	closetext
 	end
 
 OaksLab2FBlackbeltWaiScript:
+	loadvar VAR_BATTLETYPE, BATTLETYPE_INVERSE
+	trainer BLACKBELT_T, WAI, EVENT_INVERSE_BEAT_BLACKBELT_WAI, InverseBlackbeltWaiSeenText, InverseBlackbeltWaiBeatenText, 0, .Script
 
+.Script:
+	endifjustbattled
+	opentext
+	writetext InverseBlackbeltWaiAfterBattleText
+	waitbutton
+	closetext
 	end
 
 OaksLab2FPokedexMachine:
@@ -432,15 +528,15 @@ OaksLab2F_MapEvents:
 	def_object_events
 	object_event 27, 22, SPRITE_BUGSY, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, OaksLab2FBugsyScript, EVENT_OAKS_LAB_2F_BUGSY1
 	object_event 14,  9, SPRITE_BUGSY, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, OaksLab2FBugsyScript, EVENT_OAKS_LAB_2F_BUGSY2
-	object_event  3,  6, SPRITE_FISHING_GURU, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, OaksLab2FBugCatcherRobScript, EVENT_OAKS_LAB_2F_BUG_CATCHER_ROB
-	object_event 16, 19, SPRITE_SCIENTIST, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, OaksLab2FScientistTaylorScript, EVENT_OAKS_LAB_2F_SCIENTIST_TAYLOR
-	object_event 12, 19, SPRITE_SCIENTIST, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, OaksLab2FScientistElvinScript, EVENT_OAKS_LAB_2F_SCIENTIST_ELVIN
-	object_event 24,  1, SPRITE_FISHER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, OaksLab2FFisherJonahScript, EVENT_OAKS_LAB_2F_FISHER_JONAH
-	object_event 21, 11, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, OaksLab2FCooltrainMBlakeScript, EVENT_OAKS_LAB_2F_COOLTRAINERM_BLAKE
-	object_event 23, 11, SPRITE_LASS, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, OaksLab2FCooltrainerFStellaScript, EVENT_OAKS_LAB_2F_COOLTRAINERF_STELLA
-	object_event  5, 14, SPRITE_SUPER_NERD, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, OaksLab2FSuperNerdShawnScript, EVENT_OAKS_LAB_2F_SUPER_NERD_SHAWN
-	object_event  5, 17, SPRITE_SUPER_NERD, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, OaksLab2FPokemaniacTannerScript, EVENT_OAKS_LAB_2F_POKEMANIAC_TANNER
-	object_event  7, 20, SPRITE_POKEFAN_M, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, OaksLab2FPokefanMJoshuaScript, EVENT_OAKS_LAB_2F_POKEFANM_JOSHUA
-	object_event 15, 24, SPRITE_POKEFAN_M, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, OaksLab2FHikerKennyScript, EVENT_OAKS_LAB_2F_HIKER_KENNY
-	object_event  4, 24, SPRITE_POKEFAN_F, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, OaksLab2FPokefanFGeorgiaScript, EVENT_OAKS_LAB_2F_POKEFANF_GEORGIA
-	object_event 20, 16, SPRITE_BLACK_BELT, SPRITEMOVEDATA_SPINCLOCKWISE, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, OaksLab2FBlackbeltWaiScript, EVENT_OAKS_LAB_2F_BLACKBELT_WAI
+	object_event  3,  6, SPRITE_FISHING_GURU, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 0, OaksLab2FBugCatcherRobScript, EVENT_OAKS_LAB_2F_BUG_CATCHER_ROB
+	object_event 16, 19, SPRITE_SCIENTIST, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 0, OaksLab2FScientistTaylorScript, EVENT_OAKS_LAB_2F_SCIENTIST_TAYLOR
+	object_event 12, 19, SPRITE_SCIENTIST, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 0, OaksLab2FScientistElvinScript, EVENT_OAKS_LAB_2F_SCIENTIST_ELVIN
+	object_event 24,  1, SPRITE_FISHER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_TRAINER, 0, OaksLab2FFisherJonahScript, EVENT_OAKS_LAB_2F_FISHER_JONAH
+	object_event 21, 11, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_TRAINER, 0, OaksLab2FCooltrainMBlakeScript, EVENT_OAKS_LAB_2F_COOLTRAINERM_BLAKE
+	object_event 23, 11, SPRITE_LASS, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_TRAINER, 0, OaksLab2FCooltrainerFStellaScript, EVENT_OAKS_LAB_2F_COOLTRAINERF_STELLA
+	object_event  5, 14, SPRITE_SUPER_NERD, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_TRAINER, 0, OaksLab2FSuperNerdShawnScript, EVENT_OAKS_LAB_2F_SUPER_NERD_SHAWN
+	object_event  5, 17, SPRITE_SUPER_NERD, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 0, OaksLab2FPokemaniacTannerScript, EVENT_OAKS_LAB_2F_POKEMANIAC_TANNER
+	object_event  7, 20, SPRITE_POKEFAN_M, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_TRAINER, 0, OaksLab2FPokefanMJoshuaScript, EVENT_OAKS_LAB_2F_POKEFANM_JOSHUA
+	object_event 15, 24, SPRITE_POKEFAN_M, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_TRAINER, 0, OaksLab2FHikerKennyScript, EVENT_OAKS_LAB_2F_HIKER_KENNY
+	object_event  4, 24, SPRITE_POKEFAN_F, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_TRAINER, 0, OaksLab2FPokefanFGeorgiaScript, EVENT_OAKS_LAB_2F_POKEFANF_GEORGIA
+	object_event 20, 16, SPRITE_BLACK_BELT, SPRITEMOVEDATA_SPINCLOCKWISE, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_TRAINER, 0, OaksLab2FBlackbeltWaiScript, EVENT_OAKS_LAB_2F_BLACKBELT_WAI
