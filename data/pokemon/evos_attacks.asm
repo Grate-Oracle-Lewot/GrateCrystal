3138ba4e5cb3eec8098e076ef108727101edd4f0
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-03-17 18:23:57 -0700

    Update evos_attacks.asm

--- data/pokemon/evos_attacks.asm
+++ data/pokemon/evos_attacks.asm
@@ -224,8 +224,8 @@ BeedrillEvosAttacks:
 PidgeyEvosAttacks:
 	db EVOLVE_LEVEL, 18, PIDGEOTTO
 	db 0 ; no more evolutions
-	db 1, SUBSTITUTE
-	db 2, BATON_PASS
+	db 1, TACKLE
+	db 5, SAND_ATTACK
 	db 9, GUST
 	db 13, QUICK_ATTACK
 	db 17, TWISTER
