1c3ae57e9b57ac15428ce4c524a3e2207058ba4c
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-03-23 11:25:58 -0700

    Update item.asm

--- home/item.asm
+++ home/item.asm
@@ -71,210 +71,210 @@ CheckItem::
 
 ItemNameOrder::
  	table_width 1, ItemNameOrder
-	db NO_ITEM      ; 00
 
-	db REPEL        ; 01
-	db SUPER_REPEL  ; 02
-	db MAX_REPEL    ; 03
-	db POKE_DOLL    ; 04
-	db SMOKE_BALL   ; 05
-	db ESCAPE_ROPE  ; 06
-	db SWAG_BEACON  ; 07
-	db EXP_SHARE    ; 08
-	db LUCKY_EGG    ; 09
+	db REPEL
+	db SUPER_REPEL
+	db MAX_REPEL
+	db POKE_DOLL
+	db SMOKE_BALL
+	db ESCAPE_ROPE
+	db SWAG_BEACON
+	db EXP_SHARE
+	db LUCKY_EGG
 
-	db REVIVE       ; 0a
-	db MAX_REVIVE   ; 0b
-	db POTION       ; 0c
-	db RAGECANDYBAR ; 0d
-	db SUPER_POTION ; 0e
-	db FRESH_WATER  ; 0f
-	db SODA_POP     ; 10
-	db LEMONADE     ; 11
-	db MOOMOO_MILK  ; 12
-	db HYPER_POTION ; 13
-	db MAX_POTION   ; 14
-	db FULL_RESTORE ; 15
-	db FULL_HEAL    ; 16
-	db AWAKENING    ; 17
-	db PARLYZ_HEAL  ; 18
-	db ANTIDOTE     ; 19
-	db BURN_HEAL    ; 1a
-	db ICE_HEAL     ; 1b
+	db REVIVE
+	db MAX_REVIVE
+	db POTION
+	db RAGECANDYBAR
+	db SUPER_POTION
+	db FRESH_WATER
+	db SODA_POP
+	db LEMONADE
+	db MOOMOO_MILK
+	db HYPER_POTION
+	db MAX_POTION
+	db FULL_RESTORE
+	db FULL_HEAL
+	db AWAKENING
+	db PARLYZ_HEAL
+	db ANTIDOTE
+	db BURN_HEAL
+	db ICE_HEAL
 
-	db ENERGYPOWDER ; 1c
-	db ENERGY_ROOT  ; 1d
-	db REVIVAL_HERB ; 1e
-	db HEAL_POWDER  ; 1f
+	db ETHER
+	db MAX_ETHER
+	db ELIXER
+	db MAX_ELIXER
 
-	db ETHER        ; 20
-	db MAX_ETHER    ; 21
-	db ELIXER       ; 22
-	db MAX_ELIXER   ; 23
+	db ENERGYPOWDER
+	db ENERGY_ROOT
+	db REVIVAL_HERB
+	db HEAL_POWDER
+	db SACRED_ASH
 
-	db X_ATTACK     ; 24
-	db X_DEFEND     ; 25
-	db X_SPECIAL    ; 26
-	db X_SP_DEFEND  ; 27
-	db X_SPEED      ; 28
-	db X_ACCURACY   ; 29
-	db X_EVADE      ; 2a
-	db DIRE_HIT     ; 2b
-	db GUARD_SPEC   ; 2c
+	db X_ATTACK
+	db X_DEFEND
+	db X_SPECIAL
+	db X_SP_DEFEND
+	db X_SPEED
+	db X_ACCURACY
+	db X_EVADE
+	db DIRE_HIT
+	db GUARD_SPEC
 
-	db SACRED_ASH   ; 2d
-	db RARE_CANDY   ; 2e
-	db PP_UP        ; 2f
-	db HP_UP        ; 30
-	db PROTEIN      ; 31
-	db IRON         ; 32
-	db CARBOS       ; 33
-	db CALCIUM      ; 34
+	db RARE_CANDY
+	db PP_UP
+	db HP_UP
+	db PROTEIN
+	db IRON
+	db CARBOS
+	db CALCIUM
 
-	db UP_GRADE     ; 35
-	db LEAF_STONE   ; 36
-	db FIRE_STONE   ; 37
-	db WATER_STONE  ; 38
-	db THUNDERSTONE ; 39
-	db MOON_STONE   ; 3a
-	db SUN_STONE    ; 3b
-	db EVERSTONE    ; 3c
+	db UP_GRADE
+	db LEAF_STONE
+	db FIRE_STONE
+	db WATER_STONE
+	db THUNDERSTONE
+	db MOON_STONE
+	db SUN_STONE
+	db EVERSTONE
 
-	db SLOWPOKETAIL ; 3d
-	db LEFTOVERS    ; 3e
-	db CANDIED_YAM  ; 3f
+	db SLOWPOKETAIL
+	db LEFTOVERS
+	db CANDIED_YAM
 
-	db QUICK_CLAW   ; 40
-	db KINGS_ROCK   ; 41
-	db FOCUS_BAND   ; 42
-	db SCOPE_LENS   ; 43
-	db BRIGHTPOWDER ; 44
-	db DICTIONARY   ; 45
-	db NOISEMAKER   ; 46
-	db LIMBO_STICK  ; 47
-	db ANTIVENOM    ; 48
-	db ALOE_VERA    ; 49
-	db HANDWARMER   ; 4a
-	db BIG_PEARL    ; 4b
-	db HARDHAT      ; 4c
-	db AMULET_COIN  ; 4d
-	db CATCH_CHARM  ; 4e
-	db CLEANSE_TAG  ; 4f
+	db QUICK_CLAW
+	db KINGS_ROCK
+	db FOCUS_BAND
+	db SCOPE_LENS
+	db BRIGHTPOWDER
+	db DICTIONARY
+	db NOISEMAKER
+	db LIMBO_STICK
+	db ANTIVENOM
+	db ALOE_VERA
+	db HANDWARMER
+	db BIG_PEARL
+	db HARDHAT
+	db AMULET_COIN
+	db CATCH_CHARM
+	db CLEANSE_TAG
 
-	db PINK_BOW     ; 50
-	db BLACKBELT_I  ; 51
-	db BLACKGLASSES ; 52
-	db MIRACLE_SEED ; 53
-	db CHARCOAL     ; 54
-	db MYSTIC_WATER ; 55
-	db NEVERMELTICE ; 56
-	db MAGNET       ; 57
-	db HARD_STONE   ; 58
-	db SOFT_SAND    ; 59
-	db METAL_COAT   ; 5a
-	db SHARP_BEAK   ; 5b
-	db SILVERPOWDER ; 5c
-	db POISON_BARB  ; 5d
-	db TWISTEDSPOON ; 5e
-	db SPELL_TAG    ; 5f
-	db DRAGON_FANG  ; 60
-	db POLKADOT_BOW ; 61
-	db TRIDENT      ; 62
+	db PINK_BOW
+	db BLACKBELT_I
+	db BLACKGLASSES
+	db MIRACLE_SEED
+	db CHARCOAL
+	db MYSTIC_WATER
+	db NEVERMELTICE
+	db MAGNET
+	db HARD_STONE
+	db SOFT_SAND
+	db METAL_COAT
+	db SHARP_BEAK
+	db SILVERPOWDER
+	db POISON_BARB
+	db TWISTEDSPOON
+	db SPELL_TAG
+	db DRAGON_FANG
+	db POLKADOT_BOW
+	db TRIDENT
 
-	db LIGHT_BALL   ; 63
-	db THICK_CLUB   ; 64
-	db STICK        ; 65
-	db LUCKY_PUNCH  ; 66
-	db DRAGON_SCALE ; 67
+	db LIGHT_BALL
+	db THICK_CLUB
+	db STICK
+	db LUCKY_PUNCH
+	db DRAGON_SCALE
 
-	db NUGGET       ; 68
-	db PEARL        ; 69
-	db STARDUST     ; 6a
-	db STAR_PIECE   ; 6b
-	db TINYMUSHROOM ; 6c
-	db BIG_MUSHROOM ; 6d
-	db GOLD_LEAF    ; 6e
-	db SILVER_LEAF  ; 6f
-	db HELIX_FOSSIL ; 70
-	db DOME_FOSSIL  ; 71
-	db OLD_AMBER    ; 72
-	db BRICK_PIECE  ; 73
+	db NUGGET
+	db PEARL
+	db STARDUST
+	db STAR_PIECE
+	db TINYMUSHROOM
+	db BIG_MUSHROOM
+	db GOLD_LEAF
+	db SILVER_LEAF
+	db HELIX_FOSSIL
+	db DOME_FOSSIL
+	db OLD_AMBER
+	db BRICK_PIECE
 
-	db FLOWER_MAIL  ; 74
-	db LOVELY_MAIL  ; 75
-	db MUSIC_MAIL   ; 76
-	db SURF_MAIL    ; 77
-	db EON_MAIL     ; 78
-	db MORPH_MAIL   ; 79
-	db LITEBLUEMAIL ; 7a
-	db BLUESKY_MAIL ; 7b
-	db MIRAGE_MAIL  ; 7c
-	db PORTRAITMAIL ; 7d
+	db FLOWER_MAIL
+	db LOVELY_MAIL
+	db MUSIC_MAIL
+	db SURF_MAIL
+	db EON_MAIL
+	db MORPH_MAIL
+	db LITEBLUEMAIL
+	db BLUESKY_MAIL
+	db MIRAGE_MAIL
+	db PORTRAITMAIL
 
-	db POKE_BALL    ; 7e
-	db GREAT_BALL   ; 7f
-	db ULTRA_BALL   ; 80
-	db DUSK_BALL    ; 81
-	db TIMER_BALL   ; 82
-	db LEVEL_BALL   ; 83
-	db LURE_BALL    ; 84
-	db FAST_BALL    ; 85
-	db FRIEND_BALL  ; 86
-	db LOVE_BALL    ; 87
-	db HEAVY_BALL   ; 88
-	db MOON_BALL    ; 89
-	db MASTER_BALL  ; 8a
-	db PARK_BALL    ; 8b
+	db POKE_BALL
+	db GREAT_BALL
+	db ULTRA_BALL
+	db DUSK_BALL
+	db TIMER_BALL
+	db LEVEL_BALL
+	db LURE_BALL
+	db FAST_BALL
+	db FRIEND_BALL
+	db LOVE_BALL
+	db HEAVY_BALL
+	db MOON_BALL
+	db MASTER_BALL
+	db PARK_BALL
 
-	db BERRY        ; 8c
-	db GOLD_BERRY   ; 8d
-	db BERRY_JUICE  ; 8e
-	db MYSTERYBERRY ; 8f
-	db MIRACLEBERRY ; 90
-	db MINT_BERRY   ; 91
-	db PRZCUREBERRY ; 92
-	db PSNCUREBERRY ; 93
-	db BURNT_BERRY  ; 94
-	db ICE_BERRY    ; 95
-	db BITTER_BERRY ; 96
+	db BERRY
+	db GOLD_BERRY
+	db BERRY_JUICE
+	db MYSTERYBERRY
+	db MIRACLEBERRY
+	db MINT_BERRY
+	db PRZCUREBERRY
+	db PSNCUREBERRY
+	db BURNT_BERRY
+	db ICE_BERRY
+	db BITTER_BERRY
 
-	db RED_APRICORN ; 97
-	db BLU_APRICORN ; 98
-	db YLW_APRICORN ; 99
-	db GRN_APRICORN ; 9a
-	db PNK_APRICORN ; 9b
-	db BLK_APRICORN ; 9c
-	db WHT_APRICORN ; 9d
+	db RED_APRICORN
+	db BLU_APRICORN
+	db YLW_APRICORN
+	db GRN_APRICORN
+	db PNK_APRICORN
+	db BLK_APRICORN
+	db WHT_APRICORN
 
-	db POCKET_PC    ; 9e
-	db CLEAR_BELL   ; 9f
-	db FEAROWBOT    ; a0
-	db DISCO_BALL   ; a1
-	db SUPER_ROD    ; a2
-	db ODD_ROD      ; a3
-	db GOOD_ROD     ; a4
-	db OLD_ROD      ; a5
-	db BICYCLE      ; a6
-	db HEDGER       ; a7
-	db PICKAXE      ; a8
-	db FLOATIE      ; a9
-	db EGG_BEATER   ; aa
-	db GRAVITY_BUOY ; ab
-	db COIN_CASE    ; ac
-	db BLUE_CARD    ; ad
-	db S_S_TICKET   ; ae
-	db PASS         ; af
-	db MYSTERY_EGG  ; b0
-	db SQUIRTBOTTLE ; b1
-	db GS_BALL      ; b2
-	db SECRETPOTION ; b3
-	db RED_SCALE    ; b4
-	db BASEMENT_KEY ; b5
-	db CARD_KEY     ; b6
-	db LOST_ITEM    ; b7
-	db MACHINE_PART ; b8
-	db RAINBOW_WING ; b9
-	db SILVER_WING  ; ba
+	db POCKET_PC
+	db CLEAR_BELL
+	db FEAROWBOT
+	db DISCO_BALL
+	db SUPER_ROD
+	db ODD_ROD
+	db GOOD_ROD
+	db OLD_ROD
+	db BICYCLE
+	db HEDGER
+	db PICKAXE
+	db FLOATIE
+	db EGG_BEATER
+	db GRAVITY_BUOY
+	db COIN_CASE
+	db BLUE_CARD
+	db S_S_TICKET
+	db PASS
+	db MYSTERY_EGG
+	db SQUIRTBOTTLE
+	db GS_BALL
+	db SECRETPOTION
+	db RED_SCALE
+	db BASEMENT_KEY
+	db CARD_KEY
+	db LOST_ITEM
+	db MACHINE_PART
+	db RAINBOW_WING
+	db SILVER_WING
 
+	db NO_ITEM
 	assert_table_length NUM_ITEMS + 1
 	db -1 ; end
