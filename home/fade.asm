1874d812a7a0a9701899fb9d3bd624ba7403896a
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-04-07 14:34:03 -0700

    Update fade.asm

--- home/fade.asm
+++ home/fade.asm
@@ -24,6 +24,8 @@ RotateThreePalettesRight::
 .dmg
 	ld hl, IncGradGBPalTable_13
 	ld b, 3
+	; fallthrough
+
 RotatePalettesRight::
 ; Rotate palettes to the right and fill with loaded colors from the left
 ; If we're already at the leftmost color, fill with the leftmost color
@@ -66,6 +68,8 @@ RotateThreePalettesLeft::
 .dmg
 	ld hl, IncGradGBPalTable_15 - 1
 	ld b, 3
+	; fallthrough
+
 RotatePalettesLeft::
 ; Rotate palettes to the left and fill with loaded colors from the right
 ; If we're already at the rightmost color, fill with the rightmost color
