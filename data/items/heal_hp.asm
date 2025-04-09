678e1fc02be91eb5d10750f7f30e7c078963f452
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-04-08 19:36:42 -0700

    Update heal_hp.asm

--- data/items/heal_hp.asm
+++ data/items/heal_hp.asm
@@ -1,17 +1,18 @@
 HealingHPAmounts:
+	dbw POTION,        20
+	dbw SUPER_POTION,  50
+	dbw HYPER_POTION, 200
+	dbw MAX_POTION,   MAX_STAT_VALUE
+	dbw FULL_RESTORE, MAX_STAT_VALUE
 	dbw FRESH_WATER,   50
 	dbw SODA_POP,      64
 	dbw LEMONADE,      84
-	dbw HYPER_POTION, 200
-	dbw SUPER_POTION,  50
-	dbw POTION,        20
-	dbw MAX_POTION,   MAX_STAT_VALUE
-	dbw FULL_RESTORE, MAX_STAT_VALUE
 	dbw MOOMOO_MILK,  100
 	dbw BERRY,         10
 	dbw GOLD_BERRY,    42
+	dbw BERRY_JUICE,   84
 	dbw ENERGYPOWDER,  75
 	dbw ENERGY_ROOT,  210
 	dbw RAGECANDYBAR,  42
-	dbw BERRY_JUICE,   84
+	dbw STICK,        150
 	dbw -1, 0 ; end
