20137119763f45e34f7a1d4450dbeb5614795f11
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-04-27 12:55:54 -0700

    Update reminders_2.asm

--- engine/phone/scripts/reminders_2.asm
+++ engine/phone/scripts/reminders_2.asm
@@ -2,14 +2,6 @@ RalphHurryScript:
 	farwritetext RalphHurryText
 	end
 
-AnthonyHurryScript:
-	farwritetext AnthonyHurryText
-	end
-
-ArnieHurryScript:
-	farwritetext ArnieHurryText
-	end
-
 ToddHurryScript:
 	farwritetext ToddHaveYouGoneToDepartmentStoreText
 	end
