fde1f4d274bcdb697b0fb1028f8074094b8f21c9
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-04-27 17:47:07 -0700

    Update anthony.asm

--- engine/phone/scripts/anthony.asm
+++ engine/phone/scripts/anthony.asm
@@ -21,15 +21,17 @@ AnthonyPhoneCallerScript:
 	gettrainername STRING_BUFFER_3, HIKER, ANTHONY2
 	farscall PhoneScript_GreetPhone_Male
 	checkflag ENGINE_FLYPOINT_GOLDENROD
-	iffalse .Generic
+	iffalse .TriesSpecial
 	checkflag ENGINE_ANTHONY_READY_FOR_REMATCH
-	iftrue .Generic
+	iftrue .TriesSpecial
 	checkflag ENGINE_ANTHONY_FRIDAY_NIGHT
-	iftrue .Generic
+	iftrue .TriesSpecial
 	farscall PhoneScript_Random2
 	ifequal 0, AnthonyWantsBattle
 
-.Generic:
+.TriesSpecial:
+	farscall PhoneScript_Random5
+	ifequal 0, AnthonyUnionCaveSpeech
 	farsjump Phone_GenericCall_Male
 
 AnthonyFridayNight:
@@ -39,3 +41,6 @@ AnthonyWantsBattle:
 	getlandmarkname STRING_BUFFER_5, LANDMARK_ROUTE_33
 	setflag ENGINE_ANTHONY_READY_FOR_REMATCH
 	farsjump PhoneScript_WantsToBattle_Male
+
+AnthonyUnionCaveSpeech:
+	farsjump AnthonyUnionCaveScript
