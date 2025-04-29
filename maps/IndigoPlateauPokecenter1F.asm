3856172a421cf152cf10e35b6f945db63abe1fa1
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-04-28 21:45:20 -0700

    Update IndigoPlateauPokecenter1F.asm

--- maps/IndigoPlateauPokecenter1F.asm
+++ maps/IndigoPlateauPokecenter1F.asm
@@ -342,6 +342,7 @@ IndigoPlateauPokecenter1F_MapEvents:
 	def_bg_events
 	bg_event 14,  1, BGEVENT_READ, PokemonLeagueStatue
 	bg_event 17,  1, BGEVENT_READ, PokemonLeagueStatue
+	bg_event  9,  7, BGEVENT_READ, PokemonLeagueStatue
 
 	def_object_events
 	object_event  3,  7, SPRITE_NURSE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, IndigoPlateauPokecenter1FNurseScript, -1
