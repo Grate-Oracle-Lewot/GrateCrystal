5521584c5b8c1971ee4a7181a4b7168a4099069a
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-05-03 09:37:12 -0700

    Update main.asm

--- main.asm
+++ main.asm
@@ -269,7 +269,6 @@ INCLUDE "engine/overworld/player_movement.asm"
 INCLUDE "engine/events/engine_flags.asm"
 INCLUDE "engine/overworld/variables.asm"
 INCLUDE "data/text/battle.asm"
-INCLUDE "engine/debug/color_picker.asm"
 
 
 SECTION "bank21", ROMX
@@ -656,13 +655,6 @@ INCLUDE "engine/pokegear/townmap_convertlinebreakcharacters.asm"
 SECTION "European Mail", ROMX
 
 INCLUDE "engine/pokemon/european_mail.asm"
-
-
-SECTION "Debug Room", ROMX
-
-if DEF(_DEBUG)
-INCLUDE "engine/debug/debug_room.asm"
-endc
 
 
 SECTION "Battle Tower Text", ROMX
