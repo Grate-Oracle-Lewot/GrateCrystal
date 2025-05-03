4523f80c56a18ac9b772fdabb82eb715bda20df0
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-05-03 13:24:14 -0700

    Update VioletGym.asm

--- maps/VioletGym.asm
+++ maps/VioletGym.asm
@@ -22,9 +22,6 @@ VioletGymFalknerScript:
 	startbattle
 	reloadmapafterbattle
 	setevent EVENT_BEAT_FALKNER
-	setevent EVENT_CYNDAQUIL_POKEBALL_IN_ELMS_LAB
-	setevent EVENT_TOTODILE_POKEBALL_IN_ELMS_LAB
-	setevent EVENT_CHIKORITA_POKEBALL_IN_ELMS_LAB
 	opentext
 	writetext ReceivedZephyrBadgeText
 	playsound SFX_GET_BADGE
