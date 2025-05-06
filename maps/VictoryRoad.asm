aa9ca336a809b3d236a83c7fc653a7ee2b3a6742
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-05-06 16:07:19 -0700

    Update VictoryRoad.asm

--- maps/VictoryRoad.asm
+++ maps/VictoryRoad.asm
@@ -95,7 +95,8 @@ if DEF(_HITMON_STARTERS) || DEF(_NIDORAN_STARTERS) || DEF(_CATTLE_STARTERS) || D
 	loadtrainer RIVAL1, RIVAL1_5_CYNDAQUIL
 else
 	loadtrainer RIVAL1, RIVAL1_5_TOTODILE
-endc endc
+endc
+endc
 .FinishBattle:
 	startbattle
 	dontrestartmapmusic
