06b8fe805539ef787e4f2490c205cfa47e461431
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-05-07 15:26:14 -0700

    Update typhlosion.asm

--- data/pokemon/base_stats/typhlosion.asm
+++ data/pokemon/base_stats/typhlosion.asm
@@ -6,7 +6,7 @@
 	db FIRE, FIRE ; type
 	db 45 ; catch rate
 	db 210 ; base exp
-	db GOLD_BERRY, CHARCOAL ; items
+	db ENERGY_ROOT, CHARCOAL ; items
 	db GENDER_F12_5 ; gender ratio
 	db 11 ; step cycles to hatch
 	INCBIN "gfx/pokemon/typhlosion/front.dimensions"
