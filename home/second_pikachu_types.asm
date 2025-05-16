721b192ccd3f742b4931c860f66bcc6e74b8897e
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-05-15 20:17:35 -0700

    Update second_pikachu_types.asm

--- home/second_pikachu_types.asm
+++ home/second_pikachu_types.asm
@@ -1,13 +1,13 @@
 SecondPikachuTypes::
 ; entries correspond to PIKACHU_* constants
-  table_width 1, SecondPikachuTypes
-  db ELECTRIC ; PIKACHU_RETRO
-  db NORMAL   ; PIKACHU_SILVER
-  db FAIRY    ; PIKACHU_CRYSTAL
-  db FIGHTING ; PIKACHU_FIGHTING
-  db WATER    ; PIKACHU_SURFING
-  db FLYING   ; PIKACHU_FLYING
-  assert_table_length NUM_PIKACHU_FORMS
+	table_width 1, SecondPikachuTypes
+	db ELECTRIC ; PIKACHU_RETRO
+	db NORMAL   ; PIKACHU_SILVER
+	db FAIRY    ; PIKACHU_CRYSTAL
+	db FIGHTING ; PIKACHU_FIGHTING
+	db WATER    ; PIKACHU_SURFING
+	db FLYING   ; PIKACHU_FLYING
+	assert_table_length NUM_PIKACHU_FORMS
 
 GetSecondPikachuType::
 ; must be called when [wCurPartySpecies] = PIKACHU
