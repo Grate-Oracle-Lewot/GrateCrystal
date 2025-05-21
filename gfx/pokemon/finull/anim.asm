f012e22f55664355db0d9c2f3d12903afb645fa9
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-05-20 18:46:10 -0700

    Update anim.asm

--- gfx/pokemon/finull/anim.asm
+++ gfx/pokemon/finull/anim.asm
@@ -1,12 +1,12 @@
 	frame 0, 42
-	frame 1, 08
-	frame 2, 08
-	frame 3, 08
-	frame 4, 08
-	frame 5, 08
-	frame 4, 08
-	frame 3, 08
-	frame 2, 08
-	frame 1, 08
+	frame 1, 07
+	frame 2, 07
+	frame 3, 07
+	frame 4, 07
+	frame 5, 07
+	frame 4, 07
+	frame 3, 07
+	frame 2, 07
+	frame 1, 07
 	frame 0, 42
 	endanim
