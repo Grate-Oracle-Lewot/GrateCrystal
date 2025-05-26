761acb70b37180b7b7ed582a6873315da4dad0e6
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-05-26 01:39:00 -0700

    Update battle_anim_constants.asm

--- constants/battle_anim_constants.asm
+++ constants/battle_anim_constants.asm
@@ -217,6 +217,8 @@ BATTLEANIM_BASE_TILE EQU 7 * 7  ; Maximum size of a pokemon picture
 	const ANIM_OBJ_U_TURN_FALL
 	const ANIM_OBJ_BLUE_BEAM
 	const ANIM_OBJ_BLUE_BEAM_TIP
+	const ANIM_OBJ_GREEN_BEAM
+	const ANIM_OBJ_GREEN_BEAM_TIP
 NUM_ANIM_OBJS EQU const_value
 
 ; DoBattleAnimFrame arguments (see engine/battle_anims/functions.asm)
