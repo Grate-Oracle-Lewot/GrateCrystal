d1e3b9d4ed40c25a8327cab03dff71e1d2fd09fe
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-05-25 23:01:05 -0700

    Update animations.asm

--- data/moves/animations.asm
+++ data/moves/animations.asm
@@ -1837,7 +1837,7 @@ BattleAnim_Supersonic:
 BattleAnim_MetalSound:
 	anim_1gfx ANIM_GFX_PSYCHIC
 .loop
-	anim_sound 0, 0, SFX_ZAP_CANNON
+	anim_sound 0, 2, SFX_ZAP_CANNON
 	anim_obj ANIM_OBJ_WAVE, 64, 88, $2
 	anim_wait 4
 	anim_loop 10, .loop
