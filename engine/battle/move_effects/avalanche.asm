3bf59242e2b7f235c17c5cd18e31d811a8564dee
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-05-29 21:16:26 -0700

    Create avalanche.asm

---
+++ engine/battle/move_effects/avalanche.asm
@@ -0,0 +1,19 @@
+BattleCommand_Avalanche:
+	xor a
+	ld [wBattleAnimParam], a
+	call CheckOpponentWentFirst
+	ret z
+
+	ld hl, wPlayerDamageTaken
+	ldh a, [hBattleTurn]
+	and a
+	jr z, .ok
+	ld hl, wEnemyDamageTaken
+.ok
+	ld a, [hli]
+	or [hl]
+	ret z
+
+	ld a, 1
+	ld [wBattleAnimParam], a
+	jp DoubleDamage
