59bcec99393719e9e2060ee16195218915d2341f
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-05-30 19:31:01 -0700

    Update primeape.asm

--- data/pokemon/base_stats/primeape.asm
+++ data/pokemon/base_stats/primeape.asm
@@ -6,7 +6,7 @@
 	db FIGHTING, DARK ; type
 	db 75 ; catch rate
 	db 159 ; base exp
-	db TM_THIEF, TM_IRON_TAIL ; items
+	db X_ATTACK, TM_THIEF ; items
 	db GENDER_F50 ; gender ratio
 	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/primeape/front.dimensions"
