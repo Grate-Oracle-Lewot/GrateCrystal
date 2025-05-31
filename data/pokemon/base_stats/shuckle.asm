0f98b7cfada2975d74e711d54f9cdfe33635d536
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-05-30 19:24:00 -0700

    Update shuckle.asm

--- data/pokemon/base_stats/shuckle.asm
+++ data/pokemon/base_stats/shuckle.asm
@@ -6,7 +6,7 @@
 	db BUG, ROCK ; type
 	db 190 ; catch rate
 	db 80 ; base exp
-	db BERRY_JUICE, BERRY ; items
+	db BERRY_JUICE, TM_ENDURE ; items
 	db GENDER_F50 ; gender ratio
 	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/shuckle/front.dimensions"
