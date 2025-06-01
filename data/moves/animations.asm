990a83465f4b5f74dcf745e1d4de312714b6092d
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-05-31 21:12:12 -0700

    Update animations.asm

--- data/moves/animations.asm
+++ data/moves/animations.asm
@@ -1389,7 +1389,6 @@ BattleAnim_RockSlide:
 
 BattleAnim_Avalanche:
 	anim_1gfx ANIM_GFX_ROCKS
-	anim_if_param_equal $0, .loop
 	anim_bgeffect ANIM_BG_SHAKE_SCREEN_X, $c0, $1, $0
 .loop
 	anim_sound 0, 1, SFX_HEADBUTT
@@ -1411,7 +1410,7 @@ BattleAnim_Avalanche:
 	anim_wait 4
 	anim_sound 0, 1, SFX_HEADBUTT
 	anim_wait 4
-	anim_sound 0, 1, SFX_HEADBUTT
+	anim_sound 0, 1, SFX_MEGA_PUNCH
 	anim_wait 4
 	anim_loop 4, .loop
 	anim_wait 96
