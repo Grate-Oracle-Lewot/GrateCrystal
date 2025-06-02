77c8b5a45838ae76629fd9756a145505538226d2
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-06-01 18:58:09 -0700

    Update evos_attacks.asm

--- data/pokemon/evos_attacks.asm
+++ data/pokemon/evos_attacks.asm
@@ -2277,10 +2277,10 @@ MewEvosAttacks:
 ChikoritaEvosAttacks:
 	db EVOLVE_LEVEL, 16, BAYLEEF
 	db 0 ; no more evolutions
-	db 1, DRAGON_RAGE
-	db 2, DRAGONBREATH
-	db 3, AVALANCHE
-	db 4, AEROBLAST
+	db 1, AVALANCHE
+	db 2, BIDE
+	db 8, RAZOR_LEAF
+	db 12, REFLECT
 	db 12, LIGHT_SCREEN
 	db 15, SLEEP_POWDER
 	db 22, SYNTHESIS
