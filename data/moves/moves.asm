bd985c800180f9e10988c5b6705fad762aa5ad24
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-06-02 12:09:23 -0700

    Update moves.asm

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -172,7 +172,7 @@ Moves:
 	move ROCK_SLIDE,   EFFECT_FLINCH_HIT,         84, ROCK,         PHYSICAL, 100, 15,  20
 	move HYPER_FANG,   EFFECT_LEECH_HIT,          80, NORMAL,       PHYSICAL,  90, 10,   0
 	move SILVER_WIND,  EFFECT_ALL_UP_HIT,         60, BUG,          SPECIAL,  100, 10,  10
-	move CONVERSION,   EFFECT_CONVERSION,          0, NORMAL,       STATUS,   100, 30,   0
+	move ROCK_POLISH,  EFFECT_SPEED_UP_2,          0, ROCK,         STATUS,   100, 20,   0
 	move TRI_ATTACK,   EFFECT_TRI_ATTACK,         90, CURSE_TYPE,   SPECIAL,  100, 10,  33
 	move SUPER_FANG,   EFFECT_SUPER_FANG,          1, NORMAL,       PHYSICAL, 100, 10,   0
 	move SLASH,        EFFECT_NORMAL_HIT,         70, NORMAL,       PHYSICAL, 100, 20,   0
