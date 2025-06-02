14fb25bad3080efd5d076009a166b085bc927744
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-06-01 21:36:56 -0700

    Update search_types.asm

--- data/types/search_types.asm
+++ data/types/search_types.asm
@@ -2,21 +2,21 @@ PokedexTypeSearchConversionTable:
 ; entries correspond with PokedexTypeSearchStrings (see data/types/search_strings.asm)
 	table_width 1, PokedexTypeSearchConversionTable
 	db NORMAL
+	db FIGHTING
+	db DARK
+	db FLYING
+	db GRASS
 	db FIRE
 	db WATER
-	db GRASS
 	db ELECTRIC
-	db ICE
-	db FIGHTING
-	db POISON
+	db ROCK
 	db GROUND
-	db FLYING
+	db STEEL
+	db ICE
+	db POISON
 	db PSYCHIC_TYPE
 	db BUG
-	db ROCK
 	db GHOST
 	db DRAGON
-	db DARK
-	db STEEL
 	db FAIRY
 	assert_table_length NUM_TYPES
