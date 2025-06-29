c1089787c52ed2d1ae37adeb2531b9b6cd3bdcb4
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-06-29 13:16:59 -0700

    Update metronome_exception_moves.asm

--- data/moves/metronome_exception_moves.asm
+++ data/moves/metronome_exception_moves.asm
@@ -1,6 +1,18 @@
 ; Metronome cannot turn into these moves.
 
 MetronomeExcepts:
+if DEF(_METRONOME_ONLY)
+	db NO_MOVE
+	db METRONOME
+	db SKETCH
+	db COUNTER
+	db MIRROR_COAT
+	db PROTECT
+	db ENDURE
+	db DESTINY_BOND
+	db SLEEP_TALK
+	db STRUGGLE ; -1
+else
 	db NO_MOVE
 	db METRONOME
 	db SKETCH
@@ -12,3 +24,4 @@ MetronomeExcepts:
 	db SLEEP_TALK
 	db THIEF
 	db STRUGGLE ; -1
+endc
