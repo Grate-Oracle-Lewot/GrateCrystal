a1004fc4ee75efd4e60bdc704f06927639f58709
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-07-11 18:14:41 -0700

    Update BurnedTower1F.asm

--- maps/BurnedTower1F.asm
+++ maps/BurnedTower1F.asm
@@ -60,6 +60,15 @@ BurnedTowerRivalBattleScript:
 	closetext
 	winlosstext BurnedTowerSilver_WinText, 0
 	setlasttalked BURNEDTOWER1F_SILVER
+if DEF(_SIX_PIKACHUS)
+	checkevent EVENT_LOST_FIRST_RIVAL_BATTLE
+	iftrue .Flareon
+	loadtrainer RIVAL1, RIVAL1_3_TOTODILE
+	sjump .FinishBattle
+
+.Flareon:
+	loadtrainer RIVAL1, RIVAL1_3_CYNDAQUIL
+else
 if DEF(_LETS_GO_STARTERS) || DEF(_SLOW_STARTERS) || DEF(_TURBIN_STARTERS)
 	checkevent EVENT_GOT_CHIKORITA_FROM_ELM
 	iftrue .PlayerChosePikachu
@@ -95,6 +104,7 @@ if DEF(_HITMON_STARTERS) || DEF(_NIDORAN_STARTERS) || DEF(_CATTLE_STARTERS) || D
 	loadtrainer RIVAL1, RIVAL1_3_CYNDAQUIL
 else
 	loadtrainer RIVAL1, RIVAL1_3_TOTODILE
+endc
 endc
 endc
 .FinishBattle:
