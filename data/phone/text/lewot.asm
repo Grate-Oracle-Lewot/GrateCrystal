8dbb60873e2dcbf0c143b5bf8698287a0ba22f88
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-07-27 17:22:29 -0700

    Create lewot.asm

---
+++ data/phone/text/lewot.asm
@@ -0,0 +1,209 @@
+LewotPhoneGreetingText:
+	text "Urgh… hello?"
+
+	para "Oh… <PLAYER>,"
+	line "huh?"
+	done
+
+LewotPhonePopcornText:
+	text "One time I put a"
+	line "hot bag of popcorn"
+	cont "on top of my DS…"
+
+	para "The top screen"
+	line "got ruined, like"
+
+	para "when you take a"
+	line "magnet to a compu-"
+	cont "ter screen."
+
+	para "But the bottom"
+	line "screen was fine,"
+
+	para "so I could still"
+	line "play some games on"
+	cont "it, like #MON…"
+	done
+
+LewotPhoneZiggText:
+	text "You know that my"
+	line "FERALIGATR is"
+	cont "named ZIGG…"
+
+	para "This is indeed"
+	line "what I named him"
+	cont "as a kid."
+
+	para "It doesn't really"
+	line "have any special"
+	cont "meaning, though."
+
+	para "I just made it up."
+	done
+
+LewotPhoneWaterText:
+	text "I usually pick the"
+	line "water starter,"
+
+	para "hence my FERALI-"
+	line "GATR…"
+
+	para "I do have a fond-"
+	line "ness for FUECOCO"
+
+	para "due to its resemb-"
+	line "lance to TOTODILE."
+
+	para "I like water bec-"
+	line "ause of its few"
+	cont "weaknesses."
+
+	para "Aesthetically, I'm"
+	line "also a fan of ice"
+	cont "and electric."
+	done
+
+LewotPhoneGenText:
+	text "Let me tell you"
+	line "the story of how I"
+
+	para "ruined my own ex-"
+	line "perience of Gen 3."
+
+	para "It all goes back"
+	line "to how in Gen 1,"
+
+	para "moves that claimed"
+	line "to be fully accur-"
+	cont "ate still had a 1"
+	cont "in 256 chance to"
+	cont "miss."
+
+	para "I assumed this was"
+	line "true all the way"
+
+	para "through Gen 3, and"
+	line "and thus didn't"
+
+	para "value accuracy very"
+	line "much."
+
+	para "So I liked to have"
+	line "an ultimate move"
+
+	para "such as HYDRO PUMP"
+	line "on my starter."
+
+	para "Problem is, while"
+	line "MUDKIP learns it,"
+
+	para "MARSHTOMP and"
+	line "SWAMPERT do not."
+
+	para "MUDKIP isn't yet"
+	line "ground-type, so"
+
+	para "you can imagine"
+	line "how I did against"
+	cont "WATTSON."
+
+	para "It keeps going on"
+	line "like that, but I'll"
+	cont "spare you."
+	done
+
+LewotPhoneIceSculptureText:
+	text "I suppose I could"
+	line "tell you about the"
+
+	para "time me and A.D."
+	line "stole an ice"
+	cont "sculpture."
+
+	para "We were at a hotel"
+	line "where there was"
+
+	para "this wedding going"
+	line "on that we weren't"
+	cont "a part of…"
+
+	para "They had an ice"
+	line "sculpture of two"
+
+	cont "swans making a"
+	line "heart with their"
+	cont "necks, you know…"
+
+	para "Somehow we had ac-"
+	line "quired an infinite"
+
+	para "supply of small"
+	line "plastic cups."
+
+	para "So every so often,"
+	line "one of us would go"
+
+	para "over and scoop up"
+	line "some of the water"
+
+	para "from the melting"
+	line "ice sculpture."
+
+	para "Eventually, it was"
+	line "completely melted"
+
+	para "and we technically"
+	line "had most of the"
+	cont "water."
+
+	para "We didn't really"
+	line "know what to do"
+
+	para "with it afterward,"
+	line "though, so we just"
+	cont "left it there."
+	done
+
+LewotPhonePikminText:
+	text "In PIKMIN 4, I"
+	line "like to do a no-"
+	cont "Pikmin-deaths run…"
+
+	para "To 100% the game,"
+	line "the minimum amount"
+
+	para "of days with no"
+	line "deaths is 32, al-"
+	cont "though the game"
+	cont "displays it as 33"
+
+	para "because it waits"
+	line "until morning to"
+
+	para "cure any remaining"
+	line "leaflings."
+
+	para "You have to spend"
+	line "3 days in the SUN-"
+	cont "SPECKLED TERRACE"
+
+	para "and SERENE SHORES,"
+	line "but the other"
+	cont "areas only need 2."
+
+	para "The rest of the"
+	line "days are the night"
+
+	para "missions, the tu-"
+	line "torial, and the"
+
+	para "super dandori cave"
+	line "thing."
+	done
+
+LewotPhoneHangUpText:
+	text "I'm sure you feel"
+	line "enlightened."
+
+	text "Enjoy the hack."
+	done
