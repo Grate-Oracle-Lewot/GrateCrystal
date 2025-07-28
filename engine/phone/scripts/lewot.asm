b1e0642e407b5fdf4cfe2b94067aa60a6efc21b7
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-07-27 17:10:23 -0700

    Update lewot.asm

--- engine/phone/scripts/lewot.asm
+++ engine/phone/scripts/lewot.asm
@@ -1,29 +1,35 @@
 LewotPhoneCalleeScript:
 	farwritetext LewotPhoneGreetingText
 	promptbutton
-	random 5
+	random 6
 	ifequal 0, .Zigg
 	ifequal 1, .Water
 	ifequal 2, .Gen
 	ifequal 3, .IceSculpture
+	ifequal 4, .Pikmin4
 
-	farwritetext LewotPikmin4Text
-  sjump .hangup
+; else
+	farwritetext LewotPhonePopcornText
+	sjump .hangup
 
 .Zigg
 	farwritetext LewotPhoneZiggText
-  sjump .hangup
+	sjump .hangup
 
 .Water
 	farwritetext LewotPhoneWaterText
-  sjump .hangup
+	sjump .hangup
 
 .Water
 	farwritetext LewotPhoneGenText
-  sjump .hangup
+	sjump .hangup
 
 .IceSculpture
 	farwritetext LewotPhoneIceSculptureText
+	sjump .hangup
+
+.Pikmin4
+	farwritetext LewotPikmin4Text
 
 .hangup
 	promptbutton
