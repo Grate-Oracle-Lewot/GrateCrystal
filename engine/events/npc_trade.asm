ad9fb01b2da83b021b016fa7cd398d648da95617
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-08-06 19:31:27 -0700

    Update npc_trade.asm

--- engine/events/npc_trade.asm
+++ engine/events/npc_trade.asm
@@ -181,7 +181,7 @@ DoNPCTrade:
 	xor a
 	ld [wMonType], a ; PARTYMON
 	ld [wPokemonWithdrawDepositParameter], a ; REMOVE_PARTY
-	callfar RemoveMonFromPartyOrBox
+	farcall RemoveMonFromPartyOrBox
 	predef TryAddMonToParty
 
 	ld e, NPCTRADE_DIALOG
