006e2f086f664d133c2868571cac15a499142043
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-08-06 19:30:30 -0700

    Update bg_effects.asm

--- engine/battle_anims/bg_effects.asm
+++ engine/battle_anims/bg_effects.asm
@@ -509,7 +509,7 @@ BattleBGEffect_BattlerObj_2Row:
 	jp EndBattleBGEffect
 
 _QueueBattleAnimation:
-	callfar QueueBattleAnimation
+	farcall QueueBattleAnimation
 	ret
 
 BattleBGEffect_RemoveMon:
@@ -2655,12 +2655,12 @@ BattleBGEffects_CheckSGB:
 
 BattleBGEffects_Sine:
 	ld e, a
-	callfar BattleAnim_Sine_e
+	farcall BattleAnim_Sine_e
 	ld a, e
 	ret
 
 BattleBGEffects_Cosine:
 	ld e, a
-	callfar BattleAnim_Cosine_e
+	farcall BattleAnim_Cosine_e
 	ld a, e
 	ret
