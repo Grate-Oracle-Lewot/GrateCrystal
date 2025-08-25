2fe130efc092759bca61649426302599813735a9
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-08-24 17:23:42 -0700

    Update attributes.asm

--- data/trainers/attributes.asm
+++ data/trainers/attributes.asm
@@ -2,7 +2,7 @@ TrainerClassAttributes:
 ; entries correspond to trainer classes (see constants/trainer_constants.asm)
 	table_width NUM_TRAINER_ATTRIBUTES, TrainerClassAttributes
 
-; SWITCH_* flags can be used alone or combined, and no flag means they never switch. Similarly, no *_USE flag means they never use items.
+; A lack of a *_USE flag means they never use items. Functionally identical to giving them no usable items.
 ; SWITCH_STATUS will switch out in response to volatile statuses regardless of other switch flags. Without it, volatile statuses will not trigger a switch.
 ; SWITCH_RARELY can be combined with _SOMETIMES or _OFTEN to get the latter switch frequency but avoid taking stat levels into account.
 ; SWITCH_OFTEN skips a check to prevent switching at low HP, in addition to its general increase of frequency.
