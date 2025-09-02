1e226e3879fd16d9202cba6a40978057030da3ae
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-09-02 13:18:20 -0700

    Update move_constants.asm

--- constants/move_constants.asm
+++ constants/move_constants.asm
@@ -195,7 +195,7 @@
 	const BELLY_DRUM   ; bb
 	const SLUDGE_BOMB  ; bc
 	const MUD_SLAP     ; bd
-	const OCTAZOOKA    ; be
+	const CONVERSION   ; be
 	const SPIKES       ; bf
 	const BLITZKRIEG   ; c0
 	const FORESIGHT    ; c1
