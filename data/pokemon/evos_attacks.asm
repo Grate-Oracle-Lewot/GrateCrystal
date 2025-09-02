7597eae2d697a959b6842488867dc86a9f3ec17b
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-09-02 13:01:40 -0700

    Update evos_attacks.asm

--- data/pokemon/evos_attacks.asm
+++ data/pokemon/evos_attacks.asm
@@ -2046,7 +2046,7 @@ PorygonEvosAttacks:
 	db EVOLVE_ITEM, UP_GRADE, PORYGON2
 	db 0 ; no more evolutions
 	db 1, TACKLE
-	db 4, BARRIER
+	db 4, CONVERSION
 	db 9, GROWTH
 	db 9, MINIMIZE
 	db 12, PAY_DAY
@@ -3559,7 +3559,7 @@ DonphanEvosAttacks:
 Porygon2EvosAttacks:
 	db 0 ; no more evolutions
 	db 1, TACKLE
-	db 4, BARRIER
+	db 4, CONVERSION
 	db 9, GROWTH
 	db 9, MINIMIZE
 	db 12, PAY_DAY
