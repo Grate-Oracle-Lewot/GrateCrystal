d9448d929daf6b02f18743d6dbb16955868a1bc9
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-09-01 22:48:12 -0700

    Update PlayersHouse1F.asm

--- maps/PlayersHouse1F.asm
+++ maps/PlayersHouse1F.asm
@@ -17,6 +17,7 @@ PlayersHouse1F_MapScripts:
 
 MeetMomLeftScript:
 	setevent EVENT_TEMPORARY_UNTIL_MAP_RELOAD_1
+	; fallthrough
 
 MeetMomRightScript:
 	playmusic MUSIC_MOM
@@ -29,7 +30,25 @@ MeetMomRightScript:
 
 .OnRight:
 	applymovement PLAYERSHOUSE1F_MOM1, MomWalksToPlayerMovement
+	; fallthrough
+
 MeetMomScript:
+	setflag ENGINE_ZEPHYRBADGE
+	setflag ENGINE_HIVEBADGE
+	setflag ENGINE_PLAINBADGE
+	setflag ENGINE_FOGBADGE
+	setflag ENGINE_STORMBADGE
+	setflag ENGINE_MINERALBADGE
+	setflag ENGINE_GLACIERBADGE
+	setflag ENGINE_RISINGBADGE
+	setflag ENGINE_BOULDERBADGE
+	setflag ENGINE_CASCADEBADGE
+	setflag ENGINE_THUNDERBADGE
+	setflag ENGINE_RAINBOWBADGE
+	setflag ENGINE_MARSHBADGE
+	setflag ENGINE_SOULBADGE
+	setflag ENGINE_VOLCANOBADGE
+	setflag ENGINE_EARTHBADGE
 	setmapscene CELADON_GYM, SCENE_FINISHED
 	checkflag ENGINE_PLAYER_IS_FEMALE
 	iftrue .SkipBouncer
