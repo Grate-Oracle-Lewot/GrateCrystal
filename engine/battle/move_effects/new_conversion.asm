e0a5b5ff326ecd68d4168bc59bdb8774572ee555
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-09-02 19:46:52 -0700

    Create new_conversion.asm

---
+++ engine/battle/move_effects/new_conversion.asm
@@ -0,0 +1,217 @@
+BattleCommand_Conversion:
+
+; Conversion1, changes both types
+	ld hl, wBattleMonMoves
+	ld de, wBattleMonType1
+	ldh a, [hBattleTurn]
+	and a
+	jr z, .got_moves
+	ld hl, wEnemyMonMoves
+	ld de, wEnemyMonType1
+.got_moves
+	push de
+	ld c, 0
+	ld de, wStringBuffer1
+.loop
+	push hl
+	ld b, 0
+	add hl, bc
+	ld a, [hl]
+	pop hl
+	and a
+	jr z, .okay
+	push hl
+	push bc
+	dec a
+	ld hl, Moves + MOVE_TYPE
+	call GetMoveAttr
+	and TYPE_MASK
+	ld [de], a
+	inc de
+	pop bc
+	pop hl
+	inc c
+	ld a, c
+	cp NUM_MOVES
+	jr c, .loop
+.okay
+	ld a, $ff
+	ld [de], a
+	inc de
+	ld [de], a
+	inc de
+	ld [de], a
+	pop de
+	ld hl, wStringBuffer1
+.loop2
+	ld a, [hl]
+	cp -1
+	jp z, FailMove
+	cp CURSE_TYPE
+	jr z, .next
+	ld a, [de]
+	cp [hl]
+	jr z, .next
+	inc de
+	ld a, [de]
+	dec de
+	cp [hl]
+	jr nz, .loop3
+.next
+	inc hl
+	jr .loop2
+
+.loop3
+	call BattleRandom
+	maskbits NUM_MOVES
+	ld c, a
+	ld b, 0
+	ld hl, wStringBuffer1
+	add hl, bc
+	ld a, [hl]
+	cp -1
+	jr z, .loop3
+	cp CURSE_TYPE
+	jr z, .loop3
+	ld a, [de]
+	cp [hl]
+	jr z, .loop3
+	inc de
+	ld a, [de]
+	dec de
+	cp [hl]
+	jr z, .loop3
+	ld a, [hl]
+	ld [de], a
+	inc de
+	ld [de], a
+	jp ConversionAnimateMoveAndPrintText
+
+; Conversion2 after a successful Conversion1, only changes second type
+	ld a, [wBattleType]
+	cp BATTLETYPE_TYPELESS
+	ret z
+
+	ld hl, wBattleMonType2
+	ldh a, [hBattleTurn]
+	and a
+	jr z, .got_type_2
+	ld hl, wEnemyMonType2
+.got_type_2
+	ld a, BATTLE_VARS_LAST_COUNTER_MOVE_OPP
+	call GetBattleVar
+	and a
+	ret z
+	call ConversionOptimization1
+
+.loop4
+	call ConversionGetRandomType
+	ld [hl], a
+	call ConversionOptimization2
+	call ConversionCheckTypeMatchup
+	call ConversionOptimization3
+	jr nc, .loop4
+	call BattleCommand_SwitchTurn
+
+	ld a, [hl]
+	ld [wNamedObjectIndex], a
+	jr ConversionPrintTookOnType
+
+.Fail1:
+; Conversion2 after a failed Conversion1, changes both types
+	ld a, [wBattleType]
+	cp BATTLETYPE_TYPELESS
+	jp z, FailMove
+
+	ld hl, wBattleMonType1
+	ldh a, [hBattleTurn]
+	and a
+	jr z, .got_type
+	ld hl, wEnemyMonType1
+.got_type
+	ld a, BATTLE_VARS_LAST_COUNTER_MOVE_OPP
+	call GetBattleVar
+	and a
+	jp z, FailMove
+	call ConversionOptimization1
+
+.loop5
+	call ConversionGetRandomType
+	ld [hli], a
+	ld [hld], a
+	call ConversionOptimization2
+	call BattleCheckTypeMatchup
+	call ConversionOptimization3
+	jr nc, .loop5
+	call BattleCommand_SwitchTurn
+
+	ld a, [hl]
+	; fallthrough
+
+ConversionAnimateMoveAndPrintText:
+	ld [wNamedObjectIndex], a
+	call AnimateCurrentMove
+	; fallthrough
+
+ConversionPrintTookOnType:
+	predef GetTypeName
+	ld hl, TookOnTypeText
+	jp StdBattleTextbox
+
+ConversionGetRandomType:
+	call BattleRandom
+	maskbits TYPES_END
+	cp UNUSED_TYPES
+	ret c
+	cp UNUSED_TYPES_END
+	jr c, ConversionGetRandomType
+	cp TYPES_END
+	jr nc, ConversionGetRandomType
+	ret
+
+ConversionOptimization1:
+	push hl
+	dec a
+	ld hl, Moves + MOVE_TYPE
+	call GetMoveAttr
+	and TYPE_MASK
+	ld d, a
+	pop hl
+	jp BattleCommand_SwitchTurn
+
+ConversionOptimization2:
+	push hl
+	ld a, BATTLE_VARS_MOVE_TYPE
+	call GetBattleVarAddr
+	and TYPE_MASK
+	push af
+	push hl
+	ld a, d
+	ld [hl], a
+	ret
+
+ConversionOptimization3:
+	pop hl
+	pop af
+	ld [hl], a
+	pop hl
+	ld a, [wTypeMatchup]
+	cp EFFECTIVE
+	ret
+
+ConversionCheckTypeMatchup:
+	ld hl, wEnemyMonType2
+	ldh a, [hBattleTurn]
+	and a
+	jr z, .get_type
+	ld hl, wBattleMonType2
+.get_type
+	ld a, BATTLE_VARS_MOVE_TYPE
+	call GetBattleVar ; preserves hl, de, and bc
+	and TYPE_MASK
+	push hl
+	push de
+	push bc
+	ld d, a
+	ld b, [hl]
+	jp CheckType2Matchup
