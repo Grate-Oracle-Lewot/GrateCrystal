f0dd8c17ad3766399c3a23f0635f07de88f070d7
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-09-05 21:38:38 -0700

    Update special_pointers.asm

--- data/events/special_pointers.asm
+++ data/events/special_pointers.asm
@@ -180,6 +180,3 @@ SpecialsPointers::
 	add_special AskRememberPassword
 	add_special LoadMapPalettes
 	add_special UnusedFindItemInPCOrBag
-
-	add_special InitialSetDSTFlag
-	add_special InitialClearDSTFlag
