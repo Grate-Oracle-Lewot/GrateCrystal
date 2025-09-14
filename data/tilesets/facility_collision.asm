23cb671e5cd9f87bb0ca086a38bb3418c602e58c
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-09-13 19:20:18 -0700

    Update facility_collision.asm

--- data/tilesets/facility_collision.asm
+++ data/tilesets/facility_collision.asm
@@ -1,4 +1,4 @@
-	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 00
+	tilecoll WALL, WALL, WALL, WALL ; 00
 	tilecoll WALL, WALL, FLOOR, FLOOR ; 01
 	tilecoll WALL, WALL, WALL, WALL ; 02
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 03
