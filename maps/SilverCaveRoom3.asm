7fdd24b3e285b8e7e73169cb9320c07e351be19e
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-09-15 17:11:53 -0700

    Update SilverCaveRoom3.asm

--- maps/SilverCaveRoom3.asm
+++ maps/SilverCaveRoom3.asm
@@ -69,11 +69,11 @@ SilverCaveRoom3_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event  9, 33, SILVER_CAVE_ROOM_2, 2
+	warp_event 19, 55, SILVER_CAVE_ROOM_2, 2
 
 	def_coord_events
 
 	def_bg_events
 
 	def_object_events
-	object_event 10, 10, SPRITE_RED, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, Red, EVENT_RED_IN_MT_SILVER
+	object_event 20, 10, SPRITE_RED, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, Red, EVENT_RED_IN_MT_SILVER
