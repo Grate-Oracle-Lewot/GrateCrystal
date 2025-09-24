9798b9581c37da87ca99f1f151ae52ee479605a0
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-09-24 15:48:08 -0700

    Update move_constants.asm

--- constants/move_constants.asm
+++ constants/move_constants.asm
@@ -264,29 +264,29 @@
 NUM_ATTACKS EQU const_value - 1
 
 ; Battle animations use the same constants as the moves up to this point
-	const ANIM_SWEET_SCENT_2     ; 100
-	const ANIM_THROW_POKE_BALL   ; 101
-	const ANIM_SEND_OUT_MON      ; 102
-	const ANIM_RETURN_MON        ; 103
-	const ANIM_CONFUSED          ; 104
-	const ANIM_SLP               ; 105
-	const ANIM_BRN               ; 106
-	const ANIM_PSN               ; 107
-	const ANIM_SAP               ; 108
-	const ANIM_FRZ               ; 109
-	const ANIM_PAR               ; 10a
-	const ANIM_IN_LOVE           ; 10b
-	const ANIM_IN_SANDSTORM      ; 10c
-	const ANIM_IN_NIGHTMARE      ; 10d
+	const ANIM_SWEET_SCENT_2    ; 100
+	const ANIM_THROW_POKE_BALL  ; 101
+	const ANIM_SEND_OUT_MON     ; 102
+	const ANIM_RETURN_MON       ; 103
+	const ANIM_CONFUSED         ; 104
+	const ANIM_SLP              ; 105
+	const ANIM_BRN              ; 106
+	const ANIM_PSN              ; 107
+	const ANIM_SAP              ; 108
+	const ANIM_FRZ              ; 109
+	const ANIM_PAR              ; 10a
+	const ANIM_IN_LOVE          ; 10b
+	const ANIM_IN_SANDSTORM     ; 10c
+	const ANIM_IN_NIGHTMARE     ; 10d
 ; battle anims
-	const ANIM_MISS              ; 10e
-	const ANIM_ENEMY_DAMAGE      ; 10f
-	const ANIM_ENEMY_STAT_DOWN   ; 110
-	const ANIM_PLAYER_STAT_DOWN  ; 111
-	const ANIM_PLAYER_DAMAGE     ; 112
-	const ANIM_WOBBLE            ; 113
-	const ANIM_SHAKE             ; 114
-	const ANIM_HIT_CONFUSION     ; 115
+	const ANIM_MISS             ; 10e
+	const ANIM_ENEMY_DAMAGE     ; 10f
+	const ANIM_ENEMY_STAT_DOWN  ; 110
+	const ANIM_PLAYER_STAT_DOWN ; 111
+	const ANIM_PLAYER_DAMAGE    ; 112
+	const ANIM_WOBBLE           ; 113
+	const ANIM_SHAKE            ; 114
+	const ANIM_HIT_CONFUSION    ; 115
 NUM_BATTLE_ANIMS EQU const_value - 1
 
 ; wNumHits uses offsets from ANIM_MISS
