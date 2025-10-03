e4d9ef13b69f11346aed1f063fe1ce973e4eedeb
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-10-02 20:49:00 -0700

    Update trainer_constants.asm

--- constants/trainer_constants.asm
+++ constants/trainer_constants.asm
@@ -553,8 +553,6 @@ KRIS EQU __trainer_class__
 	const IRVING
 	const DALTON
 	const CYAN
-	const IRWIN2
-	const IRWIN3
 
 	trainerclass BLACKBELT_T ; 32
 	const KENJI1
@@ -682,7 +680,6 @@ KRIS EQU __trainer_class__
 	const ALEX
 	const REX
 	const ALLAN
-	const DEREK2
 
 	trainerclass KIMONO_GIRL ; 3c
 	const NAOKO
