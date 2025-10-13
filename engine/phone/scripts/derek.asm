5c9fce9ad1e87d9ddfe21be21c9b3b8a610d310d
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-10-13 00:14:03 -0700

    Update derek.asm

--- engine/phone/scripts/derek.asm
+++ engine/phone/scripts/derek.asm
@@ -1,22 +1,33 @@
 DerekPhoneCalleeScript:
 	gettrainername STRING_BUFFER_3, POKEFANM, DEREK1
+	checkflag ENGINE_DEREK_READY_FOR_REMATCH
+	iftrue .WantsBattle
 	farscall PhoneScript_AnswerPhone_Male
 	checkflag ENGINE_DEREK_HAS_NUGGET
 	iftrue .Nugget
+	checkflag ENGINE_DEREK_SATURDAY_MORNING
+	iftrue .NotSaturday
+	readvar VAR_WEEKDAY
+	ifnotequal SATURDAY, .NotSaturday
+	checktime MORN
+	iftrue DerekSaturdayMorning
+
+.NotSaturday:
 	farscall PhoneScript_Random2
 	ifequal 0, .NoContest
 	checkflag ENGINE_DAILY_BUG_CONTEST
 	iftrue .NoContest
 	readvar VAR_WEEKDAY
-	ifequal TUESDAY, .ContestToday
-	ifequal THURSDAY, .ContestToday
-	ifequal SATURDAY, .ContestToday
+	ifequal TUESDAY, DerekContestToday
+	ifequal THURSDAY, DerekContestToday
+	ifequal SATURDAY, DerekContestToday
 
 .NoContest:
 	farsjump DerekHangUpScript
 
-.ContestToday:
-	farsjump PhoneScript_BugCatchingContest
+.WantsBattle:
+	getlandmarkname STRING_BUFFER_5, LANDMARK_ROUTE_39
+	farsjump DerekReminderScript
 
 .Nugget:
 	getlandmarkname STRING_BUFFER_5, LANDMARK_ROUTE_39
@@ -25,24 +36,40 @@ DerekPhoneCalleeScript:
 DerekPhoneCallerScript:
 	gettrainername STRING_BUFFER_3, POKEFANM, DEREK1
 	farscall PhoneScript_GreetPhone_Male
+	checkflag ENGINE_DEREK_READY_FOR_REMATCH
+	iftrue .NoBattle
+	checkflag ENGINE_DEREK_SATURDAY_MORNING
+	iftrue .NoBattle
+	farscall PhoneScript_Random2
+	ifequal 0, DerekWantsBattle
+
+.NoBattle:
 	farscall PhoneScript_Random2
 	ifequal 0, .NoContest
 	checkflag ENGINE_DAILY_BUG_CONTEST
 	iftrue .NoContest
 	readvar VAR_WEEKDAY
-	ifequal TUESDAY, .ContestToday
-	ifequal THURSDAY, .ContestToday
-	ifequal SATURDAY, .ContestToday
+	ifequal TUESDAY, DerekContestToday
+	ifequal THURSDAY, DerekContestToday
+	ifequal SATURDAY, DerekContestToday
 
 .NoContest:
 	farscall PhoneScript_Random4
 	ifequal 0, .Nugget
 	farsjump Phone_GenericCall_Male
 
-.ContestToday:
-	farsjump PhoneScript_BugCatchingContest
-
 .Nugget:
 	setflag ENGINE_DEREK_HAS_NUGGET
 	getlandmarkname STRING_BUFFER_5, LANDMARK_ROUTE_39
 	farsjump PhoneScript_FoundItem_Male
+
+DerekSaturdayMorning:
+	setflag ENGINE_DEREK_SATURDAY_MORNING
+
+DerekWantsBattle:
+	getlandmarkname STRING_BUFFER_5, LANDMARK_ROUTE_39
+	setflag ENGINE_DEREK_READY_FOR_REMATCH
+	farsjump PhoneScript_WantsToBattle_Male
+
+DerekContestToday:
+	farsjump PhoneScript_BugCatchingContest
