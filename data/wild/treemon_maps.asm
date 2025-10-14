e607cec20ef05f77f718a805381a560678dc0f8b
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-10-14 15:45:24 -0700

    Update treemon_maps.asm

--- data/wild/treemon_maps.asm
+++ data/wild/treemon_maps.asm
@@ -21,13 +21,13 @@ TreeMonMaps:
 	treemon_map ROUTE_42,                  TREEMON_SET_CITY
 	treemon_map ROUTE_43,                  TREEMON_SET_LAKE
 	treemon_map ROUTE_44,                  TREEMON_SET_CITY
-	treemon_map ROUTE_46,                  TREEMON_SET_CITY
+	treemon_map ROUTE_46,                  TREEMON_SET_ROUTE
 	treemon_map NEW_BARK_TOWN,             TREEMON_SET_TOWN
 	treemon_map CHERRYGROVE_CITY,          TREEMON_SET_TOWN
 	treemon_map VIOLET_CITY,               TREEMON_SET_TOWN
 	treemon_map AZALEA_TOWN,               TREEMON_SET_TOWN
 	treemon_map ECRUTEAK_CITY,             TREEMON_SET_TOWN
-	treemon_map MAHOGANY_TOWN,             TREEMON_SET_TOWN
+	treemon_map MAHOGANY_TOWN,             TREEMON_SET_CITY
 	treemon_map ILEX_FOREST,               TREEMON_SET_FOREST
 	treemon_map BATTLE_TOWER_OUTSIDE,      TREEMON_SET_CITY
 	treemon_map LAKE_OF_RAGE,              TREEMON_SET_LAKE
