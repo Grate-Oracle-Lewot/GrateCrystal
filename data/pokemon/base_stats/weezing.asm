5cc08de988851033d16f97b115c37b8de65af4ff
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-10-17 20:50:32 -0700

    Update weezing.asm

--- data/pokemon/base_stats/weezing.asm
+++ data/pokemon/base_stats/weezing.asm
@@ -3,7 +3,11 @@
 	db  65,  90, 120,  60,  85,  70
 	;   hp  atk  def  spd  sat  sdf
 
+if DEF(_VANILLA_TYPES)
+	db POISON, POISON ; type
+else
 	db POISON, FAIRY ; type
+endc
 	db 60 ; catch rate
 	db 173 ; base exp
 	db SMOKE_BALL, EXP_SHARE ; items
