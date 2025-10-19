23f04eb7e7909584d985fece49d7c0260d7ab2b7
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-10-19 14:03:59 -0700

    Update float_mons.asm

--- home/float_mons.asm
+++ home/float_mons.asm
@@ -1,4 +1,5 @@
 FloatMons::
+if DEF(_VANILLA_TYPES)
 	db BEEDRILL
 	db VENOMOTH
 	db MAGNEMITE
@@ -9,13 +10,29 @@ FloatMons::
 	db KOFFING
 	db WEEZING
 	db MEW
-	db LEDYBA ; no longer Flying type in Grate Crystal
-	db LEDIAN ; no longer Flying type in Grate Crystal
 	db MISDREAVUS
 	db UNOWN
 	db JIRK
-	db LUGIA ; no longer Flying type in Grate Crystal
-	db HO_OH ; no longer Flying type in Grate Crystal
 	db CELEBI
-	db YANMEGA ; no longer Flying type in Grate Crystal
+else
+	db BEEDRILL
+	db VENOMOTH
+	db MAGNEMITE
+	db MAGNETON
+	db GASTLY
+	db HAUNTER
+	db GENGAR
+	db KOFFING
+	db WEEZING
+	db MEW
+	db LEDYBA
+	db LEDIAN
+	db MISDREAVUS
+	db UNOWN
+	db JIRK
+	db LUGIA
+	db HO_OH
+	db CELEBI
+	db YANMEGA
+endc
 	db -1 ; end
