a9a255f795a43c47f07e0d344f7a22889e8d72cd
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-10-29 18:18:41 -0700

    Update field_move_blocks.asm

--- data/collision/field_move_blocks.asm
+++ data/collision/field_move_blocks.asm
@@ -16,38 +16,38 @@ CutTreeBlockPointers:
 ; facing block, replacement block, animation
 	db $23, $02, $01 ; dry grass
 .johto:
-	db $5b, $3c, 0 ; tree
-	db $5f, $3d, 0 ; tree
-	db $63, $3f, 0 ; tree
-	db $67, $3e, 0 ; tree
+	db $5b, $3c, $00 ; tree
+	db $5f, $3d, $00 ; tree
+	db $63, $3f, $00 ; tree
+	db $67, $3e, $00 ; tree
 .johto_modern:
 	db $03, $02, $01 ; grass
 	db -1 ; end
 
 .kanto:
 ; facing block, replacement block, animation
-	db $0b, $0a, 1 ; grass
-	db $32, $6d, 0 ; tree
-	db $33, $6c, 0 ; tree
-	db $34, $6f, 0 ; tree
-	db $35, $4c, 0 ; tree
-	db $60, $6e, 0 ; tree
+	db $0b, $0a, $01 ; grass
+	db $32, $6d, $00 ; tree
+	db $33, $6c, $00 ; tree
+	db $34, $6f, $00 ; tree
+	db $35, $4c, $00 ; tree
+	db $60, $6e, $00 ; tree
 	db -1 ; end
 
 .park:
 ; facing block, replacement block, animation
-	db $13, $03, 1 ; tall grass
-	db $03, $04, 1 ; grass
+	db $13, $03, $01 ; tall grass
+	db $03, $04, $01 ; grass
 	db -1 ; end
 
 .forest:
 ; facing block, replacement block, animation
-	db $0f, $17, 0
+	db $0f, $17, $00 ; tree
 	db -1 ; end
 
 .train_station:
 ; facing block, replacement block, animation
-	db $17, $0d, 0
+	db $17, $0d, $00 ; tree
 	db -1 ; end
 
 
@@ -60,5 +60,5 @@ WhirlpoolBlockPointers:
 
 .johto:
 ; facing block, replacement block, animation
-	db $07, $36, 0
+	db $07, $36, $00 ; whirlpool
 	db -1 ; end
