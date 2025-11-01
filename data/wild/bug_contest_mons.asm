482341a15df0876c185f232d8573402951cc8843
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-11-01 10:42:23 -0700

    Update bug_contest_mons.asm

--- data/wild/bug_contest_mons.asm
+++ data/wild/bug_contest_mons.asm
@@ -1,5 +1,23 @@
 ContestMons::
 	;   %, species,   min, max
+if DEF(_LITTLE_CUP)
+	db 10, CATERPIE,    1, 20
+	db 10, WEEDLE,      1, 20
+	db  5, CATERPIE,    5, 20
+	db  5, WEEDLE,      5, 20
+	db  9, VENONAT,     5, 20
+	db  9, PARAS,       5, 20
+	db  8, LEDYBA,      5, 20
+	db  8, SPINARAK,    5, 20
+	db  6, YANMA,       5, 20
+	db  5, PINECO,      5, 20
+	db  5, PINECO,      1, 20
+	db  5, CATERPIE,   10, 20
+	db  5, WEEDLE,     10, 20
+	db  5, SCYTHER,    10, 20
+	db  5, KRABBY,     10, 20
+	db -1, PINECO,     10, 10 ; end
+else
 	db 10, CATERPIE,    1, 20
 	db 10, WEEDLE,      1, 20
 	db  5, METAPOD,     5, 20
@@ -16,3 +34,4 @@ ContestMons::
 	db  5, SCYTHER,    10, 20
 	db  5, PINSIR,     10, 20
 	db -1, PINECO,     10, 10 ; end
+endc
