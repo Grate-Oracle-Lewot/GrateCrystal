25a49a9be8b370056ef0592248230e697affeb4a
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-11-03 20:41:08 -0800

    Update SilphCo1F.asm

--- maps/SilphCo1F.asm
+++ maps/SilphCo1F.asm
@@ -1,5 +1,6 @@
 	object_const_def
 	const SILPHCO1F_RECEPTIONIST
+	const SILPHCO1F_CLERK
 
 SilphCo1F_MapScripts:
 	def_scene_scripts
@@ -9,22 +10,8 @@ SilphCo1F_MapScripts:
 SilphCoReceptionistScript:
 	jumptextfaceplayer SilphCoReceptionistText
 
-SilphCoOfficerScript:
-	faceplayer
-	opentext
-	checkevent EVENT_GOT_UP_GRADE
-	iftrue .GotUpGrade
-	writetext SilphCoOfficerText
-	promptbutton
-	verbosegiveitem UP_GRADE
-	iffalse .NoRoom
-	setevent EVENT_GOT_UP_GRADE
-.GotUpGrade:
-	writetext SilphCoOfficerText_GotUpGrade
-	waitbutton
-.NoRoom:
-	closetext
-	end
+SilphCo1FClerkScript:
+	jumptextfaceplayer SilphCo1FClerkText
 
 SilphCoReceptionistText:
 	text "Welcome to SILPH!"
@@ -37,37 +24,24 @@ SilphCoReceptionistText:
 	cont "floor."
 	done
 
-SilphCoOfficerText:
-	text "Only employees are"
-	line "permitted to go"
-	cont "upstairs."
-
-	para "But since you came"
-	line "such a long way,"
-
-	para "have this neat"
-	line "little souvenir."
-	done
-
-SilphCoOfficerText_GotUpGrade:
-	text "It's SILPH CO.'s"
-	line "latest product."
-
-	para "It's not for sale"
-	line "anywhere yet."
+SilphCo1FClerkText:
+	text "Sorry, the water"
+	line "feature is under"
+	cont "maintenance."
 	done
 
 SilphCo1F_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event  2,  7, SAFFRON_CITY, 7
-	warp_event  3,  7, SAFFRON_CITY, 7
-	warp_event 13,  0, SILPH_CO_2F, 1
+	warp_event 10, 17, SAFFRON_CITY, 7
+	warp_event 11, 17, SAFFRON_CITY, 7
+	warp_event 25,  0, SILPH_CO_2F, 1
 
 	def_coord_events
 
 	def_bg_events
 
 	def_object_events
-	object_event  4,  2, SPRITE_RECEPTIONIST, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, SilphCoReceptionistScript, -1
+	object_event  6,  2, SPRITE_RECEPTIONIST, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, SilphCoReceptionistScript, -1
+	object_event 17, 12, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, SilphCo1FClerkScript, -1
