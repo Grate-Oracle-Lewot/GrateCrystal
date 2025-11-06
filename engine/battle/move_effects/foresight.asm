c74e469bf3bf4111d682bf68a830c719d7e979b8
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-11-05 20:14:16 -0800

    Update foresight.asm

--- engine/battle/move_effects/foresight.asm
+++ engine/battle/move_effects/foresight.asm
@@ -4,6 +4,8 @@ BattleCommand_Foresight:
 
 	ld a, BATTLE_VARS_SUBSTATUS1_OPP
 	call GetBattleVarAddr
+	bit SUBSTATUS_PROTECT, [hl]
+	jr nz, .failed
 	bit SUBSTATUS_IDENTIFIED, [hl]
 	jr nz, .failed
 
