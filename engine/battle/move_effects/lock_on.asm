cd655921901206d22d8d9fc3d84a80a541853e2b
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-11-05 20:16:04 -0800

    Update lock_on.asm

--- engine/battle/move_effects/lock_on.asm
+++ engine/battle/move_effects/lock_on.asm
@@ -2,6 +2,11 @@ BattleCommand_LockOn:
 	call CheckSubstituteOpp
 	jr nz, .fail
 
+	ld a, BATTLE_VARS_SUBSTATUS1_OPP
+	call GetBattleVarAddr
+	bit SUBSTATUS_PROTECT, [hl]
+	jr nz, .fail
+
 	ld a, BATTLE_VARS_SUBSTATUS5_OPP
 	call GetBattleVarAddr
 	set SUBSTATUS_LOCK_ON, [hl]
