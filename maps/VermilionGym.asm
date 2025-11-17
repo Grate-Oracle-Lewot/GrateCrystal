2a3d8eae48f50c28d78567558313f1c0cf56e20e
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-11-17 14:44:06 -0800

    Update VermilionGym.asm

--- maps/VermilionGym.asm
+++ maps/VermilionGym.asm
@@ -18,13 +18,13 @@ VermilionGym_MapScripts:
 .HiddenDoors:
 	checkevent EVENT_VERMILION_GYM_SWITCH_1
 	iftrue .NextCheck
-	changeblock 4, 4, $04 ; shelf
+	changeblock 4, 4, $33 ; electric gate
 	endcallback
 
 .NextCheck:
 	checkevent EVENT_VERMILION_GYM_SWITCH_2
 	iftrue .OpenPath
-	changeblock 4, 4, $04 ; shelf
+	changeblock 4, 4, $33 ; electric gate
 	endcallback
 
 .OpenPath
