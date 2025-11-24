f8e752272c25ed16055114d36f08de7ec4d9134c
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-11-24 13:10:49 -0800

    Update hoothoot.asm

--- data/pokemon/dex_entries/hoothoot.asm
+++ data/pokemon/dex_entries/hoothoot.asm
@@ -1,10 +1,10 @@
-	db "OWL@" ; species name
+	db "OWLET@" ; species name
 	dw 204, 470 ; height, weight
 
-	db   "It's unconfirmed"
-	next "whether it only"
-	next "has one leg or if"
+	db   "It is thought to"
+	next "use the planet's"
+	next "magnetic field to"
 
-	page "it switches betw-"
-	next "een legs faster"
-	next "than can be seen.@"
+	page "keep time. It only"
+	next "hoots at a given"
+	next "second on the dot.@"
