fbf45289607311f3081133e52dc073da0f8fc920
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-11-27 22:08:37 -0800

    Update pokemon_data_constants.asm

--- constants/pokemon_data_constants.asm
+++ constants/pokemon_data_constants.asm
@@ -45,6 +45,8 @@ GENDER_UNKNOWN EQU -1
 	const GROWTH_MEDIUM_SLOW
 	const GROWTH_FAST
 	const GROWTH_SLOW
+	const GROWTH_ERRATIC
+	const GROWTH_FLUCTUATING
 NUM_GROWTH_RATES EQU const_value
 
 ; wBaseEggGroups values
