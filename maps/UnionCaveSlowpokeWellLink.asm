dfafbd18e10bffdceda52e8f299c9544989c58dc
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-12-09 20:01:57 -0800

    Update UnionCaveSlowpokeWellLink.asm

--- maps/UnionCaveSlowpokeWellLink.asm
+++ maps/UnionCaveSlowpokeWellLink.asm
@@ -1,16 +1,14 @@
 	object_const_def
-	const UNIONCAVESLOWPOKEWELLLINK_BOULDER
 	const UNIONCAVESLOWPOKEWELLLINK_PHARMACIST
 	const UNIONCAVESLOWPOKEWELLLINK_ROCKET
+	const UNIONCAVESLOWPOKEWELLLINK_BOULDER
+	const UNIONCAVESLOWPOKEWELLLINK_ROCK
 	const UNIONCAVESLOWPOKEWELLLINK_POKE_BALL
 
 UnionCaveSlowpokeWellLink_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
-
-UnionCaveSlowpokeWellLinkBoulder:
-	jumpstd StrengthBoulderScript
 
 TrainerBurglarFizbo:
 	trainer BURGLAR, FIZBO, EVENT_BEAT_BURGLAR_FIZBO, BurglarFizboSeenText, BurglarFizboBeatenText, 0, .AfterScript
@@ -33,6 +31,12 @@ TrainerGruntMStray:
 	waitbutton
 	closetext
 	end
+
+UnionCaveSlowpokeWellLinkBoulder:
+	jumpstd StrengthBoulderScript
+
+UnionCaveSlowpokeWellLinkRock:
+	jumpstd SmashRockScript
 
 UnionCaveSlowpokeWellLinkPickaxe:
 if DEF(_HM_ITEMS_START)
@@ -95,7 +99,8 @@ UnionCaveSlowpokeWellLink_MapEvents:
 	bg_event 10,  3, BGEVENT_ITEM, UnionCaveSlowpokeWellLinkHiddenDuskBall
 
 	def_object_events
-	object_event  8,  7, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, UnionCaveSlowpokeWellLinkBoulder, -1
 	object_event 28,  5, SPRITE_PHARMACIST, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 3, TrainerBurglarFizbo, -1
 	object_event 13,  8, SPRITE_ROCKET, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_TRAINER, 3, TrainerGruntMStray, -1
+	object_event  8,  7, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, UnionCaveSlowpokeWellLinkBoulder, -1
+	object_event 34, 11, SPRITE_ROCK, SPRITEMOVEDATA_SMASHABLE_ROCK, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, UnionCaveSlowpokeWellLinkRock, -1
 	object_event 34, 12, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, PAL_OW_GREEN, OBJECTTYPE_ITEMBALL, 0, UnionCaveSlowpokeWellLinkPickaxe, EVENT_UNION_CAVE_SLOWPOKE_WELL_LINK_PICKAXE
