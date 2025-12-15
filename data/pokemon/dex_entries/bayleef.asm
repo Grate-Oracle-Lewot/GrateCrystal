dd32d5213ce19ea49947add1dd89c6153c4ee740
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-12-15 12:17:44 -0800

    Update bayleef.asm

--- data/pokemon/dex_entries/bayleef.asm
+++ data/pokemon/dex_entries/bayleef.asm
@@ -1,10 +1,10 @@
-	db "SAUROPOD@" ; species name
+	db "SYRUPOD@" ; species name
 	dw 311, 350 ; height, weight
 
 	db   "It's playful, but"
 	next "careful not to"
 	next "step on the feet"
 
-	page "of its trainer as"
-	next "it romps about."
-	next "@"
+	page "of its trainer."
+	next "Its leaf is often"
+	next "chewed by bugs.@"
