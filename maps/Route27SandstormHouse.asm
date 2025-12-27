4b6ea90f960488dc59a26ab8a1724242d05e61d9
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-12-26 21:53:50 -0800

    Update Route27SandstormHouse.asm

--- maps/Route27SandstormHouse.asm
+++ maps/Route27SandstormHouse.asm
@@ -9,32 +9,41 @@ Route27SandstormHouse_MapScripts:
 SandstormHouseWoman:
 	faceplayer
 	opentext
-	checkevent EVENT_GOT_TM37_SANDSTORM
-	iftrue .AlreadyGotItem
-	special GetFirstPokemonHappiness
 	writetext SandstormHouseWomanText1
 	promptbutton
-	ifgreater 150 - 1, .Loyal
-	sjump .Disloyal
-
-.Loyal:
-	writetext SandstormHouseWomanLoyalText
-	promptbutton
-	verbosegiveitem TM_SANDSTORM, 5
-	iffalse .Done
-	setevent EVENT_GOT_TM37_SANDSTORM
-.AlreadyGotItem:
-	writetext SandstormHouseSandstormDescription
+	special GetFirstPokemonHappiness
+	ifgreater 190 - 1, .Loyal
+	writetext SandstormHouseWomanDisloyalText
+.EndText:
 	waitbutton
 .Done:
 	closetext
 	end
 
-.Disloyal:
-	writetext SandstormHouseWomanDisloyalText
-	waitbutton
-	closetext
-	end
+.Loyal:
+	writetext SandstormHouseWomanLoyalText
+	promptbutton
+.CheckIcePunch:
+	checkevent EVENT_GOT_ICE_PUNCH_FROM_SANDSTORM_LADY
+	iftrue .CheckThunderpunch
+	verbosegiveitem TM_ICE_PUNCH, 5
+	iffalse .CheckThunderpunch
+	setevent EVENT_GOT_ICE_PUNCH_FROM_SANDSTORM_LADY
+.CheckThunderpunch:
+	checkevent EVENT_GOT_THUNDERPUNCH_FROM_SANDSTORM_LADY
+	iftrue .CheckFirePunch
+	verbosegiveitem TM_THUNDERPUNCH, 5
+	iffalse .CheckFirePunch
+	setevent EVENT_GOT_THUNDERPUNCH_FROM_SANDSTORM_LADY
+.CheckFirePunch:
+	checkevent EVENT_GOT_FIRE_PUNCH_FROM_SANDSTORM_LADY
+	iftrue .AlreadyGotAllPunches
+	verbosegiveitem TM_FIRE_PUNCH, 5
+	iffalse .Done
+	setevent EVENT_GOT_FIRE_PUNCH_FROM_SANDSTORM_LADY
+.AlreadyGotAllPunches:
+	writetext SandstormHouseSandstormDescription
+	sjump .EndText
 
 SandstormHouseBookshelf:
 	jumpstd MagazineBookshelfScript
@@ -60,24 +69,21 @@ SandstormHouseWomanLoyalText:
 	para "It's nice to see a"
 	line "good trainer."
 
-	para "Here. A gift for"
-	line "your journey."
+	para "Here. These will"
+	line "come in handy."
 	done
 
 SandstormHouseSandstormDescription:
-	text "TM37 happens to be"
-	line "SANDSTORM."
+	text "The elemental pun-"
+	line "ches aren't the"
 
-	para "It's a move that"
-	line "inflicts damage on"
-	cont "both battlers."
+	para "strongest attacks,"
+	line "but they're useful"
+	cont "physical coverage."
 
-	para "You'll see natural"
-	line "sandstorms inside"
-
-	para "VICTORY ROAD, so"
-	line "you'd best learn"
-	cont "to deal with it."
+	para "A trainer like you"
+	line "should know what"
+	cont "to do with them."
 	done
 
 SandstormHouseWomanDisloyalText:
@@ -106,4 +112,4 @@ Route27SandstormHouse_MapEvents:
 	bg_event  1,  1, BGEVENT_READ, SandstormHouseBookshelf
 
 	def_object_events
-	object_event  2,  4, SPRITE_GRANNY, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SandstormHouseWoman, -1
+	object_event  2,  4, SPRITE_LOTUS_LADY, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SandstormHouseWoman, -1
