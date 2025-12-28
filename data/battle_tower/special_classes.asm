d501cbb775efde683adf838de44595f5ba0f6b93
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2025-12-28 02:34:49 -0800

    Update special_classes.asm

--- data/battle_tower/special_classes.asm
+++ data/battle_tower/special_classes.asm
@@ -1,14 +1,6 @@
 BattleTowerSpecialTrainers:
 	table_width (NAME_LENGTH - 1) + 1, BattleTowerSpecialTrainers
 	; name, class
-	db "BROCK@@@@@", BROCK
-	db "MISTY@@@@@", MISTY
-	db "SURGE@@@@@", LT_SURGE
-	db "ERIKA@@@@@", ERIKA
-	db "JANINE@@@@", JANINE
-	db "SABRINA@@@", SABRINA
-	db "BLAINE@@@@", BLAINE
-	db "BLUE@@@@@@", BLUE
 	db "FALKNER@@@", FALKNER
 	db "BUGSY@@@@@", BUGSY
 	db "WHITNEY@@@", WHITNEY
@@ -17,12 +9,20 @@ BattleTowerSpecialTrainers:
 	db "JASMINE@@@", JASMINE
 	db "PRYCE@@@@@", PRYCE
 	db "CLAIR@@@@@", CLAIR
-	db "KOGA@@@@@@", REAL_KOGA
-	db "KAREN@@@@@", REAL_KAREN
 	db "A.D.@@@@@@", WILL
 	db "LUCAS@@@@@", KOGA
 	db "PERCY@@@@@", BRUNO
 	db "LEWOT@@@@@", KAREN
+	db "KAREN@@@@@", REAL_KAREN
+	db "BROCK@@@@@", BROCK
+	db "MISTY@@@@@", MISTY
+	db "SURGE@@@@@", LT_SURGE
+	db "ERIKA@@@@@", ERIKA
+	db "JANINE@@@@", JANINE
+	db "SABRINA@@@", SABRINA
+	db "BLAINE@@@@", BLAINE
+	db "BLUE@@@@@@", BLUE
+	db "KOGA@@@@@@", REAL_KOGA
 	db "LANCE@@@@@", CHAMPION
 	db "RED@@@@@@@", RED
 	assert_table_length BATTLETOWER_NUM_SPECIAL_TRAINERS
