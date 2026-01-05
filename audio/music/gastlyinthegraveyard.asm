28bdfd6414a6c4184452bb7f7efb85d2e0d4376a
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-01-04 17:08:09 -0800

    Update gastlyinthegraveyard.asm

--- audio/music/gastlyinthegraveyard.asm
+++ audio/music/gastlyinthegraveyard.asm
@@ -374,3 +374,22 @@ Music_GastlyInTheGraveyard_branch_3dfb0:
 	note G_, 1
 	note F#, 1
 	sound_loop 0, Music_GastlyInTheGraveyard_Ch3
+
+
+Music_HaunterInTheGraveyard:
+	channel_count 3
+	channel 1, Music_HaunterInTheGraveyard_Ch1
+	channel 2, Music_HaunterInTheGraveyard_Ch2
+	channel 3, Music_HaunterInTheGraveyard_Ch3
+
+Music_HaunterInTheGraveyard_Ch1:
+	transpose 0, 2
+	sound_call Music_GastlyInTheGraveyard_Ch1
+
+Music_HaunterInTheGraveyard_Ch2:
+	transpose 0, 2
+	sound_call Music_GastlyInTheGraveyard_Ch2
+
+Music_HaunterInTheGraveyard_Ch3:
+	transpose 0, 2
+	sound_call Music_GastlyInTheGraveyard_Ch3
