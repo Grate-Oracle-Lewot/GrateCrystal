6cc086f4d6170d4538da00143d2e3e2c71532146
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-01-11 20:08:51 -0800

    Update SafariZoneMainOffice.asm

--- maps/SafariZoneMainOffice.asm
+++ maps/SafariZoneMainOffice.asm
@@ -11,8 +11,8 @@ SafariZoneMainOffice_MapScripts:
 SafariZoneMainOfficeBoulder:
 	jumpstd StrengthBoulderScript
 
-SafariZoneMainOfficeStick:
-	itemball STICK
+SafariZoneMainOfficeLeek:
+	itemball LEEK
 
 SafariZoneMainOffice_MapEvents:
 	db 0, 0 ; filler
@@ -28,4 +28,4 @@ SafariZoneMainOffice_MapEvents:
 	def_object_events
 	object_event  1,  5, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SafariZoneMainOfficeBoulder, -1
 	object_event  1,  4, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SafariZoneMainOfficeBoulder, -1
-	object_event  0,  5, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SafariZoneMainOfficeStick, EVENT_SAFARI_ZONE_MAIN_OFFICE_STICK
+	object_event  0,  5, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SafariZoneMainOfficeLeek, EVENT_SAFARI_ZONE_MAIN_OFFICE_LEEK
