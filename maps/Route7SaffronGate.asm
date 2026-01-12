3a95dc119c97f088511f14fa5a431c872bfaf69d
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-01-11 22:46:13 -0800

    Update Route7SaffronGate.asm

--- maps/Route7SaffronGate.asm
+++ maps/Route7SaffronGate.asm
@@ -49,12 +49,13 @@ Route7SaffronGate_MapEvents:
 	def_warp_events
 	warp_event  0,  4, ROUTE_7, 1
 	warp_event  0,  5, ROUTE_7, 2
-	warp_event  9,  4, SAFFRON_CITY, 10
-	warp_event  9,  5, SAFFRON_CITY, 11
+	warp_event  7,  4, SAFFRON_CITY, 10
+	warp_event  7,  5, SAFFRON_CITY, 11
+	warp_event  6,  8, GATE_2F
 
 	def_coord_events
 
 	def_bg_events
 
 	def_object_events
-	object_event  5,  2, SPRITE_OFFICER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, Route7SaffronGuardScript, -1
+	object_event  4,  1, SPRITE_OFFICER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, Route7SaffronGuardScript, -1
