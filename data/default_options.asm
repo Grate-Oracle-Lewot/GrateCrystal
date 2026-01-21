8f76994baa60510322ca35be3f80e18f5325d1a4
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-01-21 15:25:59 -0800

    Update default_options.asm

--- data/default_options.asm
+++ data/default_options.asm
@@ -10,13 +10,14 @@ DefaultOptions:
 ; wGBPrinterBrightness: normal
 	db GBPRINTER_NORMAL
 ; wOptions2: battle items on
-	db 1 << BATTLE_ITEMS
+	db 1 << BATTLE_ITEMS | 1 << MENU_SIDEBAR
+	; BATTLE_ITEMS:         1 (ON)
 	; LEVEL_CAPS_ON_OFF:    0 (OFF)
 	; LEVEL_CAPS_OBEDIENCE: 0 (DISOBEY)
 	; EASY_MODE:            0 (OFF)
 	; HARD_MODE:            0 (OFF)
 	; NUZLOCKE:             0 (OFF)
-	; MENU_CLOCK:           0 (OFF)
+	; MENU_SIDEBAR:         1 (ON)
 	; FONT_NORMAL_UNOWN:    0 (NORMAL)
 
 	db $00
