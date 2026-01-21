fb946b376666279e18e8d713db2d395b40cd8d42
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-01-20 20:31:06 -0800

    Update options_menu.asm

--- engine/menus/options_menu.asm
+++ engine/menus/options_menu.asm
@@ -1,14 +1,23 @@
 ; GetOptionPointer.Pointers indexes
 	const_def
-	const OPT_TEXT_SPEED   ; 0
-	const OPT_BATTLE_SCENE ; 1
-	const OPT_BATTLE_STYLE ; 2
-	const OPT_BATTLE_ITEMS ; 3
-	const OPT_LEVEL_CAPS   ; 4
-	const OPT_SOUND        ; 5
-	const OPT_FRAME        ; 6
-	const OPT_CANCEL       ; 7
-NUM_OPTIONS EQU const_value    ; 8
+	const OPT_TEXT_SPEED      ; 0
+	const OPT_BATTLE_SCENE    ; 1
+	const OPT_BATTLE_STYLE    ; 2
+	const OPT_BATTLE_ITEMS    ; 3
+	const OPT_SOUND           ; 4
+	const OPT_FRAME           ; 5
+	const OPT_MORE_OPTIONS    ; 6
+	const OPT_CANCEL          ; 7
+NUM_OPTIONS EQU const_value   ; 8
+
+; GetOption2Pointer.Pointers indexes
+	const_def
+	const OPT2_DIFFICULTY     ; 0
+	const OPT2_LEVEL_CAPS     ; 1
+	const OPT2_PRINT          ; 2
+	const OPT2_BACK           ; 3
+	const OPT2_CANCEL         ; 4
+NUM_OPTIONS_2 EQU const_value ; 5
 
 	const_def
 	const OPT_TEXT_SPEED_FAST ; 0
@@ -21,7 +30,19 @@ NUM_OPTIONS EQU const_value    ; 8
 	const OPT_LEVELCAPS_DISOBEY ; 1
 	const OPT_LEVELCAPS_HARDCAP ; 2
 
-_Option:
+	const_def
+	const OPT_DIFFICULTY_EASY   ; 0
+	const OPT_DIFFICULTY_NORMAL ; 1
+	const OPT_DIFFICULTY_HARD   ; 2
+
+	const_def
+	const OPT_PRINT_LIGHTEST ; 0
+	const OPT_PRINT_LIGHTER  ; 1
+	const OPT_PRINT_NORMAL   ; 2
+	const OPT_PRINT_DARKER   ; 3
+	const OPT_PRINT_DARKEST  ; 4
+
+OptionsMenuSetup:
 	call ClearJoypad
 	ld hl, hInMenu
 	ld a, [hl]
@@ -33,13 +54,27 @@ _Option:
 	ld c, SCREEN_WIDTH - 2
 	call Textbox
 	hlcoord 2, 2
+	ret
+
+OptionsMenuOptimization:
+	xor a
+	ld [wJumptableIndex], a
+	inc a
+	ldh [hBGMapMode], a
+	call WaitBGMap
+	ld b, SCGB_DIPLOMA
+	call GetSGBLayout
+	jp SetPalettes
+
+_Option:
+	call OptionsMenuSetup
 	ld de, StringOptions
 	call PlaceString
 	xor a
 	ld [wJumptableIndex], a
 
 ; display the settings of each option when the menu is opened
-	ld c, NUM_OPTIONS - 2 ; omit frame type, the last option
+	ld c, NUM_OPTIONS - 2 ; omit frame type
 .print_text_loop
 	push bc
 	xor a
@@ -50,40 +85,25 @@ _Option:
 	inc [hl]
 	dec c
 	jr nz, .print_text_loop
-	call UpdateFrame ; display the frame type
 
-	xor a
-	ld [wJumptableIndex], a
-	inc a
-	ldh [hBGMapMode], a
-	call WaitBGMap
-	ld b, SCGB_DIPLOMA
-	call GetSGBLayout
-	call SetPalettes
+	call UpdateFrame ; display the frame type
+	call OptionsMenuOptimization
 
 .joypad_loop
 	call JoyTextDelay
 	ldh a, [hJoyPressed]
 	and START | B_BUTTON
-	jr nz, .ExitOptions
+	jr nz, ExitOptions
 	call OptionsControl
 	jr c, .dpad
 	call GetOptionPointer
-	jr c, .ExitOptions
+	jr c, ExitOptions
 
 .dpad
 	call Options_UpdateCursorPosition
 	ld c, 3
 	call DelayFrames
 	jr .joypad_loop
-
-.ExitOptions:
-	ld de, SFX_TRANSACTION
-	call PlaySFX
-	call WaitSFX
-	pop af
-	ldh [hInMenu], a
-	ret
 
 StringOptions:
 	db "TEXT SPEED<LF>"
@@ -94,12 +114,11 @@ StringOptions:
 	db "        :<LF>"
 	db "BATTLE ITEMS<LF>"
 	db "        :<LF>"
-	db "LEVEL CAPS<LF>"
-	db "        :<LF>"
 	db "SOUND<LF>"
 	db "        :<LF>"
 	db "FRAME<LF>"
 	db "        :TYPE<LF>"
+	db "MORE OPTIONS@"
 	db "CANCEL@"
 
 GetOptionPointer:
@@ -111,9 +130,74 @@ GetOptionPointer:
 	dw Options_BattleScene
 	dw Options_BattleStyle
 	dw Options_BattleItems
-	dw Options_LevelCaps
 	dw Options_Sound
 	dw Options_Frame
+	dw _Option2
+	dw Options_Cancel
+
+_Option2:
+	call OptionsMenuSetup
+	ld de, StringOptions2
+	call PlaceString
+	xor a
+	ld [wJumptableIndex], a
+
+; display the settings of each option when the menu is opened
+	ld c, NUM_OPTIONS_2 - 1
+.print_text_loop
+	push bc
+	xor a
+	ldh [hJoyLast], a
+	call GetOption2Pointer
+	pop bc
+	ld hl, wJumptableIndex
+	inc [hl]
+	dec c
+	jr nz, .print_text_loop
+
+	call OptionsMenuOptimizaton
+
+.joypad_loop
+	call JoyTextDelay
+	ldh a, [hJoyPressed]
+	and START | B_BUTTON
+	jr nz, ExitOptions
+	call Options2Control
+	jr c, .dpad
+	call GetOption2Pointer
+	jr c, ExitOptions
+
+.dpad
+	call Options_UpdateCursorPosition
+	ld c, 3
+	call DelayFrames
+	jr .joypad_loop
+
+	const OPT2_DIFFICULTY     ; 0
+	const OPT2_LEVEL_CAPS     ; 1
+	const OPT2_PRINT          ; 2
+	const OPT2_BACK           ; 3
+	const OPT2_CANCEL         ; 4
+
+StringOptions2:
+	db "DIFFICULTY<LF>"
+	db "        :<LF>"
+	db "LEVEL_CAPS<LF>"
+	db "        :<LF>"
+	db "GB PRINTER<LF>"
+	db "        :<LF>"
+	db "BACK@"
+	db "CANCEL@"
+
+GetOption2Pointer:
+	jumptable .Pointers, wJumptableIndex
+
+.Pointers:
+; entries correspond to OPT2_* constants
+	dw Options_Difficulty
+	dw Options_LevelCaps
+	dw Options_Print
+	dw _Option
 	dw Options_Cancel
 
 Options_TextSpeed:
@@ -320,6 +404,177 @@ Options_BattleItems:
 .Off: db "FORBID@"
 .On:  db "ALLOW @"
 
+Options_Sound:
+	ld hl, wOptions
+	ldh a, [hJoyPressed]
+	bit D_LEFT_F, a
+	jr nz, .LeftPressed
+	bit D_RIGHT_F, a
+	jr z, .NonePressed
+	bit STEREO, [hl]
+	jr nz, .SetMono
+	jr .SetStereo
+
+.LeftPressed:
+	bit STEREO, [hl]
+	jr z, .SetStereo
+	jr .SetMono
+
+.NonePressed:
+	bit STEREO, [hl]
+	jr nz, .ToggleStereo
+	jr .ToggleMono
+
+.SetMono:
+	res STEREO, [hl]
+	call RestartMapMusic
+
+.ToggleMono:
+	ld de, .Mono
+	jr .Display
+
+.SetStereo:
+	set STEREO, [hl]
+	call RestartMapMusic
+
+.ToggleStereo:
+	ld de, .Stereo
+
+.Display:
+	hlcoord 11, 11
+	call PlaceString
+	and a
+	ret
+
+.Mono:   db "MONO  @"
+.Stereo: db "STEREO@"
+
+Options_Frame:
+	ld hl, wTextboxFrame
+	ldh a, [hJoyPressed]
+	bit D_LEFT_F, a
+	jr nz, .LeftPressed
+	bit D_RIGHT_F, a
+	jr nz, .RightPressed
+	and a
+	ret
+
+.RightPressed:
+	ld a, [hl]
+	inc a
+	jr .Save
+
+.LeftPressed:
+	ld a, [hl]
+	dec a
+
+.Save:
+	maskbits NUM_FRAMES
+	ld [hl], a
+	; fallthrough
+
+UpdateFrame:
+	ld a, [wTextboxFrame]
+	hlcoord 16, 13 ; where on the screen the number is drawn
+	add "1"
+	ld [hl], a
+	call LoadFontsExtra
+	and a
+	ret
+
+Options_Difficulty:
+	call GetDifficultySetting
+	ldh a, [hJoyPressed]
+	bit D_LEFT_F, a
+	jr nz, .LeftPressed
+	bit D_RIGHT_F, a
+	jr z, .NonePressed
+	ld a, c ; right pressed
+	cp OPT_DIFFICULTY_HARD
+	jr c, .Increase
+	ld c, OPT_DIFFICULTY_EASY - 1
+
+.Increase:
+	inc c
+	jr .Save
+
+.LeftPressed:
+	ld a, c
+	and a
+	jr nz, .Decrease
+	ld c, OPT_DIFFICULTY_HARD + 1
+
+.Decrease:
+	dec c
+
+.Save:
+	ld a, c
+	and a
+	jr z, .SetNone
+	ld a, c
+	cp OPT_DIFFICULTY_NORAML
+	jr z, .SetNormal
+; .SetHard:
+	res EASY_MODE, [hl]
+	set HARD_MODE, [hl]
+	jr .NonePressed
+
+.SetNormal:
+	res EASY_MODE, [hl]
+	res HARD_MODE, [hl]
+	jr .NonePressed
+
+.SetEasy:
+	set EASY_MODE, [hl]
+	res HARD_MODE, [hl]
+.NonePressed:
+	ld b, 0
+	ld hl, .Strings
+	add hl, bc
+	add hl, bc
+	ld e, [hl]
+	inc hl
+	ld d, [hl]
+	hlcoord 11, 3
+	call PlaceString
+	and a
+	ret
+
+.Strings:
+; entries correspond to OPT_DIFFICULTY_* constants
+	dw .Easy
+	dw .Normal
+	dw .Hard
+
+.Easy:   db "EASY  @"
+.Normal: db "NORAML@"
+.Hard:   db "HARD  @"
+
+GetDifficultySetting:
+; reads current difficulty settings to return OPT_DIFFICULTY_* value in c
+; both Easy and Hard being on at once should be impossible but results in Normal as a failsafe
+
+	ld hl, wOptions2
+	bit EASY_MODE, [hl]
+	jr z, .EasyOff
+; .EasyOn:
+	bit HARD_MODE, [hl]
+	jr z, .Easy
+	jr .Normal
+
+.EasyOff:
+	bit HARD_MODE, [hl]
+	jr z, .Normal
+.Hard:
+	ld c, OPT_DIFFICULTY_HARD
+	ret
+.Normal:
+	ld c, OPT_DIFFICULTY_NORMAL
+	ret
+.Easy:
+	ld c, OPT_DIFFICULTY_EASY
+	ret
+
 Options_LevelCaps:
 	call GetLevelCapSetting
 	ldh a, [hJoyPressed]
@@ -365,7 +620,6 @@ Options_LevelCaps:
 .SetNone:
 	res LEVEL_CAPS_ON_OFF, [hl]
 	res LEVEL_CAPS_OBEDIENCE, [hl]
-
 .NonePressed:
 	ld b, 0
 	ld hl, .Strings
@@ -406,80 +660,107 @@ GetLevelCapSetting:
 	ld c, OPT_LEVELCAPS_DISOBEY
 	ret
 
-Options_Sound:
-	ld hl, wOptions
+Options_Print:
+	call GetPrinterSetting
 	ldh a, [hJoyPressed]
 	bit D_LEFT_F, a
 	jr nz, .LeftPressed
 	bit D_RIGHT_F, a
 	jr z, .NonePressed
-	bit STEREO, [hl]
-	jr nz, .SetMono
-	jr .SetStereo
+	ld a, c
+	cp OPT_PRINT_DARKEST
+	jr c, .Increase
+	ld c, OPT_PRINT_LIGHTEST - 1
+
+.Increase:
+	inc c
+	ld a, e
+	jr .Save
 
 .LeftPressed:
-	bit STEREO, [hl]
-	jr z, .SetStereo
-	jr .SetMono
+	ld a, c
+	and a
+	jr nz, .Decrease
+	ld c, OPT_PRINT_DARKEST + 1
+
+.Decrease:
+	dec c
+	ld a, d
+
+.Save:
+	ld b, a
+	ld [wGBPrinterBrightness], a
 
 .NonePressed:
-	bit STEREO, [hl]
-	jr nz, .ToggleStereo
-	jr .ToggleMono
-
-.SetMono:
-	res STEREO, [hl]
-	call RestartMapMusic
-
-.ToggleMono:
-	ld de, .Mono
-	jr .Display
-
-.SetStereo:
-	set STEREO, [hl]
-	call RestartMapMusic
-
-.ToggleStereo:
-	ld de, .Stereo
-
-.Display:
-	hlcoord 11, 13
+	ld b, 0
+	ld hl, .Strings
+	add hl, bc
+	add hl, bc
+	ld e, [hl]
+	inc hl
+	ld d, [hl]
+	hlcoord 11, 7
 	call PlaceString
 	and a
 	ret
 
-.Mono:   db "MONO  @"
-.Stereo: db "STEREO@"
+.Strings:
+; entries correspond to OPT_PRINT_* constants
+	dw .Lightest
+	dw .Lighter
+	dw .Normal
+	dw .Darker
+	dw .Darkest
 
-Options_Frame:
-	ld hl, wTextboxFrame
-	ldh a, [hJoyPressed]
-	bit D_LEFT_F, a
-	jr nz, .LeftPressed
-	bit D_RIGHT_F, a
-	jr nz, .RightPressed
+.Lightest: db "LIGHTEST@"
+.Lighter:  db "LIGHTER @"
+.Normal:   db "NORMAL  @"
+.Darker:   db "DARKER  @"
+.Darkest:  db "DARKEST @"
+
+GetPrinterSetting:
+; converts GBPRINTER_* value in a to OPT_PRINT_* value in c,
+; with previous/next GBPRINTER_* values in d/e
+	ld a, [wGBPrinterBrightness]
 	and a
+	jr z, .IsLightest
+	cp GBPRINTER_LIGHTER
+	jr z, .IsLight
+	cp GBPRINTER_DARKER
+	jr z, .IsDark
+	cp GBPRINTER_DARKEST
+	jr z, .IsDarkest
+	; none of the above
+	ld c, OPT_PRINT_NORMAL
+	lb de, GBPRINTER_LIGHTER, GBPRINTER_DARKER
 	ret
 
-.RightPressed:
-	ld a, [hl]
-	inc a
-	jr .Save
+.IsLightest:
+	ld c, OPT_PRINT_LIGHTEST
+	lb de, GBPRINTER_DARKEST, GBPRINTER_LIGHTER
+	ret
 
-.LeftPressed:
-	ld a, [hl]
-	dec a
+.IsLight:
+	ld c, OPT_PRINT_LIGHTER
+	lb de, GBPRINTER_LIGHTEST, GBPRINTER_NORMAL
+	ret
 
-.Save:
-	maskbits NUM_FRAMES
-	ld [hl], a
-UpdateFrame:
-	ld a, [wTextboxFrame]
-	hlcoord 16, 15 ; where on the screen the number is drawn
-	add "1"
-	ld [hl], a
-	call LoadFontsExtra
-	and a
+.IsDark:
+	ld c, OPT_PRINT_DARKER
+	lb de, GBPRINTER_NORMAL, GBPRINTER_DARKEST
+	ret
+
+.IsDarkest:
+	ld c, OPT_PRINT_DARKEST
+	lb de, GBPRINTER_DARKER, GBPRINTER_LIGHTEST
+	ret
+
+ExitOptions:
+	ld de, SFX_TRANSACTION
+	call PlaySFX
+	call WaitSFX
+	pop af
+	ldh [hInMenu], a
 	ret
 
 Options_Cancel:
@@ -525,6 +806,40 @@ OptionsControl:
 
 .Roll_Down:
 	ld [hl], OPT_CANCEL ; maximum option index
+	jr .Done
+
+Options2Control:
+	ld hl, wJumptableIndex
+	ldh a, [hJoyLast]
+	cp D_DOWN
+	jr z, .DownPressed
+	cp D_UP
+	jr z, .UpPressed
+	and a
+	ret
+
+.DownPressed:
+	ld a, [hl]
+	cp OPT2_CANCEL ; maximum option index
+	jr z, .Roll_Up
+	inc [hl]
+	jr .Done
+
+.Roll_Up:
+	ld [hl], OPT2_DIFFICULTY ; minimum option index
+.Done:
+	scf
+	ret
+
+.UpPressed:
+	ld a, [hl]
+	and a ; OPT2_DIFFICULTY, minimum option index
+	jr z, .Roll_Down
+	dec [hl]
+	jr .Done
+
+.Roll_Down:
+	ld [hl], OPT2_CANCEL ; maximum option index
 	jr .Done
 
 Options_UpdateCursorPosition:
