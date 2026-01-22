698cb00cd6aeb9fb2fc9bd3bfa58d26b470af72e
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-01-21 19:35:15 -0800

    Update default_options.asm

--- data/default_options.asm
+++ data/default_options.asm
@@ -9,6 +9,8 @@ DefaultOptions:
 	db 1 << FAST_TEXT_DELAY_F
 ; wGBPrinterBrightness: normal
 	db GBPRINTER_NORMAL
+; Unused
+	db $00
 ; wOptions2: battle items on
 	db %01000001
 	; BATTLE_ITEMS:         1 (ON)
@@ -19,8 +21,7 @@ DefaultOptions:
 	; NUZLOCKE:             0 (OFF)
 	; MENU_SIDEBAR:         1 (ON)
 	; FONT_NORMAL_UNOWN:    0 (NORMAL)
-
-	db $00
+; Unused
 	db $00
 .End
 	assert DefaultOptions.End - DefaultOptions == wOptionsEnd - wOptions
