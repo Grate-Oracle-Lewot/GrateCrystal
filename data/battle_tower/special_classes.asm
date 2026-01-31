91da124bdd6d5de6e3061e18beb345183c5e475e
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-01-30 21:28:04 -0800

    Update special_classes.asm

--- data/battle_tower/special_classes.asm
+++ data/battle_tower/special_classes.asm
@@ -9,11 +9,11 @@ BattleTowerSpecialTrainers:
 	db "JASMINE@@@", JASMINE
 	db "PRYCE@@@@@", PRYCE
 	db "CLAIR@@@@@", CLAIR
-	db "A.D.@@@@@@", WILL
-	db "LUCAS@@@@@", KOGA
-	db "PERCY@@@@@", BRUNO
-	db "LEWOT@@@@@", KAREN
-	db "KAREN@@@@@", REAL_KAREN
+	db "A.D.@@@@@@", A_D
+	db "LUCAS@@@@@", LUCAS
+	db "PERCY@@@@@", PERCY
+	db "LEWOT@@@@@", GRATE_ORACLE
+	db "KAREN@@@@@", KAREN
 	db "BROCK@@@@@", BROCK
 	db "MISTY@@@@@", MISTY
 	db "SURGE@@@@@", LT_SURGE
@@ -22,7 +22,7 @@ BattleTowerSpecialTrainers:
 	db "SABRINA@@@", SABRINA
 	db "BLAINE@@@@", BLAINE
 	db "BLUE@@@@@@", BLUE
-	db "KOGA@@@@@@", REAL_KOGA
+	db "KOGA@@@@@@", KOGA
 	db "LANCE@@@@@", CHAMPION
 	db "RED@@@@@@@", RED
 	assert_table_length BATTLETOWER_NUM_SPECIAL_TRAINERS
