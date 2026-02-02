6ea83566d0bf17b1a0eb0d531041ad3c6ff502ca
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-02-01 19:45:19 -0800

    Update VermilionGym.asm

--- maps/VermilionGym.asm
+++ maps/VermilionGym.asm
@@ -10,26 +10,185 @@ VermilionGym_MapScripts:
 	scene_script .DummyScene ; SCENE_FINISHED
 
 	def_callbacks
-	callback MAPCALLBACK_TILES, .HiddenDoors
+	callback MAPCALLBACK_OBJECTS, .VermilionGymDoorsScript
+	callback MAPCALLBACK_TILES, .VermilionGymDoorsCallback
 
 .DummyScene:
 	end
 
-.HiddenDoors:
-	checkevent EVENT_VERMILION_GYM_SWITCH_1
-	iftrue .NextCheck
-	changeblock 4, 4, $31 ; symbol block
-	endcallback
-
-.NextCheck:
+.VermilionGymDoorsScript:
 	checkevent EVENT_VERMILION_GYM_SWITCH_2
-	iftrue .OpenPath
-	changeblock 4, 4, $31 ; symbol block
+	iftrue .done
+	checkevent EVENT_VERMILION_GYM_SWITCH_1
+	iffalse .resample
+.resample
+	callasm SampleVermilionGymTrashCans
+.done
 	endcallback
 
-.OpenPath
-	changeblock 4, 4, $01 ; walkable floor
+.VermilionGymDoorsCallback:
+	checkevent EVENT_VERMILION_GYM_SWITCH_2
+	iftrue .NoDoors
 	endcallback
+
+.NoDoors:
+	changeblock 4, 4, $01 ; floor
+	endcallback
+
+VermilionGymTrashCanScript:
+	checkevent EVENT_VERMILION_GYM_SWITCH_2
+	iftrue .trash_can
+	callasm CheckVermilionGymTrashCan
+	iftrue .open_lock
+	checkevent EVENT_VERMILION_GYM_SWITCH_1
+	iftrue .reset_switches
+.trash_can
+	jumpstd TrashCanScript
+
+.open_lock
+	opentext
+	writetext VermilionGymFoundSwitchText
+	playsound SFX_PUSH_BUTTON
+	promptbutton
+	checkevent EVENT_VERMILION_GYM_SWITCH_1
+	iftrue .second_switch
+	writetext VermilionGymFoundFirstSwitchText
+	playsound SFX_ENTER_DOOR
+	setevent EVENT_VERMILION_GYM_SWITCH_1
+	waitbutton
+	closetext
+	end
+
+.second_switch
+	writetext VermilionGymFoundSecondSwitchText
+	waitbutton
+	playsound SFX_ENTER_DOOR
+	setevent EVENT_VERMILION_GYM_SWITCH_2
+	changeblock 4, 4, $01 ; floor
+	reloadmappart
+	closetext
+	end
+
+.reset_switches
+	opentext
+	writetext VermilionGymTrashCanText
+	promptbutton
+	writetext VermilionGymResetSwitchesText
+	playsound SFX_WRONG
+	waitbutton
+	closetext
+	callasm SampleVermilionGymTrashCans
+	clearevent EVENT_VERMILION_GYM_SWITCH_1
+	end
+
+SampleVermilionGymTrashCans:
+	ldh a, [rSVBK]
+	push af
+	ld a, BANK(wVermilionGymTrashCan1)
+	ldh [rSVBK], a
+.loop
+	call Random
+	ld e, a
+	swap e
+	and $f
+	jr z, .loop
+	dec a
+	ld [wVermilionGymTrashCan1], a
+	call .GetSecondTrashCan
+	ld [wVermilionGymTrashCan2], a
+	pop af
+	ldh [rSVBK], a
+	ret
+
+.GetSecondTrashCan:
+	ld hl, .AdjacencyTable
+	add a
+	add a
+	ld c, a
+	ld a, e
+	and %11
+	add c
+	ld c, a
+	ld b, 0
+	add hl, bc
+	ld a, [hl]
+	ret
+
+.AdjacencyTable:
+	; left, right, up, down
+	db  1,  1,  5,  5 ;  0 ( 1, 7)
+	db  0,  2,  6,  6 ;  1 ( 3, 7)
+	db  1,  3,  7,  7 ;  2 ( 5, 7)
+	db  2,  4,  8,  8 ;  3 ( 7, 7)
+	db  3,  3,  9,  9 ;  4 ( 9, 7)
+	db  6,  6,  0, 10 ;  5 ( 1, 9)
+	db  5,  7,  1, 11 ;  6 ( 3, 9)
+	db  6,  8,  2, 12 ;  7 ( 5, 9)
+	db  7,  9,  3, 13 ;  8 ( 7, 9)
+	db  8,  8,  4, 14 ;  9 ( 9, 9)
+	db 11, 11,  5,  5 ; 10 ( 1,11)
+	db 10, 12,  6,  6 ; 11 ( 3,11)
+	db 11, 13,  7,  7 ; 12 ( 5,11)
+	db 12, 14,  8,  8 ; 13 ( 7,11)
+	db 13, 13,  9,  9 ; 14 ( 9,11)
+
+CheckVermilionGymTrashCan:
+	ldh a, [rSVBK]
+	push af
+	ld a, BANK(wVermilionGymTrashCan1)
+	ldh [rSVBK], a
+	ld de, EVENT_VERMILION_GYM_SWITCH_1
+	ld b, CHECK_FLAG
+	call EventFlagAction
+	ld a, c
+	and a
+	jr z, .first
+	ld a, [wVermilionGymTrashCan2]
+	call .CheckTrashCan
+	ld a, TRUE
+	jr z, .done
+	dec a ; FALSE
+.done
+	ld [wScriptVar], a
+	pop af
+	ldh [rSVBK], a
+	ret
+
+.first:
+	ld a, [wVermilionGymTrashCan1]
+	call .CheckTrashCan
+	jr z, .yes
+	ld a, [wVermilionGymTrashCan2]
+	call .CheckTrashCan
+	ld a, FALSE
+	jr nz, .done
+	ld a, [wVermilionGymTrashCan1]
+	ld [wVermilionGymTrashCan2], a
+.yes
+	ld a, TRUE
+	jr .done
+
+.CheckTrashCan:
+	ld c, a
+	call GetFacingTileCoord
+	call .ConvertCoordsToTrashCan
+	cp c
+	ret
+
+.ConvertCoordsToTrashCan:
+	ld a, d
+	sub 5
+	srl a
+	ld d, a
+	ld a, e
+	sub 11
+	srl a
+	ld e, a
+	add a
+	add a
+	add e
+	add d
+	ret
 
 VermilionGymSurgeScript:
 	faceplayer
@@ -122,60 +281,6 @@ VermilionGymGuideScript:
 
 .VermilionGymGuideWinScript:
 	writetext VermilionGymGuideWinText
-	waitbutton
-	closetext
-	end
-
-VermilionGymTrashCan:
-	jumptext VermilionGymTrashCanText
-
-VermilionGymSwitch1:
-	opentext
-	checkevent EVENT_VERMILION_GYM_SWITCH_1
-	iftrue .AlreadyFlipped
-	writetext VermilionGymSwitch1FlipText
-	waitbutton
-	closetext
-	setevent EVENT_VERMILION_GYM_SWITCH_1
-	playsound SFX_TRANSACTION
-	waitsfx
-	end
-
-.AlreadyFlipped:
-	writetext VermilionGymSwitchesAfterText
-	waitbutton
-	closetext
-	end
-
-VermilionGymSwitch2:
-	opentext
-	checkevent EVENT_VERMILION_GYM_SWITCH_2
-	iftrue .AlreadyFlipped
-	checkevent EVENT_VERMILION_GYM_SWITCH_1
-	iftrue .CompletePuzzle
-	writetext VermilionGymTrashCanText
-	waitbutton
-	closetext
-	end
-
-.CompletePuzzle:
-	writetext VermilionGymSwitch2FlipText
-	waitbutton
-	closetext
-	playsound SFX_TRANSACTION
-	pause 21
-	earthquake 42
-	pause 21
-	playsound SFX_STRENGTH
-	changeblock 4, 4, $01 ; walkable floor
-	reloadmappart
-	earthquake 42
-	setevent EVENT_VERMILION_GYM_SWITCH_2
-	setscene SCENE_FINISHED
-	end
-
-.AlreadyFlipped:
-	writetext VermilionGymSwitchesAfterText
 	waitbutton
 	closetext
 	end
@@ -372,19 +477,21 @@ VermilionGymTrashCanText:
 	line "but trash."
 	done
 
-VermilionGymSwitch1FlipText:
-	text "A hidden switch!"
-	line "That's one!"
+VermilionGymFoundSwitchText:
+	text "…A hidden switch!"
 	done
 
-VermilionGymSwitch2FlipText:
-	text "The second switch!"
-	line "That's both!"
+VermilionGymFoundFirstSwitchText:
+	text "One more to go!"
 	done
 
-VermilionGymSwitchesAfterText:
-	text "This switch is al-"
-	line "ready flipped."
+VermilionGymFoundSecondSwitchText:
+	text "The path opened!"
+	done
+
+VermilionGymResetSwitchesText:
+	text "Just trash… The"
+	line "switches reset…"
 	done
 
 VermilionGym_MapEvents:
@@ -397,21 +504,21 @@ VermilionGym_MapEvents:
 	def_coord_events
 
 	def_bg_events
-	bg_event  1,  7, BGEVENT_READ, VermilionGymTrashCan
-	bg_event  3,  7, BGEVENT_READ, VermilionGymTrashCan
-	bg_event  5,  7, BGEVENT_READ, VermilionGymTrashCan
-	bg_event  7,  7, BGEVENT_READ, VermilionGymTrashCan
-	bg_event  9,  7, BGEVENT_READ, VermilionGymSwitch2
-	bg_event  1,  9, BGEVENT_READ, VermilionGymTrashCan
-	bg_event  3,  9, BGEVENT_READ, VermilionGymSwitch1
-	bg_event  5,  9, BGEVENT_READ, VermilionGymTrashCan
-	bg_event  7,  9, BGEVENT_READ, VermilionGymTrashCan
-	bg_event  9,  9, BGEVENT_READ, VermilionGymTrashCan
-	bg_event  1, 11, BGEVENT_READ, VermilionGymTrashCan
-	bg_event  3, 11, BGEVENT_READ, VermilionGymTrashCan
-	bg_event  5, 11, BGEVENT_READ, VermilionGymTrashCan
-	bg_event  7, 11, BGEVENT_READ, VermilionGymTrashCan
-	bg_event  9, 11, BGEVENT_READ, VermilionGymTrashCan
+	bg_event  1,  7, BGEVENT_READ, VermilionGymTrashCanScript
+	bg_event  3,  7, BGEVENT_READ, VermilionGymTrashCanScript
+	bg_event  5,  7, BGEVENT_READ, VermilionGymTrashCanScript
+	bg_event  7,  7, BGEVENT_READ, VermilionGymTrashCanScript
+	bg_event  9,  7, BGEVENT_READ, VermilionGymTrashCanScript
+	bg_event  1,  9, BGEVENT_READ, VermilionGymTrashCanScript
+	bg_event  3,  9, BGEVENT_READ, VermilionGymTrashCanScript
+	bg_event  5,  9, BGEVENT_READ, VermilionGymTrashCanScript
+	bg_event  7,  9, BGEVENT_READ, VermilionGymTrashCanScript
+	bg_event  9,  9, BGEVENT_READ, VermilionGymTrashCanScript
+	bg_event  1, 11, BGEVENT_READ, VermilionGymTrashCanScript
+	bg_event  3, 11, BGEVENT_READ, VermilionGymTrashCanScript
+	bg_event  5, 11, BGEVENT_READ, VermilionGymTrashCanScript
+	bg_event  7, 11, BGEVENT_READ, VermilionGymTrashCanScript
+	bg_event  9, 11, BGEVENT_READ, VermilionGymTrashCanScript
 	bg_event  3, 15, BGEVENT_READ, VermilionGymStatue
 	bg_event  6, 15, BGEVENT_READ, VermilionGymStatue
 
