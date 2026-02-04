7784d926cd983a4d1d518f1314fcef74aafc39c6
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-02-03 19:05:09 -0800

    Update map_constants.asm

--- constants/map_constants.asm
+++ constants/map_constants.asm
@@ -466,7 +466,7 @@ ENDM
 	map_const RUINS_OF_ALPH_HO_OH_ITEM_ROOM,                4,  5 ;  45
 	map_const RUINS_OF_ALPH_HO_OH_WORD_ROOM,               10, 12 ;  46
 	map_const RUINS_OF_ALPH_RESEARCH_CENTER,                4,  4 ;  47
-	map_const UNION_CAVE_1F,                               10, 18 ;  48
+	map_const UNION_CAVE_1F,                               10, 27 ;  48
 	map_const UNION_CAVE_B1F,                              10, 18 ;  49
 	map_const UNION_CAVE_B2F,                              10, 18 ;  50
 	map_const UNION_CAVE_SLOWPOKE_WELL_LINK,               20,  9 ;  51
