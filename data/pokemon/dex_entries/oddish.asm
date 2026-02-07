105ecca1a6ca5f3c4ab46324eb7ad879d7053de2
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-02-06 20:51:46 -0800

    Update oddish.asm

--- data/pokemon/dex_entries/oddish.asm
+++ data/pokemon/dex_entries/oddish.asm
@@ -1,10 +1,10 @@
 	db "MANDRAKE@" ; species name
 	dw 108, 120 ; height, weight
 
-	db   "Not quite odd, but"
-	next "oddish. Mistaking"
-	next "it for a tasty"
+	db   "It retains harmful"
+	next "substances that"
+	next "would normally be"
 
-	page "radish could be"
-	next "fatal, as it's"
-	next "quite toxic.@"
+	page "expelled in order"
+	next "to make itself"
+	next "poisonous.@"
