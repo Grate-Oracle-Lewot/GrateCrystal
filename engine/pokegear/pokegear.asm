e01b2152c8b4cfef8a2b63a82d76d7b3f34ff7b9
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-02-11 18:40:27 -0800

    Update pokegear.asm

--- engine/pokegear/pokegear.asm
+++ engine/pokegear/pokegear.asm
@@ -43,7 +43,7 @@ PokeGear:
 	call UpdateTime
 	call JoyTextDelay
 	ld a, [wJumptableIndex]
-	bit 7, a
+	bit JUMPTABLE_EXIT_F, a
 	jr nz, .done
 	call PokegearJumptable
 	farcall PlaySpriteAnimations
@@ -500,7 +500,7 @@ PokegearClock_Joypad:
 
 .quit
 	ld hl, wJumptableIndex
-	set 7, [hl]
+	set JUMPTABLE_EXIT_F, [hl]
 	ret
 
 .UpdateClock:
@@ -683,7 +683,7 @@ PokegearMap_ContinueMap:
 
 .cancel
 	ld hl, wJumptableIndex
-	set 7, [hl]
+	set JUMPTABLE_EXIT_F, [hl]
 	ret
 
 .DPad:
@@ -871,7 +871,7 @@ PokegearRadio_Joypad:
 
 .cancel
 	ld hl, wJumptableIndex
-	set 7, [hl]
+	set JUMPTABLE_EXIT_F, [hl]
 	ret
 
 PokegearPhone_Init:
@@ -930,7 +930,7 @@ PokegearPhone_Joypad:
 
 .b
 	ld hl, wJumptableIndex
-	set 7, [hl]
+	set JUMPTABLE_EXIT_F, [hl]
 	ret
 
 .a
