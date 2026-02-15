87b314e02235297eff0c443bb0d4c1ec68b73c53
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-02-15 15:38:30 -0800

    Update print_move_description.asm

--- engine/pokemon/print_move_description.asm
+++ engine/pokemon/print_move_description.asm
@@ -1,18 +1,6 @@
 PrintMoveDescription:
 	push hl
 	ld hl, MoveDescriptions
-	call PrintMoveDescriptionOptimization
-	pop hl
-	jp PlaceString
-
-PrintMoveDescriptionScrunched:
-	push hl
-	ld hl, MoveDescriptionsScrunched
-	call PrintMoveDescriptionOptimization
-	pop hl
-	jp PlaceString
-
-PrintMoveDescriptionOptimization:
 	ld a, [wCurSpecies]
 	dec a
 	ld c, a
@@ -22,8 +10,7 @@ PrintMoveDescriptionOptimization:
 	ld a, [hli]
 	ld e, a
 	ld d, [hl]
-	ret
+	pop hl
+	jp PlaceString
 
 INCLUDE "data/moves/descriptions.asm"
-
-INCLUDE "data/moves/descriptions_scrunched.asm"
