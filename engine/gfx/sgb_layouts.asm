6c686a67ca67e0fc8bda03fe79c1078729a407b0
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-02-15 16:17:09 -0800

    Update sgb_layouts.asm

--- engine/gfx/sgb_layouts.asm
+++ engine/gfx/sgb_layouts.asm
@@ -49,6 +49,7 @@ SGBLayoutJumptable:
 	dw .SGB_PlayerOrMonFrontpicPals
 	dw .SGB_TradeTube
 	dw .SGB_TrainerOrMonFrontpicPals
+	dw .SGB_MysteryGift
 	assert_table_length NUM_SCGB_LAYOUTS
 
 .SGB_BattleGrayscale:
@@ -283,6 +284,7 @@ SGBLayoutJumptable:
 	ret
 
 .SGB_Diploma:
+.SGB_MysteryGift:
 	ld hl, PalPacket_Diploma
 	ld de, BlkPacket_AllPal0
 	ret
