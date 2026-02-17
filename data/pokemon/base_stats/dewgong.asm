7bd7c7e76245a68c1ee457561c681d5d716f5111
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-02-16 17:50:49 -0800

    Update dewgong.asm

--- data/pokemon/base_stats/dewgong.asm
+++ data/pokemon/base_stats/dewgong.asm
@@ -6,7 +6,7 @@
 	db WATER, ICE ; type
 	db 75 ; catch rate
 	db 176 ; base exp
-	db FRESH_WATER, NEVERMELTICE ; items
+	db SPRING_WATER, NEVERMELTICE ; items
 	db GENDER_F50 ; gender ratio
 	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/dewgong/front.dimensions"
