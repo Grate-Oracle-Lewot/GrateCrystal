3ba51f02b8a5fe758091ece3a759ae37e5c1c483
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-03-12 15:39:37 -0700

    Update load_overworld_font.asm

--- engine/gfx/load_overworld_font.asm
+++ engine/gfx/load_overworld_font.asm
@@ -1,13 +1,5 @@
 LoadOverworldFont::
-	ld de, .OverworldUnownFontGFX
-	ld a, [wOptions2]
-	bit FONT_NORMAL_UNOWN, a
-	jr z, .normal
-	jr .continue
-
-.normal
 	ld de, .OverworldFontGFX
-.continue
 	ld hl, vTiles1
 	lb bc, BANK(.OverworldFontGFX), $80
 	call Get2bpp
@@ -18,9 +10,6 @@ LoadOverworldFont::
 
 .OverworldFontGFX:
 INCBIN "gfx/font/overworld.2bpp"
-
-.OverworldUnownFontGFX:
-INCBIN "gfx/font/unown_speaking_font_overworld.2bpp"
 
 .OverworldFontSpaceGFX:
 INCBIN "gfx/font/overworld_space.2bpp"
