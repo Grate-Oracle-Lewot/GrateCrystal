c922441f5e0ab57f5bbff619de16bbfd7af42d31
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-03-26 19:19:53 -0700

    Update name_input_chars.asm

--- data/text/name_input_chars.asm
+++ data/text/name_input_chars.asm
@@ -1,11 +1,26 @@
 ; see engine/menus/naming_screen.asm
 
+NameInputUpper:
+	db "A B C D E F G H I"
+	db "J K L M N O P Q R"
+	db "S T U V W X Y Z  "
+	db ". <INV_QU> ? <INV_EX> ! ' - & é"
+	db "lower  DEL   END "
+
 NameInputLower:
 	db "a b c d e f g h i"
 	db "j k l m n o p q r"
-	db "s t u v w x y z  "
-	db "é ( ) : ", $9d, " ♂ ♀ <PK> <MN>"
+	db "s t u v w x y z 0"
+	db "1 2 3 4 5 6 7 8 <NINE>"
 	db "UPPER  DEL   END "
+
+BoxNameInputUpper:
+	db "A B C D E F G H I"
+	db "J K L M N O P Q R"
+	db "S T U V W X Y Z  "
+	db ". <INV_QU> ? <INV_EX> ! ' - & …"
+	db "/ ( ) : ", $9d, " ♂ ♀ ¥ <FLOAT>"
+	db "lower  DEL   END "
 
 BoxNameInputLower:
 	db "a b c d e f g h i"
@@ -14,18 +29,3 @@ BoxNameInputLower:
 	db "é 'd 'l 'm 'r 's 't 'v 0"
 	db "1 2 3 4 5 6 7 8 9"
 	db "UPPER  DEL   END "
-
-NameInputUpper:
-	db "A B C D E F G H I"
-	db "J K L M N O P Q R"
-	db "S T U V W X Y Z  "
-	db "- <INV_QU> ? <INV_EX> ! / . , &"
-	db "lower  DEL   END "
-
-BoxNameInputUpper:
-	db "A B C D E F G H I"
-	db "J K L M N O P Q R"
-	db "S T U V W X Y Z  "
-	db "× ( ) : ", $9d, " ♂ ♀ <PK> <MN>"
-	db "- <INV_QU> ? <INV_EX> ! / . , &"
-	db "lower  DEL   END "
