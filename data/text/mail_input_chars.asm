a87a63ca47ef244ef5fc6df2986ddf0bd2a3ee34
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-03-26 18:56:42 -0700

    Update mail_input_chars.asm

--- data/text/mail_input_chars.asm
+++ data/text/mail_input_chars.asm
@@ -3,15 +3,15 @@
 MailEntry_Uppercase:
 	db "A B C D E F G H I J"
 	db "K L M N O P Q R S T"
-	db "U V W X Y Z   , ? !"
-	db "1 2 3 4 5 6 7 8 9 0"
-	db "<PK> <MN> <PO> <KE> é ♂ ♀ ¥ … ×"
+	db "U V W X Y Z   . ? !"
+	db "0 1 2 3 4 5 6 7 8 9"
+	db "“ ” ( ) [ ] - : ", $9d, " &"
 	db "lower  DEL   END   "
 
 MailEntry_Lowercase:
 	db "a b c d e f g h i j"
 	db "k l m n o p q r s t"
-	db "u v w x y z   . - /"
-	db "'d 'l 'm 'r 's 't 'v & ( )"
-	db "“ ” [ ] ' : ", $9d, " <INV_QU> <INV_EX> <FLOAT>"
+	db "u v w x y z   , <INV_QU> <INV_EX>"
+	db "' 'd 'l 'm 'r 's 't 'v é <FLOAT>"
+	db "… ♂ ♀ ¥ / × <PK> <MN> <PO> <KE>"
 	db "UPPER  DEL   END   "
