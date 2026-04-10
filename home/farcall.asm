75016a7fef138f69b66aa84a3a096696de37480a
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-04-10 05:33:15 -0700

    Update farcall.asm

--- home/farcall.asm
+++ home/farcall.asm
@@ -21,21 +21,13 @@ FarCall_hl::
 	; fallthrough
 
 ReturnFarCall::
-; We want to retain the contents of f.
-; To do this, we can pop to bc instead of af.
-	ld a, b
-	ld [wFarCallBC], a
-	ld a, c
-	ld [wFarCallBC + 1], a
-
-; Restore the working bank.
-	pop bc
-	ld a, b
+	push af
+	push hl
+	ld hl, sp + 5
+	ld a, [hl]
+	pop hl
 	rst Bankswitch
-
-; Restore the contents of bc.
-	ld a, [wFarCallBC]
-	ld b, a
-	ld a, [wFarCallBC + 1]
-	ld c, a
+	pop af
+	inc sp
+	inc sp
 	ret
