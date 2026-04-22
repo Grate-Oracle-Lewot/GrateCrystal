1fb9cc5df4f03e05d3edcc736839582e2730b6f9
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-04-22 15:55:52 -0700

    Update CianwoodCity.asm

--- maps/CianwoodCity.asm
+++ maps/CianwoodCity.asm
@@ -46,8 +46,6 @@ CianwoodCitySuicuneAndEusine:
 	disappear CIANWOODCITY_SUICUNE
 	pause 10
 	setscene SCENE_CIANWOODCITY_NOTHING
-	clearevent EVENT_SAW_SUICUNE_ON_ROUTE_42
-	setmapscene ROUTE_42, SCENE_ROUTE42_SUICUNE
 	checkevent EVENT_FOUGHT_EUSINE
 	iftrue .Done
 	setevent EVENT_FOUGHT_EUSINE
