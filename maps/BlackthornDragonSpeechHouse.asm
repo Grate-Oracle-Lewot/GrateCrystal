b82108611fc4527a93c3d01dfbde11ef522a6d3b
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-04-26 15:29:58 -0700

    Update BlackthornDragonSpeechHouse.asm

--- maps/BlackthornDragonSpeechHouse.asm
+++ maps/BlackthornDragonSpeechHouse.asm
@@ -44,14 +44,14 @@ BlackthornDragonSpeechHouse_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event  2,  7, BLACKTHORN_CITY, 2
 	warp_event  3,  7, BLACKTHORN_CITY, 2
+	warp_event  4,  7, BLACKTHORN_CITY, 2
 
 	def_coord_events
 
 	def_bg_events
-	bg_event  0,  1, BGEVENT_READ, BlackthornDragonSpeechHouseBookshelf
+	bg_event  1,  1, BGEVENT_READ, BlackthornDragonSpeechHouseBookshelf
 
 	def_object_events
 	object_event  2,  3, SPRITE_GRANNY, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, BlackthornDragonSpeechHouseGrannyScript, -1
-	object_event  5,  5, SPRITE_EKANS, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, BlackthornDragonSpeechHouseDratiniScript, -1
+	object_event  5,  4, SPRITE_EKANS, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, BlackthornDragonSpeechHouseDratiniScript, -1
