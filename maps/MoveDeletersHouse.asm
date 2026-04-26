220caa0acc50dded9b848235759dbd256f3f17e5
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-04-26 15:27:53 -0700

    Update MoveDeletersHouse.asm

--- maps/MoveDeletersHouse.asm
+++ maps/MoveDeletersHouse.asm
@@ -14,21 +14,16 @@ MoveDeleter:
 	closetext
 	end
 
-MoveDeletersHouseBookshelf:
-	jumpstd DifficultBookshelfScript
-
 MoveDeletersHouse_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event  2,  7, BLACKTHORN_CITY, 6
 	warp_event  3,  7, BLACKTHORN_CITY, 6
+	warp_event  4,  7, BLACKTHORN_CITY, 6
 
 	def_coord_events
 
 	def_bg_events
-	bg_event  0,  1, BGEVENT_READ, MoveDeletersHouseBookshelf
-	bg_event  1,  1, BGEVENT_READ, MoveDeletersHouseBookshelf
 
 	def_object_events
 	object_event  2,  3, SPRITE_SUPER_NERD, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_SILVER, OBJECTTYPE_SCRIPT, 0, MoveDeleter, -1
