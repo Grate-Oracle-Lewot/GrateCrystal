57555f452885f18fccc257b59d065953bd43286a
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-05-06 05:27:38 -0700

    Update get_square_root.asm

--- engine/math/get_square_root.asm
+++ engine/math/get_square_root.asm
@@ -1,30 +1,75 @@
-NUM_SQUARE_ROOTS EQU 255
+; optimized by DamienDoury
 
 GetSquareRoot:
 ; Return the square root of de in b.
+; Special case: we return ceil(sqrt(0)) = 1, which is wrong but matches the vanilla function.
 
-; Rather than calculating the result, we take the index of the
-; first value in a table of squares that isn't lower than de.
-
-	ld hl, .Squares
-	ld b, 0
-.loop
-; Make sure we don't go past the end of the table.
-	inc b
-	ld a, b
-	cp NUM_SQUARE_ROOTS
+	ld a, d
+	or e
+	ld b, 1
 	ret z
 
-; Iterate over the table until b**2 >= de.
-	ld a, [hli]
+	push bc
+	ld h, d
+	ld l, e
+	ld de, 0
+	ld bc, $4000
+
+.loop:
+	push hl
+
+	ld a, l
+	sub c
+	ld l, a
+	ld a, h
+	sbc a, b
+	ld h, a
+	jr c, .no_bit
+
+	ld a, l
 	sub e
-	ld a, [hli]
-	sbc d
+	ld l, a
+	ld a, h
+	sbc a, d
+	ld h, a
+	jr c, .no_bit
 
-	jr c, .loop
+.take_bit:
+	pop af
+	srl d
+	rr e
+	ld a, e
+	add a, c
+	ld e, a
+	ld a, d
+	adc a, b
+	ld d, a
+	jr .next
+
+.no_bit:
+	pop hl
+	srl d
+	rr e
+
+.next:
+	srl b
+	rr c
+	srl b
+	rr c
+	ld a, b
+	or c
+	jr nz, .loop
+
+	pop bc
+	ld b, e
+	ld a, h
+	or l
+	ret z
+
+; ceiling
+	inc b
+	ret nz
+
+; capped at 255
+	dec b
 	ret
-
-.Squares:
-for x, 1, NUM_SQUARE_ROOTS + 1
-	dw x**2
-endr
