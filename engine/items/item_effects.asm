b2f9f56de5127edffd7e268dcd375051e19282e1
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-05-12 13:34:14 -0700

    Update item_effects.asm

--- engine/items/item_effects.asm
+++ engine/items/item_effects.asm
@@ -207,6 +207,10 @@ PokeBallEffect:
 	dec a
 	jp nz, UseBallInTrainerBattle
 
+	ld a, [wBattleType]
+	cp BATTLETYPE_TUTORIAL
+	jr z, .room_in_party
+
 	ld a, [wOptions2]
 	bit GIFT_ONLY, a
 	jr nz, .gift_only
@@ -230,10 +234,6 @@ PokeBallEffect:
 	jr .not_used
 
 .valid
-	ld a, [wBattleType]
-	cp BATTLETYPE_TUTORIAL
-	jr z, .room_in_party
-
 	ld a, [wPartyCount]
 	cp PARTY_LENGTH
 	jr nz, .room_in_party
