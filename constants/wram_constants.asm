9b7e769068673751d52265f44de17aacbded5a9f
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-05-12 05:26:15 -0700

    Update wram_constants.asm

--- constants/wram_constants.asm
+++ constants/wram_constants.asm
@@ -100,7 +100,7 @@ NUM_FONTS EQU const_value
 	const EASY_MODE            ; 3
 	const HARD_MODE            ; 4
 	const NUZLOCKE             ; 5
-	const_skip                 ; 6
+	const GIFT_ONLY            ; 6
 	const DIFFICULTY_DOUBLE    ; 7
 
 ; wExperienceSetting::
