fb20190d785d56d4f485420b0a152b33600d9f8e
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-05-26 10:28:13 -0700

    Create types_cats_status_pals.asm

---
+++ gfx/types_cats_status_pals.asm
@@ -0,0 +1,68 @@
+StatusIconPals:
+; OK
+	RGB 31, 31, 31
+; PSN
+	RGB 27, 11, 27
+; PAR
+	RGB 30, 20, 00
+; SLP
+	RGB 17, 17, 17
+; BRN
+	RGB 31, 08, 02
+; FRZ
+	RGB 09, 18, 31
+; FNT
+	RGB 31, 31, 31
+; TOX
+	RGB 27, 06, 28
+
+CategoryIconPals:
+; PHYSICAL
+	RGB 31, 28, 00
+	RGB 27, 04, 02
+; SPECIAL
+	RGB 27, 31, 31
+	RGB 00, 14, 29
+; STATUS
+	RGB 31, 31, 31
+	RGB 21, 21, 14
+
+TypeIconPals:
+; NORMAL      (0)
+	RGB 21, 21, 14
+; FIGHTING    (1)
+	RGB 25, 05, 02
+; FLYING      (2)
+	RGB 22, 17, 30
+; POISON      (3)
+	RGB 22, 07, 19
+; GROUND      (4)
+	RGB 29, 24, 12
+; ROCK        (5)
+	RGB 24, 20, 07
+; BUG         (6)
+	RGB 21, 23, 06
+; GHOST       (7)
+	RGB 15, 11, 18
+; STEEL       (8)
+	RGB 23, 23, 25
+; FIRE        (9)
+	RGB 31, 15, 04
+; WATER      (10)
+	RGB 11, 18, 30
+; GRASS      (11)
+	RGB 11, 25, 11
+; ELECTRIC   (12)
+	RGB 31, 24, 06
+; PSYCHIC_T  (13)
+	RGB 31, 09, 15
+; ICE        (14)
+	RGB 16, 27, 27
+; DRAGON     (15)
+	RGB 15, 07, 31
+; DARK       (16)
+	RGB 15, 11, 09
+; FAIRY      (17)
+	RGB 31, 20, 29
+; CURSE_TYPE (18)
+	RGB 13, 19, 19
