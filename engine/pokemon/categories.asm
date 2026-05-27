5be93e15b57049980ad4bd95e5b28a2f7bf68940
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-05-26 23:44:51 -0700

    Update categories.asm

--- engine/pokemon/categories.asm
+++ engine/pokemon/categories.asm
@@ -1,8 +1,8 @@
 GetMoveCategoryIndex::
 	ld a, b
 	dec a
-	ld bc, MOVE_LENGTH
 	ld hl, Moves + MOVE_TYPE
+	ld bc, MOVE_LENGTH
 	call AddNTimes
 	ld a, BANK(Moves)
 	call GetFarByte
@@ -12,5 +12,21 @@ GetMoveCategoryIndex::
 ; Shift the category bits into the range 0-2
 	rlc a
 	rlc a
+	dec a
+	ret
+
+GetMoveTypeIndex::
+	ld a, b
+	dec a
+	ld hl, Moves + MOVE_TYPE
+	ld bc, MOVE_LENGTH
+	call AddNTimes
+	ld a, BANK(Moves)
+	call GetFarByte
+
+	and ~TYPE_MASK
+	swap a
+	srl a
+	srl a
 	dec a
 	ret
