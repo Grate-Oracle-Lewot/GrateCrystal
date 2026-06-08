655e3199a5c4838e4403bdbe3bce8b7bc93ba595
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-06-07 19:53:13 -0700

    Update battle_tower.asm

--- engine/events/battle_tower/battle_tower.asm
+++ engine/events/battle_tower/battle_tower.asm
@@ -563,7 +563,7 @@ BattleTower_GiveReward:
 	cp c
 	jr nz, .next
 	ld a, [hl]
-	cp 95
+	cp MAX_ITEM_STACK - BATTLETOWER_REWARD_QUANTITY + 1
 	ret c
 .next
 	inc hl
@@ -596,12 +596,13 @@ BattleTower_RandomlyChooseReward:
 .loop
 	call Random
 	ldh a, [hRandomAdd]
-	and $7
-	cp 6
+	maskbits BATTLETOWER_MAX_REWARD - BATTLETOWER_MIN_REWARD + 1
+	cp BATTLETOWER_MAX_REWARD - BATTLETOWER_MIN_REWARD + 1
 	jr c, .okay
-	sub 6
+	sub BATTLETOWER_MAX_REWARD - BATTLETOWER_MIN_REWARD + 1
 .okay
-	add FIBER
+	add BATTLETOWER_MIN_REWARD
+	assert LUCKY_PUNCH >= BATTLETOWER_MIN_REWARD && LUCKY_PUNCH <= BATTLETOWER_MAX_REWARD
 	cp LUCKY_PUNCH
 	jr z, .loop
 	push af
