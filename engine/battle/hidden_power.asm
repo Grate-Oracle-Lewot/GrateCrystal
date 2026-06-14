f5c76c18ba7402b3e1274a6a86ad065a77ee35d9
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-06-14 15:43:29 -0700

    Update hidden_power.asm

--- engine/battle/hidden_power.asm
+++ engine/battle/hidden_power.asm
@@ -1,42 +1,15 @@
 HiddenPowerDamage:
 ; Override Hidden Power's type based on the user's DVs.
 
-	ld hl, wBattleMonDVs
+	ld bc, wBattleMonDVs
 	ldh a, [hBattleTurn]
 	and a
 	jr z, .got_dvs
-	ld hl, wEnemyMonDVs
+	ld bc, wEnemyMonDVs
 .got_dvs
 
-	; Def & 3
-	ld a, [hl]
-	and %0011
-	ld b, a
+	farcall FarHiddenPowerType
 
-	; + (Atk & 3) << 2
-	ld a, [hli]
-	and %0011 << 4
-	swap a
-	add a
-	add a
-	or b
-
-	; add the least significant bit of the Speed DV to increment 50% of the time (to reach Fairy type)
-	ld b, a
-	ld a, [hl]
-	swap a
-	and %0001
-	add b
-
-; Skip Normal
-	inc a
-
-; Skip unused types
-	cp UNUSED_TYPES
-	jr c, .done
-	add UNUSED_TYPES_END - UNUSED_TYPES
-
-.done
 ; Overwrite the current move type.
 	push af
 	ld a, BATTLE_VARS_MOVE_TYPE
