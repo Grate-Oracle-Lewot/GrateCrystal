a9016592df78a138246723a025ece86d6c931be0
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-06-21 21:39:03 -0700

    Update RuinsOfAlphOmanyteItemRoom.asm

--- maps/RuinsOfAlphOmanyteItemRoom.asm
+++ maps/RuinsOfAlphOmanyteItemRoom.asm
@@ -25,10 +25,8 @@ RuinsOfAlphOmanyteItemRoomAncientReplica:
 	jumptext RuinsOfAlphOmanyteItemRoomAncientReplicaText
 
 RuinsOfAlphOmanyteItemRoomAncientReplicaText:
-	text "It's a replica of"
-	line "an ancient #-"
-	cont "MON."
-	done
+	text_far _AncientPokemonStatueText
+	text_end
 
 RuinsOfAlphOmanyteItemRoom_MapEvents:
 	db 0, 0 ; filler
