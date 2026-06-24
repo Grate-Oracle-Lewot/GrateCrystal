3419224cb3e19063fdc4fa50b3c095bed68d3f74
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-06-23 19:32:49 -0700

    Update ekans.asm

--- data/pokemon/base_stats/ekans.asm
+++ data/pokemon/base_stats/ekans.asm
@@ -10,7 +10,7 @@ else
 endc
 	db 255 ; catch rate
 	db 62 ; base exp
-	db ANTIDOTE, LUCKY_EGG ; items
+	db ANTIDOTE, FABERGE_EGG ; items
 	db GENDER_F50 ; gender ratio
 	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/ekans/front.dimensions"
