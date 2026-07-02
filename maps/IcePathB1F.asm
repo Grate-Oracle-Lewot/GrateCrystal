71635cdb3ca1226475432cfa0acf1c635b8a1bef
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-07-01 18:08:58 -0700

    Update IcePathB1F.asm

--- maps/IcePathB1F.asm
+++ maps/IcePathB1F.asm
@@ -74,13 +74,13 @@ IcePathB1F_MapEvents:
 
 	def_warp_events
 	warp_event  3, 15, ICE_PATH_1F, 3
-	warp_event 17,  3, ICE_PATH_B2F_MAHOGANY_SIDE, 1
-	warp_event 11,  2, ICE_PATH_B2F_MAHOGANY_SIDE, 3 ; hole
-	warp_event  4,  7, ICE_PATH_B2F_MAHOGANY_SIDE, 4 ; hole
-	warp_event  5, 12, ICE_PATH_B2F_MAHOGANY_SIDE, 5 ; hole
-	warp_event 12, 13, ICE_PATH_B2F_MAHOGANY_SIDE, 6 ; hole
+	warp_event 17,  3, ICE_PATH_B2F, 1
+	warp_event 11,  2, ICE_PATH_B2F, 3 ; hole
+	warp_event  4,  7, ICE_PATH_B2F, 4 ; hole
+	warp_event  5, 12, ICE_PATH_B2F, 5 ; hole
+	warp_event 12, 13, ICE_PATH_B2F, 6 ; hole
 	warp_event  5, 25, ICE_PATH_1F, 4
-	warp_event 11, 27, ICE_PATH_B2F_BLACKTHORN_SIDE, 1
+	warp_event 11, 27, ICE_PATH_B2F, 7
 
 	def_coord_events
 
