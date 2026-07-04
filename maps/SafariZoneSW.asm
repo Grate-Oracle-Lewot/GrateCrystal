8a9445a9c23f1b614fd359f39705cb05e5d4cfd3
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-07-03 21:20:44 -0700

    Create SafariZoneSW.asm

---
+++ maps/SafariZoneSW.asm
@@ -0,0 +1,98 @@
+	object_const_def
+	const SAFARIZONESW_FISH
+	const SAFARIZONESW_FLOWER
+	const SAFARIZONESW_SUDOWOODO
+	const SAFARIZONESW_CHANSEY
+	const SAFARIZONESW_POKE_BALL1
+	const SAFARIZONESW_POKE_BALL2
+
+SafariZoneSW_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+SafariZoneSWMagikarpScript:
+	waitsfx
+	cry MAGIKARP
+	loadvar VAR_BATTLETYPE, BATTLETYPE_TRAP
+	loadwildmon MAGIKARP, 57
+	startbattle
+	disappear SAFARIZONESW_FISH
+	reloadmapafterbattle
+	end
+
+SafariZoneSWSunfloraScript:
+	waitsfx
+	cry SUNFLORA
+	loadvar VAR_BATTLETYPE, BATTLETYPE_TRAP
+if DEF(_LITTLE_CUP)
+	loadwildmon SUNKERN, 53
+else
+	loadwildmon SUNFLORA, 53
+endc
+	startbattle
+	disappear SAFARIZONESW_FLOWER
+	reloadmapafterbattle
+	end
+
+SafariZoneSWSudowoodoScript:
+	waitsfx
+	playsound SFX_SANDSTORM
+	applymovement SAFARIZONESW_SUDOWOODO, SafariZoneSWShakeMovement
+	loadvar VAR_BATTLETYPE, BATTLETYPE_TRAP
+if DEF(_LITTLE_CUP)
+	loadwildmon GEODUDE, 54
+else
+	loadwildmon SUDOWOODO, 54
+endc
+	startbattle
+	disappear SAFARIZONESW_SUDOWOODO
+	reloadmapafterbattle
+	end
+
+SafariZoneSWBlisseyScript:
+	waitsfx
+	cry BLISSEY
+	loadvar VAR_BATTLETYPE, BATTLETYPE_TRAP
+if DEF(_LITTLE_CUP)
+	loadwildmon CHANSEY, 58
+else
+	loadwildmon BLISSEY, 58
+endc
+	startbattle
+	disappear SAFARIZONESW_CHANSEY
+	reloadmapafterbattle
+	end
+
+SafariZoneSWBone:
+	itemball BONE
+
+SafariZoneSWCalcium:
+	itemball CALCIUM
+
+SafariZoneSWShakeMovement:
+	tree_shake
+	step_end
+
+SafariZoneSWTrashcan:
+	jumpstd TrashCanScript
+
+SafariZoneSW_MapEvents:
+	db 0, 0 ; filler
+
+	def_warp_events
+	warp_event 42, 45, SAFARI_ZONE_GATE, 1
+	warp_event 43, 45, SAFARI_ZONE_GATE, 2
+
+	def_coord_events
+
+	def_bg_events
+	bg_event 44, 44, BGEVENT_READ, SafariZoneSWTrashcan
+
+	def_object_events
+	object_event 41, 31, SPRITE_FISH, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, SafariZoneSWMagikarpScript, EVENT_SAFARI_ZONE_SW_MAGIKARP
+	object_event 17, 20, SPRITE_FLOWER, SPRITEMOVEDATA_POKEMON, 0, 0, MORN_HOUR, NITE_HOUR, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, SafariZoneSWSunfloraScript, EVENT_SAFARI_ZONE_SW_SUNFLORA
+	object_event 26, 46, SPRITE_SUDOWOODO, SPRITEMOVEDATA_SUDOWOODO, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, SafariZoneSWSudowoodoScript, EVENT_SAFARI_ZONE_SW_SUDOWOODO
+	object_event 44, 38, SPRITE_CHANSEY, SPRITEMOVEDATA_POKEMON, 0, 0, 7, 8, PAL_NPC_PINK, OBJECTTYPE_SCRIPT, 0, SafariZoneSWBlisseyScript, EVENT_SAFARI_ZONE_SW_BLISSEY
+	object_event  4, 10, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SafariZoneSWBone, EVENT_SAFARI_ZONE_SW_BONE
+	object_event 28, 20, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SafariZoneSWCalcium, EVENT_SAFARI_ZONE_SW_CALCIUM
