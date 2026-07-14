c024909d8cde8a896a6174ca4ea0a7fcf6038389
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-07-13 20:27:13 -0700

    Update VermilionGym.asm

--- maps/VermilionGym.asm
+++ maps/VermilionGym.asm
@@ -491,8 +491,6 @@ VermilionGymResetSwitchesText:
 	done
 
 VermilionGym_MapEvents:
-	db 0, 0 ; filler
-
 	def_warp_events
 	warp_event  4, 17, VERMILION_CITY, 7
 	warp_event  5, 17, VERMILION_CITY, 7
