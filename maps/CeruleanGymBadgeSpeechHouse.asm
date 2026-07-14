3175e2c99607f90ec0ff4491b7dda321d2ba42ee
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-07-14 10:09:06 -0700

    Update CeruleanGymBadgeSpeechHouse.asm

--- maps/CeruleanGymBadgeSpeechHouse.asm
+++ maps/CeruleanGymBadgeSpeechHouse.asm
@@ -14,8 +14,6 @@ CeruleanGymBadgeSpeechHousePokefanMScript:
 	end
 
 CeruleanGymBadgeSpeechHouse_MapEvents:
-	db 0, 0 ; filler
-
 	def_warp_events
 	warp_event  2,  7, CERULEAN_CITY, 1
 	warp_event  3,  7, CERULEAN_CITY, 1
