e44634eb2fafb28b57583dcebd91d564eaca75fe
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-07-13 22:01:25 -0700

    Update Route35NationalParkGate.asm

--- maps/Route35NationalParkGate.asm
+++ maps/Route35NationalParkGate.asm
@@ -422,8 +422,6 @@ BugCatchingContestExplanationText:
 	done
 
 Route35NationalParkGate_MapEvents:
-	db 0, 0 ; filler
-
 	def_warp_events
 	warp_event  3,  0, NATIONAL_PARK, 3
 	warp_event  4,  0, NATIONAL_PARK, 4
