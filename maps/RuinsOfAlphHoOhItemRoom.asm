5e9684e303d4371f7693cefee6d527e0e27b08db
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-07-13 21:17:51 -0700

    Update RuinsOfAlphHoOhItemRoom.asm

--- maps/RuinsOfAlphHoOhItemRoom.asm
+++ maps/RuinsOfAlphHoOhItemRoom.asm
@@ -29,8 +29,6 @@ RuinsOfAlphHoOhItemRoomAncientReplicaText:
 	text_end
 
 RuinsOfAlphHoOhItemRoom_MapEvents:
-	db 0, 0 ; filler
-
 	def_warp_events
 	warp_event  3,  9, RUINS_OF_ALPH_HO_OH_CHAMBER, 5
 	warp_event  4,  9, RUINS_OF_ALPH_HO_OH_CHAMBER, 5
