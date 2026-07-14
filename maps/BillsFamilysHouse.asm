795b3c5ab9a8efe8dcc7cd7bbfac1334eec7c893
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-07-14 10:35:36 -0700

    Update BillsFamilysHouse.asm

--- maps/BillsFamilysHouse.asm
+++ maps/BillsFamilysHouse.asm
@@ -281,8 +281,6 @@ BillsFamilysHousePhotoText:
 	done
 
 BillsFamilysHouse_MapEvents:
-	db 0, 0 ; filler
-
 	def_warp_events
 	warp_event  2,  7, GOLDENROD_CITY, 4
 	warp_event  3,  7, GOLDENROD_CITY, 4
