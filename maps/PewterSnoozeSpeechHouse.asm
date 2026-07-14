ec155d2d1a7880424b146de078398b8d23cbcd31
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-07-14 01:29:33 -0700

    Update PewterSnoozeSpeechHouse.asm

--- maps/PewterSnoozeSpeechHouse.asm
+++ maps/PewterSnoozeSpeechHouse.asm
@@ -24,8 +24,6 @@ PewterSnoozeSpeechHouseGrampsText:
 	done
 
 PewterSnoozeSpeechHouse_MapEvents:
-	db 0, 0 ; filler
-
 	def_warp_events
 	warp_event  2,  7, PEWTER_CITY, 5
 	warp_event  3,  7, PEWTER_CITY, 5
