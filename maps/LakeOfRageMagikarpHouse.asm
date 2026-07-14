0e82158f46cb6fcd026276bb095be256ab8f0051
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-07-14 09:11:00 -0700

    Update LakeOfRageMagikarpHouse.asm

--- maps/LakeOfRageMagikarpHouse.asm
+++ maps/LakeOfRageMagikarpHouse.asm
@@ -219,8 +219,6 @@ MagikarpLengthRaterText_Refused:
 	done
 
 LakeOfRageMagikarpHouse_MapEvents:
-	db 0, 0 ; filler
-
 	def_warp_events
 	warp_event  2,  7, LAKE_OF_RAGE, 2
 	warp_event  3,  7, LAKE_OF_RAGE, 2
