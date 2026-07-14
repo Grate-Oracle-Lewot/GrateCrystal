84582cceacb43387cf5b5b982de1a45d08694279
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-07-14 10:05:07 -0700

    Update CherrygroveCity.asm

--- maps/CherrygroveCity.asm
+++ maps/CherrygroveCity.asm
@@ -529,8 +529,6 @@ GuideGentsHouseSignText:
 	done
 
 CherrygroveCity_MapEvents:
-	db 0, 0 ; filler
-
 	def_warp_events
 	warp_event 23,  5, CHERRYGROVE_MART, 2
 	warp_event 29,  5, CHERRYGROVE_POKECENTER_1F, 1
