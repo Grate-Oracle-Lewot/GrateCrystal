44c7084d40b1a9e6b492f3eee46b71546eb8a4a5
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-07-14 00:58:20 -0700

    Update Route16FuchsiaSpeechHouse.asm

--- maps/Route16FuchsiaSpeechHouse.asm
+++ maps/Route16FuchsiaSpeechHouse.asm
@@ -17,8 +17,6 @@ Route16FuchsiaSpeechHouseBookshelf:
 	jumpstd PictureBookshelfScript
 
 Route16FuchsiaSpeechHouse_MapEvents:
-	db 0, 0 ; filler
-
 	def_warp_events
 	warp_event  2,  7, ROUTE_16, 1
 	warp_event  3,  7, ROUTE_16, 1
