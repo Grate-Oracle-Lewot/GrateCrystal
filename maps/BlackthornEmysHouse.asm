8edb0b49529e96039787c512b4addef0ffdbda0c
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-07-14 10:32:46 -0700

    Update BlackthornEmysHouse.asm

--- maps/BlackthornEmysHouse.asm
+++ maps/BlackthornEmysHouse.asm
@@ -20,8 +20,6 @@ Emy:
 	jumpstd NuzlockeNPCScript
 
 BlackthornEmysHouse_MapEvents:
-	db 0, 0 ; filler
-
 	def_warp_events
 	warp_event  2,  7, BLACKTHORN_CITY, 3
 	warp_event  3,  7, BLACKTHORN_CITY, 3
