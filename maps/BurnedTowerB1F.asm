b82acc72ac50d9e911840f8e1d5f29f29c6f643d
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-07-14 10:28:52 -0700

    Update BurnedTowerB1F.asm

--- maps/BurnedTowerB1F.asm
+++ maps/BurnedTowerB1F.asm
@@ -236,8 +236,6 @@ BurnedTowerB1FEusineText:
 	done
 
 BurnedTowerB1F_MapEvents:
-	db 0, 0 ; filler
-
 	def_warp_events
 	warp_event 10,  9, BURNED_TOWER_1F, 3
 	warp_event  7, 15, BURNED_TOWER_1F, 4
