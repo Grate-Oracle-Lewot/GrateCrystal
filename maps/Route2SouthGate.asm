5c939f0494bac78720fcf806fbbe3dfa7c8febf8
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-07-13 22:51:36 -0700

    Update Route2SouthGate.asm

--- maps/Route2SouthGate.asm
+++ maps/Route2SouthGate.asm
@@ -42,8 +42,6 @@ Route2SouthGateScientistText:
 	done
 
 Route2SouthGate_MapEvents:
-	db 0, 0 ; filler
-
 	def_warp_events
 	warp_event  4,  0, VIRIDIAN_FOREST, 3
 	warp_event  5,  0, VIRIDIAN_FOREST, 4
