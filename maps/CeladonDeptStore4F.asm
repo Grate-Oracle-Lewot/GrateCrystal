60cdf8b79443ae4cf9e9c6151ee2d2026ade6193
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-07-14 10:22:05 -0700

    Update CeladonDeptStore4F.asm

--- maps/CeladonDeptStore4F.asm
+++ maps/CeladonDeptStore4F.asm
@@ -72,15 +72,13 @@ CeladonDeptStore4FYoungsterText:
 	done
 
 CeladonDeptStore4FDirectoryText:
-	text "Hold On Loosely,"
+	text "Hold On Loosely<SEMI>"
 	line "Don't Let Go!"
 
 	para "4F: ACCOUTREMENTS"
 	done
 
 CeladonDeptStore4F_MapEvents:
-	db 0, 0 ; filler
-
 	def_warp_events
 	warp_event 12,  0, CELADON_DEPT_STORE_5F, 1
 	warp_event 15,  0, CELADON_DEPT_STORE_3F, 2
