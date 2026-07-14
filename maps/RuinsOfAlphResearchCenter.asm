35132e66253062c88055b3dca00c60db199546e1
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-07-13 21:10:37 -0700

    Update RuinsOfAlphResearchCenter.asm

--- maps/RuinsOfAlphResearchCenter.asm
+++ maps/RuinsOfAlphResearchCenter.asm
@@ -471,8 +471,6 @@ RuinsOfAlphResearchCenterMoveTutorTaught:
 	done
 
 RuinsOfAlphResearchCenter_MapEvents:
-	db 0, 0 ; filler
-
 	def_warp_events
 	warp_event  2,  7, RUINS_OF_ALPH_OUTSIDE, 6
 	warp_event  3,  7, RUINS_OF_ALPH_OUTSIDE, 6
