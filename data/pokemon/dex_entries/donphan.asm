70618d7873b62190af9f9563e13184014032c662
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-07-15 09:29:30 -0700

    Update donphan.asm

--- data/pokemon/dex_entries/donphan.asm
+++ data/pokemon/dex_entries/donphan.asm
@@ -1,10 +1,10 @@
 	db "ELEPHANT@" ; species name
 	dw 307, 2650 ; height, weight
 
-	db   "A willing DONPHAN"
-	next "makes a good spare"
-	next "tire in a pinch."
+	db   "It's known to have"
+	next "auditory hallucin-"
+	next "ations about"
 
-	page "Some are more"
-	next "willing than"
-	next "others.@"
+	page "little people who"
+	next "live inside of"
+	next "dust specks.@"
