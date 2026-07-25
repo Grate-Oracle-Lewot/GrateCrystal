4ac4b65d2feeac509dc486fcff4a9aa4cbf5d718
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-07-24 17:47:40 -0700

    Update mankey.asm

--- data/pokemon/base_stats/mankey.asm
+++ data/pokemon/base_stats/mankey.asm
@@ -10,7 +10,7 @@ else
 endc
 	db 190 ; catch rate
 	db 74 ; base exp
-	db X_ATTACK, TM_THIEF ; items
+	db RED_APRICORN, TM_THIEF ; items
 	db GENDER_F50 ; gender ratio
 	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/mankey/front.dimensions"
