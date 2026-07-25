4e0e98a33c6d85c71f65bcded6f539be537da718
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-07-24 21:04:22 -0700

    Update core.asm

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -2235,6 +2235,7 @@ HandleEnemyMonFaint:
 	ld [wBattlePlayerAction], a
 	call HandleEnemySwitch
 	jp z, WildFled_EnemyFled_LinkBattleCanceled
+	call HandleStatBoostingHeldItems
 	xor a ; BATTLEPLAYERACTION_USEMOVE
 	ld [wBattlePlayerAction], a
 	ret
@@ -2319,6 +2320,7 @@ DoubleSwitch:
 	call PlayerPartyMonEntrance
 
 .done
+	call HandleStatBoostingHeldItems
 	xor a ; BATTLEPLAYERACTION_USEMOVE
 	ld [wBattlePlayerAction], a
 	ret
