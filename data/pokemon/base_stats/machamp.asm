03bc3e9915e2e146ff76f57046740ab55d4fa4ab
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-07-24 17:47:08 -0700

    Update machamp.asm

--- data/pokemon/base_stats/machamp.asm
+++ data/pokemon/base_stats/machamp.asm
@@ -6,7 +6,7 @@
 	db FIGHTING, FIGHTING ; type
 	db 45 ; catch rate
 	db 193 ; base exp
-	db BLACKBELT_I, MOON_STONE ; items
+	db BLACKBELT_I, RED_APRICORN ; items
 	db GENDER_F25 ; gender ratio
 	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/machamp/front.dimensions"
