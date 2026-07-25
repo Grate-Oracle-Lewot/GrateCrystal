d0636689b56d2c5f2707ce81fa9777f53d621357
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-07-24 17:55:14 -0700

    Update ponyta.asm

--- data/pokemon/base_stats/ponyta.asm
+++ data/pokemon/base_stats/ponyta.asm
@@ -10,7 +10,7 @@ else
 endc
 	db 190 ; catch rate
 	db 152 ; base exp
-	db X_SPEED, CHARCOAL ; items
+	db YLW_APRICORN, CHARCOAL ; items
 	db GENDER_F50 ; gender ratio
 	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/ponyta/front.dimensions"
