b971d6c39115b64170529670c7750c38aa22330d
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-07-26 13:14:28 -0700

    Update baton_pass_teleport_u_turn.asm

--- engine/battle/move_effects/baton_pass_teleport_u_turn.asm
+++ engine/battle/move_effects/baton_pass_teleport_u_turn.asm
@@ -49,7 +49,7 @@ BattleCommand_BatonPass:
 	ld hl, ApplyStatLevelMultiplierOnAllStats
 	call CallBattleCore
 
-	ld hl, SpikesDamage
+	ld hl, HandleApricornsAndSpikes
 	call CallBattleCore
 	; fallthrough
 
