8afcb5925a332ecc3049fb63acd709f61475cdb8
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-07-26 17:14:16 -0700

    Update battle_constants.asm

--- constants/battle_constants.asm
+++ constants/battle_constants.asm
@@ -226,7 +226,7 @@ ALL_STATUS EQU (1 << PSN) | (1 << BRN) | (1 << FRZ) | (1 << PAR) | SLP
 ; wPlayerScreens or wEnemyScreens bit flags
 	const_def
 	const SCREENS_SPIKES
-	const_skip
+	const SCREENS_SPIKES_2
 	const SCREENS_SAFEGUARD
 	const SCREENS_LIGHT_SCREEN
 	const SCREENS_REFLECT
