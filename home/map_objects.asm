e66532a9fb06a5e02211517a55ed0f0175d88ffb
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-08-16 11:47:29 -0700

    Update map_objects.asm

--- home/map_objects.asm
+++ home/map_objects.asm
@@ -130,46 +130,32 @@ CheckGrassTile::
 
 CheckSuperTallGrassTile::
 	cp COLL_LONG_GRASS
-	ret z
-	cp COLL_LONG_GRASS_1C
 	ret
 
 CheckCutTreeTile::
 	cp COLL_CUT_TREE
-	ret z
-	cp COLL_CUT_TREE_1A
 	ret
 
 CheckHeadbuttTreeTile::
 	cp COLL_HEADBUTT_TREE
-	ret z
-	cp COLL_HEADBUTT_TREE_1D
 	ret
 
 CheckCounterTile::
 	cp COLL_COUNTER
-	ret z
-	cp COLL_COUNTER_98
 	ret
 
 CheckPitTile::
 	cp COLL_PIT
-	ret z
-	cp COLL_PIT_68
 	ret
 
 CheckIceTile::
 	cp COLL_ICE
-	ret z
-	cp COLL_ICE_2B
 	ret z
 	scf
 	ret
 
 CheckWhirlpoolTile::
 	cp COLL_WHIRLPOOL
-	ret z
-	cp COLL_WHIRLPOOL_2C
 	ret z
 	scf
 	ret
@@ -183,8 +169,6 @@ CheckWaterfallTile::
 CheckStandingOnEntrance::
 	ld a, [wPlayerStandingTile]
 	cp COLL_DOOR
-	ret z
-	cp COLL_DOOR_79
 	ret z
 	cp COLL_STAIRCASE
 	ret z
