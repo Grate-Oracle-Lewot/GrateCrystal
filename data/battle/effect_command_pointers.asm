99a761b8e68f2bd50f21705b32ee04b7f9398bfe
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-08-30 20:24:51 -0700

    Update effect_command_pointers.asm

--- data/battle/effect_command_pointers.asm
+++ data/battle/effect_command_pointers.asm
@@ -174,4 +174,5 @@ BattleCommandPointers:
 	dw BattleCommand_Avalanche
 	dw BattleCommand_Conversion
 	dw BattleCommand_EggBomb
+	dw BattleCommand_DoubleHeldItemDamage
 	assert_table_length NUM_EFFECT_COMMANDS
