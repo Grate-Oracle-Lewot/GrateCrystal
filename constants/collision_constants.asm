41c88a10ddbcd57fa9744cf6673b20e7de634942
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-09-13 16:25:42 -0700

    Update collision_constants.asm

--- constants/collision_constants.asm
+++ constants/collision_constants.asm
@@ -7,27 +7,16 @@ TALK       EQU $10
 ; collision data types (see data/tilesets/*_collision.asm)
 ; TileCollisionTable indexes (see data/collision/collision_permissions.asm)
 COLL_FLOOR             EQU $00
-COLL_01                EQU $01 ; garbage
-COLL_03                EQU $03 ; garbage
-COLL_04                EQU $04 ; garbage
 COLL_WALL              EQU $07
-COLL_CUT_08            EQU $08 ; unused
-COLL_TALL_GRASS_10     EQU $10 ; unused
 COLL_CUT_TREE          EQU $12
 COLL_LONG_GRASS        EQU $14
 COLL_HEADBUTT_TREE     EQU $15
 COLL_TALL_GRASS        EQU $18
-COLL_CUT_TREE_1A       EQU $1a ; unused
-COLL_LONG_GRASS_1C     EQU $1c ; unused
-COLL_HEADBUTT_TREE_1D  EQU $1d ; unused
 COLL_DIVE_SPOT         EQU $21 ; unused
 COLL_ICE               EQU $23
 COLL_WHIRLPOOL         EQU $24
 COLL_BUOY              EQU $27
-COLL_CUT_28            EQU $28 ; garbage
 COLL_WATER             EQU $29
-COLL_ICE_2B            EQU $2b ; unused
-COLL_WHIRLPOOL_2C      EQU $2c ; unused
 COLL_WATERFALL_RIGHT   EQU $30 ; unused
 COLL_WATERFALL_LEFT    EQU $31 ; unused
 COLL_WATERFALL_UP      EQU $32 ; unused
@@ -41,44 +30,21 @@ COLL_WALK_RIGHT        EQU $41 ; unused
 COLL_WALK_LEFT         EQU $42 ; unused
 COLL_WALK_UP           EQU $43 ; unused
 COLL_WALK_DOWN         EQU $44 ; unused
-COLL_BRAKE_45          EQU $45 ; garbage
-COLL_BRAKE_46          EQU $46 ; unused
-COLL_BRAKE_47          EQU $47 ; unused
-COLL_GRASS_48          EQU $48 ; unused
-COLL_GRASS_49          EQU $49 ; unused
-COLL_GRASS_4A          EQU $4a ; garbage
-COLL_GRASS_4B          EQU $4b ; garbage
-COLL_GRASS_4C          EQU $4c ; unused
 COLL_WALK_RIGHT_ALT    EQU $50 ; unused
 COLL_WALK_LEFT_ALT     EQU $51 ; unused
 COLL_WALK_UP_ALT       EQU $52 ; unused
 COLL_WALK_DOWN_ALT     EQU $53 ; unused
 COLL_BRAKE_ALT         EQU $54 ; unused
-COLL_BRAKE_55          EQU $55 ; unused
-COLL_BRAKE_56          EQU $56 ; unused
-COLL_BRAKE_57          EQU $57 ; unused
-COLL_5B                EQU $5b ; garbage
 COLL_PIT               EQU $60
-COLL_VIRTUAL_BOY       EQU $61 ; garbage
-COLL_64                EQU $64 ; garbage
-COLL_65                EQU $65 ; garbage
-COLL_PIT_68            EQU $68 ; unused
 COLL_WARP_CARPET_DOWN  EQU $70
 COLL_DOOR              EQU $71
 COLL_LADDER            EQU $72
-COLL_STAIRCASE_73      EQU $73 ; unused
-COLL_CAVE_74           EQU $74 ; unused
-COLL_DOOR_75           EQU $75 ; unused
 COLL_WARP_CARPET_LEFT  EQU $76
-COLL_WARP_77           EQU $77 ; unused
 COLL_WARP_CARPET_UP    EQU $78
-COLL_DOOR_79           EQU $79 ; unused
 COLL_STAIRCASE         EQU $7a
 COLL_CAVE              EQU $7b
 COLL_WARP_PANEL        EQU $7c
-COLL_DOOR_7D           EQU $7d ; unused
 COLL_WARP_CARPET_RIGHT EQU $7e
-COLL_WARP_7F           EQU $7f ; unused
 COLL_STOP_SPIN         EQU $80
 COLL_SPIN_UP           EQU $81
 COLL_SPIN_DOWN         EQU $82
@@ -91,8 +57,6 @@ COLL_RADIO             EQU $94
 COLL_TOWN_MAP          EQU $95
 COLL_MART_SHELF        EQU $96
 COLL_TV                EQU $97
-COLL_COUNTER_98        EQU $98 ; unused
-COLL_9C                EQU $9c ; garbage
 COLL_WINDOW            EQU $9d
 COLL_INCENSE_BURNER    EQU $9f
 COLL_HOP_RIGHT         EQU $a0
