72507ee368c97a75cdd0057738ffe314e76b81c2
Grate Oracle Lewot <88916087+Grate-Oracle-Lewot@users.noreply.github.com>
2026-09-14 19:38:05 -0700

    Update bug_contest_mons.asm

--- data/wild/bug_contest_mons.asm
+++ data/wild/bug_contest_mons.asm
@@ -1,14 +1,14 @@
 ContestMons::
 	;   %, species,   min, max
 if DEF(_LITTLE_CUP)
-	db 10, CATERPIE,    1, 20
-	db 10, WEEDLE,      1, 20
-	db  5, CATERPIE,    5, 20
-	db  5, WEEDLE,      5, 20
+	db  9, CATERPIE,    1, 20
+	db  9, WEEDLE,      1, 20
+	db  6, CATERPIE,    5, 20
+	db  6, WEEDLE,      5, 20
 	db  9, VENONAT,     5, 20
 	db  9, PARAS,       5, 20
-	db  8, LEDYBA,      5, 20
 	db  8, SPINARAK,    5, 20
+	db  7, LEDYBA,      5, 20
 	db  6, YANMA,       5, 20
 	db  5, PINECO,      5, 20
 	db  5, YANMA,       1, 20
@@ -16,16 +16,17 @@ if DEF(_LITTLE_CUP)
 	db  5, WEEDLE,     10, 20
 	db  5, SCYTHER,    10, 20
 	db  5, KRABBY,     10, 20
+	db  1, LEDBYA,     18, 20
 	db -1, PINECO,     10, 10 ; end
 else
-	db 10, CATERPIE,    1, 20
-	db 10, WEEDLE,      1, 20
-	db  5, METAPOD,     5, 20
-	db  5, KAKUNA,      5, 20
+	db  9, CATERPIE,    1, 20
+	db  9, WEEDLE,      1, 20
+	db  6, METAPOD,     5, 20
+	db  6, KAKUNA,      5, 20
 	db  9, VENONAT,     5, 20
 	db  9, PARAS,       5, 20
-	db  8, LEDYBA,      5, 20
 	db  8, SPINARAK,    5, 20
+	db  7, LEDYBA,      5, 20
 	db  6, YANMA,       5, 20
 	db  5, HERACROSS,   5, 20
 	db  5, JIRK,        1, 20
@@ -33,5 +34,6 @@ else
 	db  5, BEEDRILL,   10, 20
 	db  5, SCYTHER,    10, 20
 	db  5, PINSIR,     10, 20
+	db  1, LEDIAN,     18, 20
 	db -1, PINECO,     10, 10 ; end
 endc
